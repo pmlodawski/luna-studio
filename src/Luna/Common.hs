6d42acd6240f64d103566e687ac83fa5395e506b
remdezx <remdezx@gmail.com>
2013-07-12 14:24:58 +0200

    Fixed compilation error ;>

--- src/Luna/Common.hs
+++ src/Luna/Common.hs
@@ -80,22 +80,22 @@ instance (Serialize a, Serialize b) => Serialize (DG.Gr a b) where
 
 instance Serialize Node where
   put i = case i of 
-            TypeNode     name'      -> Serialize.put (0 :: Word8, name')
-            CallNode     name'      -> Serialize.put (1 :: Word8, name')
-            ClassNode    name'      -> Serialize.put (2 :: Word8, name')
-            FunctionNode name'      -> Serialize.put (3 :: Word8, name')
-            PackageNode  name'      -> Serialize.put (4 :: Word8, name')
-            DefaultNode  value'     -> Serialize.put (5 :: Word8, value')
+            TypeNode     name'    -> Serialize.put (0 :: Word8, name')
+            CallNode     name'    -> Serialize.put (1 :: Word8, name')
+            ClassNode    name'  _ -> Serialize.put (2 :: Word8, name')
+            FunctionNode name'  _ -> Serialize.put (3 :: Word8, name')
+            PackageNode  name'  _ -> Serialize.put (4 :: Word8, name')
+            DefaultNode  value'   -> Serialize.put (5 :: Word8, value')
 
   get   = do 
             t <- Serialize.get :: Serialize.Get Word8
             case t of 
-              0 -> do name'         <- Serialize.get; return $ TypeNode     name'
-              1 -> do name'         <- Serialize.get; return $ CallNode     name'
-              2 -> do name'         <- Serialize.get; return $ ClassNode    name'  NotLoaded
-              3 -> do name'         <- Serialize.get; return $ FunctionNode name'  NotLoaded
-              4 -> do name'         <- Serialize.get; return $ PackageNode  name'  NotLoaded
-              5 -> do value'        <- Serialize.get; return $ DefaultNode  value'
+              0 -> do name'       <- Serialize.get; return $ TypeNode     name'
+              1 -> do name'       <- Serialize.get; return $ CallNode     name'
+              2 -> do name'       <- Serialize.get; return $ ClassNode    name'  NotLoaded
+              3 -> do name'       <- Serialize.get; return $ FunctionNode name'  NotLoaded
+              4 -> do name'       <- Serialize.get; return $ PackageNode  name'  NotLoaded
+              5 -> do value'      <- Serialize.get; return $ DefaultNode  value'
               _ -> error "Unknown Node Type (unserialize)"
 
 --------------------------------------------------------------
