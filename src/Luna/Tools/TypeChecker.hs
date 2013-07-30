5f527bd3b641f82549888cebb6237cbb2527b3f6
remdezx <remdezx@gmail.com>
2013-07-30 09:38:16 +0200

    Cabal configuration file restructure

--- src/Luna/Tools/TypeChecker.hs
+++ src/Luna/Tools/TypeChecker.hs
@@ -6,28 +6,28 @@
 ---------------------------------------------------------------------------
 
 module Luna.Tools.TypeChecker(
-    typeCheck
+    --typeCheck
 ) where
 
-import qualified Data.Graph.Inductive as DG
-import qualified Data.Map     as Map
-import           Data.Map       (Map)
+--import qualified Data.Graph.Inductive as DG
+--import qualified Data.Map     as Map
+--import           Data.Map       (Map)
 
-import qualified Luna.DefManager as DefManager
-import           Luna.DefManager   (DefManager)
-import qualified Luna.Graph   as Graph
-import           Luna.Graph     (Graph)
-import qualified Luna.Node    as Node
-import           Luna.Node      (Node)
--- import qualified Luna.NodeDef as NodeDef
+--import qualified Luna.Network.Def.DefManager as DefManager
+--import           Luna.Network.Def.DefManager   (DefManager)
+--import qualified Luna.Network.Graph.Graph   as Graph
+--import           Luna.Network.Graph.Graph     (Graph)
+--import qualified Luna.Network.Graph.Node    as Node
+--import           Luna.Network.Graph.Node      (Node)
+---- import qualified Luna.NodeDef as NodeDef
 
 
-typeCheck :: Graph -> DefManager -> Map DG.Node (Maybe Node)
-typeCheck graph manager = m where -- TODO[PM] implement typeCheck
-    nodes = DG.labNodes $ Graph.repr graph
-    callNodes = filter (\(_, node) -> case node of 
-                                 Node.CallNode _ -> True;
-                                 _               -> False) nodes
-    types = map (\(nid, node) -> 
-                  (nid, DefManager.nodesByCName (Node.name node) manager)) callNodes
-    m = Map.fromList types
+--typeCheck :: Graph -> DefManager -> Map DG.Node (Maybe Node)
+--typeCheck graph manager = m where -- TODO[PM] implement typeCheck
+--    nodes = DG.labNodes $ Graph.repr graph
+--    callNodes = filter (\(_, node) -> case node of 
+--                                 Node.CallNode _ -> True;
+--                                 _               -> False) nodes
+--    types = map (\(nid, node) -> 
+--                  (nid, DefManager.nodesByCName (Node.name node) manager)) callNodes
+--    m = Map.fromList types
