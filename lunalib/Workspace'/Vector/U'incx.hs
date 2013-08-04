37ae7e023cc5e764119a88be874cbcd3534b0a26
Wojciech Danilo <wojtek.danilo@gmail.com>
2013-08-04 03:45:06 +0200

    Small fixes.

--- lunalib/Workspace'/Vector/U'incx.hs
+++ lunalib/Workspace'/Vector/U'incx.hs
@@ -28,20 +28,6 @@ incx inputs' =
         outputs' = v'9
     in outputs'
 
-incx''M inputs' = do
-    let
-        v'4 = 1 :: Int
-        v'0 = inputs'
-        v'1 = select0 v'0
-        v'2 = x'getter v'1
-        v'3 = select0 v'2
-        v'5 = (v'4, v'3)
-        v'6 = add v'5
-        v'7 = select0 v'6
-        v'8 = (v'1, v'7)
-        v'9 = x'setter v'8
-        outputs' = v'9
-    return outputs'
-
+incx''M inputs' = return $ incx inputs'
 
 -- expressions
