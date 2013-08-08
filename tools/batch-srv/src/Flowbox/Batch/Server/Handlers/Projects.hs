6e7f193186527e7227c28bc57fc29aecce6b8bad
remdezx <remdezx@gmail.com>
2013-08-09 00:17:02 +0200

    Stub implementation for Batch/Server/Handlers/Projects

--- tools/batch-srv/src/Flowbox/Batch/Server/Handlers/Projects.hs
+++ tools/batch-srv/src/Flowbox/Batch/Server/Handlers/Projects.hs
@@ -24,19 +24,26 @@ import qualified Flowbox.Batch.Project.Project                             as Pr
 import           Flowbox.Batch.Project.Project                               (Project(..))
 import qualified Flowbox.Luna.Core                                         as Core
 import           Flowbox.Luna.Core                                           (Core(..))
-import qualified Flowbox.Luna.Lib.LibManager                               as LibManager
-import qualified Flowbox.Luna.Lib.Library                                  as Library
-import           Flowbox.Luna.Lib.Library                                    (Library(..))
 import           Flowbox.Luna.Tools.Serialize.Thrift.Conversion.Conversion
-import           Flowbox.Luna.Tools.Serialize.Thrift.Conversion.Defs         ()
-import           Flowbox.Luna.Tools.Serialize.Thrift.Conversion.Libs         ()
+import           Flowbox.Batch.Tools.Serialize.Thrift.Conversion.Projects    ()
 
+
+------ public api helpers -----------------------------------------
+projectOperation :: (IORef Project -> (Project.ID, Project) -> a)
+                 ->  IORef Project -> Maybe TProjects.Project -> a
+projectOperation operation batchHandler mtproject = case mtproject of 
+    Nothing       -> throw' "`project` argument is missing";
+    Just tproject -> case decode (tproject, Core.empty) of
+        Left  message      -> throw' message
+        Right (projectID, project) -> operation batchHandler (projectID, project)
 
 
 ------ public api -------------------------------------------------
 
 projects :: IORef Project -> IO (Vector TProjects.Project)
-projects batchHandler = undefined 
+projects batchHandler = do
+    putStrLn "NOT IMPLEMENTED"
+    return undefined
     --putStrLn "call libraries"
     --project <- readIORef batchHandler
     --let core        = Project.core project
@@ -48,13 +55,17 @@ projects batchHandler = undefined
 
 
 createProject :: IORef Project -> Maybe TProjects.Project -> IO TProjects.Project
-createProject = undefined --libOperation (\ batchHandler (_, library) -> do
+createProject = projectOperation (\ batchHandler (projectID, project) -> do
+    putStrLn "NOT IMPLEMENTED"
+    return undefined) --libOperation (\ batchHandler (_, library) -> do
     --putStrLn "call createLibrary - NOT YET IMPLEMENTED"
     --return $ encode (-1, library))
 
 
 openProject :: IORef Project -> Maybe TProjects.Project -> IO TProjects.Project
-openProject = undefined --libOperation (\ batchHandler (_, library) -> do
+openProject = projectOperation (\ batchHandler (projectID, project) -> do
+    putStrLn "NOT IMPLEMENTED"
+    return undefined) --libOperation (\ batchHandler (_, library) -> do
     --putStrLn "call loadLibrary"
     --project <- readIORef batchHandler
     --let core        = Project.core project
@@ -66,7 +77,8 @@ openProject = undefined --libOperation (\ batchHandler (_, library) -> do
 
 
 closeProject :: IORef Project -> Maybe TProjects.Project -> IO ()
-closeProject = undefined --libOperation (\ batchHandler (libID, _) -> do
+closeProject = projectOperation (\ batchHandler (projectID, project) -> do
+    putStrLn "NOT IMPLEMENTED") --libOperation (\ batchHandler (libID, _) -> do
     --putStrLn "call unloadLibrary"
     --project <- readIORef batchHandler
     --let core       = Project.core project
@@ -76,9 +88,12 @@ closeProject = undefined --libOperation (\ batchHandler (libID, _) -> do
 
 
 storeProject :: IORef Project -> Maybe TProjects.Project -> IO ()
-storeProject = undefined --libOperation (\ batchHandler (libID, _) -> do
+storeProject = projectOperation (\ batchHandler (projectID, project) -> do
+    putStrLn "NOT IMPLEMENTED") --libOperation (\ batchHandler (libID, _) -> do
     --putStrLn "call storeLibrary - NOT YET IMPLEMENTED")
 
 
 setActiveProject :: IORef Project -> Maybe TProjects.Project -> IO ()
-setActiveProject = undefined
+setActiveProject = projectOperation (\ batchHandler (projectID, project) -> do
+    putStrLn "NOT IMPLEMENTED")
+
