679379b46f9ee4d087d4633a0ca6eb4541e9bd74
Wojciech Danilo <wojtek.danilo@gmail.com>
2013-08-09 22:56:41 +0200

    Moved Serializer to utils lib.

---
+++ src/Flowbox/System/IO/Serializer.hs
@@ -0,0 +1,36 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2013
+---------------------------------------------------------------------------
+module Flowbox.System.IO.Serializer(
+    Serializable(..),
+
+    serializeMany,
+    serialize
+) where
+
+import System.Directory                              as Dir
+import qualified System.IO                           as IO
+
+import qualified Flowbox.System.UniPath         as UniPath
+import           Flowbox.System.UniPath           (UniPath)
+
+
+data Serializable = Serializable UniPath (IO.Handle -> IO())
+
+
+serializeMany :: [Serializable] -> IO()
+serializeMany serializables = do
+    sequence_ $ map (\s -> serialize s) serializables
+    return ()
+
+
+serialize :: Serializable -> IO()
+serialize (Serializable upath save) = do
+    let foldername = UniPath.toUnixString $ init upath
+        filename   = UniPath.toUnixString upath
+    Dir.createDirectoryIfMissing True foldername
+    IO.withFile filename IO.WriteMode save
+
