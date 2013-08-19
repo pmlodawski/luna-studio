26fab3d7d7a54cd9147035bb25f552d75220cb62
Wojciech Danilo <wojtek.danilo@gmail.com>
2013-08-19 13:14:57 +0200

    Merge feature/parser manually, updated batch sample library.

--- libs/batch/src/Flowbox/Batch/Samples/Modules.hs
+++ libs/batch/src/Flowbox/Batch/Samples/Modules.hs
@@ -21,6 +21,11 @@ import qualified Flowbox.Luna.Lib.Library            as Library
 import           Flowbox.Luna.Lib.Library              (Library(..))
 import           Flowbox.Luna.Type.Type                (Type(..))
 import qualified Flowbox.Luna.Type.Type              as Type
+import qualified Flowbox.Luna.Network.Graph.Node         as Node
+import           Flowbox.Luna.Network.Graph.Node           (Node)
+import qualified Flowbox.Luna.Network.Graph.DefaultValue as DefaultValue
+import qualified Flowbox.Luna.Network.Graph.Edge         as Edge
+import           Flowbox.Luna.Network.Graph.Edge           (Edge(..))
 
 mkDefinition :: Type -> Definition
 mkDefinition cls = Definition.empty{ Definition.cls = cls
@@ -50,15 +55,59 @@ cls_console = Definition.empty { Definition.cls   = Type.Class "Console" [] []
                                , Definition.graph = Graph.empty
                                }
 
+cls_vector = Definition.empty{ Definition.cls   = Type.Class "Vector" ["a"] [Type.Named "x" (Type.TypeVariable "a"), Type.Named "y" (Type.TypeVariable "a"), Type.Named "z" (Type.TypeVariable "a")]
+                    , Definition.graph = Graph.empty
+                    }
+
 addSomeDefs :: DefManager -> DefManager
 addSomeDefs defs = DefManager.addToParentMany (listToDefs atrybuty 2000 20)
                  $ DefManager.addToParent (0, 20, mkModule "atrybuty")
                  $ DefManager.addToParentMany (listToDefs wladcyPolski 1000 10)
                  $ DefManager.addToParent (0, 10, mkModule "wladcyPolski")
-                 $ DefManager.addToParent (2, 3 , cls_console)
+                 $ DefManager.addToParent (4, 5 , func_vec_incx)
+                 $ DefManager.addToParent (3, 4 , cls_vector)
+                 $ DefManager.addToParent (1, 3 , mkModule "Math")
                  $ DefManager.addToParent (1, 2 , mkModule "IO")
                  $ DefManager.addToParent (0, 1 , mkModule "Std")
                  $ defs
+
+func_vec_incx_graph = Graph.insEdges [
+                              (0, 1, Edge.standard),
+                              (1, 2, Edge.standard),
+                              (2, 3, Edge.standard),
+                              (3, 4, Edge.standard),
+                              (5, 6, Edge.standard),
+                              (4, 6, Edge.standard),
+                              (6, 7, Edge.standard),
+                              (7, 8, Edge.standard),
+                              (8, 9, Edge.standard),
+                              (1, 9, Edge.standard),
+                              (9, 10, Edge.standard),
+                              (10, 11, Edge.standard)
+                             ]
+
+           $ Graph.insNodes [(0,  Node.mkInputs             ),
+                             (1,  Node.mkCall     "select0" ),
+                             (2,  Node.mkNTuple             ),
+                             (3,  Node.mkCall     "x'getter"),
+                             (4,  Node.mkCall     "select0" ),
+                             (5,  Node.Default $ DefaultValue.DefaultInt 1),
+                             (6,  Node.mkNTuple             ),
+                             (7,  Node.mkCall     "add"     ),
+                             (8,  Node.mkCall     "select0" ),
+                             (9,  Node.mkNTuple             ),
+                             (10, Node.mkCall     "x'setter"),
+                             (11, Node.mkOutputs            )
+                            ]
+           $ Graph.empty
+
+func_vec_incx_inputs = Type.Tuple [Type.Named "self" $ Type.TypeVariable "a", 
+                                   Type.Named "in2" $ Type.TypeVariable "a"]
+
+
+func_vec_incx = Definition.empty{ Definition.cls   = (Type.Function "incx" func_vec_incx_inputs Type.noOutputs)
+                      , Definition.graph = func_vec_incx_graph
+                      }
 
 emptyStdLibrary :: Library
 emptyStdLibrary = Library.make "std"           $ UniPath.fromUnixString "dummylibs/stdlib.lunalib"
---
+++ libs/luna/src/Flowbox/Luna/Parser/Tokens.hs
@@ -0,0 +1,33 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2013
+---------------------------------------------------------------------------
+
+module Flowbox.Luna.Parser.Tokens where
+
+--import qualified Prelude
+--import           Prelude                        (Show, Eq)
+--import qualified Flowbox.Luna.Parser.Keywords as Keywords
+
+--data Token = Space 
+--           | NewLine
+--		   | Tab
+--		   | Indent      Prelude.Int
+--		   | Ident       Prelude.String
+--		   | Keyword     Keywords.Keyword
+--		   | Operator    Prelude.String
+--		   | Assignment
+--		   | Integer     Prelude.String
+--		   | Double      Prelude.String
+--		   | Accessor
+--		   | Lambda
+--		   | GrpOpen
+--		   | GrpClose
+--		   | Separator
+--		   | Terminator
+--           | String      Prelude.String
+--           | StringBegin
+--           | StringEnd
+--		   deriving(Show, Eq)  
