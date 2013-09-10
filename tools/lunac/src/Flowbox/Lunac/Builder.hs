6fc70b839393100e0741b6d65678bf53d0132fa9
remdezx <remdezx@gmail.com>
2013-09-10 17:05:17 +0200

    Fixed compile error

---
+++ tools/lunac/src/Flowbox/Lunac/Builder.hs
@@ -0,0 +1,76 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2013
+---------------------------------------------------------------------------
+{-# LANGUAGE ConstraintKinds, FlexibleContexts, NoMonomorphismRestriction, ScopedTypeVariables #-}
+
+module Flowbox.Lunac.Builder where
+
+import           Control.Monad.State                       
+import           Data.Maybe                                (fromJust)
+import qualified Text.Show.Pretty                        as PP
+import           System.TimeIt                             
+
+import           Flowbox.Prelude                           
+import qualified Flowbox.Luna.AST.AST                    as AST
+import qualified Flowbox.Luna.Lib.Library                as Library
+import           Flowbox.Luna.Lib.Library                  (Library)
+import qualified Flowbox.Luna.Network.Def.Definition     as Definition
+import           Flowbox.Luna.Network.Def.Definition       (Definition)
+import qualified Flowbox.Luna.Network.Def.DefManager     as DefManager
+import           Flowbox.Luna.Network.Def.DefManager       (DefManager)
+import qualified Flowbox.Luna.Passes.Graph2AST.Graph2AST as Graph2AST
+import qualified Flowbox.Luna.Passes.HSGen.HSC           as HSC
+import qualified Flowbox.Luna.Passes.HSGen.HSGen         as HSGen
+import qualified Flowbox.Luna.Passes.Luna.Luna           as Luna
+import qualified Flowbox.Luna.Passes.Pass                as Pass
+import           Flowbox.Luna.Passes.Pass                  (PassMonad)
+import qualified Flowbox.Luna.Passes.SSA.SSA             as SSA
+import           Flowbox.System.Log.Logger                 
+
+
+
+logger :: Logger
+logger = getLogger "Flowbox.Lunac.Builder"
+
+
+buildLibrary :: Library -> IO ()
+buildLibrary library = do
+    let defManger = Library.defs library
+        rootDefID = Library.rootDefID
+        rootDef = fromJust $ DefManager.lab defManger rootDefID
+    out <- timeIt $ buildGraph defManger (rootDefID, rootDef)
+    case out of
+        Right _ -> return ()
+        Left  e -> fail e
+
+
+buildGraph :: DefManager -> (Definition.ID, Definition) -> IO (Either String ())
+buildGraph defManager def = Luna.run $ do 
+    ast <- Graph2AST.run defManager def
+    putStrLn "\n-------- AST --------"
+    putStrLn $ PP.ppShow ast
+
+    buildAST ast
+
+
+buildAST :: (MonadIO m, PassMonad s m) => AST.Expr -> Pass.Result m ()
+buildAST (ast :: AST.Expr) = do
+    putStrLn "\n-------- SSA --------"
+    ssa <- SSA.run     ast
+    putStrLn $ PP.ppShow ssa
+
+    putStrLn "\n-------- HAST --------" 
+    hast <- HSGen.run  ssa
+    putStrLn $ PP.ppShow hast
+
+    putStrLn "\n-------- HSC --------" 
+    hsc <- HSC.run  hast
+    putStrLn $ hsc
+
+    return ()
+
+
+
