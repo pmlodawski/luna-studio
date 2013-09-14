921be50338f209c926e00cf6d91d828ea57ce13c
Wojciech Danilo <wojtek.danilo@gmail.com>
2013-09-15 01:33:47 +0200

    Updated passes to the new structure.

--- libs/luna/src/Flowbox/Luna/Passes/SSA/SSA.hs
+++ libs/luna/src/Flowbox/Luna/Passes/SSA/SSA.hs
@@ -8,78 +8,78 @@
 
 module Flowbox.Luna.Passes.SSA.SSA where
 
---import qualified Flowbox.Luna.AST.Expr         as Expr
---import qualified Flowbox.Luna.AST.Type         as Type
---import           Flowbox.Luna.AST.Type           (Type)
---import qualified Flowbox.Luna.AST.Pat          as Pat
---import           Flowbox.Luna.AST.Pat            (Pat)
---import qualified Flowbox.Luna.Passes.SSA.State as SSAState
---import           Flowbox.Luna.Passes.SSA.State   (SSAState)
---import qualified Flowbox.Luna.Passes.Pass      as Pass
---import           Flowbox.Luna.Passes.Pass        (PassMonad)
+import qualified Flowbox.Luna.AST.Expr         as Expr
+import qualified Flowbox.Luna.AST.Type         as Type
+import           Flowbox.Luna.AST.Type           (Type)
+import qualified Flowbox.Luna.AST.Pat          as Pat
+import           Flowbox.Luna.AST.Pat            (Pat)
+import qualified Flowbox.Luna.Passes.SSA.State as SSAState
+import           Flowbox.Luna.Passes.SSA.State   (SSAState)
+import qualified Flowbox.Luna.Passes.Pass      as Pass
+import           Flowbox.Luna.Passes.Pass        (PassMonad)
 
---import           Control.Monad.State             
---import           Control.Applicative             
+import           Control.Monad.State             
+import           Control.Applicative             
 
---import           Flowbox.System.Log.Logger       
+import           Flowbox.System.Log.Logger       
 
---import qualified Flowbox.Prelude               as Prelude
---import           Flowbox.Prelude               hiding (error)
-
-
---logger :: Logger
---logger = getLogger "Flowbox.Luna.Passes.SSA.SSA"
+import qualified Flowbox.Prelude               as Prelude
+import           Flowbox.Prelude               hiding (error)
 
 
---type SSAMonad m = PassMonad SSAState m
+logger :: Logger
+logger = getLogger "Flowbox.Luna.Passes.SSA.SSA"
 
 
---run :: PassMonad s m => Expr.Expr -> Pass.Result m Expr.Expr
---run = (Pass.runM SSAState.empty) . ssaAST
+type SSAMonad m = PassMonad SSAState m
 
 
---runNested :: SSAMonad m => Pass.Transformer SSAState a m b 
---runNested f = do
---    s <- get
---    Pass.runM s f
+run :: PassMonad s m => Expr.Expr -> Pass.Result m Expr.Expr
+run = (Pass.runM SSAState.empty) . ssaAST
 
 
---ssaAST :: SSAMonad m => Expr.Expr -> Pass.Result m Expr.Expr
---ssaAST ast = case ast of
---    Expr.Module     cls imports classes fields 
---                    methods modules     -> Expr.Module cls imports <$> ssaExprMap classes <*> ssaExprMap fields <*> ssaExprMap methods <*> ssaExprMap modules
---    Expr.Function   name signature body -> runNested $ do
---                                               SSAState.registerVar (name, name)
---                                               mapM ssaPat signature
---                                               Expr.Function name signature <$> ssaExprMap body
---    Expr.Assignment src dst             -> flip Expr.Assignment <$> ssaAST dst <*> ssaAST src
---    Expr.Pattern    pat                 -> Expr.Pattern         <$> ssaPat pat
---    Expr.Var        name                -> do
---                                           v <- SSAState.lookupVar name
---                                           case v of
---                                               Nothing    -> (logger error $ "Not in scope: '" ++ name ++ "'") *> Pass.fail "Not in scope"
---                                               Just lname -> return $ Expr.Var lname
---    Expr.Infix      name src dst        -> Expr.Infix name <$> ssaAST src <*> ssaAST dst
---    Expr.App        src args            -> Expr.App <$> ssaAST src <*> ssaExprMap args
---    Expr.Class      cls classes fields 
---                    methods             -> do ssaType cls
---                                              Expr.Class cls <$> ssaExprMap classes 
---                                                             <*> ssaExprMap fields 
---                                                             <*> ssaExprMap methods
---    Expr.Field      {}                  -> return ast
---    Expr.Lit        {}                  -> return ast
---    _                                   -> logger error "SSA Pass error: Unknown expression." *> Pass.fail "Unknown expression"
---    where
---        ssaExprMap = mapM ssaAST
+runNested :: SSAMonad m => Pass.Transformer SSAState a m b 
+runNested f = do
+    s <- get
+    Pass.runM s f
 
---ssaPat :: SSAMonad m => Pat -> Pass.Result m Pat
---ssaPat pat = case pat of
---	Pat.Var     name                    -> Pat.Var <$> SSAState.handleVar name
---	_                                   -> logger error "SSA Pass error: Unknown pattern." *> Pass.fail "Unknown pattern"
 
---ssaType :: SSAMonad m => Type -> Pass.Result m ()
---ssaType ast = case ast of
---    Type.Tuple  items          -> mapM ssaType items *> return ()
---    Type.Var    name           -> SSAState.registerVar (name, name)
---    Type.Class  name   _       -> SSAState.registerVar (name, name)
---    _                          -> logger error "SSA Pass error: Unknown type." *> Pass.fail "Unknown type"
+ssaAST :: SSAMonad m => Expr.Expr -> Pass.Result m Expr.Expr
+ssaAST ast = case ast of
+    Expr.Module     id cls imports classes fields 
+                    methods modules        -> Expr.Module id cls imports <$> ssaExprMap classes <*> ssaExprMap fields <*> ssaExprMap methods <*> ssaExprMap modules
+    Expr.Function   id name signature body -> runNested $ do
+                                                  SSAState.registerVar (name, name)
+                                                  mapM ssaPat signature
+                                                  Expr.Function id name signature <$> ssaExprMap body
+    Expr.Assignment id src dst             -> flip (Expr.Assignment id) <$> ssaAST dst <*> ssaAST src
+    Expr.Pattern    id pat                 -> Expr.Pattern id           <$> ssaPat pat
+    Expr.Var        id name                -> do
+                                              v <- SSAState.lookupVar name
+                                              case v of
+                                                  Nothing    -> (logger error $ "Not in scope: '" ++ name ++ "'") *> Pass.fail "Not in scope"
+                                                  Just lname -> return $ Expr.Var id lname
+    Expr.Infix      id name src dst        -> Expr.Infix id name <$> ssaAST src <*> ssaAST dst
+    Expr.App        id src args            -> Expr.App id <$> ssaAST src <*> ssaExprMap args
+    Expr.Class      id cls classes fields 
+                    methods                -> do ssaType cls
+                                                 Expr.Class id cls <$> ssaExprMap classes 
+                                                                   <*> ssaExprMap fields 
+                                                                   <*> ssaExprMap methods
+    Expr.Field      {}                     -> return ast
+    Expr.Lit        {}                     -> return ast
+    _                                      -> logger error "SSA Pass error: Unknown expression." *> Pass.fail "Unknown expression"
+    where
+        ssaExprMap = mapM ssaAST
+
+ssaPat :: SSAMonad m => Pat -> Pass.Result m Pat
+ssaPat pat = case pat of
+	Pat.Var     id name                 -> Pat.Var id <$> SSAState.handleVar name
+	_                                   -> logger error "SSA Pass error: Unknown pattern." *> Pass.fail "Unknown pattern"
+
+ssaType :: SSAMonad m => Type -> Pass.Result m ()
+ssaType ast = case ast of
+    Type.Tuple  id items          -> mapM ssaType items *> return ()
+    Type.Var    id name           -> SSAState.registerVar (name, name)
+    Type.Class  id name   _       -> SSAState.registerVar (name, name)
+    _                             -> logger error "SSA Pass error: Unknown type." *> Pass.fail "Unknown type"
