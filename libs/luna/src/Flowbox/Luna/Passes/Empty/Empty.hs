6a1c3f1441a85687bed49ff167145a5617ba62d2
Wojciech Danilo <wojtek.danilo@gmail.com>
2013-09-16 17:23:03 +0200

    Added Empty example Pass and sketch of SSA pass.

---
+++ libs/luna/src/Flowbox/Luna/Passes/Empty/Empty.hs
@@ -0,0 +1,30 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2013
+---------------------------------------------------------------------------
+{-# LANGUAGE FlexibleContexts, ConstraintKinds #-}
+
+module Flowbox.Luna.Passes.Empty.Empty where
+
+import qualified Flowbox.Luna.Passes.Pass     as Pass
+import           Flowbox.Luna.Passes.Pass       (PassMonad)
+
+import           Flowbox.System.Log.Logger      
+import           Flowbox.Prelude              hiding (error, id)
+
+logger :: Logger
+logger = getLogger "Flowbox.Luna.Passes.Empty.Empty"
+
+
+type SSAMonad m = PassMonad Pass.NoState m
+
+
+run :: PassMonad s m => a -> Pass.Result m a
+run = (Pass.run_ Pass.NoState) . pass
+
+
+pass :: SSAMonad m => a -> Pass.Result m a
+pass a = return a
+
---
+++ libs/luna/src/Flowbox/Luna/Passes/SSA/SSA.hs
@@ -0,0 +1,41 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2013
+---------------------------------------------------------------------------
+{-# LANGUAGE FlexibleContexts, NoMonomorphismRestriction, ConstraintKinds #-}
+
+module Flowbox.Luna.Passes.SSA.SSA where
+
+import qualified Flowbox.Luna.AST.Expr        as Expr
+import qualified Flowbox.Luna.AST.Type        as Type
+import           Flowbox.Luna.AST.Type          (Type)
+import qualified Flowbox.Luna.AST.Pat         as Pat
+import           Flowbox.Luna.AST.Pat           (Pat)
+import qualified Flowbox.Luna.Passes.Pass     as Pass
+import           Flowbox.Luna.Passes.Pass       (PassMonad)
+
+import           Control.Monad.State            
+import           Control.Applicative            
+
+import           Flowbox.System.Log.Logger      
+
+import           Flowbox.Prelude              hiding (error, id)
+
+
+
+logger :: Logger
+logger = getLogger "Flowbox.Luna.Passes.SSA.SSA"
+
+
+type SSAMonad m = PassMonad Pass.NoState m
+
+
+run :: PassMonad s m => Expr.Expr -> Pass.Result m ()
+run = (Pass.run_ Pass.NoState) . ssaExpr
+
+
+ssaExpr :: SSAMonad m => Expr.Expr -> Pass.Result m ()
+ssaExpr ast = return ()
+
