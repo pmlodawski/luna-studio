db5e7f54a9609c3373fc9c48db21e05ccc52d7c0
Piotr Młodawski <remdezx@gmail.com>
2013-09-19 15:31:57 +0200

    Added NoImplicitPrelude extension to libs/utils

--- src/Flowbox/System/Directory/Scanner.hs
+++ src/Flowbox/System/Directory/Scanner.hs
@@ -7,30 +7,17 @@
 
 module Flowbox.System.Directory.Scanner where
 
-import           Control.Applicative      
-import qualified Data.List              as List
-import qualified System.Directory       as Directory
+import           Control.Applicative        
 
-import           Flowbox.Prelude          
-import qualified Flowbox.System.UniPath as UniPath
-import           Flowbox.System.UniPath   (UniPath)
-
-
-getDirectoryRecursive :: UniPath -> IO [UniPath]
-getDirectoryRecursive upath = do
-    path <- UniPath.toUnixString <$> UniPath.expand upath
-    isDir <- Directory.doesDirectoryExist path
-    if isDir 
-        then do paths <- Directory.getDirectoryContents path
-                children <- mapM getDirectoryRecursive $ map UniPath.fromUnixString paths
-                let upaths = map UniPath.fromUnixString paths
-                return $ upaths ++ (List.concat children)
-        else return [upath]
+import           Flowbox.Prelude            
+import qualified Flowbox.System.Directory as Directory
+import qualified Flowbox.System.UniPath   as UniPath
+import           Flowbox.System.UniPath     (UniPath)
 
 
 
 scan :: UniPath -> (UniPath -> Bool) -> IO [UniPath]
-scan rootPath predicate = filter predicate <$> getDirectoryRecursive rootPath
+scan rootPath predicate = filter predicate <$> Directory.getDirectoryRecursive rootPath
 
 
 scanByExts :: UniPath -> String -> IO [UniPath]
