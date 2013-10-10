eb734b8671a10917a8f144627e58754ed8ebc6d9
remdezx <remdezx@gmail.com>
2013-10-10 14:46:25 +0200

    Fixed lunac

--- src/Flowbox/Lunac/Builder.hs
+++ src/Flowbox/Lunac/Builder.hs
@@ -10,16 +10,12 @@ module Flowbox.Lunac.Builder where
 
 import           Control.Monad.RWS                                         hiding (mapM_)
 import qualified Data.Maybe                                                as Maybe
-import qualified Data.List                                                 as List
 import qualified Data.Set                                                  as Set
 
 import           Flowbox.Prelude                                             
 import qualified Flowbox.Config.Config                                     as Config
 import           Flowbox.Config.Config                                       (Config)
 import qualified Flowbox.Luna.Data.AST.Module                              as ASTModule
-import qualified Flowbox.Luna.Data.Cabal.Config                            as CabalConfig
-import qualified Flowbox.Luna.Data.Cabal.Section                           as CabalSection
-import qualified Flowbox.Luna.Data.Source                                  as Source
 import           Flowbox.Luna.Data.Source                                    (Source)
 import qualified Flowbox.Luna.Lib.Library                                  as Library
 import           Flowbox.Luna.Lib.Library                                    (Library)
@@ -28,8 +24,9 @@ import           Flowbox.Luna.Network.Def.Definition                         (De
 import qualified Flowbox.Luna.Network.Def.DefManager                       as DefManager
 import           Flowbox.Luna.Network.Def.DefManager                         (DefManager)
 import qualified Flowbox.Luna.Passes.Analysis.FuncPool.FuncPool            as FuncPool
-import           Flowbox.Luna.Passes.Analysis.FuncPool.Pool                  (Pool(Pool))
+import           Flowbox.Luna.Passes.Analysis.FuncPool.Pool                as Pool
 import qualified Flowbox.Luna.Passes.Analysis.VarAlias.VarAlias            as VarAlias
+import qualified Flowbox.Luna.Passes.CodeGen.Cabal.Gen                     as CabalGen
 import qualified Flowbox.Luna.Passes.CodeGen.Cabal.Install                 as CabalInstall
 import qualified Flowbox.Luna.Passes.CodeGen.Cabal.Store                   as CabalStore
 import qualified Flowbox.Luna.Passes.CodeGen.FClass.Filter                 as FClassFliter
@@ -56,6 +53,7 @@ import           Flowbox.System.UniPath                                      (Un
 import qualified Flowbox.Text.Show.Pretty                                  as PP
 -- TODO [PM] Split into multiple files: there are too many responsibilities in this file
 
+
 logger :: Logger
 logger = getLogger "Flowbox.Lunac.Builder"
 
@@ -72,47 +70,40 @@ cabalExt :: String
 cabalExt = ".cabal"
 
 
-either2io :: IO (Either String a) -> IO a
-either2io f = do 
-    out <- f
-    case out of
-        Right r -> return r
-        Left  e -> fail e
-
-
 -- TODO [PM] : Refactor needed
 getBuildPath :: Config -> UniPath
 getBuildPath = UniPath.fromUnixString . Config.path . Config.local 
 
 
 -- TODO [PM] Refactor needed 
-buildLibrary :: Config -> Diagnostics -> Library -> UniPath -> String -> String -> IO ()
-buildLibrary config diag library outputPath name tmpName = either2io $ Luna.run $ do
+buildLibrary :: Config -> Diagnostics -> Library -> String -> UniPath -> IO ()
+buildLibrary config diag library name outputPath = Luna.runIO $ do
     let defManger = Library.defs library
         rootDefID = Library.rootDefID
         rootDef = Maybe.fromJust $ DefManager.lab defManger rootDefID
     ast <- parseGraph diag defManger (rootDefID, rootDef)
-    buildAST config diag outputPath name tmpName False [] ast
+    buildAST config diag outputPath name False [] ast
 
 
 -- TODO [PM] Refactor needed 
 buildFile :: Config -> CmdArgs -> Diagnostics -> UniPath -> IO ()
-buildFile config cmd diag path = either2io $ Luna.run $ do 
+buildFile config cmd diag path = Luna.runIO $ do 
     let outputPath = UniPath.fromUnixString $ CmdArgs.output cmd
         name       = CmdArgs.name cmd
-        tmpName    = "tmp/" ++ name
-        
         rootPath = case CmdArgs.rootPath cmd of 
                         "" -> UniPath.basePath path
                         a  -> UniPath.fromUnixString a
 
     ast  <- parseFile diag rootPath path
-    buildAST config diag outputPath name tmpName (CmdArgs.library cmd) (CmdArgs.link cmd) ast
+    buildAST config diag outputPath name (CmdArgs.library cmd) (CmdArgs.link cmd) ast
 
+
+notImplementedList = []
 
 -- TODO [PM] Refactor needed 
-buildAST :: PassMonadIO s m => Config -> Diagnostics -> UniPath -> String -> String -> Bool -> [String] -> ASTModule.Module -> Pass.Result m ()
-buildAST config diag outputPath name tmpName isLibrary libs ast = do 
+buildAST :: PassMonadIO s m => Config -> Diagnostics -> UniPath -> String -> Bool -> [String] -> ASTModule.Module -> Pass.Result m ()
+buildAST config diag outputPath name isLibrary libs ast = do 
+    let flags = notImplementedList
     va   <- VarAlias.run ast
     Diagnostics.printVA va diag 
     fp <- FuncPool.run ast
@@ -123,19 +114,23 @@ buildAST config diag outputPath name tmpName isLibrary libs ast = do
     Diagnostics.printHAST hast diag
     hsc  <- HSC.run hast
     Diagnostics.printHSC hsc diag
-    
-    newfp <- FClassFliter.run (getBuildPath config) fp
-    FClassInstall.run (getBuildPath config) newfp
+    newfp <- FClassFliter.run config fp
+    FClassInstall.run config newfp flags
 
-    let cabal    = genCabal name isLibrary hsc fp libs 
-    -- CR[wd] czemu funkcja o nazwie buildAST to w ogole robi:
-    writeSources config tmpName hsc
-    runCabal config tmpName name cabal
-    if isLibrary
-        then return ()
-        else moveExecutable config tmpName name outputPath
-    -- CR[wd] czemu funkcja niszczy swoj argument???
-    cleanUp config tmpName
+    let allLibs = libs ++ (map FClassGen.packageName $ Set.toList $ Pool.names fp)
+
+    Directory.withTmpDirectory "lunac" (\tmpDir -> do
+        writeSources tmpDir hsc
+        let cabal = if isLibrary 
+                        then CabalGen.genLibrary hsc name allLibs         
+                        else CabalGen.genExecutable  name allLibs 
+        CabalStore.run cabal $ UniPath.append (name ++ cabalExt) tmpDir
+        CabalInstall.run config tmpDir flags
+        if isLibrary
+            then return ()
+            else copyExecutable tmpDir name outputPath
+        )
+    
 
 
 parseGraph :: PassMonad s m => Diagnostics -> DefManager -> (Definition.ID, Definition) -> Pass.Result m ASTModule.Module
@@ -156,57 +151,15 @@ parseFile diag rootPath path = do
     return ast
 
 
+writeSources :: PassMonadIO s m => UniPath -> [Source] -> Pass.Result m ()
+writeSources outputPath sources = mapM_ writeSource sources where
+    writeSource :: PassMonadIO s m => Source -> Pass.Result m ()
+    writeSource source = FileWriter.run path hsExt source where
+        path = UniPath.append srcFolder outputPath
 
+    
+copyExecutable :: PassMonadIO s m => UniPath -> String -> UniPath -> Pass.Result m ()
+copyExecutable location name outputPath = liftIO $ do 
+    let executable = UniPath.append ("dist/build/" ++ name ++ "/" ++ name) location
+    Directory.copyFile executable outputPath
 
--- TODO [PM] Refactor needed 
-genCabal :: String -> Bool -> [Source] -> Pool -> [String] -> CabalConfig.Config
-genCabal name isLibrary sources (Pool names) libs  = conf where
-    getModuleName :: Source -> String
-    getModuleName source = List.intercalate "." $ Source.path source
-
-    section_base = if isLibrary 
-                     then CabalSection.mkLibrary { CabalSection.exposedModules = map getModuleName sources }
-                     else CabalSection.mkExecutable name 
-    section = section_base { CabalSection.buildDepends = "pretty-show"
-                                                       : "random"
-                                                       : "base"
-                                                       : "OneTuple"
-                                                       : "template-haskell"
-                                                       : "flowboxM-stdlib-io"
-                                                       :  (map FClassGen.packageName $ Set.toList names)
-                                                       ++ libs
-                           }
-    conf = CabalConfig.addSection section 
-         $ CabalConfig.make name
-
-
-writeSources :: PassMonadIO s m => Config -> String -> [Source] -> Pass.Result m ()
-writeSources config location sources = do 
-    let outputPath = UniPath.append location (getBuildPath config)
-
-        writeSource :: PassMonadIO s m => Source -> Pass.Result m ()
-        writeSource source = do 
-            let path = UniPath.append srcFolder outputPath
-            FileWriter.run path hsExt source
-
-    mapM_ writeSource sources
-
-
-runCabal :: PassMonadIO s m => Config -> String -> String -> CabalConfig.Config -> Pass.Result m ()
-runCabal config location name cabal = do 
-    let outputPath = UniPath.append location (getBuildPath config)
-    CabalStore.run cabal $ UniPath.append (name ++ cabalExt) outputPath
-    CabalInstall.run (getBuildPath config) location
-
-
-moveExecutable :: PassMonadIO s m => Config -> String -> String -> UniPath -> Pass.Result m ()
-moveExecutable config location name outputPath = liftIO $ do 
-    rootPath      <- UniPath.expand $ UniPath.append location (getBuildPath config)
-    let executable = UniPath.append ("dist/build/" ++ name ++ "/" ++ name) rootPath
-    Directory.renameFile executable outputPath
-
-
-cleanUp :: PassMonadIO s m => Config -> String -> Pass.Result m ()
-cleanUp config location = liftIO $ do 
-    outputPath <- UniPath.expand $ UniPath.append location (getBuildPath config)
-    Directory.removeDirectoryRecursive outputPath
