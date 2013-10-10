7df271e53d8f14d4d8cf89656eb81cd761b4cf3e
Piotr Młodawski <remdezx@gmail.com>
2013-10-10 22:22:15 +0200

    Fixed exit codes in wrappers; Fixed Process.runProcess

--- src/Cabal.hs
+++ src/Cabal.hs
@@ -2,21 +2,25 @@
 
 module Cabal where
 
-import qualified System.Environment    as Env
 import qualified System.Cmd            as Cmd
+import qualified System.Environment    as Env
+import qualified System.Exit           as Exit
+
 import qualified Flowbox.Config.Config as Cfg
 
 
+
+main :: IO ()
 main = do
     cfg     <- Cfg.load
     args    <- Env.getArgs
     let exec = (Cfg.cabalBin . Cfg.cabalTP . Cfg.thirdparty) cfg
-    if "install" `elem` args
-        then Cmd.rawSystem exec $ ("--config-file=" ++ (Cfg.cabal . Cfg.config) cfg)
-                                -- : "--package-db=clear"
-                                -- : "--package-db=global"
-                                -- : ("--package-db=" ++ (Cfg.pkgDb . Cfg.local) cfg)
-                                : args
-        else Cmd.rawSystem exec $ ("--config-file=" ++ (Cfg.cabal . Cfg.config) cfg)
-                                : args
-
+    exitCode <- if "install" `elem` args
+                    then Cmd.rawSystem exec $ ("--config-file=" ++ (Cfg.cabal . Cfg.config) cfg)
+                                            -- : "--package-db=clear"
+                                            -- : "--package-db=global"
+                                            -- : ("--package-db=" ++ (Cfg.pkgDb . Cfg.local) cfg)
+                                            : args
+                    else Cmd.rawSystem exec $ ("--config-file=" ++ (Cfg.cabal . Cfg.config) cfg)
+                                            : args
+    Exit.exitWith exitCode
