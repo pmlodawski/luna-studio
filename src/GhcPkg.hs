711e356f835a01e4b3cade8055862a2990e880b1
Wojciech Danilo <wojtek.danilo@gmail.com>
2013-10-14 15:51:44 +0200

    Patched ghc-pkg to allow relocatable libraries from cabal --global installs

--- src/GhcPkg.hs
+++ src/GhcPkg.hs
@@ -3,22 +3,46 @@
 module GhcPkg where
 
 import qualified System.Cmd            as Cmd
+import qualified System.Process        as Process
 import qualified System.Environment    as Env
 import qualified System.Exit           as Exit
 
 import qualified Flowbox.Config.Config as Cfg
-
+import           Data.String.Utils       (replace)
+import System.IO
+import Control.Monad
 
 
 main :: IO ()
 main = do
     cfg     <- Cfg.load
     args    <- Env.getArgs
+    
+    --appendFile "/tmp/test.txt" "-----------\n\n"
+    --appendFile "/tmp/test.txt" $ "args: " ++ show args ++ "\n"
+
+    stdinTxt <- if "-" `elem` args
+                then do 
+                     inTxt <- hGetContents stdin
+                     out <- return $ if "update" `elem` args 
+                        then replace ((Cfg.path . Cfg.ffs) cfg) "${pkgroot}/../../../.." inTxt
+                        else inTxt
+                     --appendFile "/tmp/test.txt" out
+                     return out
+                else return ""
+
+    
     let exec = (Cfg.ghcPkgBin . Cfg.ghcTP . Cfg.thirdparty) cfg
-    exitCode <- Cmd.rawSystem exec $ "--global-package-db"
-                       : (Cfg.pkgConf . Cfg.ghcTP . Cfg.thirdparty) cfg
-                       : "--global"
-                       : ("--package-db=" ++ (Cfg.pkgDb . Cfg.local) cfg)
-                       : args
+
+    (exitCode, outResult, errResult) <- Process.readProcessWithExitCode 
+                                        exec ( "--global-package-db"
+                                        : (Cfg.pkgConf . Cfg.ghcTP . Cfg.thirdparty) cfg
+                                        : "--global"
+                                        : ("--package-db=" ++ (Cfg.pkgDb . Cfg.local) cfg)
+                                        : args
+                                        )
+                                        stdinTxt
+    hPutStr stderr errResult 
+    putStr outResult
     Exit.exitWith exitCode
     
