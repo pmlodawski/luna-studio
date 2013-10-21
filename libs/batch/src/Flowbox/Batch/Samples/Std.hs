2d6010f9d2b3b81c58266e0191d9054fb5ebb13f
remdezx <remdezx@gmail.com>
2013-10-21 16:30:47 +0200

    Std lib fixes

--- libs/batch/src/Flowbox/Batch/Samples/Std.hs
+++ libs/batch/src/Flowbox/Batch/Samples/Std.hs
@@ -59,29 +59,23 @@ cls_console = Definition.empty { Definition.cls   = Type.Class "Console" [] []
 
 
 addSomeDefs :: DefManager -> DefManager
-addSomeDefs defs = DefManager.addToParent (10, 14, mkFunction "add"      [ Type.Named "self"    $ Type.TypeName "Float"
-                                                                         , Type.Named "value"   $ Type.TypeName "Float" ] 
-                                                                         [ Type.Named "result"  $ Type.TypeName "Float"])
-                 $ DefManager.addToParent (10, 13, mkFunction "sub"      [ Type.Named "self"    $ Type.TypeName "Float"
-                                                                         , Type.Named "value"   $ Type.TypeName "Float" ] 
-                                                                         [ Type.Named "result"  $ Type.TypeName "Float"])
-                 $ DefManager.addToParent (10, 12, mkFunction "mul"      [ Type.Named "self"    $ Type.TypeName "Float"
-                                                                         , Type.Named "value"   $ Type.TypeName "Float" ] 
-                                                                         [ Type.Named "result"  $ Type.TypeName "Float"])
-                 $ DefManager.addToParent (10, 11, mkFunction "div"      [ Type.Named "self"    $ Type.TypeName "Float"
-                                                                         , Type.Named "value"   $ Type.TypeName "Float" ] 
-                                                                         [ Type.Named "result"  $ Type.TypeName "Float"])
-                 $ DefManager.addToParent (0, 10, mkClass    "Float")
-                 $ DefManager.addToParent (6, 9 , mkFunction "add"       [ Type.Named "self"    $ Type.TypeName "Int"
+addSomeDefs defs = DefManager.addToParent (0, 12, mkFunction "at"        [ Type.Named "self"    $ Type.TypeName "String"
+                                                                         , Type.Named "index"   $ Type.TypeName "Int"] 
+                                                                         [ Type.Named "element" $ Type.TypeName "Char"])
+                 $ DefManager.addToParent (0, 11, mkFunction "contains"  [ Type.Named "self"    $ Type.TypeName "String"
+                                                                         , Type.Named "element" $ Type.TypeName "Char"] 
+                                                                         [ Type.Named "contains"$ Type.TypeName "Bool"])
+                 $ DefManager.addToParent (0, 10, mkFunction "length"    [ Type.Named "self"    $ Type.TypeName "String"]
+                                                                         [ Type.Named "length"  $ Type.TypeName "Int"])
+                 $ DefManager.addToParent (0, 9 , mkFunction "add"       [ Type.Named "self"    $ Type.TypeName "Int"
                                                                          , Type.Named "value"   $ Type.TypeName "Int" ] 
                                                                          [ Type.Named "result"  $ Type.TypeName "Int"])
-                 $ DefManager.addToParent (6, 8 , mkFunction "sub"       [ Type.Named "self"    $ Type.TypeName "Int"
+                 $ DefManager.addToParent (0, 8 , mkFunction "sub"       [ Type.Named "self"    $ Type.TypeName "Int"
                                                                          , Type.Named "value"   $ Type.TypeName "Int" ] 
                                                                          [ Type.Named "result"  $ Type.TypeName "Int"])
-                 $ DefManager.addToParent (6, 7 , mkFunction "mul"       [ Type.Named "self"    $ Type.TypeName "Int"
+                 $ DefManager.addToParent (0, 7 , mkFunction "mul"       [ Type.Named "self"    $ Type.TypeName "Int"
                                                                          , Type.Named "value"   $ Type.TypeName "Int" ] 
                                                                          [ Type.Named "result"  $ Type.TypeName "Int"])
-                 $ DefManager.addToParent (0, 6 , mkClass    "Int")
                  $ DefManager.addToParent (4, 5 , mkFunction "print"     [ Type.Named "self"    $ Type.TypeName "Console"
                                                                          , Type.Named "obj"     $ Type.TypeName "String" ] 
                                                                          [ Type.Named "console" $ Type.TypeName "Console"])
