032baf879a63f7cc848fd4a65e67ed2e3233ac3a
Wojciech Danilo <wojtek.danilo@gmail.com>
2013-11-19 07:26:43 +0100

    Updated config and Lunac arg parser.

---
+++ src/Flowbox/Lunac/Config.hs
@@ -0,0 +1,13 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2013
+---------------------------------------------------------------------------
+
+module Flowbox.Lunac.Config (
+	module Flowbox.Lunac.Config,
+	module Paths_flowbox_lunac
+) where
+
+import Paths_flowbox_lunac (version)
