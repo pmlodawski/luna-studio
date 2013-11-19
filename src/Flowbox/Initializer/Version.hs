12c5a4cd0a30dfafca90052dfc530466104d0c8e
remdezx <remdezx@gmail.com>
2013-11-19 14:38:11 +0100

    Fixed lunac back to work

---
+++ src/Flowbox/Initializer/Version.hs
@@ -0,0 +1,22 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2013
+---------------------------------------------------------------------------
+
+module Flowbox.Initializer.Version where
+
+import qualified Data.Version               as Version
+
+import           Flowbox.Prelude              
+import qualified Flowbox.Initializer.Config as Config
+
+
+
+full :: Bool -> String
+full = initializer 
+
+
+initializer :: Bool -> String
+initializer numeric = (if numeric then "" else "Luna initializer version ") ++ Version.showVersion Config.version
