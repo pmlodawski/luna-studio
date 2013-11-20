bafd25d498814fac7969a44a516dc9e2aedb7478
remdezx <remdezx@gmail.com>
2013-11-20 11:09:14 +0100

    Conversions refactor

---
+++ src/Flowbox/Tools/Serialize/Proto/Conversion/Basic.hs
@@ -0,0 +1,17 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2013
+---------------------------------------------------------------------------
+
+module Flowbox.Tools.Serialize.Proto.Conversion.Basic (
+	module Flowbox.Tools.Conversion.Proto,
+	module Flowbox.Tools.Serialize.Proto.Conversion.List,
+) where
+
+import           Flowbox.Tools.Conversion.Proto                     
+import           Flowbox.Tools.Serialize.Proto.Conversion.Int       ()
+import           Flowbox.Tools.Serialize.Proto.Conversion.List      
+import           Flowbox.Tools.Serialize.Proto.Conversion.String    ()
+import           Flowbox.Tools.Serialize.Proto.Conversion.UniPath   ()
