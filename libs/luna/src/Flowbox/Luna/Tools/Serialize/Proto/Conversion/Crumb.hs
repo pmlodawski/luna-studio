1cbcb7840e477e9d0be40b9e61e6ff99116442c2
remdezx <remdezx@gmail.com>
2013-11-29 16:49:58 +0100

    Added Crumbs serialization

---
+++ libs/luna/src/Flowbox/Luna/Tools/Serialize/Proto/Conversion/Crumb.hs
@@ -0,0 +1,43 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2013
+---------------------------------------------------------------------------
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
+
+module Flowbox.Luna.Tools.Serialize.Proto.Conversion.Crumb where
+
+import           Control.Applicative                              
+
+import           Flowbox.Prelude                                  
+import           Flowbox.Control.Error                            
+import           Flowbox.Luna.Data.AST.Crumb.Crumb                (Breadcrumbs, Crumb)
+import qualified Flowbox.Luna.Data.AST.Crumb.Crumb              as Crumb
+import           Flowbox.Tools.Serialize.Proto.Conversion.Basic   
+import qualified Generated.Proto.Crumb.Breadcrumbs              as Gen
+import qualified Generated.Proto.Crumb.Crumb                    as Gen
+import qualified Generated.Proto.Crumb.Crumb.Cls                as GenCls
+
+
+
+instance Convert Crumb Gen.Crumb where
+    encode crumb = case crumb of
+        Crumb.FunctionCrumb name -> Gen.Crumb GenCls.FunctionCrumb $ encodePJ name
+        Crumb.ClassCrumb    name -> Gen.Crumb GenCls.ClassCrumb    $ encodePJ name
+        Crumb.ModuleCrumb   name -> Gen.Crumb GenCls.ModuleCrumb   $ encodePJ name
+    decode (Gen.Crumb tcls mtname) = do
+        name <- decodeP <$> mtname <?> "Failed to decode Crumb: `name` field is missing"
+        let cls = case tcls of
+                    GenCls.FunctionCrumb -> Crumb.FunctionCrumb
+                    GenCls.ClassCrumb    -> Crumb.ClassCrumb
+                    GenCls.ModuleCrumb   -> Crumb.ModuleCrumb
+        return $ cls name
+
+instance Convert Breadcrumbs Gen.Breadcrumbs where
+    encode = Gen.Breadcrumbs . encodeList
+    decode (Gen.Breadcrumbs b) = decodeList b
