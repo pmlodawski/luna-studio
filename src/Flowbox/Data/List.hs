7f58edfee8d994ec3f136b485506e15a6e102433
Wojciech Danilo <wojtek.danilo@gmail.com>
2013-12-10 14:46:11 +0100

    Updated package listing. Package info was taken from the oldest, not newest pkg version.

--- src/Flowbox/Data/List.hs
+++ src/Flowbox/Data/List.hs
@@ -5,16 +5,16 @@
 -- Flowbox Team <contact@flowbox.io>, 2013
 ---------------------------------------------------------------------------
 
-module Flowbox.Data.List(
-    foldri,
-    count,
-) where
+module Flowbox.Data.List where
 
 import qualified Data.List       as List
 import           Flowbox.Prelude
 
 foldri :: (a -> b -> b) -> [a] -> b -> b
 foldri a b c = foldr a c b
+
+foldli :: (a -> b -> a) -> [b] -> a -> a
+foldli a b c = foldl a c b
 
 --foldli :: (a -> b -> b) -> [a] -> b -> b
 --foldli a b c = foldr a c b
