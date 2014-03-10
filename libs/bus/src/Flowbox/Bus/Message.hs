19e68ba21db0322ac0041b7503df3d3c1bb57a27
remdezx <remdezx@gmail.com>
2014-03-10 10:43:51 +0100

    Implemented basic bus api

---
+++ libs/bus/src/Flowbox/Bus/Message.hs
@@ -0,0 +1,29 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Flowbox Team <contact@flowbox.io>, 2014
+-- Proprietary and confidential
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+---------------------------------------------------------------------------
+module Flowbox.Bus.Message where
+
+import           Data.ByteString              (ByteString)
+import qualified Flowbox.Text.ProtocolBuffers as Proto
+
+import Flowbox.Prelude
+
+
+
+type ID = Proto.Int32
+
+type MessageID = ID
+type ClientID  = ID
+
+
+data CorrelationID = CorrelationID { clientID  :: ClientID
+                                   , messageID :: MessageID
+                                   } deriving (Read, Show, Eq)
+
+
+data Message = Message { topic   :: ByteString
+                       , message :: ByteString
+                       } deriving (Read, Show, Eq)
