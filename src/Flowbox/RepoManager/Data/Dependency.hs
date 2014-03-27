477638a28e7d6228379473d4e51d5bb722132737
remdezx <remdezx@gmail.com>
2014-03-27 19:21:25 +0100

    Refactor in repo-manager

---
+++ src/Flowbox/RepoManager/Data/Dependency.hs
@@ -0,0 +1,17 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2014
+---------------------------------------------------------------------------
+module Flowbox.RepoManager.Data.Dependency where
+
+import           Flowbox.Prelude
+import qualified Flowbox.RepoManager.Data.Item.Name as Item
+import qualified Flowbox.RepoManager.Data.Version   as Version
+
+
+
+data Dependency = Dependency { name       :: Item.Name
+                             , constrains :: [Version.Constrain]
+                             } deriving (Show)
