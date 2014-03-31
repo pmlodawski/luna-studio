acdf4709cd84e979982d58298b7024ff87173c74
Krzysiek <you@example.com>
2014-03-31 15:55:34 +0200

    working with git repository VCS.create - create repository record basic operations with repo: Git.clone, Git.update, Git.remove

--- libs/repo-manager/src/Flowbox/RepoManager/VCS/Git/Git.hs
+++ libs/repo-manager/src/Flowbox/RepoManager/VCS/Git/Git.hs
@@ -1,47 +1,43 @@
 module Flowbox.RepoManager.VCS.Git.Git where
 
-import  System.Directory as Directory
-import System.Process (runCommand)
-import Data.Bool 
-import Data.List (concat)
-import Flowbox.Prelude
-import System.FilePath (pathSeparator)
-import Flowbox.RepoManager.VCS.Type as Type
+import Data.Bool
+--import           Data.List                    (concat)
+import           Flowbox.Prelude
+import qualified Flowbox.RepoManager.VCS.Type as VCS
+import qualified Flowbox.RepoManager.VCS.VCS  as VCS
 
+import qualified System.Directory as Directory
+import           System.FilePath  (pathSeparator)
+import           System.Process   (ProcessHandle, runCommand)
 
---import Git.Libgit2.Repository as LG
---import  Data.Maybe
+createVCS :: VCS.Type -> String -> String -> String -> VCS.VCS
+createVCS cls' repoPath name' remotePath' = VCS.VCS { VCS.cls = cls'
+                                                   , VCS.localPath = repoPath
+                                                   , VCS.name = name'
+                                                   , VCS.remotePath = remotePath'
+                                                   , VCS.clone = clone
+                                                   , VCS.update = update
+                                                   , VCS.remove = remove
+                                                   }
 
+remove :: VCS.VCS -> IO ProcessHandle
+remove repo = do runCommand $ concat ["rm -fR ", VCS.localPath repo , [pathSeparator], VCS.name repo]
 
---initRepository local = do
---                             repo   <- openOrCreateRepository local False
---                             ref    <- resolveRef repo "HEAD"
+clone :: VCS.VCS -> IO VCS.VCS
+clone repo = do
+                Directory.createDirectoryIfMissing True (VCS.localPath repo)
+                current <- Directory.getCurrentDirectory
+                Directory.setCurrentDirectory (VCS.localPath repo)
+                runCommand $ concat ["git clone ", (VCS.remotePath repo)] -- ADD --quiet?
+                Directory.setCurrentDirectory current
+                return repo
 
---                             -- commit <- maybe (return Nothing) (lookupCommit repo) ref
- 
-remove repo = do exists <- Directory.doesDirectoryExist $ concat [(Type.local repo), [pathSeparator], (Type.name repo)]
-                 let result = case exists of
-                            True -> Just $ runCommand $ concat ["rm -fR ", Type.local repo , [pathSeparator], Type.name repo]
-                            False -> Nothing
-                 return repo
-
--- catch exceptions
--- fatal: destination path 'packages' already exists and is not an empty directory.
-clone repo = do   
-               Directory.createDirectoryIfMissing True (Type.local repo)
-               current <- getCurrentDirectory
-               Directory.setCurrentDirectory (Type.local repo)
-               runCommand $ concat ["git clone ", (Type.remote repo)] -- ADD --quiet?
+update :: VCS.VCS -> IO VCS.VCS
+update repo = do
+               current <- Directory.getCurrentDirectory
+               Directory.setCurrentDirectory (concat [VCS.localPath repo, [pathSeparator], VCS.name repo])
+               runCommand $ concat ["git pull ", VCS.remotePath repo] -- ADD --quiet?
                Directory.setCurrentDirectory current
                return repo
 
-pull repo = do    
-               current <- getCurrentDirectory
-               Directory.setCurrentDirectory (concat [Type.local repo, [pathSeparator], Type.name repo])
-               runCommand $ concat ["git pull ", Type.remote repo] -- ADD --quiet?
-               Directory.setCurrentDirectory current
-               return repo
-                                     
 
-
-                                     
