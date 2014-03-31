ea2b557dc9563f69119b57c85c6a486ea660ba0f
Krzysiek <you@example.com>
2014-03-31 17:50:10 +0200

    dummy reader of 'packages' repository

--- src/Flowbox/RepoManager/Data/Repository.hs
+++ src/Flowbox/RepoManager/Data/Repository.hs
@@ -6,13 +6,17 @@
 ---------------------------------------------------------------------------
 module Flowbox.RepoManager.Data.Repository where
 
-import Data.Map (Map)
+import Data.Map as Map
 
 import           Flowbox.Prelude
 import           Flowbox.RepoManager.Data.Item.Family (InstalledFamilies, AvailableFamilies)
 import qualified Flowbox.RepoManager.Data.Item.Name             as Item
-
-
+import qualified System.Directory as Files
+import qualified Data.Version as Version
+import qualified Data.List as List
+import qualified Flowbox.RepoManager.Data.Item.Item as Item
+import qualified Flowbox.RepoManager.Data.Dependency as Dependency
+import qualified Flowbox.RepoManager.Data.Environment as URI
 
 data Repository = Repository { items :: Map Item.Name AvailableFamilies
                              } deriving (Show)
@@ -22,4 +26,45 @@ data World = World { installed :: Map Item.Name InstalledFamilies
                    , selected  :: Map Item.Name InstalledFamilies
                    } deriving (Show)
 
+type FileName = String
 
+build repoPath = do files <- Files.getDirectoryContents repoPath
+                    let cat = List.foldl category Repository {} files
+                    return ()
+
+isProper :: String -> Bool
+isProper file = not (file `elem` [".git", "README.md", "..", "."])
+
+--category :: FileName -> FileName
+category :: FilePath -> Map Item.Name AvailableFamilies -> Map Item.Name AvailableFamilies
+category categoryDir repo = case isProper categoryDir of
+                                    False -> repo
+                                    True  -> do files <- Files.getDirectoryContents categoryDir
+                                                Map.insert (List.foldl package Map.empty files)
+
+package :: FilePath -> Map Item.Name AvailableFamilies -> Map Item.Name AvailableFamilies
+package packageDir repo = case isProper packageDir of
+                            False -> repo
+                            True  -> 
+                                     do packageFiles <- Files.getDirectoryContents packageDir
+                                        let newPackage = List.foldl versions Map.empty packageFiles
+                                        Map.insert packageDir newPackage repo
+
+versions  :: FilePath -> Map Version.Version Item.Item
+versions  file versions' = let version' = Version.versionBranch [1,2,3,4]
+                               name' = "Item.Name"
+                               source' = Map.singleton "x86" (URI.Local "local/uri")
+                               dependancies' = [Dependency.Dependency { Dependency.name = "dependancy1"
+                                                                      , Dependency.constraints = []
+                                                                      }]
+                               item = Item.Item { Item.name            = name'
+                                                , Item.version         = version'
+                                                , Item.source          = source'
+                                                , Item.installScript   = ["script"]
+                                                , Item.uninstallScript = ["script"]
+                                                , Item.dependencies    = dependancies'
+                                                }
+                           in Map.insert $ name' Map.singleton name' item versions'
+                        
+
+                             
--- test/Main.hs
+++ test/Main.hs
@@ -7,40 +7,40 @@
 module Main where
 
 import           Flowbox.Prelude
-import qualified Flowbox.RepoManager.VCS.Git.Git as Git
-import qualified Flowbox.RepoManager.VCS.Type    as VCS
-import qualified Flowbox.RepoManager.VCS.VCS  as VCS
+--import qualified Flowbox.RepoManager.VCS.Git.Git as Git
+--import qualified Flowbox.RepoManager.VCS.Type    as VCS
+--import qualified Flowbox.RepoManager.VCS.VCS  as VCS
 
-import qualified           System.Environment              as Environment
+--import qualified           System.Environment              as Environment
 import Flowbox.RepoManager.Data.Repository as Repository
 
 main :: IO ()
+main = Repository.build "repo/packages"
 
+--main = do
+--          args <- Environment.getArgs
+--          case args of
+--            []         -> Main.clone
+--            "update":_ -> Main.update
+--            "remove":_ -> Main.remove
+--            "clone": _ -> Main.clone
+--            _other     -> Main.clone
 
-main = do
-          args <- Environment.getArgs
-          case args of
-            []         -> Main.clone
-            "update":_ -> Main.update
-            "remove":_ -> Main.remove
-            "clone": _ -> Main.clone
-            _other     -> Main.clone
+--create ::  VCS.VCS
+--create = Git.createVCS VCS.Git "repo" "packages" "git@github.com:dobry/packages.git"
 
-create ::  VCS.VCS
-create = Git.createVCS VCS.Git "repo" "packages" "git@github.com:dobry/packages.git"
+--clone :: IO ()
+--clone = do 
+--           _ <- Git.clone create
+--           return ()
 
-clone :: IO ()
-clone = do 
-           _ <- Git.clone create
-           return ()
+--update :: IO ()
+--update = do
+--            _ <- Git.update create
+--            return ()
 
-update :: IO ()
-update = do
-            _ <- Git.update create
-            return ()
-
-remove :: IO ()
-remove = do 
-            _ <- Git.remove create
-            return ()
+--remove :: IO ()
+--remove = do 
+--            _ <- Git.remove create
+--            return ()
 
