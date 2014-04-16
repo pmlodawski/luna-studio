3fa23d6af616130f8dfbe7db5b8f3ba4e65633a7
Piotr Młodawski <remdezx@gmail.com>
2014-04-16 12:13:54 +0200

    Implmented getting an instance for a client

--- tests/Main.hs
+++ tests/Main.hs
@@ -4,73 +4,44 @@
 -- Proprietary and confidential
 -- Flowbox Team <contact@flowbox.io>, 2014
 ---------------------------------------------------------------------------
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Main where
 
-import Flowbox.Prelude
-
 import qualified AWS                          as AWS
+import           AWS.EC2                      (EC2)
 import qualified AWS.EC2                      as EC2
 import qualified AWS.EC2.Types                as Types
 import qualified AWS.EC2.Util                 as Util
 import qualified Control.Concurrent           as Concurrent
 import           Control.Monad                (forM)
+import           Control.Monad.IO.Class       (MonadIO, liftIO)
+import qualified Control.Monad.Loops          as Loops
 import qualified Control.Monad.Trans.Resource as Resource
 import           Data.Text                    (Text)
 import qualified Data.Text                    as Text
 import           Text.Show.Pretty             (ppShow)
-import Control.Monad.IO.Class (liftIO)
+
+import qualified Flowbox.AWS.Instance as Instance
+import           Flowbox.Prelude
 
 
 
 type Tag = Text
+type UserName = Text
+type InstanceID = Text
+
+
+region = Text.pack "eu-west-1"
 
 
 main :: IO ()
 main = do
     credential <- AWS.loadCredential
     let runEC2   = Resource.runResourceT . EC2.runEC2 credential
-        region           = Text.pack "eu-west-1"
-        userTagKey       = Text.pack "user"
-        userName         = Text.pack "zenon"
-        imageID          = Text.pack "ami-a921dfde"
-        instanceType     = Text.pack "t1.micro"
-        requestSecurityGroups = [Text.pack "launch-wizard-1"]
-    runEC2 $ do EC2.setRegion region
-                let userFilter = [(Text.append (Text.pack "tag:") userTagKey, [userName])]
-                userInstances <- concatMap Types.reservationInstanceSet <$> (Util.list $ EC2.describeInstances [] userFilter)
-                putStrLn $ ppShow userInstances
-                let instanceRequest = Types.RunInstancesRequest imageID -- ImageId :: Text
-                                                                1 -- MinCount :: Int
-                                                                1 -- MaxCount :: Int
-                                                                Nothing -- KeyName :: Maybe Text
-                                                                [] -- SecurityGroupIds :: [Text]
-                                                                requestSecurityGroups -- SecurityGroups :: [Text]
-                                                                Nothing -- UserData :: Maybe ByteString
-                                                                (Just instanceType) -- InstanceType :: Maybe Text
-                                                                Nothing -- AvailabilityZone :: Maybe Text
-                                                                Nothing -- PlacementGroup :: Maybe Text
-                                                                Nothing -- Tenancy :: Maybe Text
-                                                                Nothing -- KernelId :: Maybe Text
-                                                                Nothing -- RamdiskId :: Maybe Text
-                                                                [] -- BlockDeviceMappings :: [BlockDeviceMappingParam]
-                                                                Nothing -- MonitoringEnabled :: Maybe Bool
-                                                                Nothing -- SubnetId :: Maybe Text
-                                                                Nothing -- DisableApiTermination :: Maybe Bool
-                                                                Nothing -- ShutdownBehavior :: Maybe ShutdownBehavior
-                                                                Nothing -- PrivateIpAddress :: Maybe IPv4
-                                                                Nothing -- ClientToken :: Maybe Text
-                                                                [] -- NetworkInterfaces :: [NetworkInterfaceParam]
-                                                                Nothing -- IamInstanceProfile :: Maybe IamInstanceProfile
-                                                                Nothing -- runInstancesRequestEbsOptimized :: Maybe Bool
-                putStrLn "==== Run instances ===="
-                reservation <- EC2.runInstances instanceRequest
-                let resourceIDs = map Types.instanceId $ Types.reservationInstanceSet reservation
-                True <- EC2.createTags resourceIDs [(userTagKey, userName)]
-                putStrLn $ ppShow reservation
-                putStrLn "==== Run instances succeeded ===="
-                forM [0..] $ \i -> do putStrLn $ "==== Retry " ++ (show i) ++ " ===="
-                                      liftIO $ Concurrent.threadDelay 5000000
-                                      userInstances <- concatMap Types.reservationInstanceSet <$> (Util.list $ EC2.describeInstances [] userFilter)
-                                      putStrLn $ ppShow userInstances
-
+        userName = "zenon"
+    ip <- runEC2 $ do EC2.setRegion region
+                      Types.instanceIpAddress <$> Instance.get userName Instance.defaultInstanceRequest
+    print ip
     putStrLn "quiting"
