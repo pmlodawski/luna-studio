b974e1a65250bff0b8cd5199389f9bffdc8df1c2
Piotr Młodawski <remdezx@gmail.com>
2014-04-23 15:32:37 +0200

    Refactor in flowbox-aws; Added account-manager-mock project

--- src/Flowbox/ZMQ/RPC/RPC.hs
+++ src/Flowbox/ZMQ/RPC/RPC.hs
@@ -5,18 +5,9 @@
 -- Flowbox Team <contact@flowbox.io>, 2014
 ---------------------------------------------------------------------------
 
-module Flowbox.ZMQ.RPC.RPC (
-    module Control.Monad.Trans.Either,
-    liftIO,
-    safeLiftIO,
-    RPC,
-    Error
-) where
+module Flowbox.ZMQ.RPC.RPC where
 
-import Control.Exception          (SomeException, try)
-import Control.Monad.IO.Class     (liftIO)
 import Control.Monad.Trans.Either
-import Data.EitherR               (fmapL)
 
 import Flowbox.Prelude
 
@@ -26,10 +17,3 @@ type RPC a = EitherT Error IO a
 
 
 type Error = String
-
-
-safeLiftIO :: IO a -> RPC a
-safeLiftIO fun = do
-    result <- liftIO $ (try :: IO a -> IO (Either SomeException a)) fun
-    hoistEither $ fmapL show result
-
