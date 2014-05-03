07f7a172ad58aee9c3af389f6b4e3572a30e522e
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-05-03 20:51:59 +0200

    Implemented single package Install hook

--- src/Flowbox/RepoManager/Data/Package/Flag.hs
+++ src/Flowbox/RepoManager/Data/Package/Flag.hs
@@ -7,7 +7,14 @@
 module Flowbox.RepoManager.Data.Package.Flag where
 
 import Flowbox.Prelude
+import Data.Function   (on)
 
 data Flag = Flag { name        :: String
                  , description :: String
                  } deriving Show
+
+instance Eq Flag where
+    f1 == f2 = name f1 == name f2
+
+instance Ord Flag where
+    compare = compare `on` name
