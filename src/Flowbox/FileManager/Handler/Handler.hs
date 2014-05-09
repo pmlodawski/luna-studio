f936dac8201c4a7ba5979c1ecff9d9fc0b6a6b4a
Piotr Młodawski <remdezx@gmail.com>
2014-05-09 09:42:00 +0200

    Project restructure

---
+++ src/Flowbox/FileManager/Handler/Handler.hs
@@ -0,0 +1,47 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2014
+---------------------------------------------------------------------------
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE RankNTypes      #-}
+
+module Flowbox.FileManager.Handler.Handler where
+
+import           Flowbox.Bus.Data.Topic                 (Topic)
+import           Flowbox.Bus.RPC.BusRPCHandler          (BusRPCHandler)
+import qualified Flowbox.Bus.RPC.Processor              as P
+import qualified Flowbox.FileManager.Handler.FileSystem as FSHandler
+import           Flowbox.Prelude                        hiding (error)
+import           Flowbox.System.Log.Logger
+
+
+
+logger :: LoggerIO
+logger = getLoggerIO "Flowbox.FileManager.Handler"
+
+
+topics :: [Topic]
+topics = [ "filesystem.ls.request"
+         , "filesystem.stat.request"
+         , "filesystem.mkdir.request"
+         , "filesystem.touch.request"
+         , "filesystem.rm.request"
+         , "filesystem.cp.request"
+         , "filesystem.mv.request"
+         ]
+
+
+handler :: BusRPCHandler
+handler callback topic = case topic of
+    "filesystem.ls.request"    -> callback P.status $ P.singleResult FSHandler.ls
+    "filesystem.stat.request"  -> callback P.status $ P.singleResult FSHandler.stat
+    "filesystem.mkdir.request" -> callback P.update $ P.singleResult FSHandler.mkdir
+    "filesystem.touch.request" -> callback P.update $ P.singleResult FSHandler.touch
+    "filesystem.rm.request"    -> callback P.update $ P.singleResult FSHandler.rm
+    "filesystem.cp.request"    -> callback P.update $ P.singleResult FSHandler.cp
+    "filesystem.mv.request"    -> callback P.update $ P.singleResult FSHandler.mv
+    unsupported             -> do let errMsg = "Unknown topic: " ++ show unsupported
+                                  logger error errMsg
+                                  return $ P.respondError topic errMsg
