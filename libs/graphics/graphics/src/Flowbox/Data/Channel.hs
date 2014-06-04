4c96e368dfd60ea09dd55b3c86e4bc28eace572d
Konrad Madej <kdmadej+git@gmail.com>
2014-06-04 10:35:45 +0200

    zippers for channels from Data.Chan added

--- libs/graphics/graphics/src/Flowbox/Data/Channel.hs
+++ libs/graphics/graphics/src/Flowbox/Data/Channel.hs
@@ -7,15 +7,101 @@
 
 module Flowbox.Data.Channel where
 
-import Data.Map
-import Data.List.Split
+import Data.Map as Map
 
 import qualified Flowbox.Graphics.Image.Error as Image
-import 			 Flowbox.Prelude
+import           Flowbox.Prelude
 
 
 
 data ChannelTree name value = ChannelTree  { _children :: Map name (ChannelTree name value)
                                            , _channel  :: Maybe value
                                            }
+                            | EmptyNode
+                            deriving (Show)
 makeLenses ''ChannelTree
+
+data Crumb name value = Crumb name (Maybe value) (Map name (ChannelTree name value)) deriving (Show)
+type Breadcrumbs name value = [Crumb name value]
+type Zipper name value = (ChannelTree name value, Breadcrumbs name value)
+type ZipperResult name value = Either ZipperError (Zipper name value)
+
+data ZipperError = UnreachableError
+                 | UpUnreachableError
+                 | AttachToEmpty
+                 | AppendToEmpty
+                 | InsertToExisting
+                 | AlterEmptyError
+                 | RemoveEmptyError
+                 | GetNonExistant
+                 deriving (Show, Eq)
+
+-- == Tree ==
+
+empty :: Ord name => ChannelTree name value
+empty = ChannelTree mempty Nothing
+
+-- == Traversing ==
+
+zipper :: ChannelTree name value -> ZipperResult name value
+zipper t = return (t, [])
+
+goTo :: Ord name => name -> Zipper name value -> ZipperResult name value
+goTo name (ChannelTree tree chan, bs) = case Map.lookup name tree of
+    Nothing    -> Left UnreachableError
+    Just tree' -> Right (tree', Crumb name chan rest:bs)
+    where rest = Map.delete name tree
+
+up :: Ord name => Zipper name value -> ZipperResult name value
+up (_, []) = Left UpUnreachableError
+up (t, Crumb name chan rest:bs) = case t of
+    EmptyNode -> Right (ChannelTree rest chan, bs)
+    node      -> Right (ChannelTree (Map.insert name node rest) chan, bs)
+
+top :: Ord name => Zipper name value -> ZipperResult name value
+top z = case up z of
+    Left  _ -> Right z
+    Right u -> top u
+
+-- == Modifications ==
+
+-- = Tree =
+
+attach :: Ord name => name -> ChannelTree name value -> Zipper name value -> ZipperResult name value
+attach name new (t, bs) = case t of
+    EmptyNode             -> Left AttachToEmpty
+    ChannelTree tree chan -> Right (ChannelTree (Map.insert name new tree) chan, bs)
+
+insert :: Ord name => Maybe value -> Zipper name value -> ZipperResult name value
+insert v (t, bs) = case t of
+    EmptyNode -> Right (ChannelTree mempty v, bs)
+    _         -> Left InsertToExisting
+
+append :: Ord name => name -> Maybe value -> Zipper name value -> ZipperResult name value
+append name v = attach name (ChannelTree mempty v)
+
+delete :: Zipper name value -> ZipperResult name value
+delete (t, bs) = case t of
+    EmptyNode -> Left RemoveEmptyError
+    ChannelTree tree chan -> Right (EmptyNode, bs)
+
+-- = Value =
+
+clear :: Zipper name value -> ZipperResult name value
+clear (t, bs) = case t of
+    EmptyNode             -> Left AlterEmptyError
+    ChannelTree tree chan -> Right (ChannelTree tree $ Nothing, bs)
+
+set :: value -> Zipper name value -> ZipperResult name value
+set v (t, bs) = case t of
+    EmptyNode             -> Left AlterEmptyError
+    ChannelTree tree chan -> Right (ChannelTree tree $ Just v, bs)
+
+modify :: (value -> value) -> Zipper name value -> ZipperResult name value
+modify f (t, bs) = case t of
+    EmptyNode             -> Left AlterEmptyError
+    ChannelTree tree chan -> Right (ChannelTree tree $ fmap f chan, bs)
+
+value :: ZipperResult name value -> Either ZipperError (Maybe value)
+value (Left _) = Left GetNonExistant
+value (Right (ChannelTree _ val, _)) = Right val
--- libs/graphics/graphics/src/Flowbox/Graphics/Image/View/Internal.hs
+++ libs/graphics/graphics/src/Flowbox/Graphics/Image/View/Internal.hs
@@ -7,9 +7,12 @@
 
 module Flowbox.Graphics.Image.View.Internal where
 
-import Data.Set
+import           Data.Set
+import           Data.List.Split
+import qualified Data.Map as Map
 
 import           Flowbox.Data.Channel
+import qualified Flowbox.Graphics.Image.Error   as Image
 import           Flowbox.Graphics.Image.Channel (Channel)
 import qualified Flowbox.Graphics.Image.Channel as Channel
 import           Flowbox.Prelude
@@ -23,8 +26,23 @@ data Select = All
             | Group { names :: Set Name }
             deriving (Show)
 
-class View view where
-    name     :: Lens' view Name
-    channels :: Lens' view (ChannelTree Channel.Name Channel)
+class View v where
+    name     :: Lens' v Name
+    channels :: Lens' v (ChannelTree Channel.Name Channel)
     --viewBounds :: Bounds
     --pixelAspectRatio :: AspectRatio
+
+
+--get :: View v => v -> String -> Image.Result (Maybe Channel)
+--get v descriptor = rec parts (v ^. channels)
+--    where parts = splitOn "." descriptor
+--          rec [] _ = Left $ Image.ChannelLookupError descriptor
+--          rec (x:[]) (ChannelTree children _) = case Map.lookup x children of
+--              Nothing                -> Left $ Image.ChannelLookupError descriptor
+--              Just (ChannelTree _ n) -> Right n
+--          rec (x:xs) (ChannelTree children _) = case Map.lookup x children of
+--              Nothing   -> Left $ Image.ChannelLookupError descriptor
+--              Just node -> rec xs node
+
+--insert :: View v => v -> String -> Channel -> Image.Result v
+--insert v descriptor val = rec parts ()
