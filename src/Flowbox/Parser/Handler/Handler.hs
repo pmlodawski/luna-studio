419b96641d8addcb3d8cfb1c2fda4bd290b8123e
Piotr Młodawski <remdezx@gmail.com>
2014-06-11 10:40:29 +0200

    Refactor in bus rpc

--- src/Flowbox/Parser/Handler/Handler.hs
+++ src/Flowbox/Parser/Handler/Handler.hs
@@ -13,6 +13,7 @@ import           Flowbox.Bus.Data.Message         (Message)
 import qualified Flowbox.Bus.Data.Topic           as Topic
 import           Flowbox.Bus.RPC.HandlerMap       (HandlerMap)
 import qualified Flowbox.Bus.RPC.HandlerMap       as HandlerMap
+import           Flowbox.Bus.RPC.RPC              (RPC)
 import qualified Flowbox.Bus.RPC.Server.Processor as Processor
 import qualified Flowbox.Parser.Handler.Parser    as ParserHandler
 import           Flowbox.Prelude                  hiding (error)
@@ -34,5 +35,5 @@ handlerMap callback = HandlerMap.fromList
     ]
     where
         call :: (Proto.Serializable args, Proto.Serializable result)
-             => String -> (args -> IO result) -> IO [Message]
+             => String -> (args -> RPC result) -> IO [Message]
         call type_ = callback type_ . Processor.singleResult
