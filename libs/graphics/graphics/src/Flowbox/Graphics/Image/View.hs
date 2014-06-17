57c31054a30d5550fe9f33f3de17870ddc35cf94
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-06-17 11:30:54 +0200

    Add stubs for View RGBA

--- libs/graphics/graphics/src/Flowbox/Graphics/Image/View.hs
+++ libs/graphics/graphics/src/Flowbox/Graphics/Image/View.hs
@@ -6,10 +6,11 @@
 ---------------------------------------------------------------------------
 
 module Flowbox.Graphics.Image.View (
-    --module Flowbox.Graphics.Image.View,
     module X
 ) where
 
-import Flowbox.Graphics.Image.View.Internal    as X
-import Flowbox.Graphics.Image.View.RGB         as X
-import Flowbox.Graphics.Image.View.Convert.RGB as X
+import Flowbox.Graphics.Image.View.Internal     as X
+import Flowbox.Graphics.Image.View.RGB          as X
+import Flowbox.Graphics.Image.View.RGBA         as X
+import Flowbox.Graphics.Image.View.Convert.RGB  as X
+import Flowbox.Graphics.Image.View.Convert.RGBA as X
