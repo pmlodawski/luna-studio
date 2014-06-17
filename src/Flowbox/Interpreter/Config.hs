4193a5e5ae47ce784e749cfadba5517b92b8e581
Piotr Młodawski <remdezx@gmail.com>
2014-06-17 10:53:30 +0200

    Added libs/luna/interpreter and tools/batch/plugins/interpreter projects

---
+++ src/Flowbox/Interpreter/Config.hs
@@ -0,0 +1,14 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2014
+---------------------------------------------------------------------------
+
+module Flowbox.Interpreter.Config (
+    module Paths_flowbox_interpreter,
+) where
+
+import Paths_flowbox_interpreter (version)
+
+
