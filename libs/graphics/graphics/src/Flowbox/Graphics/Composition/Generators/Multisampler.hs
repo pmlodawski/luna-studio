50ec2c4c9a432d227f37904b9aaea28b0a614af2
Konrad Lewandowski <korrix@gmail.com>
2014-06-18 10:29:00 +0000

    Corrected version of gaussian kernel generator

--- libs/graphics/graphics/src/Flowbox/Graphics/Composition/Generators/Multisampler.hs
+++ libs/graphics/graphics/src/Flowbox/Graphics/Composition/Generators/Multisampler.hs
@@ -12,33 +12,10 @@ import Flowbox.Prelude                                    as P
 import Flowbox.Graphics.Composition.Generators.Structures
 import Flowbox.Math.Matrix                                as M
 
-import Data.Array.Accelerate                              as A (fromIntegral, lift, unlift, index0, uncurry) 
-import Math.Coordinate.Cartesian                          as Cartesian
+import Data.Array.Accelerate                              as A (fromIntegral, lift, unlift, index0, index2, uncurry, truncate) 
+import Math.Coordinate.Cartesian                          (Point2(..))
 import Math.Space.Space
 
-
-
-normalize :: Matrix2 Double -> Matrix2 Double
-normalize arr = M.map (/values_sum) arr
-    where values_sum = the $ foldAll (+) (0.0 :: Exp Double) (flatten arr)
-
-uniform :: Matrix2 Double -> Matrix2 Double
-uniform arr = M.map (/max_value) arr
-    where max_value = the $ M.maximum (flatten arr)
-
--- TODO: Debug
-gaussianKernel :: Exp DIM2 -> Exp Double -> Matrix2 Double
-gaussianKernel sh' sigma' = uniform $ normalize $ generate sh $ \i -> let Z :. y :. x = unlift i :: (Z :. Exp Int :. Exp Int)
-                                                                      in gaussian (A.fromIntegral x - width_half)
-                                                                                  (A.fromIntegral y - height_half)
-    where sigma = the $ unit sigma'
-          sh = the $ unit sh'
-          Z :. height :. width = unlift sh :: EDIM2
-          height_half = A.fromIntegral $ height `div` 2
-          width_half = A.fromIntegral $ width `div` 2
-          sg = 2 * sigma ** 2
-          e = exp 1.0
-          gaussian x y = -(e / (pi * sg) - (x ** 2 + y ** 2) / sg)
 
 multisampler :: Matrix2 Double -> Generator -> Generator
 multisampler kernel generator space point = A.uncurry (/) result
@@ -50,6 +27,6 @@ multisampler kernel generator space point = A.uncurry (/) result
           result = sfoldl calc start index0 off
 
           calc acc p = lift (acc_values + weight * generator space (point + offset), acc_weights + weight) :: Exp (Double, Double)
-              where offset = Cartesian.Point2 (dx / A.fromIntegral w) (dy / A.fromIntegral h)
+              where offset = Point2 (dx / A.fromIntegral w) (dy / A.fromIntegral h)
                     (weight, dx, dy) = unlift p :: (Exp Double, Exp Double, Exp Double)
                     (acc_values, acc_weights) = unlift acc :: (Exp Double, Exp Double)
