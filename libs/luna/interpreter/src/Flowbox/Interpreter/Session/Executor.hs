d6a5af200653af1ce4b2afb99216c00ca34fae14
Piotr Młodawski <remdezx@gmail.com>
2014-07-03 17:33:35 +0200

    Passing arguments to a functions works now

--- libs/luna/interpreter/src/Flowbox/Interpreter/Session/Executor.hs
+++ libs/luna/interpreter/src/Flowbox/Interpreter/Session/Executor.hs
@@ -8,6 +8,8 @@ module Flowbox.Interpreter.Session.Executor where
 
 import           Control.Monad.State hiding (mapM, mapM_)
 import qualified Data.List           as List
+import qualified Data.Maybe          as Maybe
+import qualified GHC.Exts            as Exts
 
 import           Flowbox.Control.Error
 import qualified Flowbox.Data.MapForest                         as MapForest
@@ -25,12 +27,15 @@ import qualified Flowbox.Interpreter.Session.Data.DefPoint      as DefPoint
 import qualified Flowbox.Interpreter.Session.Env                as Env
 import           Flowbox.Interpreter.Session.Session            (Session)
 import qualified Flowbox.Interpreter.Session.Session            as Session
+import           Flowbox.Luna.Data.Graph.Edge                   (Edge)
+import qualified Flowbox.Luna.Data.Graph.Edge                   as Edge
 import qualified Flowbox.Luna.Data.Graph.Graph                  as Graph
+import           Flowbox.Luna.Data.Graph.Node                   (Node)
 import qualified Flowbox.Luna.Data.Graph.Node                   as Node
+import qualified Flowbox.Luna.Data.Graph.Port                   as Port
 import qualified Flowbox.Luna.Passes.Analysis.NameResolver      as NameResolver
 import           Flowbox.Prelude
 import           Flowbox.System.Log.Logger
-
 
 
 logger :: LoggerIO
@@ -80,19 +85,32 @@ processNodeIfNeeded callDataPath =
 
 previous :: CallDataPath -> Session [CallDataPath]
 previous []           = return []
-previous callDataPath = do
-    let graph        = last callDataPath ^. CallData.parentGraph
-        predecessors = Graph.prel graph $ last callDataPath ^. CallData.callPoint . CallPoint.nodeID
-    List.concat <$> mapM previousCall predecessors
+previous callDataPath' =
+    Maybe.catMaybes <$> mapM (globalPredecesors callDataPath')
+                             (Exts.sortWith (view $ _3 . Edge.dst)
+                                            (localPredecessors callDataPath'))
     where
-        previousCall (nodeID, node) = case node of
+        localPredecessors :: CallDataPath -> [(Node.ID, Node, Edge)]
+        localPredecessors callDataPath = localPreds where
+            graph      = last callDataPath ^. CallData.parentGraph
+            localPreds = Graph.lprel graph $ last callDataPath ^. CallData.callPoint . CallPoint.nodeID
+
+
+        globalPredecesors :: CallDataPath -> (Node.ID, Node, Edge) -> Session (Maybe CallDataPath)
+        globalPredecesors callDataPath (nodeID, node, edge) = case node of
             Node.Inputs -> do print (">>>>>>>>", nodeID, "prev")
-                              previous $ init callDataPath
+                              matchPredecessor (init callDataPath) $ edge ^. Edge.src
             _        -> do print (">>>>>>>>", nodeID)
-                           return [init callDataPath ++ [last callDataPath & CallData.callPoint . CallPoint.nodeID .~ nodeID
-                                                                           & CallData.node .~ node
-                                                        ]
-                                  ]
+                           return $ Just $ init callDataPath ++ [last callDataPath & CallData.callPoint . CallPoint.nodeID .~ nodeID
+                                                                                   & CallData.node .~ node]
+
+        matchPredecessor :: CallDataPath -> Port.OutPort -> Session (Maybe CallDataPath)
+        matchPredecessor []            _                = return Nothing
+        matchPredecessor _             Port.All         = left "Incorrectly connected graph (1)"
+        matchPredecessor callDataPath (Port.Num portNo) = do
+            let matching (_, _, edge) = edge ^. Edge.dst ==  portNo
+            found <- List.find matching (localPredecessors callDataPath) <??> "Incorrectly connected graph (2)"
+            globalPredecesors callDataPath found
 
 
 --next :: CallDataPath -> Session [CallDataPath]
