02ed7b0a4757f75b5265dfceb7715005b80e7aa4
Konrad Madej <kdmadej+git@gmail.com>
2014-07-07 20:48:57 +0200

    merge fix

--- libs/graphics/graphics/test/Generator.hs
+++ libs/graphics/graphics/test/Generator.hs
@@ -28,7 +28,6 @@ import Data.Array.Accelerate (index2, Boundary(..))
 
 --import Utils
 
-<<<<<<< Updated upstream
 gradients = do
     let reds   = [Tick 0.0 1.0 1.0, Tick 0.25 0.0 1.0, Tick 1.0 1.0 1.0]
     let greens = [Tick 0.0 1.0 1.0, Tick 0.50 0.0 1.0, Tick 1.0 1.0 1.0]
@@ -57,40 +56,3 @@ main :: IO ()
 main = do
     putStrLn "Gradient test"
     gradients
-=======
---saveGen :: Generator -> IO ()
---saveGen gen = do
---    let res = lambdaGenerator (Grid 640 480) gen
---    testSaveRGBA "out.bmp" res res res res
-
---scaling f = do
---    (r, g, b, a) <- testLoadRGBA "lena.bmp"
---    let process x = lambdaGenerator (Grid 500 500) $ translate (V2 1.5 1.5) $ bicubic f (Constant 1.0) x
---    testSaveRGBA "out.bmp" (process r) (process g) (process b) (process a)
-
---disortion :: (Exp Double, Exp Double, Exp Double, Exp Double) -> IO ()
---disortion (x1, y1, x2, y2) = do
---    (r, g, b, a) <- testLoadRGBA "lena.bmp"
-
---    let vec = variable $ V2 (Point2 x1 y1) (Point2 x2 y2)
---                -- pozycja, wartość, waga
---    let ticks = [Tick 0.0 1 1.0, Tick 0.5 (-0.5) 1.0, Tick 1.0 2 1.0]
---    let myftrans pw nw prop = prop ** (pw / nw)
-
---    let tv = V2 x1 y2
---    let sampler    = bicubic triangle Wrap
---    let grad       = colorMapper ticks myftrans $ radialShape (Chebyshev)
---    let effect     = disort (Point2 0 0) grad . translate (tv)
---    let rasterizer = lambdaGenerator (Grid 512 512)
---    let process    = rasterizer . effect . sampler
-
---    let gradient ticks = rasterizer grad
-
---    testSaveRGBA "out.bmp" (process r) (process g) (process b) (process a)
---    testSaveRGBA "out_grad.bmp" (gradient ticks) (gradient ticks) (gradient ticks) (gradient ticks)
-
-main :: IO ()
-main = do
-    putStrLn "Testuję skalowanie"
-    --scaling lanczos3
->>>>>>> Stashed changes
