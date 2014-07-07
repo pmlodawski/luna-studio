206592e73b5f71bacc11a1dc3a735d92ae3dfe69
Konrad Gądek <kgadek@gmail.com>
2014-07-07 15:51:44 +0200

    Some basic specs

---
+++ src/Flowbox/Typechecker/Basic.hs
@@ -0,0 +1,6 @@
+module           Flowbox.Typechecker.Basic where
+
+import qualified Flowbox.Luna.Data.AST.Module                          as Module
+
+typecheck :: Module.Module -> Maybe Module.Module
+typecheck = undefined
