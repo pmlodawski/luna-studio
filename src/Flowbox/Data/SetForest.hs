27f9a1670e119fb976095d8c4588d5143dcef71c
Piotr Młodawski <remdezx@gmail.com>
2014-07-09 16:03:32 +0200

    Renamed MapForest to SetForest

--- src/Flowbox/Data/SetForest.hs
+++ src/Flowbox/Data/SetForest.hs
@@ -5,7 +5,7 @@
 -- Flowbox Team <contact@flowbox.io>, 2014
 ---------------------------------------------------------------------------
 
-module Flowbox.Data.MapForest where
+module Flowbox.Data.SetForest where
 
 import Flowbox.Prelude
 
@@ -13,19 +13,19 @@ import           Data.Map (Map)
 import qualified Data.Map as Map
 
 
-data MapTree a = SubForest (MapForest a)
+data SetTree a = SubForest (SetForest a)
                | Leaf
                deriving (Read, Show, Eq, Ord)
 
-type MapForest a = Map a (MapTree a)
+type SetForest a = Map a (SetTree a)
 
 
 
-empty :: MapForest a
+empty :: SetForest a
 empty = Map.empty
 
 
-insert :: Ord a => [a] -> MapForest a -> MapForest a
+insert :: Ord a => [a] -> SetForest a -> SetForest a
 insert []    forest = forest
 insert [a]   forest = if Map.member a forest
                         then forest
@@ -35,7 +35,7 @@ insert (h:t) forest = case Map.lookup h forest of
     _                  -> Map.insert h (SubForest $ insert t Map.empty) forest
 
 
-member :: Ord a => [a] -> MapForest a -> Bool
+member :: Ord a => [a] -> SetForest a -> Bool
 member []    _      = True
 member [a]   forest = Map.member a forest
 member (h:t) forest = case Map.lookup h forest of
@@ -43,7 +43,7 @@ member (h:t) forest = case Map.lookup h forest of
     _                  -> False
 
 
-delete :: Ord a => [a] -> MapForest a -> MapForest a
+delete :: Ord a => [a] -> SetForest a -> SetForest a
 delete []    _      = Map.empty
 delete [a]   forest = Map.delete a forest
 delete (h:t) forest = case Map.lookup h forest of
@@ -51,7 +51,7 @@ delete (h:t) forest = case Map.lookup h forest of
     _                  -> forest
 
 
-draw :: (Show a) => MapForest a -> String
+draw :: (Show a) => SetForest a -> String
 draw = unlines . map (unlines . drawTree) . Map.toList where
 
     shift first other = zipWith (++) (first : repeat other)
