cb688029770ba0bf46f6ab4318c1aa72f7d73546
Piotr Młodawski <remdezx@gmail.com>
2014-07-11 15:50:22 +0200

    batch/plugins/project-manager split to two projects - lib and exec

---
+++ src/Flowbox/ProjectManager/Handler/NodeDefault.hs
@@ -0,0 +1,61 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2014
+---------------------------------------------------------------------------
+module Flowbox.ProjectManager.Handler.NodeDefault where
+
+import qualified Flowbox.Batch.Handler.NodeDefault                                                             as BatchND
+import           Flowbox.Bus.RPC.RPC                                                                           (RPC)
+import           Flowbox.Luna.Tools.Serialize.Proto.Conversion.Crumb                                           ()
+import           Flowbox.Luna.Tools.Serialize.Proto.Conversion.NodeDefault                                     ()
+import           Flowbox.Prelude
+import           Flowbox.ProjectManager.Context                                                                (ContextRef)
+import qualified Flowbox.ProjectManager.Context                                                                as Context
+import           Flowbox.System.Log.Logger
+import           Flowbox.Tools.Serialize.Proto.Conversion.Basic
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Node.Default.Get.Request    as NodeDefaultGet
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Node.Default.Get.Status     as NodeDefaultGet
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Node.Default.Remove.Request as NodeDefaultRemove
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Node.Default.Remove.Update  as NodeDefaultRemove
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Node.Default.Set.Request    as NodeDefaultSet
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Node.Default.Set.Update     as NodeDefaultSet
+
+
+
+logger :: LoggerIO
+logger = getLoggerIO "Flowbox.ProjectManager.Handler.NodeDefault"
+
+
+get :: ContextRef -> NodeDefaultGet.Request -> RPC NodeDefaultGet.Status
+get ctxRef (NodeDefaultGet.Request tnodeID tbc tlibID tprojectID) = do
+    bc <- decodeE tbc
+    let nodeID    = decodeP tnodeID
+        libID     = decodeP tlibID
+        projectID = decodeP tprojectID
+    nodeDefaults <- Context.run ctxRef $ BatchND.nodeDefaults nodeID bc libID projectID
+    return $ NodeDefaultGet.Status (encode nodeDefaults) tnodeID tbc tlibID tprojectID
+
+
+set :: ContextRef -> NodeDefaultSet.Request -> RPC NodeDefaultSet.Update
+set ctxRef (NodeDefaultSet.Request tdstPort tvalue tnodeID tbc tlibID tprojectID) = do
+    bc <- decodeE tbc
+    let dstPort   = decodeListP tdstPort
+        value     = decodeP tvalue
+        nodeID    = decodeP tnodeID
+        libID     = decodeP tlibID
+        projectID = decodeP tprojectID
+    Context.run ctxRef $ BatchND.setNodeDefault dstPort value nodeID bc libID projectID
+    return $ NodeDefaultSet.Update tdstPort tvalue tnodeID tbc tlibID tprojectID
+
+
+remove :: ContextRef -> NodeDefaultRemove.Request -> RPC NodeDefaultRemove.Update
+remove ctxRef (NodeDefaultRemove.Request tdstPort tnodeID tbc tlibID tprojectID) = do
+    bc <- decodeE tbc
+    let dstPort   = decodeListP tdstPort
+        nodeID    = decodeP tnodeID
+        libID     = decodeP tlibID
+        projectID = decodeP tprojectID
+    Context.run ctxRef $ BatchND.removeNodeDefault dstPort nodeID bc libID projectID
+    return $ NodeDefaultRemove.Update tdstPort tnodeID tbc tlibID tprojectID
---
+++ src/Flowbox/ProjectManager/Handler/Properties.hs
@@ -0,0 +1,67 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2014
+---------------------------------------------------------------------------
+module Flowbox.ProjectManager.Handler.Properties where
+
+import qualified Flowbox.Batch.Handler.Properties                                                              as BatchP
+import           Flowbox.Bus.RPC.RPC                                                                           (RPC)
+import           Flowbox.Luna.Tools.Serialize.Proto.Conversion.Attributes                                      ()
+import           Flowbox.Prelude
+import           Flowbox.ProjectManager.Context                                                                (ContextRef)
+import qualified Flowbox.ProjectManager.Context                                                                as Context
+import           Flowbox.System.Log.Logger
+import           Flowbox.Tools.Serialize.Proto.Conversion.Basic
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Node.Properties.Get.Request as GetNodeProperties
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Node.Properties.Get.Status  as GetNodeProperties
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Node.Properties.Set.Request as SetNodeProperties
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Node.Properties.Set.Update  as SetNodeProperties
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Properties.Get.Request                     as GetASTProperties
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Properties.Get.Status                      as GetASTProperties
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Properties.Set.Request                     as SetASTProperties
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Properties.Set.Update                      as SetASTProperties
+
+
+
+logger :: LoggerIO
+logger = getLoggerIO "Flowbox.Batch.Server.Handlers.Properties"
+
+
+getASTProperties :: ContextRef -> GetASTProperties.Request -> RPC GetASTProperties.Status
+getASTProperties ctxRef (GetASTProperties.Request tnodeID tlibID tprojectID) = do
+    let nodeID    = decodeP tnodeID
+        libID     = decodeP tlibID
+        projectID = decodeP tprojectID
+    properties <- Context.run ctxRef $ BatchP.getProperties nodeID libID projectID
+    return $ GetASTProperties.Status (encode properties) tnodeID tlibID tprojectID
+
+
+setASTProperties :: ContextRef -> SetASTProperties.Request -> RPC SetASTProperties.Update
+setASTProperties ctxRef (SetASTProperties.Request tproperties tnodeID tlibID tprojectID) = do
+    properties <- decodeE tproperties
+    let nodeID    = decodeP tnodeID
+        libID     = decodeP tlibID
+        projectID = decodeP tprojectID
+    Context.run ctxRef $ BatchP.setProperties properties nodeID libID projectID
+    return $ SetASTProperties.Update tproperties tnodeID tlibID tprojectID
+
+
+getNodeProperties :: ContextRef -> GetNodeProperties.Request -> RPC GetNodeProperties.Status
+getNodeProperties ctxRef (GetNodeProperties.Request tnodeID tbc tlibID tprojectID) = do
+    let nodeID    = decodeP tnodeID
+        libID     = decodeP tlibID
+        projectID = decodeP tprojectID
+    properties <- Context.run ctxRef $ BatchP.getProperties nodeID libID projectID
+    return $ GetNodeProperties.Status (encode properties) tnodeID tbc tlibID tprojectID
+
+
+setNodeProperties :: ContextRef -> SetNodeProperties.Request -> RPC SetNodeProperties.Update
+setNodeProperties ctxRef (SetNodeProperties.Request tproperties tnodeID tbc tlibID tprojectID) = do
+    properties <- decodeE tproperties
+    let nodeID    = decodeP tnodeID
+        libID     = decodeP tlibID
+        projectID = decodeP tprojectID
+    Context.run ctxRef $ BatchP.setProperties properties nodeID libID projectID
+    return $ SetNodeProperties.Update tproperties tnodeID tbc tlibID tprojectID
