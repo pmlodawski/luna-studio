61c07b44849392766315b18d27c74636b4bab404
Piotr Młodawski <remdezx@gmail.com>
2014-07-18 17:04:14 +0200

    Interpreter Bus API via Pipes

--- src/Flowbox/Interpreter/RPC/Handler/Handler.hs
+++ src/Flowbox/Interpreter/RPC/Handler/Handler.hs
@@ -9,32 +9,24 @@
 
 module Flowbox.Interpreter.RPC.Handler.Handler where
 
-import qualified Data.Map as Map
-import           Pipes
+import Control.Monad (forever)
+import Pipes
 
-
-import           Flowbox.Bus.BusT                            (BusT (BusT))
+import           Flowbox.Bus.BusT                            (BusT)
 import           Flowbox.Bus.Data.Message                    (Message)
 import qualified Flowbox.Bus.Data.Message                    as Message
-import           Flowbox.Bus.Data.Topic                      (Topic)
 import qualified Flowbox.Bus.Data.Topic                      as Topic
 import           Flowbox.Bus.RPC.HandlerMap                  (HandlerMap)
 import qualified Flowbox.Bus.RPC.HandlerMap                  as HandlerMap
-import qualified Flowbox.Bus.RPC.Pipes                       as RPC
-import           Flowbox.Bus.RPC.RPC                         (RPC)
 import qualified Flowbox.Bus.RPC.Server.Processor            as Processor
 import           Flowbox.Control.Error
-import           Flowbox.Interpreter.Context                 (Context)
-import qualified Flowbox.Interpreter.Context                 as Context
-import qualified Flowbox.Interpreter.RPC.Handler.Interpreter as InterpreterHandler
+import qualified Flowbox.Interpreter.RPC.Handler.Interpreter as Interpreter
 import qualified Flowbox.Interpreter.RPC.Topic               as Topic
-import qualified Flowbox.Interpreter.Session.Env             as Env
 import qualified Flowbox.Interpreter.Session.Session         as Session
 import           Flowbox.Interpreter.Session.SessionT        (SessionT)
 import qualified Flowbox.Interpreter.Session.SessionT        as SessionT
 import           Flowbox.Prelude                             hiding (Context, error)
 import           Flowbox.System.Log.Logger
-import qualified Flowbox.Text.ProtocolBuffers                as Proto
 
 
 
@@ -42,91 +34,32 @@ logger :: LoggerIO
 logger = getLoggerIO "Flowbox.Interpreter.RPC.Handler.Handler"
 
 
---handlerMap :: Context -> HandlerMap
---handlerMap ctx callback = HandlerMap.fromList
---    [ (Topic.interpreterWatchPointAdd    , call Topic.update $ fmap Context.watchPointAddUpdate . handleCommand ctx . Context.WatchPointAddRequest)
---    --, (Topic.interpreterWatchPointRemove , call Topic.update $ undefined)
---    --, (Topic.interpreterWatchPointList   , call Topic.status $ undefined)
---    --, (Topic.interpreterInvalidateCall   , call Topic.update $ undefined)
---    --, (Topic.interpreterInvalidateDef    , call Topic.update $ undefined)
---    --, (Topic.interpreterInvalidateNode   , call Topic.update $ undefined)
---    --, (Topic.interpreterRun              , call Topic.update $ undefined)
---    ]
---    where
---        call :: (Proto.Serializable args, Proto.Serializable result)
---             => String -> (args -> RPC result) -> IO [Message]
---        call type_ = callback type_ . Processor.singleResult
+handlerMap :: HandlerMap SessionT
+handlerMap callback = HandlerMap.fromList
+    [ (Topic.interpreterInvalidateCall   , call1 Topic.update $ Interpreter.invalidateCall)
+    , (Topic.interpreterInvalidateDef    , call1 Topic.update $ Interpreter.invalidateDef)
+    , (Topic.interpreterInvalidateNode   , call1 Topic.update $ Interpreter.invalidateNode)
+    , (Topic.interpreterRun              , call1 Topic.update $ Interpreter.run)
+    , (Topic.interpreterWatchPointAdd    , call1 Topic.update $ Interpreter.watchPointAdd)
+    , (Topic.interpreterWatchPointRemove , call1 Topic.update $ Interpreter.watchPointRemove)
+    , (Topic.interpreterWatchPointList   , call1 Topic.status $ Interpreter.watchPointList)
+    ]
+    where
+        call1 type_ = callback type_ . Processor.singleResult
 
 
---handleCommand :: Context -> Context.Request -> RPC Context.Result
---handleCommand ctx request = do
---    undefined
+handle :: Pipe (Message, Message.CorrelationID)
+               (Message, Message.CorrelationID)
+               BusT ()
+handle = hoist
+    (\a -> liftIO $ eitherToM =<< (Session.run def $ SessionT.runSessionT a))
+    interpreterHandle
 
 
-
-busHandle :: Pipe (Message, Message.CorrelationID) (Message, Message.CorrelationID) BusT ()
-busHandle = do
-    let env = Env.mk undefined undefined
-    hoist (\a -> liftIO $ eitherToM =<< (Session.run env $ SessionT.runSessionT a)) interpreterHandle
-
-
-interpreterHandle :: Pipe (Message, Message.CorrelationID) (Message, Message.CorrelationID) SessionT ()
-interpreterHandle = do
+interpreterHandle :: Pipe (Message, Message.CorrelationID)
+                          (Message, Message.CorrelationID)
+                          SessionT ()
+interpreterHandle = forever $ do
     (message, crl) <- await
-    --let topic = message ^. Message.topic
-    --result <- safeLiftIO $ do
-    --        undefined
-    --response <- case result of
-    --    Left err               -> do logger error err
-    --                                 return $ Just $ Message.mkError topic err
-    --    Right (type_, content) -> map Message.mkResponse topic type_ content
-    --mapM_ (\r -> yield (r, crl)) response
-    undefined
-
---parseRequest :: Pipe (Message, Message.CorrelationID) (Context.Request, Message.CorrelationID) BusT ()
---parseRequest = do
---    (message, crl) <- await
---    let mkCommand wrapper = case Proto.messageGet' $ message ^. Message.message of
---            Left err   -> Context.Invalid $ "Cannot parse message : " ++ err
---            Right args -> wrapper args
---        commands = Map.fromList
---                    [ (Topic.interpreterWatchPointAdd   , mkCommand Context.WatchPointAddRequest)
---                    , (Topic.interpreterWatchPointRemove, mkCommand Context.WatchPointRemoveRequest)
---                    , (Topic.interpreterWatchPointList  , mkCommand Context.WatchPointListRequest)
---                    , (Topic.interpreterInvalidateCall  , mkCommand Context.InvalidateCallRequest)
---                    , (Topic.interpreterInvalidateDef   , mkCommand Context.InvalidateDefRequest)
---                    , (Topic.interpreterInvalidateNode  , mkCommand Context.InvalidateNodeRequest)
---                    , (Topic.interpreterRun             , mkCommand Context.RunRequest)
---                    ]
---        topic   = message ^. Message.topic
---        request = case Map.lookup topic commands of
---                    Just request -> request
---                    Nothing      -> Context.Invalid $ "Unknown topic: " ++ show topic
---    yield (request, crl)
-
-
---prepareResponse :: Pipe (Context.Response, Message.CorrelationID) (Message, Message.CorrelationID) BusT ()
---prepareResponse = do
---    (response, crl) <- await
-
---    let message = Message $ uncurry $ case response of
---            Context.WatchPointAddUpdate -> Topic.interpreterWatchPointAdd
---            Context.WatchPointRemoveUpdate
---            Context.WatchPointListStatus
---            Context.InvalidateCallUpdate
---            Context.InvalidateDefUpdate
---            Context.InvalidateNodeUpdate
---            Context.RunUpdate
---    yield (message, crl)
---    undefined
-
-
-
-
---handler :: Pipe (Message, Message.CorrelationID) (Message, Message.CorrelationID) BusT ()
---handler = do
---    (request, crl) <- await
---    case request ^. Message.topic of
-
---    yield (response, crl)
-
+    results <- lift $ Processor.processLifted handlerMap message
+    mapM_ (\r -> yield (r, crl)) results
--- src/Flowbox/Interpreter/RPC/Handler/Interpreter.hs
+++ src/Flowbox/Interpreter/RPC/Handler/Interpreter.hs
@@ -7,3 +7,85 @@
 
 module Flowbox.Interpreter.RPC.Handler.Interpreter where
 
+import Control.Monad.Trans.Class (lift)
+
+import           Flowbox.Bus.RPC.RPC                                               (RPC)
+import qualified Flowbox.Data.SetForest                                            as SetForest
+import           Flowbox.Interpreter.Proto.CallPoint                               ()
+import           Flowbox.Interpreter.Proto.CallPointPath                           ()
+import qualified Flowbox.Interpreter.Session.AST.Executor                          as Executor
+import qualified Flowbox.Interpreter.Session.AST.WatchPoint                        as WatchPoint
+import qualified Flowbox.Interpreter.Session.Cache.Invalidate                      as Invalidate
+import qualified Flowbox.Interpreter.Session.Data.CallPoint                        as CallPoint
+import           Flowbox.Interpreter.Session.SessionT                              (SessionT (SessionT))
+import           Flowbox.Prelude
+import           Flowbox.System.Log.Logger                                         hiding (error)
+import           Flowbox.Tools.Serialize.Proto.Conversion.Basic
+import qualified Generated.Proto.Interpreter.Interpreter.Invalidate.Call.Request   as InvalidateCall
+import qualified Generated.Proto.Interpreter.Interpreter.Invalidate.Call.Update    as InvalidateCall
+import qualified Generated.Proto.Interpreter.Interpreter.Invalidate.Def.Request    as InvalidateDef
+import qualified Generated.Proto.Interpreter.Interpreter.Invalidate.Def.Update     as InvalidateDef
+import qualified Generated.Proto.Interpreter.Interpreter.Invalidate.Node.Request   as InvalidateNode
+import qualified Generated.Proto.Interpreter.Interpreter.Invalidate.Node.Update    as InvalidateNode
+import qualified Generated.Proto.Interpreter.Interpreter.Run.Request               as Run
+import qualified Generated.Proto.Interpreter.Interpreter.Run.Update                as Run
+import qualified Generated.Proto.Interpreter.Interpreter.WatchPoint.Add.Request    as WatchPointAdd
+import qualified Generated.Proto.Interpreter.Interpreter.WatchPoint.Add.Update     as WatchPointAdd
+import qualified Generated.Proto.Interpreter.Interpreter.WatchPoint.List.Request   as WatchPointList
+import qualified Generated.Proto.Interpreter.Interpreter.WatchPoint.List.Status    as WatchPointList
+import qualified Generated.Proto.Interpreter.Interpreter.WatchPoint.Remove.Request as WatchPointRemove
+import qualified Generated.Proto.Interpreter.Interpreter.WatchPoint.Remove.Update  as WatchPointRemove
+
+
+
+logger :: LoggerIO
+logger = getLoggerIO "Flowbox.Interpreter.RPC.Handler.Interpreter"
+
+
+invalidateCall :: InvalidateCall.Request -> RPC SessionT InvalidateCall.Update
+invalidateCall request@(InvalidateCall.Request tcallPointPath) = do
+    callPointPath <- decodeE tcallPointPath
+    lift $ SessionT $ Invalidate.invalidate callPointPath
+    return $ InvalidateCall.Update request
+
+
+invalidateDef :: InvalidateDef.Request -> RPC SessionT InvalidateDef.Update
+invalidateDef request@(InvalidateDef.Request tlibraryID tdefID) = do
+    let libraryID = decodeP tlibraryID
+        defID     = decodeP tdefID
+    lift $ SessionT $ Invalidate.invalidateDef libraryID defID
+    return $ InvalidateDef.Update request
+
+
+invalidateNode :: InvalidateNode.Request -> RPC SessionT InvalidateNode.Update
+invalidateNode request@(InvalidateNode.Request tcallPoint) = do
+    callPoint <- decodeE tcallPoint
+    lift $ SessionT $ Invalidate.invalidateNode (callPoint ^. CallPoint.libraryID) (callPoint ^. CallPoint.nodeID)
+    return $ InvalidateNode.Update request
+
+
+run :: Run.Request -> RPC SessionT Run.Update
+run request = do
+    lift $ SessionT Executor.processMain
+    return $ Run.Update request
+
+
+watchPointAdd :: WatchPointAdd.Request -> RPC SessionT WatchPointAdd.Update
+watchPointAdd request@(WatchPointAdd.Request tcallPointPath) = do
+    callPointPath <- decodeE tcallPointPath
+    lift $ SessionT $ WatchPoint.add callPointPath
+    return $ WatchPointAdd.Update request
+
+
+watchPointRemove :: WatchPointRemove.Request -> RPC SessionT WatchPointRemove.Update
+watchPointRemove request@(WatchPointRemove.Request tcallPointPath) = do
+    callPointPath <- decodeE tcallPointPath
+    lift $ SessionT $ WatchPoint.delete callPointPath
+    return $ WatchPointRemove.Update request
+
+
+watchPointList :: WatchPointList.Request -> RPC SessionT WatchPointList.Status
+watchPointList request = do
+    list <- lift $ SessionT $ SetForest.toList <$> WatchPoint.all
+    return $ WatchPointList.Status request $ encodeList list
+
