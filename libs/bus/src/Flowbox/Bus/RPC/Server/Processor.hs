61bbcca87691d27558474f4c8455faf8f4e2a724
Piotr Młodawski <remdezx@gmail.com>
2014-07-22 12:13:43 +0200

    Progress on interpreter api

--- libs/bus/src/Flowbox/Bus/RPC/Server/Processor.hs
+++ libs/bus/src/Flowbox/Bus/RPC/Server/Processor.hs
@@ -14,6 +14,7 @@ import Control.Monad.IO.Class (MonadIO)
 
 import           Flowbox.Bus.Data.Message     (Message)
 import qualified Flowbox.Bus.Data.Message     as Message
+import           Flowbox.Bus.Data.Topic       (Topic)
 import           Flowbox.Bus.RPC.HandlerMap   (HandlerMap)
 import qualified Flowbox.Bus.RPC.HandlerMap   as HandlerMap
 import qualified Flowbox.Bus.RPC.RPC          as RPC
@@ -34,37 +35,37 @@ singleResult f a = liftM mkList $ f a
 noResult :: MonadIO m => (a -> m ()) -> a -> m [Response]
 noResult f a = f a >> return []
 
--- FIXME [PM] : Code duplication
 
+-- FIXME [PM] : Code duplication
 
 process :: HandlerMap IO -> Message -> IO [Message]
 process handlerMap msg = HandlerMap.lookupAndCall handlerMap call topic where
-    call type_ method = case Proto.messageGet' $ msg ^. Message.message of
+    call mkTopic method = case Proto.messageGet' $ msg ^. Message.message of
         Left err   -> do logger error err
                          return $ Message.mkError topic err
         Right args -> do results <- RPC.run $ method args
                          return $ case results of
                             Left err -> Message.mkError topic err
-                            Right ok -> map (respond type_) ok
+                            Right ok -> map (respond mkTopic) ok
 
     topic = msg ^. Message.topic
 
-    respond :: Proto.Serializable msg => String -> msg -> Message
-    respond = Message.mkResponse topic
+    respond :: Proto.Serializable msg => (Topic -> Topic) -> msg -> Message
+    respond mkTopic = Message.mk (mkTopic topic)
 
 
 processLifted :: MonadIO m => HandlerMap m -> Message -> m [Message]
 processLifted handlerMap msg = HandlerMap.lookupAndCall handlerMap call topic where
-    call type_ method = case Proto.messageGet' $ msg ^. Message.message of
+    call mkTopic method = case Proto.messageGet' $ msg ^. Message.message of
         Left err   -> do logger error err
                          return $ Message.mkError topic err
         Right args -> do results <- RPC.runLifted $ method args
                          return $ case results of
                             Left err -> Message.mkError topic err
-                            Right ok -> map (respond type_) ok
+                            Right ok -> map (respond mkTopic) ok
 
     topic = msg ^. Message.topic
 
-    respond :: Proto.Serializable msg => String -> msg -> Message
-    respond = Message.mkResponse topic
+    respond :: Proto.Serializable msg => (Topic -> Topic) -> msg -> Message
+    respond mkTopic = Message.mk (mkTopic topic)
 
