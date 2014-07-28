81b069a6357d825e1c5175626a14688b1543758c
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-07-28 14:42:12 +0000

    WIP HSVTool, not working

--- libs/graphics/graphics/src/Flowbox/Graphics/Image/Color.hs
+++ libs/graphics/graphics/src/Flowbox/Graphics/Image/Color.hs
@@ -6,6 +6,7 @@
 ---------------------------------------------------------------------------
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Flowbox.Graphics.Image.Color (
     module Flowbox.Graphics.Image.Color,
@@ -77,33 +78,101 @@ saturate saturation pix = toHSL pix & s %~ (*saturation) & convertColor
 
 hsvTool :: forall a t. (Elt t, IsFloating t, ColorConvert a HSV, ColorConvert HSV a,
                         A.Lift Exp (a (Exp t)), A.Unlift Exp (a (Exp t)), Elt (A.Plain (a (Exp t))))
-        => U.Range (Exp t) -> Exp t
-        -> U.Range (Exp t) -> Exp t
-        -> U.Range (Exp t) -> Exp t
+        => U.Range (Exp t) -> Exp t -> Exp t
+        -> U.Range (Exp t) -> Exp t -> Exp t
+        -> U.Range (Exp t) -> Exp t -> Exp t
         -> a (Exp t)
         -> a (Exp t)
-hsvTool hueRange hueRotation saturationRange saturationAdjustment brightnessRange brightnessAdjustment pix =
+hsvTool hueRange hueRotation hueRolloff
+        saturationRange saturationAdjustment saturationRolloff
+        brightnessRange brightnessAdjustment brightnessRolloff pix =
     A.unlift (conditionsFulfilled A.? (
-        A.lift (hsv & h %~ rotation hueRotation
-            & s %~ (+ saturationAdjustment)
-            & v %~ (+ brightnessAdjustment)
+        --A.lift (hsv & h %~ (\hue -> rotation (hueRotation * rolloff hueRange hueRolloff hue) hue) -- hue
+        --    & s %~ (\saturation -> saturation + saturationAdjustment * rolloff saturationRange saturationRolloff saturation)
+        --    & v %~ (\value -> value + brightnessAdjustment * rolloff brightnessRange brightnessRolloff value)
+        --    & convertColor :: a (Exp t))
+        A.lift (hsv & h %~ (\hue -> power hueRange hueRolloff hue) -- rotation (hueRotation * power hueRange hueRolloff hue) hue) -- hue
+            -- & s %~ (\saturation -> saturation + saturationAdjustment * power saturationRange saturationRolloff saturation)
+            -- & v %~ (\value -> value + brightnessAdjustment * power brightnessRange brightnessRolloff value)
             & convertColor :: a (Exp t))
         ,
         A.lift pix))
     where hsv = toHSV pix
-          rotation r hue = A.cond (hue' A.<* 0) (hue' + 1)
-                         $ A.cond (hue' A.>* 1) (hue' - 1)
-                         $ hue'
-              where hue' = hue + r / 360
 
-          conditionsFulfilled = (hsv ^. h) `inRange` hueRange
-                                A.&&*
-                                (hsv ^. s) `inRange` saturationRange
-                                A.&&*
-                                (hsv ^. v) `inRange` brightnessRange
+          --conditionsFulfilled = rolloff hueRange        hueRolloff        (hsv ^. h) A.>* 0
+          --                      A.&&*
+          --                      rolloff saturationRange saturationRolloff (hsv ^. s) A.>* 0
+          --                      A.&&*
+          --                      rolloff brightnessRange brightnessRolloff (hsv ^. v) A.>* 0
+          --conditionsFulfilled = power hueRange        hueRolloff        (hsv ^. h) A.>* 0
+          --                      A.&&*
+          --                      power saturationRange saturationRolloff (hsv ^. s) A.>* 0
+          --                      A.&&*
+          --                      power brightnessRange brightnessRolloff (hsv ^. v) A.>* 0
+          conditionsFulfilled = 1 A.>* (0::Exp Int)
+
+rotation r hue = A.cond (hue' A.<* 0) (hue' + 1)
+               $ A.cond (hue' A.>* 1) (hue' - 1)
+               $ hue'
+    where hue' = hue + r
 
 inRange :: (IsScalar t, Elt t) => Exp t -> U.Range (Exp t) -> Exp Bool
 inRange val (U.Range low high) = val A.>=* low A.&&* val A.<=* high
+
+rolloff :: (Elt a, IsFloating a) => U.Range (Exp a) -> Exp a -> Exp a -> Exp a
+rolloff range@(U.Range lo hi) roll val' = A.cond (val `inRange` range)   1
+                                        $ A.cond (roll A.==* 0)          0
+                                        $ A.cond (val A.<=* (lo - roll)) 0
+                                        $ A.cond (val A.>=* (hi + roll)) 1
+                                        $ A.cond (val A.<*  lo)          ((1 / roll) * val + 1 - (lo / roll))
+                                        $ (((-1) / roll) * val + 1 + (hi / roll)) -- covers val > hi
+    where val = val' + hi
+
+
+power :: forall a. (Elt a, IsFloating a) => U.Range (Exp a) -> Exp a -> Exp a -> Exp a
+power (U.Range a b) r x =
+    let (correct, pp) = intersection a b r :: (Exp Bool, Exp a)
+        (rLeft, rRight) = A.unlift (correct A.? (A.lift ((pp-1,a), (b,pp))
+                                     , A.lift ((a-r, a), (b, b+r)))) :: (A.Exp (a, a), A.Exp (a, a))
+        rLeftEquation val = A.cond (frL A./=* 0) (fxL val / frL) 1
+        rRightEquation val = A.cond (frR A./=* 0) (1 - fxR val / frR) 1
+
+        fxL val = U.frac $ val - tL
+        frL = U.frac $ a - tL
+
+        fxR val = U.frac $ val - tR
+        frR = U.frac $ r -- == b+r - tR == b+r - b == r
+
+        tL = U.frac $ a - r
+        tR = U.frac $ b
+    in check x (a,b) A.? (1,
+       check x (A.unlift rLeft) A.? (rLeftEquation x,
+       check x (A.unlift rRight) A.? (rRightEquation x, 0.6666)))
+
+intersection :: (A.Elt a, A.IsFloating a) => Exp a -> Exp a -> Exp a -> (Exp Bool, Exp a)
+intersection a b r = (y A.>* 0 A.&&* y A.<* 1, x)
+    where x = (b2 - b1) / (a1 - a2)
+          y = a1*((b2-b1)/(a1-a2))+b1
+          (a1, b1) = rise a r
+          (a2, b2) = fall b r
+
+rise a r = (1/r, 1-(a+1)/r)
+
+fall b r = ((-1)/r, 1+(b/r))
+
+check :: (A.Elt a, A.IsFloating a) => Exp a -> (Exp a, Exp a) -> Exp Bool
+check (U.frac -> x) (a,b) =
+    let a' = U.frac a
+        b' = U.frac b
+    in (b-a A.>=* 1)
+       A.||*
+       (x A.>=* a' A.&&* x A.<=* b')
+       A.||*
+       (b' A.<* a' A.&&* ((x A.>=* a' A.&&* x A.>=* b')
+           A.||*
+           (x A.<=* a' A.&&* x A.<=* b')))
+
+
 
 conditionally :: (Elt a, IsScalar a) => U.Range (Exp a) -> (Exp a -> Exp a) -> Exp a -> Exp a
 conditionally (U.Range low high) f val = val A.>=* low A.&&* val A.<=* high A.? (f val, val)
--- libs/graphics/graphics/test/Color.hs
+++ libs/graphics/graphics/test/Color.hs
@@ -10,12 +10,13 @@
 
 module Main where
 
-import Flowbox.Graphics.Color
+import Flowbox.Graphics.Color hiding (b)
 import Flowbox.Graphics.Composition.Generators.Filter
 import Flowbox.Graphics.Composition.Generators.Rasterizer
 import Flowbox.Graphics.Composition.Generators.Sampler
 import Flowbox.Graphics.Composition.Generators.Structures
-import Flowbox.Graphics.Image.Color
+import Flowbox.Graphics.Image.Color hiding (b)
+import Flowbox.Graphics.Utils
 import Flowbox.Math.Matrix as M
 import Flowbox.Prelude hiding (transform)
 
@@ -30,14 +31,116 @@ output = "out.png"
 
 ex black ev = testFunction (exposure black ev) input output
 
-bp black = testFunction (blackpointConvert black) input output
+--bp black = testFunction (blackpointConvert black) input output
 
-invertible black lift = testFunction (inverseBlackpointConvert lift . blackpointConvert black) input output
-invertibleWhite white gain = testFunction (inverseWhitepointConvert gain . whitepointConvert white) input output
+--invertible black lift = testFunction (inverseBlackpointConvert lift . blackpointConvert black) input output
+--invertibleWhite white gain = testFunction (inverseWhitepointConvert gain . whitepointConvert white) input output
 
-saturation v = testColor (saturate v) input output
+--saturation v = testColor (saturate v) input output
 
-correct s c g g' o = testColor (colorCorrect s c g g' o) input output
+--correct s c g g' o = testColor (colorCorrect s c g g' o) input output
+
+--idHSL = testColor (toRGB . toHSL)
+
+hsvT = testColor (hsvTool (Range (358/360) (359/360)) (180 / 360) (90/360) (Range 0 1) 0 0 (Range 0 1) (0) 0) input output
+
+--inRange :: (IsScalar t, Elt t) => Exp t -> U.Range (Exp t) -> Exp Bool
+inRange val (Range low high) = val >= low && val <= high
+
+--rolloff :: (Elt a, IsFloating a) => U.Range (Exp a) -> Exp a -> Exp a -> Exp a
+rolloff :: Range Float -> Float -> Float -> Float
+rolloff range@(Range lo hi) roll val' = if' (val `Main.inRange` range)   1
+                                        $ if' (roll == 0)          0
+                                        $ if' (val <= (lo - roll)) 0
+                                        $ if' (val >= (hi + roll)) 1
+                                        $ if' (val <  lo)          ((1 / roll) * val + 1 - (lo / roll))
+                                        $ (((-1) / roll) * val + 1 + (hi / roll)) -- covers val > hi
+    where val = if val' > hi && hi + roll > 1 then (-1) * (val' - 1) else val'
+
+--power x (a, b) r =
+--    let (correct, pp) = intersection a b r :: (Exp Bool, Exp Float)
+--        (rLeft, rRight) = A.unlift (correct A.? (A.lift ((pp-1,a), (b,pp))
+--                                     , A.lift ((a-r, a), (b, b+r)))) :: (A.Exp (Float, Float), A.Exp (Float, Float))
+--        rLeftEquation val = A.cond (frL A./=* 0) (fxL val / frL) 1
+--        rRightEquation val = A.cond (frR A./=* 0) (1 - fxR val / frR) 1
+
+--        fxL val = frac $ val - tL
+--        frL = frac $ a - tL
+
+--        fxR val = frac $ val - tR
+--        frR = frac $ r -- == b+r - tR == b+r - b == r
+
+--        tL = frac $ a - r
+--        tR = frac $ b
+--    in check x (a,b) A.? (1,
+--       check x (A.unlift rLeft) A.? (rLeftEquation x,
+--       check x (A.unlift rRight) A.? (rRightEquation x, 0)))
+
+--power' x (a, b) r =
+--    let (correct, pp) = intersection' a b r
+--        (rLeft, rRight) = if correct then ((pp-1,a), (b,pp))
+--                                     else ((a-r, a), (b, b+r))
+--        --rLeftEquation val  = ((1 / r) * val + 1 - (a / r))
+--        --rRightEquation val = (((-1) / r) * val + 1 + (b / r))
+--        rLeftEquation val = if frL /= 0 then fxL val / frL else 1
+--        rRightEquation val = if frR /= 0 then 1 - fxR val / frR else 1
+
+--        fxL val = frac' $ val - tL
+--        frL = frac' $ a - tL
+
+--        fxR val = frac' $ val - tR
+--        frR = frac' $ r -- == b+r - tR == b+r - b == r
+
+--        tL = frac' $ a - r
+--        tR = frac' $ b
+--    in  if check' x (a,b)
+--            then 1
+--            else if check' x rLeft
+--                     then rLeftEquation x
+--                     else if check' x rRight
+--                              then rRightEquation x
+--                              else 0
+
+
+----intersection :: ((Float, Float), (Float, Float)) -> ((Float, Float), (Float, Float)) -> Maybe (Float, Float)
+--intersection :: (A.Elt a, A.IsFloating a) => Exp a -> Exp a -> Exp a -> (Exp Bool, Exp a)
+--intersection a b r = (y A.>* 0 A.&&* y A.<* 1, x)
+--    where x = (b2 - b1) / (a1 - a2)
+--          y = a1*((b2-b1)/(a1-a2))+b1
+--          (a1, b1) = rise a r
+--          (a2, b2) = fall b r
+
+--intersection' a b r = (y > 0 && y < 1, x)
+--    where x = (b2 - b1) / (a1 - a2)
+--          y = a1*((b2-b1)/(a1-a2))+b1
+--          (a1, b1) = rise a r
+--          (a2, b2) = fall b r
+
+--rise a r = (1/r, 1-(a+1)/r)
+
+--fall b r = ((-1)/r, 1+(b/r))
+
+--check :: (A.Elt a, A.IsFloating a) => Exp a -> (Exp a, Exp a) -> Exp Bool
+--check (frac -> x) (a,b) =
+--    let a' = frac a
+--        b' = frac b
+--    in (b-a A.>=* 1)
+--       A.||*
+--       (x A.>=* a' A.&&* x A.<=* b')
+--       A.||*
+--       (b' A.<* a' A.&&* ((x A.>=* a' A.&&* x A.>=* b')
+--           A.||*
+--           (x A.<=* a' A.&&* x A.<=* b')))
+
+frac' x = x - fromIntegral (floor x)
+check' (frac' -> x) (a, b) = 
+    let a' = frac' a
+        b' = frac' b
+    in  (b-a >= 1) || (x >= a' && x <= b') || (b' < a' && ((x >= a' && x >= b') || (x <= a' && x <= b')))
+
+
+if' True  a _ = a
+if' False _ b = b
 
 main = do
     --putStrLn "Testing invertibility"
