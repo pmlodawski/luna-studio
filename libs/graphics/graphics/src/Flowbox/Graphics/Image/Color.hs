a2a3ea99c956407f6cf6f732b81bd7623ed58cdf
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-07-31 08:18:26 +0000

    Some fixes and docs in Color

--- libs/graphics/graphics/src/Flowbox/Graphics/Image/Color.hs
+++ libs/graphics/graphics/src/Flowbox/Graphics/Image/Color.hs
@@ -24,6 +24,7 @@ module Flowbox.Graphics.Image.Color (
 import qualified Data.Array.Accelerate       as A
 import           Data.Array.Accelerate.Tuple (IsTuple, TupleRepr, fromTuple, toTuple)
 import           Data.Array.Accelerate.Type  (IsScalar)
+import qualified Linear
 
 import           Flowbox.Graphics.Color
 import qualified Flowbox.Graphics.Utils as U
@@ -94,49 +95,33 @@ hsvTool hueRange hueRotation hueRolloff
         --    & s %~ (\saturation -> saturation + saturationAdjustment * rolloff saturationRange saturationRolloff saturation)
         --    & v %~ (\value -> value + brightnessAdjustment * rolloff brightnessRange brightnessRolloff value)
         --    & convertColor :: a (Exp t))
-        A.lift (hsv & h %~ (\hue -> power hueRange hueRolloff hue) -- rotation (hueRotation * power hueRange hueRolloff hue) hue) -- hue
+        A.lift (hsv & h %~ (\hue -> rotation (hueRotation * power hueRange hueRolloff hue) hue) -- hue
             -- & s %~ (\saturation -> saturation + saturationAdjustment * power saturationRange saturationRolloff saturation)
             -- & v %~ (\value -> value + brightnessAdjustment * power brightnessRange brightnessRolloff value)
             & convertColor :: a (Exp t))
         ,
         A.lift pix))
     where hsv = toHSV pix
+          rotation r hue = U.frac $ hue + r
 
           --conditionsFulfilled = rolloff hueRange        hueRolloff        (hsv ^. h) A.>* 0
           --                      A.&&*
           --                      rolloff saturationRange saturationRolloff (hsv ^. s) A.>* 0
           --                      A.&&*
           --                      rolloff brightnessRange brightnessRolloff (hsv ^. v) A.>* 0
-          --conditionsFulfilled = power hueRange        hueRolloff        (hsv ^. h) A.>* 0
+          conditionsFulfilled = power hueRange        hueRolloff        (hsv ^. h) A.>* 0
           --                      A.&&*
           --                      power saturationRange saturationRolloff (hsv ^. s) A.>* 0
           --                      A.&&*
           --                      power brightnessRange brightnessRolloff (hsv ^. v) A.>* 0
-          conditionsFulfilled = 1 A.>* (0::Exp Int)
-
-rotation r hue = A.cond (hue' A.<* 0) (hue' + 1)
-               $ A.cond (hue' A.>* 1) (hue' - 1)
-               $ hue'
-    where hue' = hue + r
-
-inRange :: (IsScalar t, Elt t) => Exp t -> U.Range (Exp t) -> Exp Bool
-inRange val (U.Range low high) = val A.>=* low A.&&* val A.<=* high
-
-rolloff :: (Elt a, IsFloating a) => U.Range (Exp a) -> Exp a -> Exp a -> Exp a
-rolloff range@(U.Range lo hi) roll val' = A.cond (val `inRange` range)   1
-                                        $ A.cond (roll A.==* 0)          0
-                                        $ A.cond (val A.<=* (lo - roll)) 0
-                                        $ A.cond (val A.>=* (hi + roll)) 1
-                                        $ A.cond (val A.<*  lo)          ((1 / roll) * val + 1 - (lo / roll))
-                                        $ (((-1) / roll) * val + 1 + (hi / roll)) -- covers val > hi
-    where val = val' + hi
+          --conditionsFulfilled = 1 A.>* (0::Exp Int)
 
 
 power :: forall a. (Elt a, IsFloating a) => U.Range (Exp a) -> Exp a -> Exp a -> Exp a
-power (U.Range a b) r x =
-    let (correct, pp) = intersection a b r :: (Exp Bool, Exp a)
+power (U.Range a b) rolloff x =
+    let (correct, pp) = intersection a b rolloff :: (Exp Bool, Exp a)
         (rLeft, rRight) = A.unlift (correct A.? (A.lift ((pp-1,a), (b,pp))
-                                     , A.lift ((a-r, a), (b, b+r)))) :: (A.Exp (a, a), A.Exp (a, a))
+                                     , A.lift ((a-rolloff, a), (b, b+rolloff)))) :: (A.Exp (a, a), A.Exp (a, a))
         rLeftEquation val = A.cond (frL A./=* 0) (fxL val / frL) 1
         rRightEquation val = A.cond (frR A./=* 0) (1 - fxR val / frR) 1
 
@@ -144,13 +129,13 @@ power (U.Range a b) r x =
         frL = U.frac $ a - tL
 
         fxR val = U.frac $ val - tR
-        frR = U.frac $ r -- == b+r - tR == b+r - b == r
+        frR = U.frac $ rolloff -- == b+r - tR == b+r - b == r
 
-        tL = U.frac $ a - r
+        tL = U.frac $ a - rolloff
         tR = U.frac $ b
     in check x (a,b) A.? (1,
        check x (A.unlift rLeft) A.? (rLeftEquation x,
-       check x (A.unlift rRight) A.? (rRightEquation x, 0.6666)))
+       check x (A.unlift rRight) A.? (rRightEquation x, 0)))
 
 intersection :: (A.Elt a, A.IsFloating a) => Exp a -> Exp a -> Exp a -> (Exp Bool, Exp a)
 intersection a b r = (y A.>* 0 A.&&* y A.<* 1, x)
@@ -180,6 +165,7 @@ check (U.frac -> x) (a,b) =
 conditionally :: (Elt a, IsScalar a) => U.Range (Exp a) -> (Exp a -> Exp a) -> Exp a -> Exp a
 conditionally (U.Range low high) f val = val A.>=* low A.&&* val A.<=* high A.? (f val, val)
 
+-- | Lowers number of colours in a picture to 2^colors
 posterize :: (Elt a, IsFloating a) => Exp a -> Exp a -> Exp a
 posterize colors val = A.cond (colors A.==* 0) 0
                      $ A.cond (colors A.==* 1) 1
@@ -194,6 +180,8 @@ type Mat3x3 a = (Vec3 a, Vec3 a, Vec3 a)
 type Vec4 a   = (Exp  a, Exp  a, Exp  a, Exp  a)
 type Mat4x4 a = (Vec4 a, Vec4 a, Vec4 a, Vec4 a)
 
+-- | Dispatches colorspace to required color matrix
+
 type family ColorMatrix (colorspace :: * -> *) t :: *
 
 type instance ColorMatrix CMY    t = Mat3x3 t
@@ -205,6 +193,14 @@ type instance ColorMatrix RGBA   t = Mat4x4 t
 type instance ColorMatrix YUV    t = Mat3x3 t
 type instance ColorMatrix YUV_HD t = Mat3x3 t
 
+-- | Multiplies given matrix by colour.
+--   Matrix needs to be in a row major order and n x n size, where n is a number
+--   of components in a colour.
+--
+--   Example:
+--   ((1, 0, 0),     (0.5,     (0.5,
+--    (0, 1, 1),  *   0.6,  =   1.6,
+--    (0, 0, 1))      1.0)      1.0)
 
 colorMatrix :: (Elt t, IsFloating t, MatrixMultiplication a) => ColorMatrix a t -> a (Exp t) -> a (Exp t)
 colorMatrix matrix colour = mmult matrix colour
