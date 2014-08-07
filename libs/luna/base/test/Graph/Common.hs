b9504a5c08f5814f0e18230ee88519dc256b0a64
Piotr Młodawski <remdezx@gmail.com>
2014-08-07 12:06:06 +0200

    Fixed some problems in graph <-> ast conversions

---
+++ libs/luna/base/test/Graph/Common.hs
@@ -0,0 +1,59 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2014
+---------------------------------------------------------------------------
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+module Graph.Common where
+
+import           Flowbox.Control.Error
+import qualified Flowbox.Luna.Data.AST.Crumb.Crumb                                       as Crumb
+import           Flowbox.Luna.Data.AST.Expr                                              (Expr)
+import qualified Flowbox.Luna.Data.AST.Zipper.Focus                                      as Focus
+import qualified Flowbox.Luna.Data.AST.Zipper.Zipper                                     as Zipper
+import           Flowbox.Luna.Data.Graph.Graph                                           (Graph)
+import           Flowbox.Luna.Data.Pass.AliasInfo                                        (AliasInfo)
+import           Flowbox.Luna.Data.Pass.Source                                           (Source (Source))
+import           Flowbox.Luna.Data.PropertyMap                                           (PropertyMap)
+import qualified Flowbox.Luna.Passes.Analysis.Alias.Alias                                as Analysis.Alias
+import qualified Flowbox.Luna.Passes.Analysis.CallGraph.CallGraph                        as Analysis.CallGraph
+import qualified Flowbox.Luna.Passes.Transform.AST.DepSort.DepSort                       as Transform.DepSort
+import qualified Flowbox.Luna.Passes.Transform.AST.Desugar.ImplicitCalls.ImplicitCalls   as Desugar.ImplicitCalls
+import qualified Flowbox.Luna.Passes.Transform.AST.Desugar.ImplicitScopes.ImplicitScopes as Desugar.ImplicitScopes
+import qualified Flowbox.Luna.Passes.Transform.AST.Desugar.ImplicitSelf.ImplicitSelf     as Desugar.ImplicitSelf
+import qualified Flowbox.Luna.Passes.Transform.AST.Desugar.TLRecUpdt.TLRecUpdt           as Desugar.TLRecUpdt
+import qualified Flowbox.Luna.Passes.Transform.AST.TxtParser.TxtParser                   as TxtParser
+import qualified Flowbox.Luna.Passes.Transform.Graph.Builder.Builder                     as GraphBuilder
+import qualified Flowbox.Luna.Passes.Transform.Graph.Parser.Parser                       as GraphParser
+import           Flowbox.Prelude
+import           Graph.SampleCodes                                                       (sampleCodes)
+
+
+
+
+getAST :: String -> IO (Expr, AliasInfo)
+getAST code = eitherStringToM' $ runEitherT $ do
+    (ast, _, astInfo) <- EitherT $ TxtParser.run $ Source ["Main"] code
+    (ast, astInfo)    <- EitherT $ Desugar.ImplicitSelf.run astInfo ast
+    (ast, astInfo)    <- EitherT $ Desugar.TLRecUpdt.run astInfo ast
+    aliasInfo         <- EitherT $ Analysis.Alias.run ast
+    callGraph         <- EitherT $ Analysis.CallGraph.run aliasInfo ast
+    ast               <- EitherT $ Transform.DepSort.run callGraph aliasInfo ast
+    (ast, astInfo)    <- EitherT $ Desugar.ImplicitScopes.run astInfo aliasInfo ast
+    (ast, astInfo)    <- EitherT $ Desugar.ImplicitCalls.run astInfo ast
+    aliasInfo         <- EitherT $ Analysis.Alias.run ast
+
+    let bc = [Crumb.Module "Main", Crumb.Function "main" []]
+    focus <- hoistEither $ Zipper.getFocus <$> Zipper.focusBreadcrumbs' bc ast
+    expr  <- Focus.getFunction focus <??> "Target is not a function"
+    return (expr, aliasInfo)
+
+
+getGraph :: AliasInfo -> PropertyMap -> Expr -> IO (Graph, PropertyMap)
+getGraph = eitherStringToM' .:. GraphBuilder.run
+
+
+getExpr :: Graph -> PropertyMap -> Expr -> IO Expr
+getExpr = eitherStringToM' .:. GraphParser.run
+
--- libs/luna/base/test/Graph/GraphSpec.hs
+++ libs/luna/base/test/Graph/GraphSpec.hs
@@ -9,65 +9,31 @@ module Graph.GraphSpec where
 
 import Test.Hspec
 
-import           Flowbox.Control.Error
-import qualified Flowbox.Luna.Data.AST.Crumb.Crumb                     as Crumb
-import           Flowbox.Luna.Data.AST.Expr                            (Expr)
-import qualified Flowbox.Luna.Data.AST.Zipper.Focus                    as Focus
-import qualified Flowbox.Luna.Data.AST.Zipper.Zipper                   as Zipper
-import qualified Flowbox.Luna.Data.Graph.Edge                          as Edge
-import           Flowbox.Luna.Data.Graph.Graph                         (Graph)
-import qualified Flowbox.Luna.Data.Graph.Graph                         as Graph
-import qualified Flowbox.Luna.Data.Graph.Node                          as Node
-import qualified Flowbox.Luna.Data.Graph.Port                          as Port
-import           Flowbox.Luna.Data.Pass.AliasInfo                      (AliasInfo)
-import           Flowbox.Luna.Data.Pass.Source                         (Source (Source))
-import           Flowbox.Luna.Data.PropertyMap                         (PropertyMap)
-import qualified Flowbox.Luna.Passes.Analysis.Alias.Alias              as Alias
-import           Flowbox.Luna.Passes.Transform.AST.IDFixer.IDFixer     (clearIDs)
-import qualified Flowbox.Luna.Passes.Transform.AST.TxtParser.TxtParser as TxtParser
-import qualified Flowbox.Luna.Passes.Transform.Graph.Builder.Builder   as GraphBuilder
-import qualified Flowbox.Luna.Passes.Transform.Graph.Parser.Parser     as GraphParser
+import qualified Flowbox.Luna.Data.Graph.Edge                      as Edge
+import qualified Flowbox.Luna.Data.Graph.Graph                     as Graph
+import qualified Flowbox.Luna.Data.Graph.Node                      as Node
+import qualified Flowbox.Luna.Data.Graph.Port                      as Port
+import           Flowbox.Luna.Passes.Transform.AST.IDFixer.IDFixer (clearIDs)
 import           Flowbox.Prelude
-import           Graph.SampleCodes                                     (sampleCodes)
+import qualified Graph.Common                                      as Common
+import           Graph.SampleCodes                                 (sampleCodes)
 
-
-
-getAST :: String -> IO (Expr, AliasInfo)
-getAST code = eitherStringToM' $ runEitherT $ do
-    (ast, _, _) <- EitherT $ TxtParser.run $ Source ["Main"] code
-    let bc = [Crumb.Module "Main", Crumb.Function "main" []]
-    focus <- hoistEither $ Zipper.getFocus <$> Zipper.focusBreadcrumbs' bc ast
-    aa    <- EitherT $ Alias.run ast
-    expr  <- Focus.getFunction focus <??> "Target is not a function"
-    return (expr, aa)
-
-
-getGraph :: AliasInfo -> PropertyMap -> Expr -> IO (Graph, PropertyMap)
-getGraph = eitherStringToM' .:. GraphBuilder.run
-
-
-getExpr :: Graph -> PropertyMap -> Expr -> IO Expr
-getExpr = eitherStringToM' .:. GraphParser.run
 
 
 backAndForth :: String -> IO ()
 backAndForth code = do
-    (expr, aa)    <- getAST code
-    (graph, pm)   <- getGraph aa def expr
+    (expr, aa)    <- Common.getAST code
+    (graph, pm)   <- Common.getGraph aa def expr
     --printLn
     --print expr
     --print graph
     --print pm
     --printLn
-    expr2         <- getExpr graph pm expr
-    (graph2, pm2) <- getGraph aa def expr
+    expr2         <- Common.getExpr graph pm expr
+    (graph2, pm2) <- Common.getGraph aa def expr
     (clearIDs 0 expr2) `shouldBe` (clearIDs 0 expr)
     graph2 `shouldBe` graph
     pm2    `shouldBe` pm
-
-
-named :: a -> b -> (a, b)
-named = (,)
 
 
 main :: IO ()
