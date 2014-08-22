d1793ce986e6a2c0e8e605facccc711321dff2df
Piotr Młodawski <remdezx@gmail.com>
2014-08-22 15:17:15 +0200

    Live reporting of computed values from interpeter

--- src/Flowbox/Bus/RPC/Pipes.hs
+++ src/Flowbox/Bus/RPC/Pipes.hs
@@ -35,15 +35,17 @@ produce = forever $ do
     Pipes.yield (frame ^. MessageFrame.message, frame ^. MessageFrame.correlation)
 
 
-consume :: Pipes.Consumer (Message, Message.CorrelationID) BusT ()
+consume :: Pipes.Consumer (Message, Maybe Message.CorrelationID) BusT ()
 consume = forever $ do
-    (msg, crl) <- Pipes.await
-    void $ lift $ BusT $ Bus.reply crl Flag.Enable msg
+    (msg, mcrl) <- Pipes.await
+    void $ lift $ BusT $ case mcrl of
+        Just crl -> Bus.reply crl Flag.Enable msg
+        Nothing  -> void $ Bus.send Flag.Enable msg
 
 
 run :: BusEndPoints -> HandlerMap s m
     -> IO (Pipes.Input  (Message, Message.CorrelationID),
-           Pipes.Output (Message, Message.CorrelationID))
+           Pipes.Output (Message, Maybe Message.CorrelationID))
 run endPoints handlerMap = do
     (output1, input1) <- Pipes.spawn Pipes.Single
     (output2, input2) <- Pipes.spawn Pipes.Single
@@ -54,3 +56,4 @@ run endPoints handlerMap = do
         mapM_ Bus.subscribe $ HandlerMap.topics handlerMap
         BusT.runBusT $ Pipes.runEffect $ Pipes.fromInput input2 >-> consume
     return (input1, output2)
+
