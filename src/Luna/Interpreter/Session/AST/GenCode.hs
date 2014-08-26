1b830f02e2ab48740bfe2b9e407d752b8ce3ea8a
Piotr Młodawski <remdezx@gmail.com>
2014-08-26 15:30:37 +0200

    Working version of lunac

---
+++ src/Luna/Interpreter/Session/AST/GenCode.hs
@@ -0,0 +1,59 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Flowbox Team <contact@flowbox.io>, 2014
+-- Proprietary and confidential
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+---------------------------------------------------------------------------
+module Luna.Interpreter.Session.AST.GenCode where
+
+import           Control.Monad.Trans.Either
+import qualified Data.List                  as List
+
+import           Flowbox.Prelude
+import           Flowbox.System.Log.Logger
+import           Flowbox.Text.Show.Hs                     (hsShow)
+import           Luna.Data.Source                         (Source)
+import qualified Luna.Data.Source                         as Source
+import qualified Luna.Interpreter.Session.Data.DefPoint   as DefPoint
+import           Luna.Interpreter.Session.Session         (Session)
+import qualified Luna.Interpreter.Session.Session         as Session
+import qualified Luna.Pass.Analysis.Alias.Alias           as Analysis.Alias
+import qualified Luna.Pass.CodeGen.HSC.HSC                as HSC
+import qualified Luna.Pass.Transform.AST.Hash.Hash        as Hash
+import qualified Luna.Pass.Transform.AST.SSA.SSA          as SSA
+import qualified Luna.Pass.Transform.HAST.HASTGen.HASTGen as HASTGen
+
+
+
+logger :: LoggerIO
+logger = getLoggerIO "Luna.Interpreter.Session.GenCode"
+
+
+genModule :: Session [Source]
+genModule = do
+    mainPtr   <- Session.getMainPtr
+    ast       <- Session.getModule $ (DefPoint.breadcrumbs %~ init) mainPtr
+    aliasInfo <- EitherT $ Analysis.Alias.run ast
+    hash      <- EitherT $ Hash.run ast
+    ssa       <- EitherT $ SSA.run aliasInfo hash
+    hast      <- EitherT $ HASTGen.run ssa
+    hsc       <- EitherT $ HSC.run  hast
+    putStrLn $ List.intercalate "\n\n" (map printSrc hsc)
+    return hsc
+
+
+loadModule :: Session ()
+loadModule = do
+    srcs <- genModule
+    mapM_ (Session.runDecls . unlines . drop 18 . lines . view Source.code) srcs
+
+
+
+
+
+
+-----------
+printSrc :: Source -> [Char]
+printSrc src = ">>> file '" ++ List.intercalate "/" (src ^. Source.path) ++ "':\n\n"
+             ++ hsShow (src ^. Source.code)
+----------
