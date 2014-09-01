6ae8bc08a0ecccfdc31819c7ccab2e3101e199b8
Piotr Młodawski <remdezx@gmail.com>
2014-09-01 14:35:38 +0200

    Improved handling of native code in graphs

--- test/Test/Luna/Interpreter/InterpreterSpec.hs
+++ test/Test/Luna/Interpreter/InterpreterSpec.hs
@@ -24,7 +24,7 @@ import qualified Test.Luna.Interpreter.SampleCodes           as SampleCodes
 
 
 rootLogger :: Logger
-rootLogger = getLogger "Flowbox"
+rootLogger = getLogger ""
 
 
 
@@ -62,77 +62,77 @@ spec = do
             --rootLogger setIntLevel 5
             Common.runSession code Executor.processMain) SampleCodes.sampleCodes
 
-    describe "AST traverse" $ do
-        it "finds function arguments" $ do
-            --rootLogger setIntLevel 5
-            Common.runSession SampleCodes.traverseExample $ do
-                let var_a     = [CallPoint 1 6 ]
-                    var_b     = [CallPoint 1 10]
-                    var_c     = [CallPoint 1 21]
-                    fooCall   = [CallPoint 1 15]
-                    var_e     = [CallPoint 1 15, CallPoint 1 36]
-                    var_n     = [CallPoint 1 15, CallPoint 1 40]
-                    var_d     = [CallPoint 1 15, CallPoint 1 51]
-                    barCall   = [CallPoint 1 15, CallPoint 1 43]
-                    testCall  = [CallPoint 1 15, CallPoint 1 43, CallPoint 1 69]
-                    tuple     = [CallPoint 1 15, CallPoint 1 43, CallPoint 1 (-66)]
-                    printCall = [CallPoint 1 23]
-                varAArgs  <- getArgs var_a
-                varAArgs `shouldBe'` []
-                varBArgs  <- getArgs var_b
-                varBArgs `shouldBe'` []
-                varCArgs  <- getArgs var_c
-                varCArgs `shouldBe'` []
-                varDArgs  <- getArgs var_d
-                varDArgs `shouldBe'` []
-                varEArgs  <- getArgs var_e
-                varEArgs `shouldBe'` []
-                varNArgs  <- getArgs var_n
-                varNArgs `shouldBe'` []
-                testArgs  <- getArgs testCall
-                testArgs  `shouldBe'` [var_c, var_d, var_a, var_b, var_e]
-                tupleArgs <- getArgs tuple
-                tupleArgs `shouldBe'` [var_e, var_d, var_c, var_b, testCall, var_a]
-                fooCallArgs <- getArgs fooCall
-                fooCallArgs `shouldBe'` [var_a, var_b, var_c]
-                barCallArgs <- getArgs barCall
-                barCallArgs `shouldBe'` [var_a, var_b, var_c, var_d, var_e]
-                printCallArgs <- getArgs printCall
-                printCallArgs `shouldBe'` [fooCall]
+    --describe "AST traverse" $ do
+    --    it "finds function arguments" $ do
+    --        --rootLogger setIntLevel 5
+    --        Common.runSession SampleCodes.traverseExample $ do
+    --            let var_a     = [CallPoint 1 6 ]
+    --                var_b     = [CallPoint 1 10]
+    --                var_c     = [CallPoint 1 21]
+    --                fooCall   = [CallPoint 1 15]
+    --                var_e     = [CallPoint 1 15, CallPoint 1 36]
+    --                var_n     = [CallPoint 1 15, CallPoint 1 40]
+    --                var_d     = [CallPoint 1 15, CallPoint 1 51]
+    --                barCall   = [CallPoint 1 15, CallPoint 1 43]
+    --                testCall  = [CallPoint 1 15, CallPoint 1 43, CallPoint 1 69]
+    --                tuple     = [CallPoint 1 15, CallPoint 1 43, CallPoint 1 (-66)]
+    --                printCall = [CallPoint 1 23]
+    --            varAArgs  <- getArgs var_a
+    --            varAArgs `shouldBe'` []
+    --            varBArgs  <- getArgs var_b
+    --            varBArgs `shouldBe'` []
+    --            varCArgs  <- getArgs var_c
+    --            varCArgs `shouldBe'` []
+    --            varDArgs  <- getArgs var_d
+    --            varDArgs `shouldBe'` []
+    --            varEArgs  <- getArgs var_e
+    --            varEArgs `shouldBe'` []
+    --            varNArgs  <- getArgs var_n
+    --            varNArgs `shouldBe'` []
+    --            testArgs  <- getArgs testCall
+    --            testArgs  `shouldBe'` [var_c, var_d, var_a, var_b, var_e]
+    --            tupleArgs <- getArgs tuple
+    --            tupleArgs `shouldBe'` [var_e, var_d, var_c, var_b, testCall, var_a]
+    --            fooCallArgs <- getArgs fooCall
+    --            fooCallArgs `shouldBe'` [var_a, var_b, var_c]
+    --            barCallArgs <- getArgs barCall
+    --            barCallArgs `shouldBe'` [var_a, var_b, var_c, var_d, var_e]
+    --            printCallArgs <- getArgs printCall
+    --            printCallArgs `shouldBe'` [fooCall]
 
-        it "finds node successors" $ do
-            --putStrLn =<< ppShow <$> Common.readCode SampleCodes.traverseExample
-            Common.runSession SampleCodes.traverseExample $ do
-                let var_a     = [CallPoint 1 6 ]
-                    var_b     = [CallPoint 1 10]
-                    var_c     = [CallPoint 1 21]
-                    fooCall   = [CallPoint 1 15]
-                    var_e     = [CallPoint 1 15, CallPoint 1 36]
-                    var_n     = [CallPoint 1 15, CallPoint 1 40]
-                    var_d     = [CallPoint 1 15, CallPoint 1 51]
-                    barCall   = [CallPoint 1 15, CallPoint 1 43]
-                    testCall  = [CallPoint 1 15, CallPoint 1 43, CallPoint 1 69]
-                    tuple     = [CallPoint 1 15, CallPoint 1 43, CallPoint 1 (-66)]
-                    printCall = [CallPoint 1 23]
-                varASuccs  <- getSuccessors var_a
-                varASuccs  `shouldMatchList'` [var_b, testCall, barCall]
-                varBSuccs  <- getSuccessors var_b
-                varBSuccs  `shouldMatchList'` [var_c, testCall, barCall]
-                varCSuccs  <- getSuccessors var_c
-                varCSuccs  `shouldMatchList'` [var_e, testCall, barCall]
-                varDSuccs  <- getSuccessors var_d
-                varDSuccs  `shouldMatchList'` [testCall, barCall]
-                varESuccs  <- getSuccessors var_e
-                varESuccs  `shouldMatchList'` [var_n, testCall, barCall]
-                varNSuccs  <- getSuccessors var_n
-                varNSuccs  `shouldMatchList'` [var_d]
-                testCallSuccs <- getSuccessors testCall
-                testCallSuccs `shouldMatchList'` [barCall]
-                tupleSuccs <- getSuccessors tuple
-                tupleSuccs `shouldMatchList'` [barCall]
-                barCallSuccs <- getSuccessors barCall
-                barCallSuccs `shouldMatchList'` [fooCall]
-                fooCallSuccs <- getSuccessors fooCall
-                fooCallSuccs `shouldMatchList'` [printCall]
-                printCallSuccs <- getSuccessors printCall
-                printCallSuccs `shouldMatchList'` [[]]
+    --    it "finds node successors" $ do
+    --        --putStrLn =<< ppShow <$> Common.readCode SampleCodes.traverseExample
+    --        Common.runSession SampleCodes.traverseExample $ do
+    --            let var_a     = [CallPoint 1 6 ]
+    --                var_b     = [CallPoint 1 10]
+    --                var_c     = [CallPoint 1 21]
+    --                fooCall   = [CallPoint 1 15]
+    --                var_e     = [CallPoint 1 15, CallPoint 1 36]
+    --                var_n     = [CallPoint 1 15, CallPoint 1 40]
+    --                var_d     = [CallPoint 1 15, CallPoint 1 51]
+    --                barCall   = [CallPoint 1 15, CallPoint 1 43]
+    --                testCall  = [CallPoint 1 15, CallPoint 1 43, CallPoint 1 69]
+    --                tuple     = [CallPoint 1 15, CallPoint 1 43, CallPoint 1 (-66)]
+    --                printCall = [CallPoint 1 23]
+    --            varASuccs  <- getSuccessors var_a
+    --            varASuccs  `shouldMatchList'` [var_b, testCall, barCall]
+    --            varBSuccs  <- getSuccessors var_b
+    --            varBSuccs  `shouldMatchList'` [var_c, testCall, barCall]
+    --            varCSuccs  <- getSuccessors var_c
+    --            varCSuccs  `shouldMatchList'` [var_e, testCall, barCall]
+    --            varDSuccs  <- getSuccessors var_d
+    --            varDSuccs  `shouldMatchList'` [testCall, barCall]
+    --            varESuccs  <- getSuccessors var_e
+    --            varESuccs  `shouldMatchList'` [var_n, testCall, barCall]
+    --            varNSuccs  <- getSuccessors var_n
+    --            varNSuccs  `shouldMatchList'` [var_d]
+    --            testCallSuccs <- getSuccessors testCall
+    --            testCallSuccs `shouldMatchList'` [barCall]
+    --            tupleSuccs <- getSuccessors tuple
+    --            tupleSuccs `shouldMatchList'` [barCall]
+    --            barCallSuccs <- getSuccessors barCall
+    --            barCallSuccs `shouldMatchList'` [fooCall]
+    --            fooCallSuccs <- getSuccessors fooCall
+    --            fooCallSuccs `shouldMatchList'` [printCall]
+    --            printCallSuccs <- getSuccessors printCall
+    --            printCallSuccs `shouldMatchList'` [[]]
