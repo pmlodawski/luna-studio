7405b44b87a1e4effedded7e1632cfbc7954d16d
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-09-02 18:57:05 +0200

    Introduce Nix, first version

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ haskellPackages ? (import <nixpkgs> {}).haskellPackages }:
+
+let inherit (haskellPackages) cabalInstall_1_20_0_3;
+in
+haskellPackages.cabal.mkDerivation (self: {
+    pname = "flowboxUtils";
+    version = "0.1";
+    src = ./.;
+    doCheck = false;
+    buildDepends = with haskellPackages; [
+        aeson ansiTerminal
+        dataDefault dlist either errors fgl genericDeriving
+        hslogger lens MissingH mmorph monadLoops mtl optparseApplicative pretty
+        prettyShow process protocolBuffers split text transformers
+    ];
+    buildTools = [ cabalInstall_1_20_0_3 ];
+    enableSplitObjs = false;
+})
