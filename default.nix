cf801e22ad46135276f19fa636f0a189ed8e050b
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-09-02 20:26:44 +0200

    Overhauled nix expressions
    Still not the best, because it doesn't call gencabal before building and
    has ugly relative paths in each nix expression. However, it's almost
    self-contained, so all relevant files are in the repo.
    
    The only thing that should be enabled in home directory is to allow
    unfree packages to be built.

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
-{ haskellPackages ? (import <nixpkgs> {}).haskellPackages,
-  flowbox }:
+{ haskellPackages ? import ../../nix/overrides.nix,
+  flowbox ? import ../../nix/flowbox.nix
+}:
 
 let inherit (haskellPackages) cabalInstall_1_20_0_3;
 in
