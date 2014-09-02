3d3d26e7406186585ec5f2b2c3a3f3b18dbc9e05
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-09-02 20:26:44 +0200

    Overhauled nix expressions
    Still not the best, because it doesn't call gencabal before building and
    has ugly relative paths in each nix expression. However, it's almost
    self-contained, so all relevant files are in the repo.
    
    The only thing that should be enabled in home directory is to allow
    unfree packages to be built.

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
-{ haskellPackages ? (import <nixpkgs> {}).haskellPackages,
-  flowbox }:
+{ haskellPackages ? import ../../../nix/flowbox.nix,
+  flowbox ? import ../../../nix/flowbox.nix
+}:
 
 haskellPackages.cabal.mkDerivation (self: {
   pname = "luna-initializer";
