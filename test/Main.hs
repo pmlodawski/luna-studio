bfb043d296948ee6f2720df2fd7ae9af3dd77aa6
Wojciech Danilo <wojciech.danilo@gmail.com>
2014-09-04 20:55:20 +0200

    WIP - moving over new Monad definition based on the Luna Errors logic. Lots of mess. Does not compile.

--- test/Main.hs
+++ test/Main.hs
@@ -96,32 +96,33 @@ logger = getLoggerIO "Flowbox"
 example :: Source
 example = Source.Source ["Main"] $
         concat $ replicate 1 $ unlines [ ""
-                    , "class Vector a:"
-                    , "    x,y,z :: a"
-                    , "    def test a b:"
-                    , "        {a,b}"
+                    --, "class Vector a:"
+                    --, "    x,y,z :: a"
+                    --, "    def test a b:"
+                    --, "        {a,b}"
 
-                    , "def print msg:"
-                    , "    ```autoLift1 print #{msg}```"
+                    --, "def print msg:"
+                    --, "    ```autoLift1 print #{msg}```"
 
-                    , "def Int.+ a:"
-                    , "    ```liftF2 (+) #{self} #{a}```"
+                    --, "def Int.+ a:"
+                    --, "    ```liftF2 (+) #{self} #{a}```"
 
-                    , "def Int.> a:"
-                    , "    ```liftF2 (>) #{self} #{a}```"
+                    --, "def Int.> a:"
+                    --, "    ```liftF2 (>) #{self} #{a}```"
 
-                    , "def Int.inc:"
-                    , "    self + 1"
+                    --, "def Int.inc:"
+                    --, "    self + 1"
 
 
                     , "def main:"
-                    , "    print $ if 1 > 2: 5"
-                    , "            else: 6"
-                    , "    print $ 1 > 2"
-                    , "    v = Vector 1 2 3"
+                    --, "    print $ if 1 > 2: 5"
+                    --, "            else: 6"
+                    --, "    print $ 1 > 2"
+                    --, "    v = Vector 1 2 3"
+                    , "    f = x:x"
                     --, "    f = x:x"
                     --, "    v.x = 5"
-                    , "    print $ v"
+                    --, "    print $ v"
 
 
 
