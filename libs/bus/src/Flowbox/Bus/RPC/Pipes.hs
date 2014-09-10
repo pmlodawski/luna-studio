d6a2b8f17ec7e4546539a25ec499787d960998ba
Piotr Młodawski <remdezx@gmail.com>
2014-09-10 09:58:52 +0200

    Fixed BATCH-11

--- libs/bus/src/Flowbox/Bus/RPC/Pipes.hs
+++ libs/bus/src/Flowbox/Bus/RPC/Pipes.hs
@@ -35,17 +35,15 @@ produce = forever $ do
     Pipes.yield (frame ^. MessageFrame.message, frame ^. MessageFrame.correlation)
 
 
-consume :: Pipes.Consumer (Message, Maybe Message.CorrelationID) BusT ()
+consume :: Pipes.Consumer (Message, Message.CorrelationID) BusT ()
 consume = forever $ do
-    (msg, mcrl) <- Pipes.await
-    void $ lift $ BusT $ case mcrl of
-        Just crl -> Bus.reply crl Flag.Enable msg
-        Nothing  -> void $ Bus.send Flag.Enable msg
+    (msg, crl) <- Pipes.await
+    void $ lift $ BusT $ Bus.reply crl Flag.Enable msg
 
 
 run :: BusEndPoints -> HandlerMap s m
     -> IO (Pipes.Input  (Message, Message.CorrelationID),
-           Pipes.Output (Message, Maybe Message.CorrelationID))
+           Pipes.Output (Message, Message.CorrelationID))
 run endPoints handlerMap = do
     (output1, input1) <- Pipes.spawn Pipes.Single
     (output2, input2) <- Pipes.spawn Pipes.Single
