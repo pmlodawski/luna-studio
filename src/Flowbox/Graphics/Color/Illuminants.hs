69d6b80d1e741d9c4aeb1f365b8b27bc0aa9dbca
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-09-18 09:07:36 +0000

    Get rid of Accelerate instances

--- src/Flowbox/Graphics/Color/Illuminants.hs
+++ src/Flowbox/Graphics/Color/Illuminants.hs
@@ -4,7 +4,7 @@
 -- Proprietary and confidential
 -- Flowbox Team <contact@flowbox.io>, 2014
 ---------------------------------------------------------------------------
-{-# LANGUAGE AutoDeriveTypeable    #-}
+{-# LANGUAGE DeriveDataTypeable    #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -15,48 +15,19 @@
 module Flowbox.Graphics.Color.Illuminants where
 
 import Data.Array.Accelerate as A
-import Data.Array.Accelerate.Smart
-import Data.Array.Accelerate.Tuple
-import Data.Array.Accelerate.Array.Sugar
 import Data.Typeable
 
 import Flowbox.Graphics.Color.CIE.XyY
 import Flowbox.Graphics.Color.CIE.XYZ
+import Flowbox.Graphics.Utils.Accelerate
 import Flowbox.Prelude
-
 
 
 
 data Chromaticity a = Chromaticity { chromaX :: a, chromaY :: a }
                     deriving (Show, Typeable)
 
-type instance EltRepr  (Chromaticity a) = EltRepr  (a, a)
-type instance EltRepr' (Chromaticity a) = EltRepr' (a, a)
-
-instance Elt a => Elt (Chromaticity a) where
-    eltType _ = eltType (undefined :: (a, a))
-    toElt p = case toElt p of
-        (lo, hi) -> Chromaticity lo hi
-    fromElt (Chromaticity lo hi) = fromElt (lo, hi)
-
-    eltType' _ = eltType' (undefined :: (a, a))
-    toElt' p = case toElt' p of
-        (lo, hi) -> Chromaticity lo hi
-    fromElt' (Chromaticity lo hi) = fromElt' (lo, hi)
-
-instance IsTuple (Chromaticity a) where
-    type TupleRepr (Chromaticity a) = TupleRepr (a, a)
-    fromTuple (Chromaticity lo hi) = fromTuple (lo, hi)
-    toTuple t = case toTuple t of
-        (lo, hi) -> Chromaticity lo hi
-
-instance (Lift Exp a, Elt (Plain a)) => Lift Exp (Chromaticity a) where
-    type Plain (Chromaticity a) = Chromaticity (Plain a)
-    lift (Chromaticity lo hi) = Exp $ Tuple $ NilTup `SnocTup` A.lift lo `SnocTup` A.lift hi
-
-instance (Elt a, e ~ Exp a) => Unlift Exp (Chromaticity e) where
-    unlift t = Chromaticity (Exp $ SuccTupIdx ZeroTupIdx `Prj` t)
-                     (Exp $ ZeroTupIdx `Prj` t)
+deriveAccelerate ''Chromaticity
 
 
 toxyY :: (Num a) => Chromaticity a -> XyY a
@@ -88,164 +59,164 @@ class Illuminant a b where
     primaries :: (Num b, Floating b) => a -> Chromaticity b
 
 data A = A
-       deriving Show
+       deriving (Show, Typeable)
 
 instance Illuminant A n where
     primaries _ = Chromaticity 0.44757 0.40745
 
 data B = B
-       deriving Show
+       deriving (Show, Typeable)
 
 instance Illuminant B n where
     primaries _ = Chromaticity 0.34842 0.35161
 
 data C = C
-       deriving Show
+       deriving (Show, Typeable)
 
 instance Illuminant C n where
     primaries _ = Chromaticity 0.31006 0.31616
 
 data D40 = D40
-         deriving Show
+         deriving (Show, Typeable)
 
 instance Illuminant D40 n where
     primaries _ = Chromaticity 0.3823 0.3838
 
 data D45 = D45
-         deriving Show
+         deriving (Show, Typeable)
 
 instance Illuminant D45 n where
     primaries _ = Chromaticity 0.3621 0.3709
 
 data D50 = D50
-         deriving Show
+         deriving (Show, Typeable)
 
 instance Illuminant D50 n where
     primaries _ = Chromaticity 0.34567 0.35850
 
 data D55 = D55
-         deriving Show
+         deriving (Show, Typeable)
 
 instance Illuminant D55 n where
     primaries _ = Chromaticity 0.33242 0.34743
 
 data D60 = D60
-         deriving Show
+         deriving (Show, Typeable)
 
 instance Illuminant D60 n where
     primaries _ = Chromaticity 0.3217 0.3378
 
 data D65 = D65
-         deriving Show
+         deriving (Show, Typeable)
 
 instance Illuminant D65 n where
     primaries _ = Chromaticity 0.31271 0.32902
 
 data D70 = D70
-         deriving Show
+         deriving (Show, Typeable)
 
 instance Illuminant D70 n where
     primaries _ = Chromaticity 0.3054 0.3216
 
 data D75 = D75
-         deriving Show
+         deriving (Show, Typeable)
 
 instance Illuminant D75 n where
     primaries _ = Chromaticity 0.29902 0.31485
 
 data D93 = D93
-         deriving Show
+         deriving (Show, Typeable)
 
 instance Illuminant D93 n where
     primaries _ = Chromaticity 0.2848 0.2932
 
 data E = E
-       deriving Show
+       deriving (Show, Typeable)
 
 instance Illuminant E n where
     primaries _ = Chromaticity (1/3)  (1/3)
 
 data F1 = F1
-        deriving Show
+        deriving (Show, Typeable)
 
 instance Illuminant F1 n where
     primaries _ = Chromaticity 0.31310 0.33727
 
 data F2 = F2
-        deriving Show
+        deriving (Show, Typeable)
 
 instance Illuminant F2 n where
     primaries _ = Chromaticity 0.37208 0.37529
 
 data F3 = F3
-        deriving Show
+        deriving (Show, Typeable)
 
 instance Illuminant F3 n where
     primaries _ = Chromaticity 0.40910 0.39430
 
 data F4 = F4
-        deriving Show
+        deriving (Show, Typeable)
 
 instance Illuminant F4 n where
     primaries _ = Chromaticity 0.44018 0.40329
 
 data F5 = F5
-        deriving Show
+        deriving (Show, Typeable)
 
 instance Illuminant F5 n where
     primaries _ = Chromaticity 0.31379 0.34531
 
 data F6 = F6
-        deriving Show
+        deriving (Show, Typeable)
 
 instance Illuminant F6 n where
     primaries _ = Chromaticity 0.37790 0.38835
 
 data F7 = F7
-        deriving Show
+        deriving (Show, Typeable)
 
 instance Illuminant F7 n where
     primaries _ = Chromaticity 0.31292 0.32933
 
 data F8 = F8
-        deriving Show
+        deriving (Show, Typeable)
 
 instance Illuminant F8 n where
     primaries _ = Chromaticity 0.34588 0.35875
 
 data F9 = F9
-        deriving Show
+        deriving (Show, Typeable)
 
 instance Illuminant F9 n where
     primaries _ = Chromaticity 0.37417 0.37281
 
 data F10 = F10
-        deriving Show
+        deriving (Show, Typeable)
 
 instance Illuminant F10 n where
     primaries _ = Chromaticity 0.34609 0.35986
 
 data F11 = F11
-         deriving Show
+         deriving (Show, Typeable)
 
 instance Illuminant F11 n where
     primaries _ = Chromaticity 0.38052 0.37713
 
 data F12 = F12
-        deriving Show
+        deriving (Show, Typeable)
 
 instance Illuminant F12 n where
     primaries _ = Chromaticity 0.43695 0.40441
 
 data ACES = ACES
-          deriving Show
+          deriving (Show, Typeable)
 
 instance Illuminant ACES n where
     primaries _ = Chromaticity 0.32168 0.33767
 
 -- correlated color temperature
 newtype CCT a = CCT a
-              deriving Show
+              deriving (Show, Typeable)
 
 instance (Num n, Floating n) => Illuminant (CCT n) n where
     primaries (CCT temperature) = Chromaticity x y
