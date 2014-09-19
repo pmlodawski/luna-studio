dbdf4bf5b6de9f27110e81c0c736a4497be6df7a
Piotr Młodawski <remdezx@gmail.com>
2014-09-19 12:04:26 +0200

    Refactor, fixed errors

--- src/Luna/Interpreter/Session/TargetHS/Instances.hs
+++ src/Luna/Interpreter/Session/TargetHS/Instances.hs
@@ -24,14 +24,13 @@ logger :: LoggerIO
 logger = getLoggerIO "Luna.Interpreter.Session.TargetHS.Instances"
 
 
-dshow :: Outputable.Outputable a => a -> String
-dshow = Outputable.showSDoc dflags . Outputable.ppr
+dshow :: Outputable.Outputable a => GHC.DynFlags -> a -> String
+dshow dflags = Outputable.showSDoc dflags . Outputable.ppr
 
 
 clean :: Session ()
 clean = lift2 $ do
     dflags <- GHC.getSessionDynFlags
-
     --printInstances
 
     -- FIXME [PM] : Code below remove all declared instances. It may be
@@ -41,14 +40,14 @@ clean = lift2 $ do
         (clsInst, famInst) = HscTypes.ic_instances hsc_IC
         clsInst'           = filter remove clsInst
         famInst'           = famInst
-        remove inst = dshow (InstEnv.is_cls_nm inst) /= "Luna.Target.HS.Data.Func.Func.Func"
-                   && dshow (InstEnv.is_cls_nm inst) /= "Luna.Target.HS.Data.Struct.Mem.HasMem"
+        remove inst = dshow dflags (InstEnv.is_cls_nm inst) /= "Luna.Target.HS.Data.Func.Func.Func"
+                   && dshow dflags (InstEnv.is_cls_nm inst) /= "Luna.Target.HS.Data.Struct.Mem.HasMem"
         hsc_IC'            = hsc_IC {HscTypes.ic_instances = (clsInst', famInst')}
         in hscEnv { HscTypes.hsc_IC = hsc_IC'}
 
 
 printInstances :: Session ()
-printInstances = do
+printInstances = lift2 $ do
     dflags <- GHC.getSessionDynFlags
     GhcMonad.withSession $ \hscEnv -> do
         let instances     = HscTypes.ic_instances $ HscTypes.hsc_IC hscEnv
@@ -56,27 +55,27 @@ printInstances = do
             clsInstances  = concatMap clsInstShow $ fst instances
             clsInstShow i = "\n--- class instance -------------------------------------------------\n" ++
                             ppShow
-                            ( dshow $ InstEnv.is_cls_nm i
-                            , dshow $ InstEnv.is_tcs    i
-                            , dshow $ InstEnv.is_tvs    i
-                            , dshow $ InstEnv.is_cls    i
-                            , dshow $ InstEnv.is_tys    i
-                            , dshow $ InstEnv.is_dfun   i
-                            , dshow $ InstEnv.is_flag   i
-                            ) ++ "\n" ++ dshow i
+                            ( dshow dflags $ InstEnv.is_cls_nm i
+                            , dshow dflags $ InstEnv.is_tcs    i
+                            , dshow dflags $ InstEnv.is_tvs    i
+                            , dshow dflags $ InstEnv.is_cls    i
+                            , dshow dflags $ InstEnv.is_tys    i
+                            , dshow dflags $ InstEnv.is_dfun   i
+                            , dshow dflags $ InstEnv.is_flag   i
+                            ) ++ "\n" ++ dshow dflags i
 
             famInstances  = concatMap famInstShow $ snd instances
             famInstShow i = "\n--- family instance ------------------------------------------------\n" ++
                             ppShow
-                            ( dshow $ FamInstEnv.fi_axiom  i
-                            --, dshow $ FamInstEnv.fi_flavor i
-                            , dshow $ FamInstEnv.fi_fam    i
-                            , dshow $ FamInstEnv.fi_tcs    i
-                            , dshow $ FamInstEnv.fi_tvs    i
-                            , dshow $ FamInstEnv.fi_tys    i
-                            , dshow $ FamInstEnv.fi_rhs    i
+                            ( dshow dflags $ FamInstEnv.fi_axiom  i
+                            --, dshow dflags $ FamInstEnv.fi_flavor i
+                            , dshow dflags $ FamInstEnv.fi_fam    i
+                            , dshow dflags $ FamInstEnv.fi_tcs    i
+                            , dshow dflags $ FamInstEnv.fi_tvs    i
+                            , dshow dflags $ FamInstEnv.fi_tys    i
+                            , dshow dflags $ FamInstEnv.fi_rhs    i
 
-                            ) ++ "\n" ++ dshow i
+                            ) ++ "\n" ++ dshow dflags i
 
         putStrLn  famInstances
         putStrLn  clsInstances
