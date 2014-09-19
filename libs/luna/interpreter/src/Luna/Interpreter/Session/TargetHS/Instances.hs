a5782fd2a48f23ba7e1cf364f20b15ff32e95d08
Piotr Młodawski <remdezx@gmail.com>
2014-09-19 11:44:46 +0200

    Fixed problem with instances in interpreter

--- libs/luna/interpreter/src/Luna/Interpreter/Session/TargetHS/Instances.hs
+++ libs/luna/interpreter/src/Luna/Interpreter/Session/TargetHS/Instances.hs
@@ -6,12 +6,13 @@
 ---------------------------------------------------------------------------
 module Luna.Interpreter.Session.TargetHS.Instances where
 
+import qualified FamInstEnv
+import qualified GHC
 import qualified GhcMonad
 import qualified HscTypes
---import qualified GHC
---import qualified InstEnv
---import qualified Outputable
---import           Text.Show.Pretty             (ppShow)
+import qualified InstEnv
+import qualified Outputable
+import           Text.Show.Pretty (ppShow)
 
 import Flowbox.Prelude
 import Flowbox.System.Log.Logger
@@ -19,33 +20,63 @@ import Luna.Interpreter.Session.Session (Session)
 
 
 
-
 logger :: LoggerIO
 logger = getLoggerIO "Luna.Interpreter.Session.TargetHS.Instances"
 
 
+dshow :: Outputable.Outputable a => a -> String
+dshow = Outputable.showSDoc dflags . Outputable.ppr
+
+
 clean :: Session ()
-clean = lift2 $
+clean = lift2 $ do
+    dflags <- GHC.getSessionDynFlags
 
-    --dflags <- GHC.getSessionDynFlags
-    --GhcMonad.withSession $ \hscEnv -> do
-    --    let instances = concatMap showPrepare $ fst $ HscTypes.ic_instances $ HscTypes.hsc_IC hscEnv
-    --        dshow :: Outputable.Outputable a => a -> String
-    --        dshow = Outputable.showSDoc dflags . Outputable.ppr
-    --        showPrepare i = ppShow ( dshow $ InstEnv.is_cls_nm i
-    --                        , dshow $ InstEnv.is_tcs    i
-    --                        , dshow $ InstEnv.is_tvs    i
-    --                        , dshow $ InstEnv.is_cls    i
-    --                        , dshow $ InstEnv.is_tys    i
-    --                        , dshow $ InstEnv.is_dfun   i
-    --                        , dshow $ InstEnv.is_flag   i
-    --                        ) ++ "\n" ++ dshow i ++ "\n--------------------------------------------------------------------\n"
-
-    --    putStrLn  instances
-
+    --printInstances
 
     -- FIXME [PM] : Code below remove all declared instances. It may be
     --              dangerous and needs to be deeply tested or removed.
     GhcMonad.modifySession $ \hscEnv -> let
-        ic = (HscTypes.hsc_IC hscEnv) {HscTypes.ic_instances = ([], []) }
-        in hscEnv { HscTypes.hsc_IC = ic}
+        hsc_IC             = HscTypes.hsc_IC       hscEnv
+        (clsInst, famInst) = HscTypes.ic_instances hsc_IC
+        clsInst'           = filter remove clsInst
+        famInst'           = famInst
+        remove inst = dshow (InstEnv.is_cls_nm inst) /= "Luna.Target.HS.Data.Func.Func.Func"
+                   && dshow (InstEnv.is_cls_nm inst) /= "Luna.Target.HS.Data.Struct.Mem.HasMem"
+        hsc_IC'            = hsc_IC {HscTypes.ic_instances = (clsInst', famInst')}
+        in hscEnv { HscTypes.hsc_IC = hsc_IC'}
+
+
+printInstances :: Session ()
+printInstances = do
+    dflags <- GHC.getSessionDynFlags
+    GhcMonad.withSession $ \hscEnv -> do
+        let instances     = HscTypes.ic_instances $ HscTypes.hsc_IC hscEnv
+
+            clsInstances  = concatMap clsInstShow $ fst instances
+            clsInstShow i = "\n--- class instance -------------------------------------------------\n" ++
+                            ppShow
+                            ( dshow $ InstEnv.is_cls_nm i
+                            , dshow $ InstEnv.is_tcs    i
+                            , dshow $ InstEnv.is_tvs    i
+                            , dshow $ InstEnv.is_cls    i
+                            , dshow $ InstEnv.is_tys    i
+                            , dshow $ InstEnv.is_dfun   i
+                            , dshow $ InstEnv.is_flag   i
+                            ) ++ "\n" ++ dshow i
+
+            famInstances  = concatMap famInstShow $ snd instances
+            famInstShow i = "\n--- family instance ------------------------------------------------\n" ++
+                            ppShow
+                            ( dshow $ FamInstEnv.fi_axiom  i
+                            --, dshow $ FamInstEnv.fi_flavor i
+                            , dshow $ FamInstEnv.fi_fam    i
+                            , dshow $ FamInstEnv.fi_tcs    i
+                            , dshow $ FamInstEnv.fi_tvs    i
+                            , dshow $ FamInstEnv.fi_tys    i
+                            , dshow $ FamInstEnv.fi_rhs    i
+
+                            ) ++ "\n" ++ dshow i
+
+        putStrLn  famInstances
+        putStrLn  clsInstances
