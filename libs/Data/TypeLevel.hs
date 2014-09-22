d815178a06c774f0ca9faa6be50a905baef24c21
Wojciech Danilo <wojciech.danilo@gmail.com>
2014-09-22 05:06:33 +0200

    Overall progress, core 4.5 + parser

--- libs/Data/TypeLevel.hs
+++
@@ -1,6 +0,0 @@
-module Data.TypeLevel (module X) where
-
-import Data.TypeLevel.Bool          as X
-import Data.TypeLevel.Maybe         as X
-import Data.TypeLevel.Set           as X
-import Data.TypeLevel.Set.Constrain as X
