254148f0233692ca6b9bf1cf8df55d06fc6edc16
Wojciech Danilo <wojciech.danilo@gmail.com>
2014-09-22 05:06:33 +0200

    Overall progress, core 4.5 + parser

---
+++ src/Control/Category/Dot/TH.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Control.Category.Dot.TH where
+
+import Language.Haskell.TH
+import Prelude
+
+mkDots :: Int -> Int -> Q [Dec]
+mkDots min max = do
+    let range      = [min..max]
+        fnames     = fmap (mkName . ("dot" ++) . show) range
+        fnamesPrev = fmap (mkName . ("dot" ++) . show . (flip (-) 1)) range
+        dot1       = mkName "dot1"
+        names      = zip fnames fnamesPrev
+
+    return $ map (\(name, pname) -> ValD (VarP name) (NormalB (AppE (AppE (VarE dot1) (VarE pname)) (VarE dot1))) []) names
+
+
