bf45091f09b3250ea4a9cb36ce308ebe15028dc5
Konrad Gądek <kgadek@gmail.com>
2014-10-13 17:02:40 +0200

    TDD 001: down into the rabbit hole

--- test/Test/Luna/TypecheckerSpec.hs
+++ test/Test/Luna/TypecheckerSpec.hs
@@ -1,22 +1,28 @@
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
+
 module Test.Luna.TypecheckerSpec (spec) where
 
 
 import Luna.Typechecker.AST
+import Luna.Typechecker.Type
+
+import Logger
 
 import Test.Hspec
 
 
+
+type TCLoggerT = LoggerT String
+
+class Typechecker a b | a -> b where
+  typechecker :: (Monad m) => String -> a -> TCLoggerT m b
+
+
+
 spec :: Spec
-spec = do
-  
-  describe "trivia" $
-
-    describe "compilation" $
-      it "works" True
-    -- compilation
-
-  -- trivia
-
-  describe "basic AST" $
-    it "exists" True
-  -- basic AST
+spec =
+  describe "basic typechecking" $
+    it "typechecks literals" $
+      True
+      --typechecker "Main.luna" (LitInt 10)        `shouldBe` Tycon (TyID "Int")
+      --typechecker "Main.luna" (ELit (LitInt 10)) `shouldBe` Tycon (TyID "Int")
