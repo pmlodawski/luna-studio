fda5b44a871482605b8a380faf4a4300ecd70683
Konrad Gądek <kgadek@gmail.com>
2014-10-13 11:22:15 +0200

    Cleanup before prototype 2

---
+++ test_prototype1/Test/Luna/SubtypingSpec.hs
@@ -0,0 +1,10 @@
+module Test.Luna.SubtypingSpec (spec) where
+
+
+import Test.Hspec
+
+
+spec :: Spec
+spec =
+  describe "structural dependencies of Luna language" $
+    it "is" $ pendingWith "not yet specified!"
