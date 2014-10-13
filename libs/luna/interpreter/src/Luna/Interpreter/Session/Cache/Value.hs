7ea7ad8bbbcac56b923f09b7441485c50b3dad2b
Piotr Młodawski <remdezx@gmail.com>
2014-10-13 13:15:10 +0200

    Bugfixes in interpreter; added more instances for serialization

--- libs/luna/interpreter/src/Luna/Interpreter/Session/Cache/Value.hs
+++ libs/luna/interpreter/src/Luna/Interpreter/Session/Cache/Value.hs
@@ -81,15 +81,16 @@ report callPointPath varName = do
 
 get :: VarName -> Session (Maybe Value)
 get varName = do
-    let expr = "toValue " ++ varName
+    let expr = "toValue $ compute " ++ varName
         excHandler exc = do
             logger warning $ show exc
             return Nothing
     Session.withImports [ "Flowbox.Data.Serialization"
+                        , "Flowbox.Graphics.Serialization"
                         , "Prelude"
                         , "Generated.Proto.Data.Value" ] 
                         $ lift2 $ GHC.handleSourceError excHandler $ do
-        let computeExpr =  concat [varName, " = compute ", varName]
-        _      <- GHC.runDecls computeExpr
+        --let computeExpr =  concat [varName, " = compute ", varName]
+        --_      <- GHC.runDecls computeExpr
         action <- HEval.interpret expr
         liftIO action
