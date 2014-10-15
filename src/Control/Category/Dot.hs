66e84e560c5db55eed3af6365b575b4d46c5ee36
Piotr Młodawski <remdezx@gmail.com>
2014-10-15 14:33:06 +0200

    BATCH-70 (Implement tests for sample programs in lunac)

--- src/Control/Category/Dot.hs
+++ src/Control/Category/Dot.hs
@@ -1,3 +1,9 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2014
+---------------------------------------------------------------------------
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE TemplateHaskell           #-}
 
@@ -5,6 +11,8 @@ module Control.Category.Dot where
 
 import Control.Category.Dot.TH
 import Prelude
+
+
 
 dot0  = ($)
 dot1  = (.)
