1f5ad29b8893fdeba9bb7d4f4222c90249bede76
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-10-20 13:56:58 +0000

    Changes in merge test

--- libs/data/graphics/test/Merge.hs
+++ libs/data/graphics/test/Merge.hs
@@ -18,28 +18,22 @@ import Flowbox.Graphics.Composition.Generators.Structures
 import Flowbox.Graphics.Image.Merge
 
 import qualified Data.Array.Accelerate as A
-
-#ifdef ACCELERATE_CUDA_BACKEND
-import Data.Array.Accelerate.CUDA        (run)
-#else
-import Data.Array.Accelerate.Interpreter (run)
-#endif
-
-import Data.Foldable
-import Math.Space.Space
+import           Data.Foldable
+import           Math.Space.Space
+import           System.IO             (hSetBuffering, stdout, BufferMode(..))
 
 import Utils
 
 merge file mode alphaBlending = do
-    (r1, g1, b1, a1) <- map4 (nearest . fromMatrix A.Wrap) <$> testLoadRGBA' "lena_premult.png"
-    (r2, g2, b2, a2) <- map4 (nearest . fromMatrix A.Wrap) <$> testLoadRGBA' "checker_premult.png"
-    let (r, g, b, a) = map4 (rasterizer . transform (fmap A.fromIntegral)) $ threeWayMerge' alphaBlending mode r1 g1 b1 r2 g2 b2 a1 a2
+    (r1, g1, b1, a1) <- map4 (nearest . fromMatrix A.Wrap) <$> testLoadRGBA' "samples/lena_alpha.png"
+    (r2, g2, b2, a2) <- map4 (nearest . fromMatrix A.Wrap) <$> testLoadRGBA' "samples/checker.png"
+    let (r, g, b, a) = map4 (rasterizer . monosampler) $ threeWayMerge' alphaBlending mode r1 g1 b1 r2 g2 b2 a1 a2
     testSaveRGBA'' file r g b a
 
 merge' file mode = do
-    (r1, g1, b1, a1) <- map4 (nearest . fromMatrix A.Wrap) <$> testLoadRGBA' "lena_premult.png"
-    (r2, g2, b2, a2) <- map4 (nearest . fromMatrix A.Wrap) <$> testLoadRGBA' "checker_premult.png"
-    let (r, g, b, a) = map4 (rasterizer . transform (fmap A.fromIntegral)) $ threeWayMerge mode r1 g1 b1 r2 g2 b2 a1 a2
+    (r1, g1, b1, a1) <- map4 (nearest . fromMatrix A.Wrap) <$> testLoadRGBA' "samples/lena_alpha.png"
+    (r2, g2, b2, a2) <- map4 (nearest . fromMatrix A.Wrap) <$> testLoadRGBA' "samples/checker.png"
+    let (r, g, b, a) = map4 (rasterizer . monosampler) $ threeWayMerge mode r1 g1 b1 r2 g2 b2 a1 a2
     testSaveRGBA'' file r g b a
 
 simpleMerges :: [BlendMode Double]
@@ -127,22 +121,23 @@ advancedMergesNames = [
 
 main :: IO ()
 main = do
+    hSetBuffering stdout NoBuffering
     putStrLn "Merge test"
-
-    -- FIXME[mm]: bang patterns here seemed to trigger some weird behaviour in Accelerate with GHC 7.8.2
-    -- in 7.8.3 it is probably fixed, but I'll leave them for future
 
     putStrLn "Simple merges with Adobe"
     forM_ (zip simpleMergesNames simpleMerges) $ \(m, n) -> do
-        !a <- return $ run $ A.generate (A.index2 (512::A.Exp Int) 512) (\_ -> 1.0 :: A.Exp Float)
-        merge (m ++ ".png") n Adobe
+        putStr $ "Merge " ++ m ++ "..."
+        merge ("samples/merge/" ++ m ++ ".png") n Adobe
+        putStrLn " DONE"
 
     putStrLn "Simple merges with Custom"
     forM_ (zip simpleMergesNames simpleMerges) $ \(m, n) -> do
-        !a <- return $ run $ A.generate (A.index2 (512::A.Exp Int) 512) (\_ -> 1.0 :: A.Exp Float)
-        merge (m ++ "_alphablend.png") n Custom
+        putStr $ "Merge " ++ m ++ "..."
+        merge ("samples/merge/" ++ m ++ "_alphablend.png") n Custom
+        putStrLn " DONE"
 
     putStrLn "Complicated merges"
     forM_ (zip advancedMergesNames advancedMerges) $ \(m, n) -> do
-        !a <- return $ run $ A.generate (A.index2 (512::A.Exp Int) 512) (\_ -> 1.0 :: A.Exp Float)
-        merge' (m ++ ".png") n
+        putStr $ "Merge " ++ m ++ "..."
+        merge' ("samples/merge/" ++ m ++ ".png") n
+        putStrLn " DONE"
