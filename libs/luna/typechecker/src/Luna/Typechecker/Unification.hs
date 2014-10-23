6471799dcfee0c3ee7beaeb2ab557d4a7bb74b6d
lachu <l.lachowski@gmail.com>
2014-10-23 15:37:00 +0200

    - new data type required by row-polymorphism - new version of the unification algorithm, changed for row-poly

--- libs/luna/typechecker/src/Luna/Typechecker/Unification.hs
+++ libs/luna/typechecker/src/Luna/Typechecker/Unification.hs
@@ -18,10 +18,41 @@ mgu (TAp t1 t2) (TAp s1 s2) = do ts1 <- mgu t1 s1
 mgu (TConst a) (TConst b) | a == b = return mempty
 mgu (TConst _) (TConst _) = err "cantunify" "blahblah"
 mgu (TVar u)   t          = varBind u t
-mgu t (TVar u)            = varBind u t
+mgu t var@(TVar u)            = mgu var t
+mgu (TRow (Row { fName, fType, rowTail1 })) (TRow (row2@Row {})) = do
+    (fType2, rowTail2, rowSub) <- matchRow row2 fName
+    if occursCheck (getVariable rowTail1) rowSub then err "row occur-check"
+    else do
+      fieldSub <- unify (apply rowSub fType) (apply rowSub fType2)
+      let sub = rowSub `mappend` fieldSub
+      subRest <- unify (apply sub rowTail1) (apply sub rowTail2)
+      return $ subRest `mappend` sub
+
 mgu t1 t2 = err "mgunotimplemented" ("mgu " ++ show t1 ++ " <> " ++ show t2)
+
+-- | unnecessary when proper unification algorithm will be used
+occursCheck a b = false 
 
 varBind :: (Monad m) => Tyvar -> Type -> LoggerT String m Subst
 varBind u t | t == TVar u        = return mempty
             | u `elem` ftv t     = err "infinitetype" "blehbleh"
             | otherwise          = return (Subst [(u, t)])
+
+matchRow EmptyRow label = err $ "label " ++ label ++ " connot be inserted"
+matchRow (Row fLabel fType row ) label 
+    | fLabel == label = return (fType, row, mempty)
+    | TVar alpha <- row = do
+          newRowVar <- newTyVarWith (lacks label) 'r'
+          tyVar <- newTyVar 'a'
+          sub <- varBindRow alpha $ Row label tyVar newRowVar
+          return (tyVar, apply sub $ Row fLabel fType newRowVar, sub)
+    | otherwise = do
+          (fieldTy, rowTail, sub) = matchRow row label
+          return (fieldTy, Row label fieldTy rowTail, sub)
+
+varBindRow var newTy = if not $ checkConstraints var newTy
+                         then err "repeated labels in constraints"
+                         else mergeRowTypes var newTy
+
+mergeRowTypes var newTy = do
+    d
