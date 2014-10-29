be3ef19a15110ed5b361cf3546b466741da1a17f
Wojciech Danilo <wojciech.danilo@gmail.com>
2014-10-29 17:32:39 +0100

    WIP.

--- src/Luna/ASTNew/Traversals/Default.hs
+++ src/Luna/ASTNew/Traversals/Default.hs
@@ -11,7 +11,14 @@ module Luna.ASTNew.Traversals.Default where
 
 import           Flowbox.Prelude        hiding (Cons, Traversal, traverse)
 import           Luna.ASTNew.Traversals.Class
+import           Luna.ASTNew.Label      (Label(Label))
 
+
+----- Basic -----
 instance DefaultTraversal base m a => Traversal base m a where traverse = defaultTraverse
-instance DefaultTraversal base m a where defaultTraverse _ = pure
+--instance DefaultTraversal base m a where defaultTraverse _ = pure
 
+
+----- Label -----
+instance Traversal base m a => Traversal base m (Label l a) where
+    traverse base (Label l a) = fmap (Label l) $ traverse base a
