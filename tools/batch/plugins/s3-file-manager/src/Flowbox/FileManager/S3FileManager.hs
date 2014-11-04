3b0dff518eb4021667ab27fe146b38b303676189
Piotr Mlodawski <remdezx@gmail.com>
2014-11-04 12:01:46 +0100

    Resolved BATCH-105 (Function for checking file status (path -> filesystem.Item)); BATCH-103 (Add created/modified timestamps to generated.proto.filesystem.Item); BATCH-104 (Function lists directory should return generated.proto.filesystem.Item for each entry)

--- tools/batch/plugins/s3-file-manager/src/Flowbox/FileManager/S3FileManager.hs
+++ tools/batch/plugins/s3-file-manager/src/Flowbox/FileManager/S3FileManager.hs
@@ -14,8 +14,8 @@ import qualified Flowbox.AWS.S3.File             as File
 import qualified Flowbox.AWS.S3.S3               as S3
 import qualified Flowbox.FileManager.Context     as Context
 import           Flowbox.FileManager.FileManager
+import           Flowbox.FileManager.Item        (toGen)
 import           Flowbox.Prelude                 hiding (Context)
-
 
 
 data S3FileManager = S3FileManager
@@ -27,7 +27,8 @@ instance FileManager S3FileManager S3.S3Env where
     fetchDirectory  _ = Context.run . Directory.fetch  "."
     createDirectory _ = Context.run . Directory.create
     directoryExists _ = Context.run . Directory.exists
-    listDirectory   _ = Context.run . Directory.getContents
+    listDirectory   _ = fmap (map toGen)
+                      . Context.run . Directory.getContents
     removeDirectory _ = Context.run . Directory.remove
     copyDirectory   _ = Context.run .: Directory.copy
     moveDirectory   _ = Context.run .: Directory.copy
@@ -40,4 +41,6 @@ instance FileManager S3FileManager S3.S3Env where
     moveFile        _ = Context.run .: File.rename
 
     resolvePath     _ = return
+    stat            _ = return . toGen -- TODO [PM] : implement me
+
 
