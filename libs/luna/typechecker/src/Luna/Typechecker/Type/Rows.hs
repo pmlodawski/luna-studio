53f5da18da033261c505ae56e774f687ecd4ce9e
lachu <l.lachowski@gmail.com>
2014-11-06 09:38:40 +0100

    just a small refactoring

--- libs/luna/typechecker/src/Luna/Typechecker/Type/Rows.hs
+++ libs/luna/typechecker/src/Luna/Typechecker/Type/Rows.hs
@@ -15,3 +15,10 @@ instance Types Tyrow where
     ftv (Row { _, fType, record }) = ftv fType `union` ftv record
     ftv RowEmpty = []
     apply s row = row { fType = (apply s (fType row)), record = (apply s (row row)) }
+
+getRowVariable tTy = let (_, value) = typeToTypeList tTy in value
+
+typeToTypeList (TVar v) = ([], Just v)
+typeToTypeList EmptyRow = ([], Nothing)
+typeToTypeList (Row l t r) = ((l, t):ls, mv) where
+  (ls, mv) = typeToTypeList r
