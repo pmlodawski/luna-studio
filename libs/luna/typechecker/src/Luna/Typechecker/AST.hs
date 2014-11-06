b66e11d6de7c52bca4a105edeaea955e0b8d7305
Konrad Gądek <kgadek@gmail.com>
2014-11-06 15:39:41 +0100

    Cleanup, removed old files. Could not compile

--- libs/luna/typechecker/src/Luna/Typechecker/AST.hs
+++
@@ -1,8 +0,0 @@
-module Luna.Typechecker.AST (
-    Expr(..), Lit(..),
-    TyID(..), VarID(..)
-  ) where
-
-import Luna.Typechecker.IDs      (TyID(..),VarID(..))
-import Luna.Typechecker.AST.Expr (Expr(..))
-import Luna.Typechecker.AST.Lit  (Lit(..))
--- libs/luna/typechecker/src_prototype1/Luna/Typechecker/AST/ClassID.hs
+++
@@ -1,6 +0,0 @@
-module Luna.Typechecker.AST.ClassID (
-    ClassID(..)
-  ) where
-
-
-import Luna.Typechecker.AST.Internal.ClassID
