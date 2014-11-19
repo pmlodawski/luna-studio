575e9b42bc840aa3a3adcbc7d9c3820fbdce9d19
Piotr Mlodawski <remdezx@gmail.com>
2014-11-19 15:36:51 +0100

    Resolved BATCH-121 (Use valid flowbox local path directory instead of ~/.flowbox (see description))

--- src/Flowbox/Initializer/Initializer.hs
+++ src/Flowbox/Initializer/Initializer.hs
@@ -17,6 +17,7 @@ import           Flowbox.Config.Config              (Config)
 import qualified Flowbox.Config.Config              as Config
 import           Flowbox.Prelude                    hiding (error)
 import qualified Flowbox.System.Directory.Directory as Directory
+import           Flowbox.System.FilePath            (expand')
 import           Flowbox.System.Log.Logger
 import qualified Flowbox.System.Process             as Process
 import           Flowbox.System.UniPath             (UniPath)
@@ -28,20 +29,20 @@ logger :: LoggerIO
 logger = getLoggerIO $(moduleName)
 
 
-successfullInstallFilePath :: Config -> UniPath
-successfullInstallFilePath config = UniPath.append "installed" localPath where
+successfullInstallFilePath :: Config -> IO UniPath
+successfullInstallFilePath config = UniPath.expand $ UniPath.append "installed" localPath where
     localPath = UniPath.fromUnixString $ Config.path $ Config.local config
 
 
 isAlreadyInitilized :: Config -> IO Bool
 isAlreadyInitilized config = do
-    let local      = Config.local  config
-        localCabal = Config.cabal local
-        localPkgDb = Config.pkgDb local
+    let local  = Config.local  config
+    localCabal <- expand' $ Config.cabal local
+    localPkgDb <- expand' $ Config.pkgDb local
     logger debug "Checking for Flowbox configuration."
     exists_localCabal <- Directory.doesDirectoryExist $ UniPath.fromUnixString localCabal
     exists_localPkgDb <- Directory.doesDirectoryExist $ UniPath.fromUnixString localPkgDb
-    exists_installed  <- Directory.doesFileExist $ successfullInstallFilePath config
+    exists_installed  <- Directory.doesFileExist =<< successfullInstallFilePath config
     let exists = exists_localCabal && exists_localPkgDb && exists_installed
     if exists
         then logger debug "Configuration already exists."
@@ -58,31 +59,32 @@ initializeIfNeeded config = do
 initialize :: Config -> IO ()
 initialize config = do
     logger info "Configuring Flowbox for the first use. Please wait..."
-    let root       = Config.root config
-        local      = Config.local  config
-        localCabal = Config.cabal local
-        localPkgDb = Config.pkgDb local
-        ghcPkgBin  = Config.ghcPkg . Config.wrappers  $ config
-        cabalConfT = Config.cabal  . Config.templates $ config
-        cabalConf  = Config.cabal  . Config.config    $ config
-        cabalBin   = Config.cabal  . Config.wrappers  $ config
-
+    let root    = Config.root config
+        local   = Config.local  config
+    localCabal  <- expand' $ Config.cabal local
+    localPkgDb  <- expand' $ Config.pkgDb local
+    ghcPkgBin   <- expand' $ Config.ghcPkg $ Config.wrappers  $ config
+    cabalConfT  <- expand' $ Config.cabal  $ Config.templates $ config
+    cabalConf   <- expand' $ Config.cabal  $ Config.config    $ config
+    cabalBin    <- expand' $ Config.cabal  $ Config.wrappers  $ config
+    fbInstall   <- expand' $ Config.path root
+    fbHomeCabal <- expand' $ Config.cabal local
     Directory.createDirectoryIfMissing True $ UniPath.fromUnixString localCabal
     Directory.createDirectoryIfMissing True $ UniPath.fromUnixString localPkgDb
     Process.runProcess Nothing ghcPkgBin ["recache", "--package-db=" ++ localPkgDb]
 
     cabalConfTContent <- IO.readFile cabalConfT
-    let cabalConfContent = StringUtils.replace "${FB_INSTALL}"    (Config.path root)
-                         $ StringUtils.replace "${FB_HOME_CABAL}" (Config.cabal local) cabalConfTContent
+    let cabalConfContent = StringUtils.replace "${FB_INSTALL}"    fbInstall
+                         $ StringUtils.replace "${FB_HOME_CABAL}" fbHomeCabal cabalConfTContent
     IO.writeFile cabalConf cabalConfContent
     Process.runProcess Nothing cabalBin ["update"]
-    Directory.touchFile $ successfullInstallFilePath config
+    Directory.touchFile =<< successfullInstallFilePath config
     logger info "Flowbox configured successfully."
 
 
 clear :: Config -> IO ()
 clear config = do
     logger info "Cleaning Flowbox configuration."
-    let localPath = UniPath.fromUnixString $ Config.path $ Config.local config
+    localPath <- UniPath.expand $ UniPath.fromUnixString $ Config.path $ Config.local config
     whenM (Directory.doesDirectoryExist localPath) $
         Directory.removeDirectoryRecursive localPath
