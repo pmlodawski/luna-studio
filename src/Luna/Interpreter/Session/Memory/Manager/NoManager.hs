11cbb1c30ef6673f82d1a83fd1d2927dc5483059
Piotr Mlodawski <remdezx@gmail.com>
2014-11-24 10:36:09 +0100

    Resolved BATCH-118 - Implement LRU cache cleaning alghoritm

--- src/Luna/Interpreter/Session/Memory/Manager/NoManager.hs
+++ src/Luna/Interpreter/Session/Memory/Manager/NoManager.hs
@@ -28,8 +28,9 @@ instance Default NoManager where
 
 
 instance MemoryManager NoManager where
-    reportUse _ _ = return ()
-    clean status  = do
+    reportUse    _ _ = return ()
+    reportDelete _ _ = return ()
+    clean status = do
         logger warning "Cleaning memory - not implemented"
         logger warning $ show status
 
