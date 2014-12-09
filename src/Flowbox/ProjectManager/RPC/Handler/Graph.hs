e9a8149dc1a7e482471d8672efdc1584c4971faa
Piotr Mlodawski <remdezx@gmail.com>
2014-12-09 16:59:02 +0100

    Code improvements

--- src/Flowbox/ProjectManager/RPC/Handler/Graph.hs
+++ src/Flowbox/ProjectManager/RPC/Handler/Graph.hs
@@ -25,8 +25,8 @@ import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Gra
 import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Get.Status                 as GetGraph
 import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Lookup.Request             as Lookup
 import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Lookup.Status              as Lookup
-import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.LookupMany.Request             as LookupMany
-import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.LookupMany.Status              as LookupMany
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.LookupMany.Request         as LookupMany
+import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.LookupMany.Status          as LookupMany
 import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Node.Add.Request           as NodeAdd
 import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Node.Add.Update            as NodeAdd
 import qualified Generated.Proto.ProjectManager.Project.Library.AST.Function.Graph.Node.Modify.Request        as NodeModify
@@ -67,7 +67,7 @@ lookup request@(Lookup.Request tnodeID tbc tlibID tprojectID _) = do
 lookupMany :: LookupMany.Request -> RPC Context IO LookupMany.Status
 lookupMany request@(LookupMany.Request tnodeIDs tbc tlibID tprojectID _) = do
     bc <- decodeE tbc
-    let nodeIDs   = decodeListP tnodeIDs
+    let nodeIDs   = decodeP tnodeIDs
         libID     = decodeP tlibID
         projectID = decodeP tprojectID
     nodes  <- BatchG.nodesByIDs nodeIDs bc libID projectID
@@ -76,7 +76,7 @@ lookupMany request@(LookupMany.Request tnodeIDs tbc tlibID tprojectID _) = do
         items    = map select nodes
         found    = Either.rights items
         notFound = Either.lefts items
-    return $ LookupMany.Status request (encodeList found) (encodeListP notFound)
+    return $ LookupMany.Status request (encode found) (encodeP notFound)
 
 
 nodeAdd :: NodeAdd.Request -> RPC Context IO NodeAdd.Update
@@ -115,7 +115,7 @@ nodeModifyInPlace request@(NodeModifyInPlace.Request tnode tbc tlibID tprojectID
 nodeRemove :: NodeRemove.Request -> RPC Context IO NodeRemove.Update
 nodeRemove request@(NodeRemove.Request tnodeIDs tbc tlibID tprojectID _) = do
     bc <- decodeE tbc
-    let nodeIDs   = decodeListP tnodeIDs
+    let nodeIDs   = decodeP tnodeIDs
         libID     = decodeP tlibID
         projectID = decodeP tprojectID
     BatchG.removeNodes nodeIDs bc libID projectID
@@ -127,9 +127,9 @@ connect :: Connect.Request -> RPC Context IO Connect.Update
 connect request@(Connect.Request tsrcNodeID tsrcPort tdstNodeID tdstPort tbc tlibID tprojectID _) = do
     bc <- decodeE tbc
     let srcNodeID = decodeP tsrcNodeID
-        srcPort   = decodeListP tsrcPort
+        srcPort   = decodeP tsrcPort
         dstNodeID = decodeP tdstNodeID
-        dstPort   = decodeListP tdstPort
+        dstPort   = decodeP tdstPort
         libID     = decodeP tlibID
         projectID = decodeP tprojectID
     BatchG.connect srcNodeID srcPort dstNodeID dstPort bc libID projectID
@@ -141,9 +141,9 @@ disconnect :: Disconnect.Request -> RPC Context IO Disconnect.Update
 disconnect request@(Disconnect.Request tsrcNodeID tsrcPort tdstNodeID tdstPort tbc tlibID tprojectID _) = do
     bc <- decodeE tbc
     let srcNodeID = decodeP tsrcNodeID
-        srcPort   = decodeListP tsrcPort
+        srcPort   = decodeP tsrcPort
         dstNodeID = decodeP tdstNodeID
-        dstPort   = decodeListP tdstPort
+        dstPort   = decodeP tdstPort
         libID     = decodeP tlibID
         projectID = decodeP tprojectID
     BatchG.disconnect srcNodeID srcPort dstNodeID dstPort bc libID projectID
