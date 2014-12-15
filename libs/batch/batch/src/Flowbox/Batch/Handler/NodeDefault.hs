9c1aefdca40695df32b71f821c535f455e4513ba
Piotr Mlodawski <remdezx@gmail.com>
2014-12-15 16:18:45 +0100

    Resolved BATCH-166 - Interpreter should not send value.update packages for nodes generated from default defined by AST

--- libs/batch/batch/src/Flowbox/Batch/Handler/NodeDefault.hs
+++ libs/batch/batch/src/Flowbox/Batch/Handler/NodeDefault.hs
@@ -8,18 +8,21 @@
 
 module Flowbox.Batch.Handler.NodeDefault where
 
-import           Flowbox.Batch.Batch                 (Batch)
-import qualified Flowbox.Batch.Handler.Common        as Batch
-import qualified Flowbox.Batch.Project.Project       as Project
+import           Flowbox.Batch.Batch                     (Batch)
+import qualified Flowbox.Batch.Handler.Common            as Batch
+import qualified Flowbox.Batch.Project.Project           as Project
+import           Flowbox.Control.Error
 import           Flowbox.Prelude
-import           Luna.AST.Control.Crumb              (Breadcrumbs)
-import qualified Luna.Graph.Node                     as Node
-import           Luna.Graph.Node.Expr                (NodeExpr)
-import qualified Luna.Graph.PropertyMap              as PropertyMap
-import           Luna.Graph.View.Default.DefaultsMap (DefaultsMap)
-import qualified Luna.Graph.View.Default.DefaultsMap as DefaultsMap
-import           Luna.Graph.View.PortDescriptor      (PortDescriptor)
-import qualified Luna.Lib.Lib                        as Library
+import           Luna.AST.Control.Crumb                  (Breadcrumbs)
+import qualified Luna.Graph.Node                         as Node
+import           Luna.Graph.Node.Expr                    (NodeExpr)
+import qualified Luna.Graph.PropertyMap                  as PropertyMap
+import           Luna.Graph.View.Default.DefaultsMap     (DefaultsMap)
+import qualified Luna.Graph.View.Default.DefaultsMap     as DefaultsMap
+import           Luna.Graph.View.PortDescriptor          (PortDescriptor)
+import qualified Luna.Lib.Lib                            as Library
+import qualified Luna.Pass.Transform.AST.IDFixer.IDFixer as IDFixer
+import qualified Luna.Graph.Flags as Flags
 
 
 
@@ -32,8 +35,10 @@ setNodeDefault :: PortDescriptor -> NodeExpr
                -> Node.ID -> Breadcrumbs -> Library.ID -> Project.ID -> Batch Node.ID
 setNodeDefault dstPort value nodeID bc libID projectID = do
     propertyMap <- Batch.getPropertyMap libID projectID
-    maxID <- Batch.getMaxID libID projectID
-    let newPM = PropertyMap.modifyDefaultsMap (DefaultsMap.insert dstPort (maxID, value)) nodeID propertyMap
+    maxID       <- Batch.getMaxID libID projectID
+    (fixedValue, newMaxID) <- EitherT $ IDFixer.runNodeExpr' maxID Nothing True value
+    let newPM1 = PropertyMap.modifyDefaultsMap (DefaultsMap.insert dstPort (maxID, fixedValue)) nodeID propertyMap
+        newPM  = foldr (PropertyMap.modifyFlags (Flags.defaultNodeGenerated .~ Just True)) newPM1 [maxID..newMaxID-1]
     Batch.setPropertyMap newPM libID projectID
     --TODO[PM] : Temporary fix
     Batch.graphViewOp bc libID projectID $ \gv pm -> return ((gv, pm), ())
