e2874e895c835aa8fdda1a2d977d48d51350e2d2
Wojciech Danilo <wojciech.danilo@gmail.com>
2014-12-23 12:48:14 +0100

    Fully working basic compiler with new AST.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,3 @@
--- extensions --
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
@@ -20,27 +19,23 @@ import Luna.Target.HS
 
 -- body --
 
-
--- ===================================================================
--- Data types
--- ===================================================================
-
--- ------ Main.Main constructor ------ --
+-- ====== Main type ====== --
 data Main  = Main deriving (Show, Eq, Ord, Generic)
 data Cls_Main  = Cls_Main deriving (Show, Eq, Ord, Generic)
+
+-- ------ Main.Main constructor ------ --
 cons_Main = member (Proxy :: Proxy "Main") (val Cls_Main)
 memSig_Cls_Main_Main = ((mkArg :: NParam "self"), ())
 memDef_Cls_Main_Main = liftCons0 Main
 $(registerMethod ''Cls_Main "Main")
 $(generateFieldAccessors 'Main [])
--- Other data types
 
 -- ------ Main methods ------ --
 
 -- ====== Method: Main.print ====== --
-memDef_Main_print (_v_43, (_v_4, ())) = do {
-     val ();
-     polyJoin . liftF1 (Value . fmap Safe . print) $  _v_4;
+memDef_Main_print (_self, (_s, ())) = do {
+     
+    polyJoin . liftF1 (Value . fmap Safe . print) $ _s;
      
 }
 memSig_Main_print = ((mkArg :: NParam "self"), ((mkArg :: Param), ()))
@@ -48,11 +43,12 @@ $(registerMethod ''Main "print")
 
 -- ====== Method: Main.main ====== --
 memDef_Main_main (_self, ()) = do {
-     call (appNext ((val (1 :: Int))) (member (Proxy :: Proxy "print") _self));
+     call (appNext (val (5 :: Int)) (member (Proxy :: Proxy "print") _self));
      
 }
 memSig_Main_main = ((mkArg :: NParam "self"), ())
 $(registerMethod ''Main "main")
+
 
 -- ===================================================================
 -- Main module wrappers
