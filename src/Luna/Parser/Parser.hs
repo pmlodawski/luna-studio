33cb9ee6aebd078d83f0970a8f3258e101f7ddcb
Wojciech Danilo <wojciech.danilo@gmail.com>
2014-12-27 20:14:58 +0100

    Parser refactoring.

--- src/Luna/Parser/Parser.hs
+++ src/Luna/Parser/Parser.hs
@@ -121,13 +121,15 @@ import qualified Luna.Syntax.Foreign as Foreign
 import Luna.Parser.Type
 import Luna.Parser.Pattern
 import Luna.Parser.Literal
+import Luna.Parser.Struct
+import Luna.Parser.Term
 
 import Luna.Parser.Builder (labeled, label, nextID, qualifiedPath, withLabeled)
 
 
 mtry p = try p <|> pure mempty
 
-vName = Name.V <$> varOp
+vName = Name.V <$> Tok.varOp
 tName = Name.T <$> Tok.typeIdent
 
 anyName = vName <|> tName
@@ -161,7 +163,6 @@ argList'      p = braces (sepBy2 p Tok.separator) <|> ((:[]) <$> p) <?> "argumen
 list          p = Tok.brackets (sepBy p Tok.separator)
 anyIdent        = choice [ Tok.varIdent, Tok.typeIdent ]
 
-varOp           = Tok.varIdent <|> Tok.operator
 
 
 
@@ -169,13 +170,8 @@ varOp           = Tok.varIdent <|> Tok.operator
 
 
 
-binary   name fun assoc = Infix   (Tok.reservedOp name *> return fun) assoc
-binaryM  name fun assoc = Infix   (Tok.reservedOp name *>        fun) assoc
-prefix   name fun       = Prefix  (Tok.reservedOp name *> return fun)
-prefixM  name fun       = Prefix  (Tok.reservedOp name *>        fun)
-prefixfM      fun       = Prefix  (fun)
-postfix  name fun       = Postfix (Tok.reservedOp name *> return fun)
-postfixM name fun       = Postfix (Tok.reservedOp name *>        fun)
+
+
 
 
 element m = do
@@ -238,7 +234,7 @@ imp = Decl.Imp <$  Tok.kwImport
                <*> (blockBegin importTarget <|> pure [])
                <?> "import declaration"
 
-importTarget =   body Decl.ImpVar varOp 
+importTarget =   body Decl.ImpVar Tok.varOp 
              <|> body Decl.ImpType Tok.typeIdent
              where body c p = c <$> p <*> ((Just <$ Tok.kwAs <*> p) <|> pure Nothing)
 
@@ -364,353 +360,13 @@ stage1Body2 = ((:) <$> (try ((<>) <$> Tok.spaces <* Indent.checkIndented <*> sta
 ----------------------------------------------------------------------
 
 -- FIXME [wd]: last parsed char is poorly written with '_' workaround when no char is available
-prevParsedChar = do
-    Caret delta bs <- careting
-    let idx = max 0 . fromIntegral $ column delta - 1
-        txt = UTF8.toString bs
-    return $ if idx > length txt then '_' else txt !! idx
 
-lastLexemeEmpty = do
-    prevChar <- prevParsedChar
-    when (isSpace prevChar) $ fail "not empty"
 
 
 -----------------------------------------------------------
 -- Expressions
 -----------------------------------------------------------
-expr       = tlExpr entBaseE
 
---FIXME[wd]: exprSimple is broken - it includes func calls. Using pEntBaseSimpleE for now
---exprSimple = tlExpr pEntBaseSimpleE
-
-
--- === Top Level pattern, variable, record updates chains === --
-
-tlRecUpd     = assignSeg $ (\vop accs expr -> Expr.RecUpd vop [Expr.FieldUpd accs expr]) <$> varOp <*> many1 recAcc
-tlExprPat    = assignSeg $ Expr.Assignment <$> pattern
-tlExprPatVar = assignSeg $ Expr.Assignment <$> varP
-
-assignSeg p = p <* Tok.assignment
-
-tlExprExtHead =   try tlExprPat
-              <|> tlExprBasicHead
-
-tlExprBasicHead =  try tlExprPatVar
-               <|> try tlRecUpd
-
-tlExprParser head base =   (labeled $ head <*> tlExprBasic base)
-                       <|> opTupleTE base
-
-tlExpr      = tlExprParser tlExprExtHead
-tlExprBasic = tlExprParser tlExprBasicHead
-
--- === / === --
-
-
-opE       = opTE entBaseE
-opTupleTE base = tupleE $ opTE base
-
-opTE base = buildExpressionParser optableE (appE base)
-
-tupleE p = p <??> ((\id xs x -> label id $ Expr.Tuple (x:xs)) <$> nextID <* Tok.separator <*> sepBy1 p Tok.separator)
-
---appE base = p <??> (appID (\i a s -> Expr.App i s a) <*> many1 (argE p)) where 
-appE base = p <??> ((\i a s -> label i $ callBuilder2 s a) <$> nextID <*> many1 (appArg p)) where 
-    p = termE base
-
-
-
-termE base = base <??> (flip applyAll <$> many1 (termBaseE base))  ------  many1 (try $ recUpdE))
-
-
-termBaseE p = choice [ accE
-                     , callTermE p
-                     ]
-
-recAcc  = (Tok.accessor *> varOp)
-
-accBaseE  = (Tok.accessor *> nameBase)
-
-nameBase =   (Name.VarName  <$> varOp)
-         <|> (Name.TypeName <$> Tok.conIdent)
-
-
-accE      = try( (\id a b -> label id $ Expr.Accessor a b) <$> nextID <*> accBaseE) -- needed by the syntax [1..10]
-
-
-
-
-
-
-
-parensE p = Tok.parens (p <|> (labeled (Expr.Tuple <$> pure []))) -- checks for empty tuple
-
-callList     p = Tok.parens (sepBy p Tok.separator)
-callTermE p = (\id a b-> label id (Expr.app b a)) <$ lastLexemeEmpty <*> nextID <*> callList (appArg p)
-
-
-entBaseE        = entConsE entComplexE
-pEntBaseSimpleE = entConsE entSimpleE
-
-entConsE base = choice [ try $ labeled (Expr.Grouped <$> parensE (tlExpr base))
-                       , base
-                       ]
-
-entComplexE = choice[ --labeled (Expr.Decl <$> labeled decl) -- FIXME: zrobic subparsowanie!
-                    entSimpleE
-                    ]
-             <?> "expression term"
-
-entSimpleE = choice[ caseE -- CHECK [wd]: removed try
-                   --, condE
-                   , labeled $ Expr.Grouped <$> parensE expr
-                   , identE
-                   --, try (labeled Expr.RefType <$  Tok.ref <*> Tok.conIdent) <* Tok.accessor <*> varOp
-                   , labeled $ Expr.Ref     <$  Tok.ref <*> entSimpleE
-                   , labeled $ Expr.Lit     <$> literal
-                   , labeled $ listE
-                   --, labeled $ Expr.Native  <$> nativeE
-                   ]
-           <?> "expression term"
-
-optableE = [ 
-           --, [ prefixM   "@"  (appID Expr.Ref)                                  ]
-             --[ binaryM   ""   (callBuilder <$> genID <*> genID)    AssocLeft ]
-             [ operator4 "^"                                  AssocLeft ]
-           , [ operator4 "*"                                  AssocLeft ]
-           , [ operator4 "/"                                  AssocLeft ]
-           , [ operator4 "+"                                  AssocLeft ]
-           , [ operator4 "-"                                  AssocLeft ]
-           , [ operator4 "<"                                  AssocLeft ]
-           , [ operator4 ">"                                  AssocLeft ]
-           , [ operator4 "=="                                 AssocLeft ]
-           , [ operator4 "in"                                 AssocLeft ]
-           , [ binaryM   "$"  (callBuilder <$> nextID)       AssocLeft ]
-           , [ postfixM  "::" ((\id a b -> label id (Expr.Typed a b)) <$> nextID <*> typeT) ]
-           ]
-           where
-              --operator op = binaryM op (binaryMatchE <$> (appID Expr.Infix <*> pure op))
-              --operator op = binaryM op (binaryMatchE <$> (appID Expr.Infix <*> pure op))
-              --operator4 op = binaryM op ( (\id1 id2 l r -> label id1 $ Expr.appInfix (label id2 $ Expr.Var $ NamePath.single $ op) l [r]
-              --                            ) <$> nextID <*> nextID)
-
-              --FIXME[wd]: remove fromText call after moving Tokenizer to Text
-              operator4 op = binaryM (fromText op) ( (\id1 id2 l r -> label id1 $ Expr.appInfix (label id2 $ Expr.Var $ Expr.Variable (vname $ NamePath.single op) ()) (Expr.unnamed l) [Expr.unnamed r]
-                                                   ) <$> nextID <*> nextID)
-              --operator op = binaryM op (binaryMatchE <$> (appID Expr.Infix <*> pure ('~':op)))
-              --operator2 op = binaryM op (binaryMatchE <$>  ( appID Expr.App <*> (appID Expr.Accessor <*> pure "add" <*> ... ) )  )
-              --operator2 op = binaryM op ( (\id1 id2 x y -> Expr.App id1 (Expr.Accessor id2 op x) [y]) <$> genID <*> genID)
-              --operator3 op = binaryM op ( (\id1 id2 x y -> Expr.App id1 (Expr.Accessor id2 "contains" y) [x]) <$> genID <*> genID)
-
---callBuilder id src@(Label lab expr) arg = label id $ case expr of
---    Expr.App src' (Expr.Seq args) -> Expr.App src' (Expr.Seq $ args ++ [Expr.Unnamed arg])
---    _                             -> Expr.App src (Expr.Seq $ [Expr.Unnamed arg])
-
---callBuilder2 src@(Label lab expr) argsx = case expr of
---    Expr.App src' (Expr.Seq args) -> Expr.App src' (Expr.Seq $ args ++ argsx)
---    _                             -> Expr.App src  (Expr.Seq argsx)
-
-callBuilder id src@(Label lab expr) arg = label id $ case expr of
-    Expr.App app -> Expr.App $ NamePat.appendLastSegmentArgs args app
-    _             -> Expr.app src args
-    where args = [Expr.unnamed arg]
-
-callBuilder2 src@(Label lab expr) args = case expr of
-    Expr.App app -> Expr.App $ NamePat.appendLastSegmentArgs args app
-    _             -> Expr.app src args
-
---callBuilder id id2 src arg = case arg of
---    Expr.App id' src' args -> Expr.App id' src (Arg.Named id2 "X!" src' : args)
---    _                      -> Expr.App id src [Arg.Named id2 "Y" arg]
-
---binaryM2  name fun assoc = PExpr.Infix   (L.reserved name *>        fun) assoc
-
---binaryMatchE  f p q = f   (Expr.aftermatch p) (Expr.aftermatch q)
-
---withReservedWords words p = do
---    mapStateVal $ State.addReserved words
---    ret <- p
---    mapStateVal $ State.delReserved words
---    return ret
-
-
--- OLDER:
---mkFuncParser func defparser = case name of
---    (Name base segments) -> multiparser
---    _                          -> defparser
---    where name          = Expr._fname func
---          argExpr       = argE expr
---          exprApp p a b = (:) <$> p <* a <*> b
---          segParsers    = fmap (Tok.symbol) segments
---          argParser     = foldr (exprApp argExpr) ((:[]) <$> argExpr) segParsers
---          (Name base segments) = name
---          multiparser   = withReservedWords segments $ tok (Expr.app <$> tok (pure $ Expr.var fname) <*> argParser)
---          [s1,s2] = fmap Tok.symbol segments
---          fname = if null segments then base 
---                                   else base ++ " " ++ join " " segments
-
--- NEWER:
---mkFuncParser func = State.withReserved (segNames segments) $ tok (Expr.app <$> tok (pure $ Expr.funcVar name) <*> argParser)
---mkFuncParser func = State.withReserved (segNames segments) $  labeled (Expr.App <$> labeled (pure $ Expr.Var name) <*> (Expr.Seq <$> argParser))
---    where name          = Name.fromName $ Decl._fname func
---          argExpr       = argE expr
---          exprApp a b   = (++) <$> a <*> b
---          segParsers    = fmap segParser segments
---          argParser     = foldr exprApp (pure []) segParsers
---          (NamePath base segments) = name
-
---          segParser seg = case seg of
---              NamePath.Hole    -> (:[]) <$> argExpr
---              NamePath.Token s -> []    <$  Tok.symbol s
-
---          segNames = segNames' []
---          segNames' names s = case s of
---              []   -> names
---              x:xs -> case x of
---                  NamePath.Token n -> segNames' (n:names) xs
---                  NamePath.Hole    -> segNames' names     xs
-
-
--- parse all patterns starting with the longest match and going down
--- if everything fails, try parsing again the longest one to show nice error message
-mkFuncParsers (a:as) x =   try (foldl (\a b -> try a <|> b) (mkFuncParser x a) (fmap (mkFuncParser x) as))
-                       <|> mkFuncParser x a -- nice error messages
-    
-
-appArg p = try (Expr.AppArg <$> just Tok.varIdent <* Tok.assignment <*> p) <|> (Expr.AppArg Nothing <$> p)
-
-mkFuncParser baseVar (id, mpatt) = case mpatt of
-    Nothing                                       -> baseVar
-    Just patt@(NamePat.NamePatDesc pfx base segs) -> ParserState.withReserved segNames 
-                                                   $ labeled $ Expr.App <$> pattParser
-        where NamePat.SegmentDesc baseName baseDefs = base
-              segParser (NamePat.SegmentDesc name defs) = NamePat.Segment <$> Tok.symbol name <*> defsParser defs
-              argExpr         = appArg pEntBaseSimpleE
-              segNames        = NamePat.segmentNames patt
-              pattParser      = NamePat Nothing <$> baseParser   <*> mapM segParser segs
-              baseParser      = NamePat.Segment <$> baseMultiVar <*> defsParser baseDefs
-              baseMultiVar    = labeled . pure $ Expr.Var $ Expr.Variable (vname $ NamePat.toNamePath patt) ()
-              defsParser defs = fmap takeJustArgs $ mapM argParser defs
-              takeJustArgs    = fmap fromJust . filter isJust 
-              argParser req   = if req then just  argExpr
-                                       else maybe argExpr
-
-
-notReserved p = do
-    rsv  <- view ParserState.adhocReserved <$> get
-    name <- p
-    if name `elem` rsv then fail $ fromText $ "'" <> name <> "' is a reserved word"
-                       else return name
-
-
----
-varE   = do
-    name <- try $ notReserved Tok.varIdent
-    ast  <- lookupAST name
-    case ast of
-        Just possibleDescs -> mkFuncParsers possibleDescs (labeled . pure $ Expr.Var $ Expr.Variable (vname $ NamePath.single name) ())
-        Nothing            -> withLabeled $ \id -> do
-                                  let np = NamePath.single name
-                                  path <- ParserState.getModPath
-                                  Namespace.regVarName (OriginInfo path id) np
-                                  return $ Expr.Var $ Expr.Variable (vname np) ()
-
-
-
-lookupAST name = do
-    scope      <- ParserState.getScope
-    structInfo <- ParserState.getStructInfo
-    pid        <- ParserState.getPid
-    --pragmaSet <- view (ParserState.conf . Config.pragmaSet) <$> get
-    let argPatts = view StructInfo.argPats structInfo
-
-    case Map.lookup pid scope of
-            Nothing                    -> fail "Internal parser error [1]"
-            Just (StructInfo.Scope varnames typenames) -> do
-                let possibleElems = reverse $ sortBy (compare `on` (length . fst))
-                                  $ MapForest.subElems name varnames
-                    possibleIDs   = fmap (view StructInfo.target . snd) possibleElems
-                    possiblePatts = fmap (flip Map.lookup argPatts) possibleIDs
-                    possibleDescs = zip possibleIDs possiblePatts
-
-                case possibleDescs of
-                    [] -> if (name == "self")
-                          then return Nothing
-                          else fail . fromText $ "name '" <> name <> "' is not defined" <> msgTip
-                               where scopedNames = "self" : ((fmap $ mjoin " ") $ MapForest.keys varnames)
-                                     simWords    = findSimWords name scopedNames
-                                     msgTip      = if length simWords > 0 then ", perhaps you ment one of {" <> mjoin ", " (fmap (fromString . show) simWords) <> "}"
-                                                                          else ""
-                    x  -> return $ Just x
-
-
-
-
-editCosts = EditCosts { deletionCosts      = ConstantCost 10
-                      , insertionCosts     = ConstantCost 10
-                      , substitutionCosts  = ConstantCost 10
-                      , transpositionCosts = ConstantCost 10
-                      }
-
-editCosts2 = EditCosts { deletionCosts     = ConstantCost 10
-                      , insertionCosts     = ConstantCost 1
-                      , substitutionCosts  = ConstantCost 10
-                      , transpositionCosts = ConstantCost 3
-                      }
-
-findSimWords word words = fmap snd simPairs
-    --where dist a b = levenshteinDistance editCosts (phonix a) (phonix b)
-    where dist a b = levenshteinDistance editCosts2 (toString a) (toString b)
-          simWords = fmap (dist word) words
-          simPairs = filter ((<20).fst) 
-                   $ List.sortBy (compare `on` fst) 
-                   $ zip simWords words
-
-
-    
---varE   = appID $ Expr.var <*> Tok.varIdent
-varOpE = labeled $ (Expr.Var . (flip Expr.Variable ()) . vname . NamePath.single)  <$> try (Tok.parens varOp)
-conE   = labeled $ Expr.Cons <$> Tok.conIdent
-
-identE = choice [ varE
-                , varOpE
-                , conE
-                ]
-
----
-
-
-
-listE = Expr.List <$> Tok.brackets listTypes
-
-listTypes = choice [ try $ Expr.RangeList <$> rangeList opE
-                   ,       Expr.SeqList   <$> sepBy opE Tok.separator
-                   ]
-
-rangeList p =   (Expr.Geometric <$> p <* Tok.range <*> p <*> endLimit)
-            <|> (Expr.Linear    <$> p <*> endLimit)
-            where endLimit = try (Tok.range *> just p) <|> pure Nothing
-
-caseE     = labeled (Expr.Case <$ Tok.kwCase <*> pEntBaseSimpleE <*> (blockBegin caseBodyE <|> return []))
-caseBodyE = labeled (Expr.Match <$> pattern <*> exprBlock)
-
-
---condE     = appID Expr.Cond <* Tok.kwIf <*> exprSimple <*> exprBlock <*> maybe (indBlockSpacesIE *> Tok.kwElse *> exprBlock)
-
-
-            --nativeE     = Tok.betweenNative (many nativeElemE)
-            --nativeElemE = choice [ nativeVarE
-            --                     , nativeCodeE
-            --                     ]
-            --nativeCodeE = appID Expr.NativeCode <*> ((:) <$> (noneOf "`#") <*> nativeCodeBodyE)
-            --nativeVarE  = appID Expr.NativeVar  <*  symbol "#{" <*> many (noneOf "}") <* symbolic '}'
-
-            --nativeCodeBodyE = (try(lookAhead $ string "#{")  *> pure [])
-            --              <|> (try(lookAhead $ string "```") *> pure [])
-            --              <|> ((++) <$> ((:) <$> anyChar <*> many (noneOf "`#")) <*> nativeCodeBodyE)
-
-
-exprBlock  = blockBegin expr
 
 
 ----------------------------------------------------------------------
@@ -720,40 +376,7 @@ exprBlock  = blockBegin expr
 
 --indBlockSpaces   = try (Tok.spaces <* Indent.checkIndent) <|> pure mempty
 
-moduleBlock p = braceBlockBegin p <|> indBlockBody p
-blockBegin  p = indBlockBegin   p <|> braceBlockBegin p
 
-indBlockBegin  p = Tok.indBlockBegin *> indBlock p
-indBlock       p = Tok.spaces *> Indent.indented *> Indent.withPos (indBlockBody p)
-indBlockBody   p = (:) <$> p <*> many (indBlockPrefix p)
-indBlockBody'  p = (:) <$> p <*> many (try $ indBlockPrefix p)
-indBlockBodyOpt p = ($) <$> (((:) <$> p) <|> pure id) 
-                        <*> many (indBlockPrefix p)
-indBlockPrefix p = try ((try (Tok.spaces *> Indent.checkIndent) <|> try (Tok.terminator *> Tok.spaces *> Indent.checkIndentedOrEq )) *> notFollowedBy eof) *> p
---indBlockBody   p = (:) <$> p <*> many (try (Tok.spaces *> Indent.checkIndent) *> p)
-indBlockSpacesIE = try (Tok.spaces <* Indent.checkIndentedOrEq) <|> pure mempty
-
-braceBlockBegin p = Tok.braceL *> Tok.spaces *> Indent.withDiscarded (many1 (p <* braceBlockSpaces)) <* Tok.braceR
-braceBlockSpaces  = Tok.terminator *> Tok.spaces
-
-codeBlock    p = indCodeBlock p -- <|> braceCodeBlock p
-indCodeBlock p = indCodeBlockStart *> indBlockBody p <* indBlockEnd
-
-indCodeBlockStart = Tok.indBlockBegin *> indBlockStart
-indBlockStart     = Tok.spaces *> Indent.indented *> Indent.startBlock
-indBlockEnd       = Indent.endBlock
-
-blockStart  = indCodeBlockStart
-blockEnd    = indBlockEnd
-blockBody p = indBlockBody p
-blockBody' p = indBlockBody' p
-blockBodyOpt p = indBlockBodyOpt p
-
-
-blockBeginFields p = Tok.indBlockBegin *> indBlockFields p
-indBlockFields   p = Tok.spaces *> Indent.indented *> Indent.withPos (indBlockBodyFields p)
-indBlockBodyFields   p = (:) <$> p <*> many (indBlockPrefixFields p)
-indBlockPrefixFields p = ((try (Tok.spaces *> Indent.checkIndentedOrEq)) *> notFollowedBy eof) *> p
 
 
 -----------------------------------------------------------
