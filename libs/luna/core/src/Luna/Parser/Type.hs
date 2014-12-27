968e6cb9e5981d66a5562ceda08ae37c0bb08f64
Wojciech Danilo <wojciech.danilo@gmail.com>
2014-12-27 23:26:54 +0100

    Parser refactoring.

--- libs/luna/core/src/Luna/Parser/Type.hs
+++ libs/luna/core/src/Luna/Parser/Type.hs
@@ -6,41 +6,40 @@ import           Flowbox.Prelude         hiding (noneOf, maybe, element, cons)
 import qualified Luna.Syntax.Type        as Type
 import           Luna.Syntax.Type        (Type)
 import qualified Luna.Parser.Token       as Tok
-import           Luna.Parser.Builder     (labeled, tuple, qualifiedPath)
+import           Luna.Parser.Builder     (labeled, qualifiedPath)
+import qualified Luna.Parser.Builder     as B
 import           Text.Parser.Combinators 
 import           Text.Parser.Token       (braces)
 import           Luna.Parser.Combinators (many1, sepBy2)
 
-typeT       = choice [ try funcT
-                     , typeSingle
-                     ] <?> "type"
+typic   = choice [ try func
+                 , single
+                 ] <?> "type"
 
-typeSingle  = choice [ try appT
-                     , termT 
-                     ] <?> "type"
+single  = choice [ try app
+                 , term 
+                 ] <?> "type"
 
-termT       = choice [ try $ Tok.parens typeT 
-                     , entT 
-                     ] <?> "type term"
+term    = choice [ try $ Tok.parens typic 
+                 , entT 
+                 ] <?> "type term"
 
-appT        = labeled (Type.App <$> appBaseT <*> many1 termT)
+app     = labeled (Type.App <$> appBase <*> many1 term)
 
+argList = braces  (sepBy2 typic Tok.separator) <|> ((:[]) <$> single) <?> "type argument list"
 
-argListT    = braces  (sepBy2 typeT Tok.separator) <|> ((:[]) <$> typeSingle) <?> "type argument list"
-funcT       = labeled (Type.Function <$> argListT <* Tok.arrow <*> typeT)
+func    = labeled (Type.Function <$> argList <* Tok.arrow <*> typic)
+var     = labeled (Type.Var      <$> Tok.typeVarIdent)
+con     = labeled (Type.Con      <$> qualifiedPath Tok.typeIdent)
+tuple   = labeled (Type.Tuple    <$> B.tuple typic)
+list    = labeled (Type.List     <$> Tok.brackets typic)
+wild    = labeled (Type.Wildcard <$  Tok.wildcard)
 
-varT        = labeled (Type.Var      <$> Tok.typeVarIdent)
-conT        = labeled (Type.Con      <$> qualifiedPath Tok.typeIdent)
-tupleT      = labeled (Type.Tuple    <$> tuple typeT)
-listT       = labeled (Type.List     <$> Tok.brackets typeT)
-wildT       = labeled (Type.Wildcard <$  Tok.wildcard)
+appBase = choice [ var, con ]
 
-appBaseT    = choice [ varT, conT
-                     ]
-
-entT        = choice [ varT
-                     , conT
-                     , tupleT
-                     , listT
-                     , wildT
-                     ]
+entT    = choice [ var
+                 , con
+                 , tuple
+                 , list
+                 , wild
+                 ]
