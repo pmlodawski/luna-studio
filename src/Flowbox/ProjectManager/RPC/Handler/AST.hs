e1dca1329b1e1480d2f8108e459be22b8f7b1bb3
Piotr Mlodawski <remdezx@gmail.com>
2014-12-30 16:30:20 +0100

    Progress on new AST zippers

--- src/Flowbox/ProjectManager/RPC/Handler/AST.hs
+++ src/Flowbox/ProjectManager/RPC/Handler/AST.hs
@@ -56,7 +56,6 @@ import qualified Generated.Proto.ProjectManager.Project.Library.AST.Remove.Reque
 import qualified Generated.Proto.ProjectManager.Project.Library.AST.Remove.Update                     as Remove
 import qualified Generated.Proto.ProjectManager.Project.Library.AST.Resolve.Request                   as ResolveDefinition
 import qualified Generated.Proto.ProjectManager.Project.Library.AST.Resolve.Status                    as ResolveDefinition
-import qualified Luna.AST.Control.Crumb                                                               as Crumb
 import qualified Luna.AST.Module                                                                      as Module
 import qualified Luna.AST.Type                                                                        as Type
 import           Luna.Data.Serialize.Proto.Conversion.Crumb                                           ()
@@ -64,6 +63,7 @@ import           Luna.Data.Serialize.Proto.Conversion.Expr                      
 import           Luna.Data.Serialize.Proto.Conversion.Focus                                           ()
 import           Luna.Data.Serialize.Proto.Conversion.Module                                          ()
 import           Luna.Data.Serialize.Proto.Conversion.Name                                            ()
+import qualified Luna.Syntax.Control.Crumb                                                            as Crumb
 import qualified Luna.Syntax.Expr                                                                     as Expr
 
 
