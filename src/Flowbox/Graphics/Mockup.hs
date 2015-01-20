4eb1055acecc24f06c7d1f921756c8cb5417fb3f
Konrad Madej <kdmadej+git@gmail.com>
2015-01-20 14:49:25 +0100

    graphics: minor changes to crop in visual luna, slight fixes and transform added to mockup

--- src/Flowbox/Graphics/Mockup.hs
+++ src/Flowbox/Graphics/Mockup.hs
@@ -88,13 +88,13 @@ import Control.PolyApplicative ((<<*>>))
 
 data SkewOrder = SkewXY | SkewYX
 
-data Skew a = Skew { _skewPoint :: Point2 a
+data Skew a = Skew { _skewPoint :: V2 a
                    , _skewOrder :: SkewOrder
                    }
 
-data Transform a = Transform { _translate :: Point2 a
+data Transform a = Transform { _translate :: V2 a
                              , _rotate    :: a
-                             , _scale     :: Point2 a
+                             , _scale     :: V2 a
                              , _skew      :: Skew a
                              , _center    :: Point2 a
                              }
@@ -430,10 +430,9 @@ noiseLuna noise (variable -> width) (variable -> height) = channelToImageRGBA no
 
           noiseShader = scale (Grid width height) noise
 
---TODO[KM]: refactor this to use V2 as the translation input
-translateLuna :: Int -> Int -> Image -> Image
-translateLuna (A.fromIntegral . variable -> x) (A.fromIntegral . variable -> y) = onEachChannel translateChannel
-    where v = V2 x (-y)
+translateLuna :: V2 Double -> Image -> Image
+translateLuna (fmap variable -> V2 x y) = onEachChannel translateChannel
+    where v    = V2 x (-y)
           mask = Nothing
           translateChannel = \case
               (Channel.asContinuous -> ChannelFloat name zeData) -> ChannelFloat name $ (\(ContinuousData shader) -> ContinuousData $ Shader.transform transformation shader) zeData
@@ -444,6 +443,7 @@ translateLuna (A.fromIntegral . variable -> x) (A.fromIntegral . variable -> y) 
           strength pt = case mask of
               Nothing      -> v
               --TODO[KM]: handle the mask properly (aka. get rid of that ugly pattern match) and uncomment the other case option
+              --          and keep in mind that applying mask for functions using a center point might cause the shader to extract the strength value from wrong mask's coordinates
               _ -> v
               --Just (VPS m) -> let
               --        Right rgba = Image.lookupPrimary m
@@ -502,7 +502,7 @@ scaleAtLuna :: V2 Double -> Point2 Double -> Image -> Image
 scaleAtLuna (fmap variable -> v) (fmap variable -> (Point2 x y)) = onEachChannel scaleChannel
     where vBefore = V2 (-x) y
           vAfter  = V2 x (-y)
-          mask = Nothing
+          mask    = Nothing
           scaleChannel = \case
               (Channel.asContinuous -> ChannelFloat name zeData) -> ChannelFloat name $ (\(ContinuousData shader) -> ContinuousData $ Shader.transform transformation shader) zeData
               (Channel.asContinuous -> ChannelInt   name zeData) -> ChannelInt   name $ (\(ContinuousData shader) -> ContinuousData $ Shader.transform transformation shader) zeData
@@ -520,7 +520,32 @@ scaleAtLuna (fmap variable -> v) (fmap variable -> (Point2 x y)) = onEachChannel
 --          foo = scale (Grid x y) . nearest . fromMatrix boundary
 
 transformLuna :: Transform Double -> Image -> Image
-transformLuna _ img = img
+transformLuna (Transform tr (variable -> phi) (fmap variable -> sc) _ ce) = onEachChannel transformChannel
+    where V2     translateX translateY = fmap variable tr
+          Point2 centerX    centerY    = fmap variable ce
+          vBefore = V2 (-centerX) centerY
+          vAfter  = V2 centerX (-centerY)
+          mask    = Nothing
+          tr      = V2 translateX (-translateY)
+          transformChannel = \case
+              (Channel.asContinuous -> ChannelFloat name zeData) -> ChannelFloat name $ (\(ContinuousData shader) -> ContinuousData $ Shader.transform transformation shader) zeData
+              (Channel.asContinuous -> ChannelInt   name zeData) -> ChannelInt   name $ (\(ContinuousData shader) -> ContinuousData $ Shader.transform transformation shader) zeData
+          transformation :: Point2 (Exp Double) -> Point2 (Exp Double)
+          transformation pt = Transform.translate (strengthTranslate pt) $ Transform.translate vAfter $ Transform.rotate (strengthRotate pt) $ Transform.scale (strengthScale pt) $ Transform.translate vBefore pt
+          strengthRotate    = strengthScalar phi
+          strengthScale     = strengthVector sc
+          strengthTranslate = strengthVector tr
+          -- TODO: extract those 2 functions as a top-level binding
+          strengthScalar :: Exp Double -> Point2 (Exp Double) -> Exp Double
+          strengthScalar val pt = case mask of
+              Nothing -> val
+              --TODO[KM]: handle the mask properly
+              _       -> val
+          strengthVector :: V2 (Exp Double) -> Point2 (Exp Double) -> V2 (Exp Double)
+          strengthVector v pt = case mask of
+              Nothing -> v
+              --TODO[KM]: handle the mask properly
+              _       -> v
 
 cropLuna :: Rectangle Int -> Image -> Image
 cropLuna rect = onEachChannel cropChannel
