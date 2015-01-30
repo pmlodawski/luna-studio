dc336dbf2bf5d10d0a408c18ac51f3424840dc2c
Konrad Gądek <kgadek@gmail.com>
2015-01-30 02:26:25 +0100

    Stuff to preserve

--- libs/luna/typechecker/playground.hs
+++ libs/luna/typechecker/playground.hs
@@ -1,33 +1,50 @@
-{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 import Data.Functor.Identity
 import Control.Monad.RWS
 import Control.Monad.Fix
 import Control.Monad.Trans.Either
+import Control.Monad.State.Strict
 
 
 
 
 main :: IO ()
-main = do print "main
-          let r1 = runIdentity $ runEitherT $ m1  0
-              r2 = runIdentity $ runEitherT $ either (EitherT . return . Left) m2 r1
-              r3 = runIdentity $ runEitherT $ either (EitherT . return . Left) m3 r2
-          print r1
-          print r2
-          print r3
+main = do
+    let a = runIdentity $ runEitherT (runStateT procedureA 0)
+    let b = runIdentity $ runStateT (runEitherT procedureB) 0
+    print $ "A = " ++ show a
+    print $ "B = " ++ show b
 
 
+procedureA :: StateT Int (EitherT String Identity) ()
+procedureA = do
+    x <- get
+    r1 <- m1 x
+    put r1
+    r2 <- m2 r1
+    put r2
+    r3 <- m3 r2
+    put r3
+    return ()
 
 
+procedureB :: EitherT String (StateT Int Identity) ()
+procedureB = do
+    x <- get
+    r1 <- m1 x
+    put r1
+    r2 <- m2b r1
+    put r2
+    r3 <- m3 r2
+    put r3
+    return ()
 
-m1 :: Monad m => Int -> EitherT String m Int
-m1 x = return (123+x)
 
+m1 x = return (10 + x)
 
-m2 :: Monad m => Int -> EitherT String m Int
-m2 = EitherT . return . Left . const "dupa"
+m2 x = return (100 + x)
 
+m2b x = EitherT $ return $ Left "no"
 
-m3 :: Monad m => Int -> EitherT String m Int
-m3 x = return (456+x)
+m3 x = return (1000 + x)
