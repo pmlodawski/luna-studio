ef64c338f364f2cb58dd4e4d69f061a1aaac185a
Piotr Mlodawski <remdezx@gmail.com>
2015-02-02 11:27:46 +0100

    Library and LibManager

--- src/Flowbox/Control/Error.hs
+++ src/Flowbox/Control/Error.hs
@@ -15,8 +15,6 @@ module Flowbox.Control.Error (
 
 import           Control.Error             as X hiding (runScript)
 import qualified Control.Exception         as Exc
-import           Control.Monad.IO.Class    (MonadIO, liftIO)
-import           Control.Monad.Trans.Class (MonadTrans)
 import qualified Data.Maybe                as Maybe
 
 import Flowbox.Prelude
