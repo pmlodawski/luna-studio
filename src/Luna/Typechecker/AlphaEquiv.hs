6e43248b556ac30acc2ea70b2452f6067ecf3e74
Konrad Gądek <kgadek@gmail.com>
2015-02-05 19:18:40 +0100

    tests: ⍺-equivalence

--- src/Luna/Typechecker/AlphaEquiv.hs
+++ src/Luna/Typechecker/AlphaEquiv.hs
@@ -1,44 +1,20 @@
-module Luna.Typechecker.AlphaEquiv where
+module Luna.Typechecker.AlphaEquiv (
+    module Luna.Typechecker.AlphaEquiv.AlphaEquivMonad,
+    AlphaEquiv(..), evalAlphaEquiv
+  ) where
 
 
-import Flowbox.Prelude
-import Luna.Typechecker.Data.Type
-import Luna.Typechecker.Data.TVar
-import Data.Map.Strict (Map)
+import            Flowbox.Prelude
+import qualified  Data.Map.Strict                             as M
+import            Data.Maybe
 
-import qualified Data.Map.Strict as M
-
-
-type TypesTranslate  = Map TVar TVar
-type FieldsTranslate = Map String String
+import            Luna.Typechecker.AlphaEquiv.AlphaEquivMonad
 
 
-runEquiv :: (AlphaEquiv a) => a -> a -> Bool
-runEquiv a b = (\(x,fmab,fmba,ttab,ttba) -> x) $ equiv M.empty M.empty M.empty M.empty a b
-
-
--- TODO [kgdk] 5 lut 2015: najbrzydszy kod EVER. Powinna być monada State, etc. Przepraszam za to :<
 class AlphaEquiv a where
-    equiv :: FieldsTranslate -> FieldsTranslate -> TypesTranslate -> TypesTranslate -> a -> a -> (Bool, FieldsTranslate, FieldsTranslate, TypesTranslate, TypesTranslate)
+    equiv :: a -> a -> AlphaEquivMonad ()
 
 
-instance AlphaEquiv Type where
-    equiv fmab fmba ttab ttba (TV a) (TV b)
-        | M.lookup a ttab == Just b  && M.lookup b ttba == Just a  = (True, fmab, fmba, ttab, ttba)
-        | M.notMember a ttab         && M.notMember b ttba         = (True, fmab, fmba, M.insert a b ttab, M.insert b a ttba)
-    equiv fmab fmba ttab ttba (p1 `Fun` p2) (q1 `Fun` q2)
-        = let (c1,fmab1,fmba1,ttab1,ttba1) = equiv fmab  fmba ttab  ttba  p1 q1
-              (c2,fmab2,fmba2,ttab2,ttba2) = equiv fmab1 fmba1 ttab1 ttba1 p2 q2
-           in (c1&&c2, fmab2, fmba2, ttab2, ttba2)
-    equiv gfmab gfmba gttab gttba (Record flds1) (Record flds2) | length flds1 == length flds2
-        = foldr merge (True, gfmab, gfmba, gttab, gttba) (zip flds1 flds2)
-      where
-        merge _                                      (False, fmab, fmba, ttab, ttba) = (False, fmab, fmba, ttab, ttba)
-        merge ((fld1lab, fld1ty), (fld2lab, fld2ty)) (True,  fmab, fmba, ttab, ttba) | M.lookup fld1lab fmab == Just fld2lab && M.lookup fld2lab fmba == Just fld1lab 
-                                                                                        = equiv fmab fmba ttab ttba fld1ty fld2ty
-                                                                                     | M.notMember fld1lab fmab              && M.notMember fld2lab fmba
-                                                                                        = let fmab1 = M.insert fld1lab fld2lab fmab
-                                                                                              fmba1 = M.insert fld1lab fld1lab fmab1
-                                                                                           in equiv fmab1 fmba1 ttab ttba fld1ty fld2ty
-        merge ((fld1lab, fld1ty), (fld2lab, fld2ty)) (True,  fmab, fmba, ttab, ttba) = (False, fmab, fmba, ttab, ttba)
-    equiv fmab fmba ttab ttba _ _ = (False, fmab, fmba, ttab, ttba)
+evalAlphaEquiv :: AlphaEquiv a => a -> a -> Bool
+evalAlphaEquiv a b = isJust $ extract $ runAlphaEq (equiv a b) M.empty M.empty M.empty M.empty
+  where extract (x,fmab,fmba,ttab,ttba) = x
--- src/Luna/Typechecker/Data/Type.hs
+++ src/Luna/Typechecker/Data/Type.hs
@@ -1,14 +1,14 @@
 module Luna.Typechecker.Data.Type where
 
-
 import Flowbox.Prelude
+import Control.Monad
 
+import Luna.Typechecker.AlphaEquiv
 import Luna.Typechecker.Data.TVar
 
 
+
 type Fieldlabel = String
-
-
 type Field = (Fieldlabel, Type)
 
 
@@ -16,3 +16,17 @@ data Type = TV TVar
           | Type `Fun` Type
           | Record [Field]
           deriving (Show,Eq)
+
+
+instance AlphaEquiv Type where
+    equiv (TV a) (TV b) = ttLookup a b >>= \case
+                              True  -> return ()
+                              False -> ttInsert a b
+    equiv (p1 `Fun` p2) (q1 `Fun` q2) = equiv p1 q1 >> equiv p2 q2
+    equiv (Record flds1) (Record flds2)
+      | length flds1 == length flds2 = zipWithM_ merge flds1 flds2
+      where 
+        merge (fld1lab, fld1ty) (fld2lab, fld2ty) = ffLookup fld1lab fld2lab >>= \case
+                                                        True  -> return ()
+                                                        False -> ffInsert fld1lab fld2lab >> equiv fld1ty fld2ty
+    equiv _ _ = fail "no"
