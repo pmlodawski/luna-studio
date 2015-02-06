476446a7733b9f4c76b29a762f6dd20d3265dc35
Krzysztof Rudko <chris.rudko@gmail.com>
2015-02-06 09:11:56 +0100

    Minor changes.

--- libs/data/graphics/test/Flowbox/Graphics/MockupSpec.hs
+++ libs/data/graphics/test/Flowbox/Graphics/MockupSpec.hs
@@ -4,6 +4,15 @@ import Test.Hspec
 import Test.QuickCheck
 import Flowbox.Graphics.Mockup as M
 import Flowbox.Graphics.Composition.EdgeBlur as EB
+import Flowbox.Graphics.Composition.Dither   as D
+import Flowbox.Graphics.Composition.Generator.Gradient
+import Flowbox.Graphics.Shader.Sampler
+import Flowbox.Graphics.Shader.Rasterizer
+import Flowbox.Graphics.Shader.Shader                  as S
+import Flowbox.Graphics.Composition.Transform
+import qualified Flowbox.Math.Matrix as M
+import Data.Array.Accelerate.CUDA as A
+import qualified Data.Array.Accelerate              as A
 import System.IO.Unsafe
 
 import Flowbox.Prelude as P
@@ -14,21 +23,32 @@ import TestHelpers
 spec :: Spec
 spec = do
     let specPath = "./Flowbox/Graphics/"
-        in let testName = "edgeBlur"
-            in describe testName $ do 
-                describe "should match previously computed picture on rgba.r channel with edge multiplier 5 and kernel size 5" $ do
-                    let actualImage = unsafePerformIO $ testEdgeBlur 15 5 "rgba.r"
-                        expectedImage = unsafePerformIO $ loadImageLuna $ specPath++testName++"/eb_result.png"
-                    it "in pixel-wise metric" $ do
-                        shouldBeCloseTo (specPath++testName) PixelWise actualImage expectedImage 
-                    it "in image-wise metric" $ do
-                        shouldBeCloseTo (specPath++testName) ImageWise actualImage expectedImage
-                --it "should match previously computed picture on rgba.r channel with dge multiplier 5 and kernel size 5 in every metric" $
-                --    property $ \x -> shouldBeCloseTo x (unsafePerformIO $ testEdgeBlur 5 5 "rgba.r") (unsafePerformIO $ loadImageLuna "./samples/eb_result.png")
-                it "should throw exception on non existing channel" $ do
-                    testSaveEdgeBlur 5 5 "rgba.x" `shouldThrow` anyException
-                --it "should be efficent" $ do
-                --    timeout 1000
+        in do let testName = "edgeBlur"
+                in describe testName $ do 
+                    describe "should match previously computed picture on rgba.r channel with edge multiplier 5 and kernel size 5" $ do
+                        let actualImage = unsafePerformIO $ testEdgeBlur 5 5 "rgba.r"
+                            expectedImage = getDefaultTestPic specPath testName
+                        it "in pixel-wise metric" $ do
+                            shouldBeCloseTo (specPath++testName) PixelWise actualImage expectedImage 
+                        it "in image-wise metric" $ do
+                            shouldBeCloseTo (specPath++testName) ImageWise actualImage expectedImage
+                    --it "should match previously computed picture on rgba.r channel with dge multiplier 5 and kernel size 5 in every metric" $
+                    --    property $ \x -> shouldBeCloseTo x (unsafePerformIO $ testEdgeBlur 5 5 "rgba.r") (unsafePerformIO $ loadImageLuna "./samples/eb_result.png")
+                    it "should throw exception on non existing channel" $ do
+                        testSaveEdgeBlur 5 5 "rgba.x" `shouldThrow` anyException
+                    --it "should be efficent" $ do
+                    --    timeout 1000
+
+              --let testName = "dither"
+              --  in describe testName $ do
+              --      describe "should match previously computed image with param 5" $ do
+              --          let actualImage = unsafePerformIO $ testDither 5
+              --              expectedImage = getDefaultTestPic specPath testName
+              --          it  "in pixel-wise metric" $ do
+              --              shouldBeCloseTo (specPath++testName) PixelWise actualImage expectedImage
+              --          it "in image-wise metric" $ do
+              --              shouldBeCloseTo (specPath++testName) ImageWise actualImage expectedImage
+
 
 testSaveEdgeBlur kernelSize edgeMultiplier channel = do
     img <- loadImageLuna "./samples/lena.png"
@@ -39,3 +59,9 @@ testEdgeBlur kernelSize edgeMultiplier channel = do
     img <- loadImageLuna "./samples/lena.png"
     let a = edgeBlur channel EB.GaussBlur kernelSize edgeMultiplier img
     return a
+
+testDither a = do
+    let mydither = dither M.Clamp floydSteinberg a
+    let grad = monosampler $ scale (512 :: Grid (M.Exp Int)) $ circularShape :: DiscreteShader (M.Exp Float)
+    result <- M.mutableProcess run mydither $ rasterizer grad
+    return result
