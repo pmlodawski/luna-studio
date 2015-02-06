60411b1aa6ac4fff85b19aa53fd9f0f977fc664f
Krzysztof Rudko <chris.rudko@gmail.com>
2015-02-06 11:54:17 +0100

    testPath puled out to let.

--- test/Flowbox/Graphics/MockupSpec.hs
+++ test/Flowbox/Graphics/MockupSpec.hs
@@ -23,21 +23,23 @@ import TestHelpers
 spec :: Spec
 spec = do
     let specPath = "./Flowbox/Graphics/"
-        in do let testName = "edgeBlur"
+        in do 
+          let testName = "edgeBlur"
+              testPath = specPath++testName
                 in describe testName $ do 
                     describe "should match previously computed picture on rgba.r channel with edge multiplier 5 and kernel size 5" $ do
                         let actualImage = unsafePerformIO $ testEdgeBlur 5 5 "rgba.r"
                             expectedImage = getDefaultTestPic specPath testName
                         it "in pixel-wise metric" $ do
-                            shouldBeCloseTo (specPath++testName) PixelWise actualImage expectedImage 
+                            shouldBeCloseTo testPath PixelWise actualImage expectedImage 
                         it "in image-wise metric" $ do
-                            shouldBeCloseTo (specPath++testName) ImageWise actualImage expectedImage
+                            shouldBeCloseTo testPath ImageWise actualImage expectedImage
                     --it "should match previously computed picture on rgba.r channel with dge multiplier 5 and kernel size 5 in every metric" $
                     --    property $ \x -> shouldBeCloseTo x (unsafePerformIO $ testEdgeBlur 5 5 "rgba.r") (unsafePerformIO $ loadImageLuna "./samples/eb_result.png")
                     it "should throw exception on non existing channel" $ do
                         testSaveEdgeBlur 5 5 "rgba.x" `shouldThrow` anyException
-                    --it "should be efficent" $ do
-                    --    timeout 1000
+                    it "should be efficent" $ do
+                        pending
 
               --let testName = "dither"
               --  in describe testName $ do
@@ -53,15 +55,15 @@ spec = do
 testSaveEdgeBlur kernelSize edgeMultiplier channel = do
     img <- loadImageLuna "./samples/lena.png"
     let a = edgeBlur channel EB.GaussBlur kernelSize edgeMultiplier img
-    saveImageLuna "./samples/test_results/x_result.png" a
+    saveImageLuna "./samples/x_result.png" a
 
 testEdgeBlur kernelSize edgeMultiplier channel = do
     img <- loadImageLuna "./samples/lena.png"
     let a = edgeBlur channel EB.GaussBlur kernelSize edgeMultiplier img
     return a
 
-testDither a = do
-    let mydither = dither M.Clamp floydSteinberg a
-    let grad = monosampler $ scale (512 :: Grid (M.Exp Int)) $ circularShape :: DiscreteShader (M.Exp Float)
-    result <- M.mutableProcess run mydither $ rasterizer grad
-    return result
+--testDither a = do
+--    let mydither = dither M.Clamp floydSteinberg a
+--    let grad = monosampler $ scale (512 :: Grid (M.Exp Int)) $ circularShape :: DiscreteShader (M.Exp Float)
+--    result <- M.mutableProcess run mydither $ rasterizer grad
+--    return result
