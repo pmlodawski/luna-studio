16828e7b577ca3100da9910a3d2f06055d78d95e
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2015-02-06 17:38:50 +0000

    WIP serialization

--- src/Flowbox/Math/Function/Model.hs
+++ src/Flowbox/Math/Function/Model.hs
@@ -4,11 +4,27 @@
 -- Proprietary and confidential
 -- Flowbox Team <contact@flowbox.io>, 2014
 ---------------------------------------------------------------------------
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE ViewPatterns          #-}
+
 module Flowbox.Math.Function.Model where
 
-import Data.Map
+import Data.Map      (Map, toAscList)
+import Data.Sequence (fromList)
 
-import Flowbox.Prelude
+import           Flowbox.Data.Serialization       (Serializable(..))
+import           Flowbox.Prelude
+import           Generated.Proto.Data.CurveData   (CurveData (CurveData))
+import qualified Generated.Proto.Data.CurveData   as CurveData
+import           Generated.Proto.Data.PointData   (PointData (PointData))
+import qualified Generated.Proto.Data.PointData   as PointData
+import           Generated.Proto.Data.TangentData (TangentData (TangentData))
+import qualified Generated.Proto.Data.TangentData as TangentData
+import qualified Generated.Proto.Data.Value.Type  as Value
+import           Generated.Proto.Data.VertexData  (VertexData (VertexData))
+import qualified Generated.Proto.Data.VertexData  as VertexData
 
 
 
@@ -20,25 +36,25 @@ type Range x     = (x, x)
 data FunctionModel x y = FunctionModel { _segments :: Map x (Maybe (FunctionSegment x y)) }
 
 data FunctionSegment x y = ContinuousHybrid { _vertices :: Map x (FunctionControlPoint y) }
-                 | Lambda           { _lambda   :: x -> y }
-                 | Repeater         { _startsAt :: x
-                                    , _function :: FunctionModel x y
-                                    , _range    :: Range x
-                                    }
+                         | Lambda           { _lambda   :: x -> y }
+                         | Repeater         { _startsAt :: x
+                                            , _function :: FunctionModel x y
+                                            , _range    :: Range x
+                                            }
 
 data FunctionControlPoint y = FunctionControlPoint { _vertexY   :: y
-                                   , _handleIn  :: Maybe FunctionHandle
-                                   , _handleOut :: Maybe FunctionHandle
-                                   } deriving (Show, Eq)
+                                                   , _handleIn  :: Maybe FunctionHandle
+                                                   , _handleOut :: Maybe FunctionHandle
+                                                   } deriving (Show, Eq)
 
 data FunctionHandle = FunctionHandle { _weight   :: Weight
-                     , _angle    :: Angle
-                     }
-            deriving (Show, Eq)
+                                     , _angle    :: Angle
+                                     }
+                    deriving (Show, Eq)
 
 data FunctionPoint x y = FunctionPoint { _pointX :: x
-                       , _pointY :: y
-                       }
+                                       , _pointY :: y
+                                       }
 
 makeLenses ''FunctionPoint
 makeLenses ''FunctionHandle
@@ -49,3 +65,36 @@ makeLenses ''FunctionModel
 
 hardJoint :: y -> FunctionControlPoint y
 hardJoint y = FunctionControlPoint y Nothing Nothing
+
+
+instance (Real x, Real y) => Serializable (FunctionPoint x y) PointData where
+    serialize (FunctionPoint (realToFrac -> x :: Double) (realToFrac -> y :: Double)) _ =
+        return $ PointData (Just x) (Just y)
+    data' _ = PointData.data'
+    val   _ = Value.Point
+
+instance Serializable (Maybe FunctionHandle) TangentData where
+    serialize mh _ = return $ TangentData w' a' b'
+        where (w', a', b') = case mh of
+                                 Just (FunctionHandle w a) -> (Just w, Just a, Just False)
+                                 Nothing                   -> (Just 0, Just 0, Just True)
+    data' _ = TangentData.data'
+    val   _ = Value.Tangent
+
+instance (Real x, Real y) => Serializable (x, FunctionControlPoint y) VertexData where
+    serialize (realToFrac -> x :: Double, FunctionControlPoint (realToFrac -> y :: Double) hIn hOut) mode = do
+        p     <- serialize (FunctionPoint x y) mode
+        hIn'  <- serialize hIn         mode
+        hOut' <- serialize hOut        mode
+        return $ VertexData (Just p) (Just hIn') (Just hOut')
+    data' _ = VertexData.data'
+    val   _ = Value.Vertex
+
+instance (Real x, Real y) =>  Serializable (FunctionSegment x y) CurveData where
+    serialize Lambda{}   _= error "Serializing the Lambda segment is not yet supported!"
+    serialize Repeater{} _= error "Serializing the Repeater segment is not yet supported!"
+    serialize (ContinuousHybrid nodes) mode = do
+        nodes' <- sequence $ flip serialize mode <$> toAscList nodes
+        return . CurveData . fromList $ nodes'
+    data' _ = CurveData.data'
+    val   _ = Value.Curve
