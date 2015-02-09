99b9125cec70f8e08bad704ce6ac4ca8a66ecd62
Wojciech Danilo <wojciech.danilo@gmail.com>
2015-02-09 09:26:46 +0100

    Fixing structural analysis to handle data args.

--- test/WIP.hs
+++ test/WIP.hs
@@ -81,60 +81,22 @@ memDef_Vector_asTuple _self = do
 memFnc_Vector_asTuple = (memSig_Vector_asTuple, memDef_Vector_asTuple)
 $(registerMethod ''Vector "asTuple")
 
--- ====== Method: Main.test ====== --
-memSig_Main_test = _rtup2(_nuSigArg("self"), _nuSigArg("x"))
-memDef_Main_test _self _x = do 
-     _x
-     
-
-memFnc_Main_test = (memSig_Main_test, memDef_Main_test)
-$(registerMethod ''Main "test")
-
 -- ====== Method: Main.foo ====== --
-memSig_Main_foo = _rtup2(_nuSigArg("self"), _nuSigArg("f"))
-memDef_Main_foo _self _f = do 
-     _call(3) (appNext (val (5 :: Int)) _f)
+memSig_Main_foo = _rtup3(_nuSigArg("self"), _nuSigArg("a"), _nuSigArg("b"))
+memDef_Main_foo _self _a _b = do 
+     val (_a, _b)
      
 
 memFnc_Main_foo = (memSig_Main_foo, memDef_Main_foo)
 $(registerMethod ''Main "foo")
 
--- ====== Method: Main.bar ====== --
-memSig_Main_bar = _rtup1(_nuSigArg("self"))
-memDef_Main_bar _self = do 
-     val ()
-     
-
-memFnc_Main_bar = (memSig_Main_bar, memDef_Main_bar)
-$(registerMethod ''Main "bar")
-
--- ====== Method: Main.baz ====== --
-memSig_Main_baz = _rtup3(_nuSigArg("self"), _npSigArg("x", val (0 :: Int)), _npSigArg("y", val (0 :: Int)))
-memDef_Main_baz _self _x _y = do 
-     val (_x, _y)
-     
-
-memFnc_Main_baz = (memSig_Main_baz, memDef_Main_baz)
-$(registerMethod ''Main "baz")
-
 -- ====== Method: Main.main ====== --
 memSig_Main_main = _rtup1(_nuSigArg("self"))
-
--- ====== Method: Main.lambda__53 ====== --
-memSig_Main_lambda__53 = _rtup2(_nuSigArg("self"), _nuSigArg("x"))
-memDef_Main_lambda__53 _self _x = do 
-     val (_x, _x)
-     
-
-memFnc_Main_lambda__53 = (memSig_Main_lambda__53, memDef_Main_lambda__53)
-$(registerMethod ''Main "lambda__53")
 memDef_Main_main _self = do 
-     _v <- _call(4) (appNext (val (3 :: Int)) (appNext (val (2 :: Int)) (appByName _name("y") (val (1 :: Int)) cons_Vector)))
-     _f <- _member("lambda__53") (_call(6) cons_Main)
-     _call(7) (appNext (_call(8) (appNext _f (_member("foo") _self))) (_member("print") _self))
-     _call(9) (appNext _v (_member("print") _self))
-     _call(10) (appNext (_call(11) (appByName _name("y") (val (7 :: Int)) (_member("baz") _self))) (_member("print") _self))
-     _call(12) (appNext (_call(13) (_member("asTuple") _v)) (_member("print") _self))
+     _v <- _call(3) (appNext (val (3 :: Int)) (appNext (val (2 :: Int)) (appByName _name("y") (val (1 :: Int)) cons_Vector)))
+     _call(4) (appNext (_call(5) (_member("asTuple") _v)) (_member("print") _self))
+     _f <- appNext (val (1 :: Int)) (_member("foo") _self)
+     _call(6) (appNext (_call(7) (appNext (val (2 :: Int)) _f)) (_member("print") _self))
      
 
 memFnc_Main_main = (memSig_Main_main, memDef_Main_main)
