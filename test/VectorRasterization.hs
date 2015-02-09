2d8292a4953ee9e3095d23b87b8b4f4dfcfa5c04
Piotr Oleksy <piotr.olek@o2.pl>
2015-02-09 13:01:17 +0000

    feathers working really fast

--- test/VectorRasterization.hs
+++ test/VectorRasterization.hs
@@ -9,49 +9,204 @@
 
 module Main where
 
-import           Data.Array.Accelerate hiding (fromIntegral)
-import           Data.Array.Accelerate.IO
-import           Data.ByteString hiding (head)
-import           Data.VectorSpace
-import           Diagrams.Prelude hiding (Path)
-import           Diagrams.Backend.Cairo
-import           Diagrams.Backend.Cairo.Internal
-import           Diagrams.Segment
-import           Diagrams.Trail
-import           Diagrams.TrailLike
-import           Graphics.Rendering.Cairo hiding (translate)
---import           Graphics.Rendering.Cairo
+import Data.Array.Accelerate hiding (fromIntegral)
+import Data.Array.Accelerate as A
+import Data.Array.Accelerate.IO
+import Data.ByteString hiding (head)
+import Data.VectorSpace
+import Data.Thyme.Clock
+import Data.Thyme.Time
+import Flowbox.Geom2D.ControlPoint
+import Flowbox.Geom2D.Path
+import Flowbox.Geom2D.Mask
+import Flowbox.Geom2D.Rasterizer hiding (makePoints, makeSegments)
+import Flowbox.Geom2D.QuadraticBezier
+import Flowbox.Geom2D.QuadraticBezier.Conversion
+import Flowbox.Geom2D.Accelerate.QuadraticBezier.Solve
+import Flowbox.Graphics.Image.IO.BMP
+import Flowbox.Graphics.Mockup (saveImageLuna)
+import Flowbox.Prelude as P hiding ((#))
+import Flowbox.Math.Matrix
+import Math.Coordinate.Cartesian (Point2 (..))
+import Data.Array.Accelerate.CUDA
+import Debug.Trace (trace)
 
-import           Flowbox.Geom2D.ControlPoint
-import           Flowbox.Geom2D.Path
-import           Flowbox.Geom2D.Mask
-import           Flowbox.Geom2D.Rasterizer hiding (makePoints, makeSegments)
-import           Flowbox.Graphics.Image.IO.BMP
-import           Flowbox.Graphics.Mockup (saveImageLuna)
-import           Flowbox.Prelude hiding ((#))
 
 
+pointsStraightSmall   = [ ControlPoint (Point2 136.668065 58.260504)  Nothing Nothing
+                        , ControlPoint (Point2 286.668065 206.260504) Nothing Nothing
+                        , ControlPoint (Point2 374.668065 76.260504)  Nothing Nothing
+                        ]
+feathersStraightSmall = [ ControlPoint (Point2 75.995120 28.297279)   Nothing Nothing
+                        , ControlPoint (Point2 293.366284 233.596489) Nothing Nothing
+                        , ControlPoint (Point2 434.615106 44.870165)  Nothing Nothing
+                        ]      
+pointsCurvedSmall   = [ ControlPoint (Point2 136.668065 58.260504)  (Just (Point2 293.366284 233.596489)) (Just (Point2 444.615106 24.870165))
+                      , ControlPoint (Point2 286.668065 206.260504) (Just (Point2 193.366284 233.596489)) (Just (Point2 134.615106 14.870165))
+                      , ControlPoint (Point2 374.668065 76.260504)  (Just (Point2 93.362284  233.596489)) (Just (Point2 134.615106 34.870165))
+                      ]
+feathersCurvedSmall = [ ControlPoint (Point2 75.995120 28.297279)   (Just (Point2 293.366284 233.596489)) (Just (Point2 434.615106 24.870165))
+                      , ControlPoint (Point2 293.366284 233.596489) (Just (Point2 193.366384 233.596489)) (Just (Point2 334.615106 34.870165))
+                      , ControlPoint (Point2 434.615106 44.870165)  (Just (Point2 293.366284 233.596489)) (Just (Point2 114.615106 44.870165))
+                      ]
+
+helper w h pointPath featherPath = do
+    P.putStrLn "------------------  Path -> Quadratics: "
+    t1 <- getCurrentTime
+    let p = makeCubics pointPath
+    let a = convertCubicsToQuadratics 5 0.0001 p
+    print $ seq (a) ()
+    t2 <- getCurrentTime
+    print $ diffUTCTime t2 t1
+
+    P.putStrLn "------------------  Path -> Matrix"
+    t1 <- getCurrentTime
+    let p = pathToMatrix w h pointPath
+    print $ case p of
+                Raw m -> seq m ()
+                Delayed m -> let calcu = run m in seq calcu ()
+    t2 <- getCurrentTime
+    print $ diffUTCTime t2 t1
+
+    P.putStrLn "------------------  Full Computation: "
+    t1 <- getCurrentTime
+    let arr = rasterizeMask w h (Mask pointPath (Just featherPath))
+    print $ case arr of
+                Raw m -> seq m ()
+                Delayed m -> let calcu = run m in seq calcu ()
+    t2 <- getCurrentTime
+    print $ diffUTCTime t2 t1
+
+    return ()
+
+profile = do 
+    let closed = True
+        (wSmall,  hSmall ) = (480,  240 ) :: (Int, Int)
+        (wMedium, hMedium) = (1920, 1080) :: (Int, Int)
+        (wBig,    hBig   ) = (4086, 2048) :: (Int, Int)
+        pointPathStraightSmall   = Path closed pointsStraightSmall
+        featherPathStraightSmall = Path closed feathersStraightSmall
+        pointPathCurvedSmall     = Path closed pointsCurvedSmall
+        featherPathCurvedSmall   = Path closed feathersCurvedSmall
+
+    P.putStrLn "--------------------------  Profiling VectorRasterization !! "
+
+    P.putStrLn "----------------------  Small Image, Small Straight Path: "
+    helper wSmall hSmall pointPathStraightSmall featherPathStraightSmall 
+
+    P.putStrLn "----------------------  Medium Image, Small Straight Path: "
+    helper wMedium hMedium pointPathStraightSmall featherPathStraightSmall
+
+    P.putStrLn "----------------------  Big Image, Small Straight Path: "
+    helper wBig hBig pointPathStraightSmall featherPathStraightSmall
+
+    P.putStrLn "----------------------  Small Image, Small Curved Path: "
+    helper wSmall hSmall pointPathCurvedSmall featherPathCurvedSmall 
+
+    P.putStrLn "----------------------  Medium Image, Small Curved Path: "
+    helper wMedium hMedium pointPathCurvedSmall featherPathCurvedSmall
+
+    P.putStrLn "----------------------  Big Image, Small Curved Path: "
+    helper wBig hBig pointPathCurvedSmall featherPathCurvedSmall
+
+    return ()
 
 main = do
     let closed = True
         (w,h)  = (640, 480) :: (Int, Int)
-        points = [ ControlPoint (Point2 212 209) Nothing                             (Just $ Point2 (211-212) (114-209))
-                  , ControlPoint (Point2 338 210) (Just $ Point2 (329-338) (109-210)) (Just $ Point2 (450-338) (211-210))
-                  , ControlPoint (Point2 343 330) (Just $ Point2 (456-343) (331-330)) Nothing
-                  ]
-        feather = [ ControlPoint (Point2 212 (209-40)) Nothing                                (Just $ Point2 (211-212) (114-209))
-                  , ControlPoint (Point2 338 (210-40)) (Just $ Point2 (329-338) (109-210)) (Just $ Point2 (450-338) (211-210))
-                  , ControlPoint (Point2 343 (330-40)) (Just $ Point2 (456-343) (331-330)) Nothing
-                  ]
+        points =   [ ControlPoint (Point2 212 209) Nothing                             (Just $ Point2 (211-212) (114-209))
+                   , ControlPoint (Point2 338 210) (Just $ Point2 (329-338) (109-210)) (Just $ Point2 (450-338) (211-210))
+                   , ControlPoint (Point2 343 330) (Just $ Point2 (456-343) (331-330)) Nothing
+                   ]
+        feather =  [ ControlPoint (Point2 212 (209-40)) Nothing                        (Just $ Point2 (211-212) (114-209))
+                   , ControlPoint (Point2 338 (210-40)) (Just $ Point2 (329-338) (109-210)) (Just $ Point2 (450-338) (211-210))
+                   , ControlPoint (Point2 343 (330-40)) (Just $ Point2 (456-343) (331-330)) Nothing
+                   ]
+        points1  = [ ControlPoint (Point2 212 209) Nothing Nothing
+                   , ControlPoint (Point2 338 210) Nothing Nothing
+                   , ControlPoint (Point2 343 330) Nothing Nothing
+                   ]
+        feather1 = [ ControlPoint (Point2 212 (209-40)) Nothing Nothing
+                   , ControlPoint (Point2 338 (210-40)) Nothing Nothing
+                   , ControlPoint (Point2 343 (330-40)) Nothing Nothing
+                   ]
+        pointsd  = [ ControlPoint (Point2 10 10) Nothing Nothing
+                   , ControlPoint (Point2 630 10)  Nothing Nothing
+                   , ControlPoint (Point2 630 470) Nothing Nothing
+                   , ControlPoint (Point2 10  470) Nothing Nothing
+                   ]
+        featherd = [ ControlPoint (Point2 10 10) Nothing Nothing
+                   , ControlPoint (Point2 630 10)  Nothing Nothing
+                   , ControlPoint (Point2 630 470) Nothing Nothing
+                   , ControlPoint (Point2 10  470) Nothing Nothing
+                   ]
+        pointsGUI  = [ ControlPoint (Point2 136.668065 58.260504)  Nothing Nothing
+                     , ControlPoint (Point2 286.668065 226.260504) Nothing Nothing
+                     , ControlPoint (Point2 374.668065 76.260504)  Nothing Nothing
+                     ]
+        featherGUI = [ ControlPoint (Point2 75.995120 28.297279)   Nothing Nothing
+                     , ControlPoint (Point2 293.366284 293.596489) Nothing Nothing
+                     , ControlPoint (Point2 434.615106 44.870165)  Nothing Nothing
+                     ]
+
+    P.putStrLn "Test rasterizeMask one line, no handles --> foo.png"
+
+    let patd = Path True pointsd
+        fead = Path True featherd
+
+    let arrD = rasterizeMask w h $ (Mask patd (Just fead))
+        imgD = matrixToImage arrD
+
+    saveImageLuna "foo.png" imgD
+
+    P.putStrLn "Test rasterizeMask no fea, no handles --> foo1.png"
+
+    let pat1 = Path True points1
+        fea1 = Path True feather1
+
+    let arrD = rasterizeMask w h $ (Mask pat1 Nothing)
+        imgD = matrixToImage arrD
+
+    saveImageLuna "foo1.png" imgD
+
+    P.putStrLn "Test rasterizeMask no handles --> foo2.png"
+
+    let arrD = rasterizeMask w h $ (Mask pat1 (Just fea1))
+        imgD = matrixToImage arrD
+
+    saveImageLuna "foo2.png" imgD
+
+    P.putStrLn "Test rasterizeMask no fea --> fooD1.png"
 
     let pat = Path True points
         fea = Path True feather
-        arr = rasterizeMask w h $ (Mask pat (Just fea))
-        img = matrixToImage arr
 
-    saveImageLuna "foo2.png" img
+    let arrD = rasterizeMask w h $ (Mask pat Nothing)
+        imgD = matrixToImage arrD
+
+    saveImageLuna "fooD1.png" imgD
+
+    P.putStrLn "Test rasterizeMask --> fooD2.png"
+
+    let arrD = rasterizeMask w h $ (Mask pat (Just fea))
+        imgD = matrixToImage arrD
+
+    saveImageLuna "fooD2.png" imgD
+
+    P.putStrLn "Test rasterizeMask GUI example --> fooD3.png"
+
+    let patGUI = Path True pointsGUI 
+        feaGUI = Path True featherGUI
+        arrGUI = rasterizeMask w h (Mask patGUI (Just feaGUI))
+        imgGUI = matrixToImage arrGUI
+
+    saveImageLuna "fooD3.png" imgGUI
+
+
+
 
     return ()
+
 
 
 
