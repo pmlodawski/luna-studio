7a86858fddd4d51fb0820b4d1f5474fbe8729661
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2015-02-13 13:56:27 +0000

    Better noise parametrization

--- libs/data/graphics/src/Flowbox/Graphics/Mockup/Noise.hs
+++ libs/data/graphics/src/Flowbox/Graphics/Mockup/Noise.hs
@@ -9,35 +9,64 @@
 {-# LANGUAGE ViewPatterns     #-}
 
 module Flowbox.Graphics.Mockup.Noise (
-	billowLuna,
-	noiseLuna,
-	perlinLuna,
+    noiseLuna,
+    Quality(..),
+    NoiseParams(..)
 ) where
 
 import qualified Data.Array.Accelerate as A
 import           Math.Space.Space      (Grid (..))
 
 import           Flowbox.Graphics.Composition.Generator.Noise.Billow
+import           Flowbox.Graphics.Composition.Generator.Noise.Internal    (Quality(..))
 import           Flowbox.Graphics.Composition.Generator.Noise.Perlin
-import qualified Flowbox.Graphics.Composition.Transform              as Transform
-import           Flowbox.Graphics.Image.Image                        (Image)
-import qualified Flowbox.Graphics.Shader.Rasterizer                  as Shader
-import qualified Flowbox.Graphics.Shader.Sampler                     as Sampler
-import           Flowbox.Graphics.Shader.Shader                      (CartesianShader)
-import           Flowbox.Graphics.Utils.Accelerate                   (variable)
-import           Flowbox.Prelude                                     as P hiding (lookup)
+import           Flowbox.Graphics.Composition.Generator.Noise.RidgedMulti
+import qualified Flowbox.Graphics.Composition.Transform                   as Transform
+import           Flowbox.Graphics.Image.Image                             (Image)
+import qualified Flowbox.Graphics.Shader.Rasterizer                       as Shader
+import qualified Flowbox.Graphics.Shader.Sampler                          as Sampler
+import           Flowbox.Graphics.Shader.Shader                           (CartesianShader)
+import           Flowbox.Graphics.Utils.Accelerate                        (variable)
+import           Flowbox.Prelude                                          as P hiding (lookup)
 
 import Flowbox.Graphics.Mockup.Basic
 
-perlinLuna :: Float -> Int -> Int -> Image
-perlinLuna (variable -> z) = noiseLuna (perlinNoise z)
 
-billowLuna :: Float -> Int -> Int -> Image
-billowLuna (variable -> z) = noiseLuna (billowNoise z)
 
-noiseLuna :: (A.IsFloating a, A.Elt a, A.Lift A.Exp e, A.Plain e ~ Int)
-          => CartesianShader (A.Exp a) (A.Exp Float) -> e -> e -> Image
-noiseLuna noise (variable -> width) (variable -> height) = channelToImageRGBA noise'
+data NoiseParams = Perlin { quality     :: Quality
+                          , frequency   :: Float
+                          , lacunarity  :: Float
+                          , octaves     :: Int
+                          , persistence :: Float
+                          , seed        :: Int
+                          , z           :: Float
+                          }
+                 | Billow { quality     :: Quality
+                          , frequency   :: Float
+                          , lacunarity  :: Float
+                          , octaves     :: Int
+                          , persistence :: Float
+                          , seed        :: Int
+                          , z           :: Float
+                          }
+                 | RidgedMulti { quality    :: Quality
+                               , frequency  :: Float
+                               , lacunarity :: Float
+                               , octaves    :: Int
+                               , maxOctave  :: Int
+                               , seed       :: Int
+                               , exponent   :: Float
+                               , offset     :: Float
+                               , gain       :: Float
+                               , z          :: Float
+                               }
+
+noiseLuna :: NoiseParams -> Int -> Int -> Image
+noiseLuna noiseParams (variable -> width) (variable -> height) = channelToImageRGBA noise'
     where noise' = Shader.rasterizer $ Sampler.monosampler $ noiseShader
 
-          noiseShader = Transform.scale (Grid width height) noise
+          noiseShader = Transform.scale (Grid width height) $ case noiseParams of
+              Perlin q f l o p s z -> perlinGen q (variable f) (variable l) (variable o) (variable p) (variable s) (variable z)
+              Billow q f l o p s z -> billowGen q (variable f) (variable l) (variable o) (variable p) (variable s) (variable z)
+              RidgedMulti q f l oc m s e o g z -> ridgedMultiGen q (variable f) (variable l) (variable oc) (variable m) (variable s)
+                                                                   (variable e) (variable o) (variable g) (variable z)
