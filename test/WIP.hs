eaecbd68409b245d2d4e42d67bc8a7116e39eb97
Wojciech Danilo <wojciech.danilo@gmail.com>
2015-02-17 18:43:47 +0100

    Updated code generation tools. !!!WARNING!!! You have to update how the HSC is being printed or written to file. Look at libs/luna/pass/test/Main.hs for reference.

--- test/WIP.hs
+++ test/WIP.hs
@@ -25,19 +25,20 @@ import Luna.Target.HS
 #include "pragmas.cpp"
 
 -- ====== Main type ====== --
-data Main = Main
+data Main 
+    = Main
     deriving (Show,Eq,Ord,Generic,Typeable)
 $(registerType ''Main)
 
 -- ------ Main.Main constructor ------ --
-cons_Main = _member("Main") (val Cls_Main)
+cons_Main = _member ("Main") (val Cls_Main)
 memDef_Cls_Main_Main = liftCons0 Main
 
 -- ====== --------------- ====== --
 
 -- ====== Method: Cls_Main.Main ====== --
-memSig_Cls_Main_Main = _rtup1(_nuSigArg("self"))
-memFnc_Cls_Main_Main = (memSig_Cls_Main_Main,memDef_Cls_Main_Main)
+memSig_Cls_Main_Main = _rtup1 (_nuSigArg ("self"))
+memFnc_Cls_Main_Main = (memSig_Cls_Main_Main, memDef_Cls_Main_Main)
 $(registerMethod ''Cls_Main "Main")
 
 
@@ -46,31 +47,32 @@ $(registerMethod ''Cls_Main "Main")
 -- ===================================================================
 
 -- ====== Ala7 type ====== --
-data Ala7 = Ola3
+data Ala7 
+    = Ola3
     | Ola4
     deriving (Show,Eq,Ord,Generic,Typeable)
 $(registerType ''Ala7)
 
 -- ------ Ala7.Ola3 constructor ------ --
-cons_Ola3 = _member("Ola3") (val Cls_Ala7)
+cons_Ola3 = _member ("Ola3") (val Cls_Ala7)
 memDef_Cls_Ala7_Ola3 = liftCons0 Ola3
 
 -- ====== --------------- ====== --
 
 -- ====== Method: Cls_Ala7.Ola3 ====== --
-memSig_Cls_Ala7_Ola3 = _rtup1(_nuSigArg("self"))
-memFnc_Cls_Ala7_Ola3 = (memSig_Cls_Ala7_Ola3,memDef_Cls_Ala7_Ola3)
+memSig_Cls_Ala7_Ola3 = _rtup1 (_nuSigArg ("self"))
+memFnc_Cls_Ala7_Ola3 = (memSig_Cls_Ala7_Ola3, memDef_Cls_Ala7_Ola3)
 $(registerMethod ''Cls_Ala7 "Ola3")
 
 -- ------ Ala7.Ola4 constructor ------ --
-cons_Ola4 = _member("Ola4") (val Cls_Ala7)
+cons_Ola4 = _member ("Ola4") (val Cls_Ala7)
 memDef_Cls_Ala7_Ola4 = liftCons0 Ola4
 
 -- ====== --------------- ====== --
 
 -- ====== Method: Cls_Ala7.Ola4 ====== --
-memSig_Cls_Ala7_Ola4 = _rtup1(_nuSigArg("self"))
-memFnc_Cls_Ala7_Ola4 = (memSig_Cls_Ala7_Ola4,memDef_Cls_Ala7_Ola4)
+memSig_Cls_Ala7_Ola4 = _rtup1 (_nuSigArg ("self"))
+memFnc_Cls_Ala7_Ola4 = (memSig_Cls_Ala7_Ola4, memDef_Cls_Ala7_Ola4)
 $(registerMethod ''Cls_Ala7 "Ola4")
 
 
@@ -84,9 +86,9 @@ $(registerMethod ''Cls_Ala7 "Ola4")
 -- ===================================================================
 
 -- ====== Method: Main.main ====== --
-memSig_Main_main = _rtup1(_nuSigArg("self"))
+memSig_Main_main = _rtup1 (_nuSigArg ("self"))
 memDef_Main_main _self = val (5 :: Int)
-memFnc_Main_main = (memSig_Main_main,memDef_Main_main)
+memFnc_Main_main = (memSig_Main_main, memDef_Main_main)
 $(registerMethod ''Main "main")
 
 
