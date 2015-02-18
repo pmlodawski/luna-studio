bb42bfc2c60136ea08baa357e6238de61a112584
Piotr Mlodawski <remdezx@gmail.com>
2015-02-18 15:34:46 +0100

    WIP on reporting rendering progress

--- tools/batch/plugins/interpreter/src/Luna/Interpreter/RPC/Handler/Renderer.hs
+++ tools/batch/plugins/interpreter/src/Luna/Interpreter/RPC/Handler/Renderer.hs
@@ -7,6 +7,11 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Luna.Interpreter.RPC.Handler.Renderer where
 
+import qualified Pipes.Concurrent as Pipes
+
+import           Flowbox.Bus.Data.Flag                            (Flag)
+import           Flowbox.Bus.Data.Message                         (Message (Message))
+import qualified Flowbox.Bus.Data.Message                         as Message
 import           Flowbox.Bus.RPC.RPC                              (RPC)
 import           Flowbox.Data.Convert
 import           Flowbox.Prelude                                  hiding (Context)
@@ -14,11 +19,16 @@ import           Flowbox.ProjectManager.Context                   (Context)
 import           Flowbox.System.Log.Logger                        hiding (error)
 import qualified Generated.Proto.Renderer.Renderer.Render.Request as Render
 import qualified Generated.Proto.Renderer.Renderer.Render.Update  as Render
+import qualified Luna.Interpreter.RPC.Topic                            as Topic
 import           Luna.Interpreter.RPC.Handler.Lift                (liftSession)
+import           Flowbox.Bus.Data.Topic                                ((/+))
+import qualified Flowbox.Text.ProtocolBuffers                          as Proto
 import           Luna.Interpreter.Session.Memory.Manager          (MemoryManager)
+import qualified Flowbox.Bus.Data.Flag                                 as Flag
 import           Luna.Interpreter.Session.Session                 (SessionST)
 import           Luna.Renderer.Proto.FrameRange                   ()
 import qualified Luna.Renderer.Renderer                           as Renderer
+import qualified Generated.Proto.Renderer.Renderer.Render.Progress as Gen
 
 
 
@@ -32,3 +42,15 @@ render request@(Render.Request ranges _) = do
     liftSession $ Renderer.render (decodeP ranges)
     logger info "Render not implemented"
     return $ Render.Update request
+
+
+reportProgress :: Message.CorrelationID
+               -> Pipes.Output (Message, Message.CorrelationID, Flag)
+               -> Int -> Int -> IO ()
+reportProgress crl output current total = do
+    let response = Gen.Progress (encodeP current) (encodeP total)
+        topic    = Topic.rendererRenderRequest /+ "progress"
+        msg      = Message topic $ Proto.messagePut' response
+        packet   = (msg, crl, Flag.Disable)
+    logger debug $ "Rendering progress " <> show current <> " / " <> show total
+    void $ Pipes.atomically $ Pipes.send output packet
