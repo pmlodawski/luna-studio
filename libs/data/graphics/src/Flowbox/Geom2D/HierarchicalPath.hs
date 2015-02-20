bc4152205986aab5fe2ca975a3b17ef86def2282
marcindziadus <dziadus.marcin@gmail.com>
2015-02-20 15:24:35 +0000

    conversion is done

--- libs/data/graphics/src/Flowbox/Geom2D/HierarchicalPath.hs
+++ libs/data/graphics/src/Flowbox/Geom2D/HierarchicalPath.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Flowbox.Geom2D.HierarchicalPath where
 
 import qualified Flowbox.Geom2D.ControlPoint            as C
@@ -10,22 +11,26 @@ import           Linear                                 (V2 (..))
 import           Math.Coordinate.Cartesian
 import           Data.IntMap                            as I
 import           Data.Vector                            as V
+import           Data.Vector.Mutable                    as MV
 import           Data.Maybe
 import           Control.Monad.ST
-import           Control.Monad
+import           Control.Monad                          as C
 import           Data.STRef
+import           Data.List                              as L
 
 type Rank      = Int
 type Angles a  = (a, a)
 type Handles a = (V2 a, V2 a)
 
-data ControlPoint a = BasePoint      { coords  :: Point2 a
-                                     , handles :: Handles a
+data ControlPoint a = BasePoint      { _coords  :: Point2 a
+                                     , _handles :: Handles a
                                      }
-                    | DependentPoint { rank    :: Rank
-                                     , angles  :: Angles a
-                                     , handles :: Handles a
+                    | DependentPoint { _rank    :: Rank
+                                     , _angles  :: Angles a
+                                     , _handles :: Handles a
                                      } deriving (Eq, Ord, Show)
+
+makeLenses ''ControlPoint
 
 data Path a = Path { isClosed      :: Bool
                    , controlPoints :: [ControlPoint a]
@@ -36,31 +41,44 @@ data OrdinaryControlPoint a = OrdinaryControlPoint { controlPoint :: Point2 a
                                    , handleOut    :: Maybe (Point2 a)
                                    } deriving (Eq, Ord, Show)
 
-type Parents   = (Maybe Int, Maybe Int)
+type Parents = (Maybe Int, Maybe Int)
 
-extract :: ControlPoint a -> Int
-extract x = 
-    case x of
+convert :: (Fractional a, Floating a) => [ControlPoint a] -> [OrdinaryControlPoint a]
+convert points = P.map convertEl $ P.zip coords points
+  where
+    ranks   = P.map extract points
+    parents = makeTree ranks
+    order   = P.map (\(_,y) -> y) $ L.sort $ P.zip ranks [0..]
+    coords  = walk (V.fromList points) parents order
+
+    convertEl :: (Fractional a, Floating a) => (Point2 a, ControlPoint a) -> OrdinaryControlPoint a
+    convertEl (center, cPoint) = absCoords center cPoint
+      where
+        absCoords :: (Fractional a, Floating a) => Point2 a -> ControlPoint a -> OrdinaryControlPoint a
+        absCoords center p = OrdinaryControlPoint center (Just (Transform.translate h1 center)) (Just (Transform.translate h2 center))
+          where
+            (h1, h2) = p ^. handles
+
+    extract :: ControlPoint a -> Int
+    extract x = 
+      case x of
         BasePoint _ _ -> 0
         DependentPoint r _ _ -> r
 
-helper :: [ControlPoint a] -> IntMap Parents
-helper points = makeTree ranks
-    where
-        ranks = P.map extract points
+walk :: (Fractional a, Floating a) => Vector (ControlPoint a) -> IntMap Parents -> [Int] -> [Point2 a]
+walk points parents order = P.map (\(_,y) -> y) $ I.toList $ aux order coords
+  where
+    coords = I.empty :: IntMap (Point2 a)
 
-convert :: [ControlPoint a] -> [OrdinaryControlPoint a] -> [OrdinaryControlPoint a]
-convert [] acc = P.reverse acc
-
-walk :: (Fractional a, Floating a) => [ControlPoint a] -> IntMap (Point2 a)
-walk l = 
-  let
-    go [] m = readSTRef m
-    go _ m  = readSTRef m
-  in
-    runST $ do
-      m <- newSTRef (I.empty :: IntMap (Point2 a))
-      go l m
+    aux [] coords = coords
+    aux (idx:l) coords = 
+      case p of
+        DependentPoint _ ang (h1,h2) -> let (Just p1, Just p2) = parents I.! idx
+                                        in
+                                          aux l (I.insert idx (computeCoords (coords I.! p1) (coords I.! p2) ang) coords)
+        BasePoint c (h1,h2) -> aux l (I.insert idx c coords)
+      where
+        p = points V.! idx
 
 computeCoords :: (Fractional a, Floating a) => Point2 a -> Point2 a -> Angles a -> Point2 a
 computeCoords p1@(Point2 x1 y1) p2@(Point2 x2 y2) (ang1, ang2) = Transform.rotate ang1 (movePoint p1 p2 ratio)
