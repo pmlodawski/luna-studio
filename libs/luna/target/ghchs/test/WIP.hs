6c6a8827dc24cad2d654b9d95a56fedc6364bff8
Wojciech Danilo <wojciech.danilo@gmail.com>
2015-02-23 20:38:18 +0100

    COMPILER-160

--- libs/luna/target/ghchs/test/WIP.hs
+++ libs/luna/target/ghchs/test/WIP.hs
@@ -41,98 +41,18 @@ $(registerMethod ''Cls_Main "Main")
 
 
 -- ===================================================================
--- Data headers
--- ===================================================================
-
--- ====== Float type ====== --
--- datatype provided externally
-$(registerType ''Float)
-
--- ====== RGB type ====== --
-data RGB 
-    = RGB Float Float Float
-    deriving (Show,Eq,Ord,Generic,Typeable)
-$(registerType ''RGB)
-$(registerCons ''RGB ["RGB"])
-
--- ------ RGB accessors ------ --
-$(generateFieldAccessors ''RGB [('RGB, [Just "r", Just "g", Just "b"])])
-$(registerFieldAccessors ''RGB ["b", "g", "r"])
-
--- ------ RGB.RGB constructor ------ --
-memDef_Cls_RGB_RGB = liftCons3 RGB
-
--- ====== Method: Cls_RGB.RGB ====== --
-memSig_Cls_RGB_RGB = _rtup4 (_nuSigArg ("self"), _nuSigArg ("r"), _nuSigArg ("g"), _nuSigArg ("b"))
-memFnc_Cls_RGB_RGB = (memSig_Cls_RGB_RGB, memDef_Cls_RGB_RGB)
-$(registerMethod ''Cls_RGB "RGB")
-
--- ====== Mult type ====== --
-data Mult a 
-    = Mult a Float
-    deriving (Show,Eq,Ord,Generic,Typeable)
-$(registerType ''Mult)
-$(registerCons ''Mult ["Mult"])
-
--- ------ Mult accessors ------ --
-$(generateFieldAccessors ''Mult [('Mult, [Just "data", Just "mult"])])
-$(registerFieldAccessors ''Mult ["data", "mult"])
-
--- ------ Mult.Mult constructor ------ --
-memDef_Cls_Mult_Mult = liftCons2 Mult
-
--- ====== Method: Cls_Mult.Mult ====== --
-memSig_Cls_Mult_Mult = _rtup3 (_nuSigArg ("self"), _nuSigArg ("data"), _nuSigArg ("mult"))
-memFnc_Cls_Mult_Mult = (memSig_Cls_Mult_Mult, memDef_Cls_Mult_Mult)
-$(registerMethod ''Cls_Mult "Mult")
-
-
--- ===================================================================
--- Data declarations
--- ===================================================================
-
--- ====== Method: RGB.toRGB ====== --
-memSig_RGB_toRGB = _rtup1 (_nuSigArg ("self"))
-memDef_RGB_toRGB _self = _self
-memFnc_RGB_toRGB = (memSig_RGB_toRGB, memDef_RGB_toRGB)
-$(registerMethod ''RGB "toRGB")
-
--- ====== Method: Mult.toRGB ====== --
-memSig_Mult_toRGB = _rtup1 (_nuSigArg ("self"))
-memDef_Mult_toRGB _self = do 
-    _r <- _call (0) (_member ("r") (_call (1) (_member ("data") _self)))
-    _g <- _call (2) (_member ("g") (_call (3) (_member ("data") _self)))
-    _b <- _call (4) (_member ("b") (_call (5) (_member ("data") _self)))
-    _call (6) (appNext _b (appNext _g (appNext (_call (7) (appNext _r (_member ("_star") (_member ("mult") _self)))) cons_RGB)))
-    _call (8) (appNext _b (appNext _g (appNext (_call (9) (appNext (_call (10) (_member ("mult") _self)) (_member ("_star") _r))) cons_RGB)))
-memFnc_Mult_toRGB = (memSig_Mult_toRGB, memDef_Mult_toRGB)
-$(registerMethod ''Mult "toRGB")
-
-
--- ===================================================================
 -- Module declarations
 -- ===================================================================
 
--- ====== Method: Main.print ====== --
-memSig_Main_print = _rtup2 (_nuSigArg ("self"), _nuSigArg ("msg"))
-memDef_Main_print self msg = autoLift1 print msg
-memFnc_Main_print = (memSig_Main_print, memDef_Main_print)
-$(registerMethod ''Main "print")
-
--- ====== Method: Float._star ====== --
-memSig_Float__star = _rtup2 (_nuSigArg ("self"), _nuSigArg ("a"))
-memDef_Float__star self a = liftF2 (*) self a
-memFnc_Float__star = (memSig_Float__star, memDef_Float__star)
-$(registerMethod ''Float "_star")
+-- ====== Method: Main.foo ====== --
+memSig_Main_foo = _rtup3 (_nuSigArg ("self"), _nuSigArg ("a"), _nuSigArg ("b"))
+memDef_Main_foo _self _a _b = val (1 :: Int)
+memFnc_Main_foo = (memSig_Main_foo, memDef_Main_foo)
+$(registerMethod ''Main "foo")
 
 -- ====== Method: Main.main ====== --
 memSig_Main_main = _rtup1 (_nuSigArg ("self"))
-memDef_Main_main _self = do 
-    _call (11) (appNext (val ("color test" :: String)) (_member ("print") _self))
-    _rgb <- _call (12) (appNext (val (3.0 :: Float)) (appNext (val (2.0 :: Float)) (appNext (val (1.0 :: Float)) cons_RGB)))
-    _mult <- _call (13) (appNext (val (0.5 :: Float)) (appNext _rgb cons_Mult))
-    _call (14) (appNext (_call (15) (_member ("toRGB") _rgb)) (_member ("print") _self))
-    _call (16) (appNext (_call (17) (_member ("toRGB") _mult)) (_member ("print") _self))
+memDef_Main_main _self = _call (0) (appNext (val (3 :: Int)) (appNext (val (2 :: Int)) (_member ("foo") _self)))
 memFnc_Main_main = (memSig_Main_main, memDef_Main_main)
 $(registerMethod ''Main "main")
 
