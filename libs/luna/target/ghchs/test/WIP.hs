5fa751fc7ba2e5b629e00729b0c40185097cc528
Wojciech Danilo <wojciech.danilo@gmail.com>
2015-02-24 02:15:21 +0100

    COMPILER-159

--- libs/luna/target/ghchs/test/WIP.hs
+++ libs/luna/target/ghchs/test/WIP.hs
@@ -19,7 +19,6 @@
 module Main where
 
 -- imports --
-import Data.Maybe (fromJust)
 import Luna.Target.HS
 
 -- body --
@@ -42,24 +41,52 @@ $(registerMethod ''Cls_Main "Main")
 
 
 -- ===================================================================
+-- Data headers
+-- ===================================================================
+
+-- ====== Float type ====== --
+-- datatype provided externally
+$(registerType ''Float)
+
+
+-- ===================================================================
+-- Data declarations
+-- ===================================================================
+
+
+-- ===================================================================
 -- Module declarations
 -- ===================================================================
 
 -- ====== Method: Main.print ====== --
-memSig_Main_print = _rtup2 (_nuSigArg ("self"), _npSigArg ("s", val ("" :: String)))
-memDef_Main_print self s = polyJoin . liftF1 (Value . fmap Safe . print) $ s
+memSig_Main_print = _rtup2 (_nuSigArg ("self"), _nuSigArg ("msg"))
+memDef_Main_print self msg = autoLift1 print msg
 memFnc_Main_print = (memSig_Main_print, memDef_Main_print)
 $(registerMethod ''Main "print")
 
+-- ====== Method: Float._star ====== --
+memSig_Float__star = _rtup2 (_nuSigArg ("self"), _nuSigArg ("a"))
+memDef_Float__star self a = liftF2 (*) self a
+memFnc_Float__star = (memSig_Float__star, memDef_Float__star)
+$(registerMethod ''Float "_star")
+
+-- ====== Method: Main._star ====== --
+memSig_Main__star = _rtup3 (_nuSigArg ("self"), _nuSigArg ("a"), _nuSigArg ("b"))
+memDef_Main__star _self _a _b = _call (0) (appNext _b (_member ("_star") _a))
+memFnc_Main__star = (memSig_Main__star, memDef_Main__star)
+$(registerMethod ''Main "_star")
+
 -- ====== Method: Main.foo ====== --
 memSig_Main_foo = _rtup3 (_nuSigArg ("self"), _nuSigArg ("a"), _nuSigArg ("b"))
-memDef_Main_foo _self _a _b = _a
+memDef_Main_foo _self _a _b = _call (1) (appNext _b (_member ("_star") _a))
 memFnc_Main_foo = (memSig_Main_foo, memDef_Main_foo)
 $(registerMethod ''Main "foo")
 
 -- ====== Method: Main.main ====== --
 memSig_Main_main = _rtup1 (_nuSigArg ("self"))
-memDef_Main_main _self = _call (0) (appNext (val (3 :: Int)) (appNext (val (2 :: Int)) (_member ("foo") _self)))
+memDef_Main_main _self = do 
+    _call (2) (appNext (_call (3) (appNext (val (2.0 :: Float)) (appNext (val (1.0 :: Float)) (_member ("_star") _self)))) (_member ("print") _self))
+    _call (4) (appNext (_call (5) (appNext (val (4.0 :: Float)) (appNext (val (3.0 :: Float)) (_member ("foo") _self)))) (_member ("print") _self))
 memFnc_Main_main = (memSig_Main_main, memDef_Main_main)
 $(registerMethod ''Main "main")
 
