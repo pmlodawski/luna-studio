2ac98db6c60712521699a40fd0c1b42e4c325be5
Konrad Gądek <kgadek@gmail.com>
2015-02-25 18:44:38 +0100

    Revert "Fixing many small errors, new sandboxes!"
    This reverts commit 58b819e5bff04f810efab78149270a656b814200.
    
    Conflicts:
    	scripts/config/packages.py
    	scripts/init.py

--- src/Luna/Data/Origin.hs
+++
@@ -1,9 +0,0 @@
-module Luna.Data.Origin where
-
-import           Luna.Syntax.Name.Path  (NamePath, QualPath)
-
-
---data Origin = Module { _path :: QualPath }
---            | Func   { _name :: NamePath } 
-
---data Module
