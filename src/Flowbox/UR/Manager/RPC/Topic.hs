8fa26834f77163a5d53cd0dd53d72b02292db295
srutownik <marek.28.93@gmail.com>
2015-03-04 15:08:23 +0100

    missing file

---
+++ src/Flowbox/UR/Manager/RPC/Topic.hs
@@ -0,0 +1,15 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2014
+---------------------------------------------------------------------------
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+
+module Flowbox.UR.Manager.RPC.Topic where
+
+urmRegisterRequest         = "urm.register.request"
+urmRegisterMultipleRequest = "urm.registermultiple.request"
+urmRedoRequest             = "urm.redo.request" 
+urmPingRequest             = "urm.ping.request"         
+urmUndoRequest             = "urm.undo.request" 
