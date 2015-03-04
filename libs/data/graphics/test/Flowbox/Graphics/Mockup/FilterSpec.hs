f6e7aa885eceb779524f21cc6cb656328acbb773
Krzysztof Rudko <chris.rudko@gmail.com>
2015-03-04 10:23:26 +0100

    Test workaround for edgeDetectLuna. Saved images are clamped, processed images not necessarily. May be a problem in current test environment.

--- libs/data/graphics/test/Flowbox/Graphics/Mockup/FilterSpec.hs
+++ libs/data/graphics/test/Flowbox/Graphics/Mockup/FilterSpec.hs
@@ -8,8 +8,10 @@ import Flowbox.Graphics.Composition.EdgeBlur as EB
 import qualified Flowbox.Math.Matrix as M
 import System.IO.Unsafe
 import Control.Monad
-
-
+import qualified Flowbox.Graphics.Image.Image   as Img
+import qualified Flowbox.Graphics.Image.Channel as Chan
+import qualified Flowbox.Graphics.Image.View    as View
+import Flowbox.Graphics.Utils.Utils (clamp') 
 
 import Flowbox.Prelude as P
 
@@ -46,7 +48,7 @@ spec = do
                             pending
                             (testSave =<< actualImage) `shouldReturn` () --(testSave =<< actualImage)
                     describe "should match reference image" $ do
-                        let actualImage = liftM (edgeDetectLuna (Sobel Horizontal)) $ loadImageLuna "./test/samples/lena.png"
+                        let actualImage = liftM (Img.map (View.map (Chan.unsafeMap (Chan.FunFloat (clamp' 0 1))))) $ liftM (edgeDetectLuna (Sobel Horizontal)) $ loadImageLuna "./test/samples/lena.png"
                             expectedImage = getDefaultTestPic specPath testName
                         it "in pixel-wise metric" $ do
                             returnShouldBeCloseTo testPath PixelWise actualImage expectedImage
