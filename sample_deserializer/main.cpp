eda4d372061396e834e2f2c25bcffdd6d9c0bbc8
mwu <mwu-tow@gazeta.pl>
2015-03-25 16:11:15 +0100

    Commented-out old, hand-written sample code. Now sample will test the actual generated code.

--- sample_deserializer/main.cpp
+++ sample_deserializer/main.cpp
@@ -15,38 +15,38 @@ int main()
 	return 0;
 }
 
-std::shared_ptr<Expr> Expr::deserializeFrom(Input &input)
-{
-	auto constructorIndex = readInt8(input);
-	switch(constructorIndex)
-	{
-	case 0: return ConstructorSelector<Expr, 0>::ConstructorType::deserializeFrom(input);
-	case 1: return ConstructorSelector<Expr, 1>::ConstructorType::deserializeFrom(input);
-	case 10: return ConstructorSelector<Expr, 10>::ConstructorType::deserializeFrom(input);
-	default: return nullptr;
-	}
-}
-
-std::shared_ptr<Expr_NOP> Expr_NOP::deserializeFrom(Input &input)
-{
-	auto ret = std::make_shared<Expr_NOP>();
-	ret->_id = readInt64(input);
-	return ret;
-}
-
-std::shared_ptr<Expr_Accessor> Expr_Accessor::deserializeFrom(Input &input)
-{
-	auto ret = std::make_shared<Expr_Accessor>();
-	ret->_id = readInt64(input);
-// 	Accessor::deserializeFrom(ret->_acc);
-// 	Expr::deserializeFrom(ret->_dst);
-	return ret;
-}
-
-std::shared_ptr<Expr_Con> Expr_Con::deserializeFrom(Input &input)
-{
-	auto ret = std::make_shared<Expr_Con>();
-	ret->_id = readInt64(input);
-	deserialize(ret->name, input);
-	return ret;
-}
+// std::shared_ptr<Expr> Expr::deserializeFrom(Input &input)
+// {
+// 	auto constructorIndex = readInt8(input);
+// 	switch(constructorIndex)
+// 	{
+// 	case 0: return ConstructorSelector<Expr, 0>::ConstructorType::deserializeFrom(input);
+// 	case 1: return ConstructorSelector<Expr, 1>::ConstructorType::deserializeFrom(input);
+// 	case 10: return ConstructorSelector<Expr, 10>::ConstructorType::deserializeFrom(input);
+// 	default: return nullptr;
+// 	}
+// }
+// 
+// std::shared_ptr<Expr_NOP> Expr_NOP::deserializeFrom(Input &input)
+// {
+// 	auto ret = std::make_shared<Expr_NOP>();
+// 	ret->_id = readInt64(input);
+// 	return ret;
+// }
+// 
+// std::shared_ptr<Expr_Accessor> Expr_Accessor::deserializeFrom(Input &input)
+// {
+// 	auto ret = std::make_shared<Expr_Accessor>();
+// 	ret->_id = readInt64(input);
+// // 	Accessor::deserializeFrom(ret->_acc);
+// // 	Expr::deserializeFrom(ret->_dst);
+// 	return ret;
+// }
+// 
+// std::shared_ptr<Expr_Con> Expr_Con::deserializeFrom(Input &input)
+// {
+// 	auto ret = std::make_shared<Expr_Con>();
+// 	ret->_id = readInt64(input);
+// 	deserialize(ret->name, input);
+// 	return ret;
+// }
