fd96fc30aa73618dfcc770350a08cfdc5f3c78ce
Piotr Mlodawski <remdezx@gmail.com>
2015-03-27 14:27:57 +0100

    WIP on new graphs: fixing wrong output nodes

--- src/Luna/Util/Label.hs
+++ src/Luna/Util/Label.hs
@@ -7,25 +7,34 @@
 
 module Luna.Util.Label where
 
-import           Flowbox.Prelude          hiding (cons)
-import           Luna.Syntax.Arg          (Arg)
-import qualified Luna.Syntax.Arg          as A
-import           Luna.Syntax.Decl         (LDecl)
-import qualified Luna.Syntax.Decl         as Decl
-import           Luna.Syntax.Expr         (LExpr)
-import qualified Luna.Syntax.Expr         as Expr
-import           Luna.Syntax.Foreign      (Foreign (Foreign))
-import           Luna.Syntax.Label        (Label (Label))
-import qualified Luna.Syntax.Label        as Label
-import qualified Luna.Syntax.Lit          as Lit
-import           Luna.Syntax.Module       (LModule)
-import qualified Luna.Syntax.Module       as Module
-import qualified Luna.Syntax.Name.Pattern as Pattern
-import qualified Luna.Syntax.Native       as Native
-import qualified Luna.Syntax.Pat          as Pat
-import           Luna.Syntax.Type         (LType)
-import qualified Luna.Syntax.Type         as Type
-
+import           Flowbox.Prelude                  hiding (cons)
+import           Luna.Syntax.Arg                  (Arg)
+import qualified Luna.Syntax.Arg                  as A
+import           Luna.Syntax.Decl                 (LDecl)
+import qualified Luna.Syntax.Decl                 as Decl
+import           Luna.Syntax.Expr                 (LExpr)
+import qualified Luna.Syntax.Expr                 as Expr
+import           Luna.Syntax.Foreign              (Foreign (Foreign))
+import           Luna.Syntax.Graph.DefaultsMap    (DefaultsMap)
+import qualified Luna.Syntax.Graph.DefaultsMap    as DefaultsMap
+import           Luna.Syntax.Graph.Graph          (Graph)
+import qualified Luna.Syntax.Graph.Graph          as Graph
+import           Luna.Syntax.Graph.Node           (Node)
+import qualified Luna.Syntax.Graph.Node           as Node
+import           Luna.Syntax.Graph.Node.Expr      (NodeExpr)
+import qualified Luna.Syntax.Graph.Node.Expr      as NodeExpr
+import           Luna.Syntax.Graph.Node.MultiPart (MultiPartExpr, MultiPartSegment)
+import qualified Luna.Syntax.Graph.Node.MultiPart as MultiPartExpr
+import           Luna.Syntax.Label                (Label (Label))
+import qualified Luna.Syntax.Label                as Label
+import qualified Luna.Syntax.Lit                  as Lit
+import           Luna.Syntax.Module               (LModule)
+import qualified Luna.Syntax.Module               as Module
+import qualified Luna.Syntax.Name.Pattern         as Pattern
+import qualified Luna.Syntax.Native               as Native
+import qualified Luna.Syntax.Pat                  as Pat
+import           Luna.Syntax.Type                 (LType)
+import qualified Luna.Syntax.Type                 as Type
 
 
 
@@ -34,18 +43,15 @@ replace conv = (Label.label %~ conv)
              . (Label.element . Module.body %~ map (replaceDecl conv))
 
 
-
 replaceCons :: (l1 -> l2) -> Decl.LCons l1 (LExpr l1 v) -> Decl.LCons l2 (LExpr l2 v)
 replaceCons conv (Label l1 (Decl.Cons name fields)) =
     Label (conv l1) $ Decl.Cons name $ map (replaceField conv) fields
-
 
 
 replaceDataDecl :: (l1 -> l2) -> Decl.DataDecl l1 (LExpr l1 v) -> Decl.DataDecl l2 (LExpr l2 v)
 replaceDataDecl conv (Decl.DataDecl name params cons decls) =
     Decl.DataDecl name params (map (replaceCons conv) cons)
                               (map (replaceDecl conv) decls)
-
 
 
 replaceDecl :: (l1 -> l2) -> LDecl l1 (LExpr l1 v) -> LDecl l2 (LExpr l2 v)
@@ -59,11 +65,9 @@ replaceDecl conv (Label l1 decl) = Label (conv l1) $ case decl of
     Decl.Pragma  pragma          -> Decl.Pragma  pragma
 
 
-
 replaceField :: (l1 -> l2) -> Decl.LField l1 (LExpr l1 v) -> Decl.LField l2 (LExpr l2 v)
 replaceField conv (Label l1 (Decl.Field ftype fname fval)) =
     Label (conv l1) $ Decl.Field (replaceType conv ftype) fname (fmap (replaceExpr conv) fval)
-
 
 
 replaceFuncDecl :: (l1 -> l2) -> Decl.FuncDecl l1 (LExpr l1 v) [LExpr l1 v] -> Decl.FuncDecl l2 (LExpr l2 v) [LExpr l2 v]
@@ -72,16 +76,13 @@ replaceFuncDecl conv (Decl.FuncDecl path sig output body) =
                        (map (replaceExpr conv) body)
 
 
-
 replaceFuncDecl' :: (l1 -> l2) -> Decl.FuncDecl l1 (LExpr l1 v) a -> Decl.FuncDecl l2 (LExpr l2 v) a
 replaceFuncDecl' conv (Decl.FuncDecl path sig output body) =
     Decl.FuncDecl path (replaceArgPat conv sig) (replaceFuncOutput conv output) body
 
 
-
 replaceLabel :: (l1 -> l2) -> Label l1 a -> Label l2 a
 replaceLabel conv = Label.label %~ conv
-
 
 
 replaceType :: (l1 -> l2) -> LType l1 -> LType l2
@@ -119,10 +120,8 @@ replaceExpr conv (Label l1 expr) = Label (conv l1) $ case expr of
     Expr.Wildcard                       -> Expr.Wildcard
 
 
-
 replaceForeign :: (l1 -> l2) -> Foreign (Decl.ForeignDecl l1 (LExpr l1 v)) -> Foreign (Decl.ForeignDecl l2 (LExpr l2 v))
 replaceForeign conv (Foreign target a) = Foreign target $ replaceForeignDecl conv a
-
 
 
 replaceForeignDecl :: (l1 -> l2) -> Decl.ForeignDecl l1 (LExpr l1 v) -> Decl.ForeignDecl l2 (LExpr l2 v)
@@ -130,7 +129,6 @@ replaceForeignDecl conv foreignDecl = case foreignDecl of
     Decl.FData dataDecl -> Decl.FData (replaceDataDecl  conv dataDecl)
     Decl.FFunc funcDecl -> Decl.FFunc (replaceFuncDecl' conv funcDecl)
     Decl.FImp  text     -> Decl.FImp  text
-
 
 
 replaceArgPat :: (l1 -> l2) -> Pattern.NamePat Pattern.SegmentName (Arg l1 (LExpr l1 v))
@@ -141,10 +139,8 @@ replaceArgPat conv (Pattern.NamePat prefix base segmentList) =
                     (map (replaceSegment conv) segmentList)
 
 
-
 replaceSegment :: (l1 -> l2) -> Pattern.Segment Pattern.SegmentName (Arg l1 (LExpr l1 v)) -> Pattern.Segment Pattern.SegmentName (Arg l2 (LExpr l2 v))
 replaceSegment conv (Pattern.Segment base args) = Pattern.Segment base (fmap (replaceArg conv) args)
-
 
 
 replaceArg :: (l1 -> l2) -> (Arg l1 (LExpr l1 v)) -> (Arg l2 (LExpr l2 v))
@@ -152,29 +148,25 @@ replaceArg conv (A.Arg pat val) = A.Arg (conv' pat) (fmap (replaceExpr conv) val
         where conv' (Label l1 pat) = Label (conv l1) (replacePat conv pat)
 
 
-
 replacePat :: (l1 -> l2) -> Pat.Pat l1 -> Pat.Pat l2
 replacePat conv a = case a of
-         Pat.App src args     -> Pat.App   (replaceLPat conv src) (fmap (replaceLPat conv) args)
-         Pat.Typed pat typ    -> Pat.Typed (replaceLPat conv pat) (replaceType conv typ)
-         Pat.Grouped pat      -> Pat.Grouped (replaceLPat conv pat)
-         Pat.Lit (Label l1 a) -> Pat.Lit (Label (conv l1) a)
-         Pat.Tuple items      -> Pat.Tuple (fmap (replaceLPat conv) items)
-         Pat.Con cname        -> Pat.Con cname
-         Pat.Var vname        -> Pat.Var vname
-         Pat.Wildcard         -> Pat.Wildcard
-         Pat.RecWildcard      -> Pat.RecWildcard
-
+    Pat.App src args     -> Pat.App   (replaceLPat conv src) (fmap (replaceLPat conv) args)
+    Pat.Typed pat typ    -> Pat.Typed (replaceLPat conv pat) (replaceType conv typ)
+    Pat.Grouped pat      -> Pat.Grouped (replaceLPat conv pat)
+    Pat.Lit (Label l1 a) -> Pat.Lit (Label (conv l1) a)
+    Pat.Tuple items      -> Pat.Tuple (fmap (replaceLPat conv) items)
+    Pat.Con cname        -> Pat.Con cname
+    Pat.Var vname        -> Pat.Var vname
+    Pat.Wildcard         -> Pat.Wildcard
+    Pat.RecWildcard      -> Pat.RecWildcard
 
 
 replaceLPat :: (l1 -> l2) -> Label l1 (Pat.Pat l1) -> Label l2 (Pat.Pat l2)
 replaceLPat conv (Label l1 pat) = Label (conv l1) (replacePat conv pat)
 
 
-
 replaceLit :: (l1 -> l2) -> Lit.LLit l1 -> Lit.LLit l2
 replaceLit conv (Label l1 lit) = Label (conv l1) lit
-
 
 
 replaceList :: (l1 -> l2) -> (Expr.List (LExpr l1 v)) -> (Expr.List (LExpr l2 v))
@@ -183,19 +175,16 @@ replaceList conv list = case list of
     Expr.RangeList seq -> Expr.RangeList (replaceSequence  conv seq)
 
 
-
 replaceSequence :: (l1 -> l2) -> Expr.Sequence (LExpr l1 v) -> Expr.Sequence (LExpr l2 v)
 replaceSequence conv seq = case seq of
                         Expr.Linear a b -> Expr.Linear (replaceExpr conv a) (fmap (replaceExpr conv) b)
                         Expr.Geometric a a1 b -> Expr.Geometric (replaceExpr conv a) (replaceExpr conv a1) (fmap (replaceExpr conv) b)
 
 
-
 replaceNative :: (l1 -> l2) -> Native.Native (LExpr l1 v) -> Native.Native (LExpr l2 v)
 replaceNative conv native = case native of
     Native.AST a  -> Native.AST $ replaceExpr conv a
     Native.Code segments -> Native.Code segments
-
 
 
 replaceNamePat :: (l1 -> l2) -> Pattern.NamePat (LExpr l1 v) (Expr.AppArg (LExpr l1 v)) ->
@@ -206,25 +195,20 @@ replaceNamePat conv (Pattern.NamePat pref b sl) = Pattern.NamePat newPref newB n
           newSl   = map (replaceSegmentApp conv) sl
 
 
-
 replaceAppArg :: (l1 -> l2) -> Expr.AppArg (LExpr l1 v) -> Expr.AppArg (LExpr l2 v)
 replaceAppArg conv (Expr.AppArg margn expr) = Expr.AppArg margn (replaceExpr conv expr)
-
 
 
 replaceSegmentApp :: (l1 -> l2) -> Pattern.Segment Pattern.SegmentName (Expr.AppArg (LExpr l1 v)) -> Pattern.Segment Pattern.SegmentName (Expr.AppArg (LExpr l2 v))
 replaceSegmentApp conv (Pattern.Segment base args) = Pattern.Segment base (fmap (replaceAppArg conv) args)
 
 
-
 replaceFuncOutput :: (l1 -> l2) -> Maybe (Type.LType l1) -> Maybe (Type.LType l2)
 replaceFuncOutput conv funcout = fmap (replaceType conv) funcout
 
 
-
 replaceFieldUpd :: (l1 -> l2) -> Expr.FieldUpd l1 v -> Expr.FieldUpd l2 v
 replaceFieldUpd conv (Expr.FieldUpd sel lexpr) = Expr.FieldUpd sel (replaceExpr conv lexpr)
-
 
 
 replaceExprArg :: (l1 -> l2) -> Expr.ExprArg l1 v -> Expr.ExprArg l2 v
@@ -235,4 +219,37 @@ replaceMatch :: (l1 -> l2) -> Expr.LMatch l1 v -> Expr.LMatch l2 v
 replaceMatch conv (Label l1 (Expr.Match lpat mbody)) = Label (conv l1) (Expr.Match newPat newMBody)
     where newPat   = replaceLPat conv lpat
           newMBody = map (replaceExpr conv) mbody
+
+
+replaceGraph :: (l1 -> l2) -> Graph l1 v -> Graph l2 v
+replaceGraph conv = Graph.nmap (replaceNode conv)
+
+
+replaceNode :: (l1 -> l2) -> Node l1 v -> Node l2 v
+replaceNode conv node = case node of
+    Node.Expr expr outputPat defaults pos -> Node.Expr (replaceNodeExpr conv expr)
+                                                       (replaceLPat conv <$> outputPat)
+                                                       (replaceDefaultsMap conv defaults)
+                                                       pos
+    Node.Inputs pos -> Node.Inputs pos
+    Node.Outputs defaults pos -> Node.Outputs (replaceDefaultsMap conv defaults) pos
+
+
+replaceDefaultsMap :: (l1 -> l2) -> DefaultsMap l1 v -> DefaultsMap l2 v
+replaceDefaultsMap conv = DefaultsMap.map (replaceExpr conv)
+
+
+replaceNodeExpr :: (l1 -> l2) -> NodeExpr l1 v -> NodeExpr l2 v
+replaceNodeExpr conv nodeExpr = case nodeExpr of
+    NodeExpr.StringExpr strExpr -> NodeExpr.StringExpr strExpr
+    NodeExpr.MultiPart mp -> NodeExpr.MultiPart $ replaceMultiPart conv mp
+    NodeExpr.ASTExpr expr -> NodeExpr.ASTExpr $ replaceExpr conv expr
+
+
+replaceMultiPart :: (l1 -> l2) -> MultiPartExpr (LExpr l1 v) -> MultiPartExpr (LExpr l2 v)
+replaceMultiPart conv = MultiPartExpr.base %~ replaceMultiPartSegment conv
+
+
+replaceMultiPartSegment :: (l1 -> l2) -> MultiPartSegment (LExpr l1 v) -> MultiPartSegment (LExpr l2 v)
+replaceMultiPartSegment conv = MultiPartExpr.segmentBase %~ replaceExpr conv
 
