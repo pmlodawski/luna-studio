8c580e57cf529eb6859966c42c411c846fa7f930
tomkee <tiomek93@gmail.com>
2015-04-01 16:36:56 +0200

    This doesnt work. Try to compile and check problems with type deduction

---
+++ src/Luna/Data/ImportInfo.hs
@@ -0,0 +1,31 @@
+module Luna.Data.ImportInfo where
+
+import qualified Data.Map                 as Map
+import           Data.Map                 (Map)
+import           Luna.Data.StructInfo     (StructInfo)
+import           Luna.Syntax.Decl         (Path)
+import           Luna.Syntax.Name.Path    (NamePath, QualPath)
+import           Luna.Data.ModuleInfo     (ImportError)
+import           Flowbox.Prelude
+
+type ID = Int
+
+data ImportInfo = ImportInfo {
+    _strInfos :: Map Path StructInfo,
+    _symTable :: Map NamePath (QualPath, ID),
+    _errors   :: [ImportError]
+}             deriving (Show)
+
+makeLenses ''ImportInfo
+
+instance Monoid ImportInfo where
+    mempty      = ImportInfo  mempty mempty mempty
+    mappend a b = ImportInfo (mappend (a ^. strInfos) (b ^. strInfos))
+                             (mappend (a ^. symTable) (b ^. symTable))
+                             (mappend (a ^. errors) (b ^. errors))
+
+
+
+
+
+
