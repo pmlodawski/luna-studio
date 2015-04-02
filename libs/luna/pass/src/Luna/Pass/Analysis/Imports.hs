28d37459796cf6efad050b533c37305397a4ebb3
tomkee <tiomek93@gmail.com>
2015-04-02 11:28:11 +0200

    Small fixes

--- libs/luna/pass/src/Luna/Pass/Analysis/Imports.hs
+++ libs/luna/pass/src/Luna/Pass/Analysis/Imports.hs
@@ -11,33 +11,26 @@ import           Luna.Data.StructInfo        (StructInfo)
 import           Luna.Data.ModuleInfo        (_strInfo)
 import qualified Luna.Pass.Import            as I
 import           Luna.Syntax.Traversals      ()
-import           Luna.Pass                    (Pass(Pass), PassMonad, PassCtx)
-import qualified Luna.Pass                    as Pass
+import           Luna.Pass                   (Pass(Pass), PassMonad, PassCtx)
+import qualified Luna.Pass                   as Pass
 import           Flowbox.Prelude             hiding(isLeft, isRight, filter, map, fromList)
 import           Luna.Data.ImportInfo        (ImportInfo(ImportInfo))
 import           Data.Map                    (elems, filter, map, fromList)
 import           Data.Either.Utils           (fromRight, fromLeft)
 import           Luna.Pass                   (PassMonad)
-import qualified Luna.Syntax.Traversals       as AST
-import           Luna.Syntax.Enum             (Enumerated)
+import qualified Luna.Syntax.Traversals      as AST
+import           Luna.Syntax.Enum            (Enumerated)
 
-import           Luna.Syntax.Unit       (Unit(Unit))
-import           Luna.Syntax.Label      (Label(Label), _element)
-import           Luna.Syntax.Module     (_body, Module(Module))
+import           Luna.Syntax.Unit            (Unit(Unit))
+import           Luna.Syntax.Label           (Label(Label), _element)
+import           Luna.Syntax.Module          (_body, Module(Module))
 
 
 
 data ImportsAnalysis = ImportsAnalysis
 
-type IMPass                 m   = PassMonad NoState m
-type IMDefaultTraversal     m a = (PassCtx m, AST.DefaultTraversal ImportsAnalysis (IMPass m) a a)
-type IMCtx              lab m a = (Enumerated lab, IMTraversal m a, MonadIO m)
-type IMTraversal            m a = (PassCtx m, AST.Traversal        ImportsAnalysis (IMPass m) a a)
 
 data NoState = NoState deriving (Read, Show)
-
-defaultTraverseM :: (IMDefaultTraversal m a) => a -> IMPass m a
-defaultTraverseM = AST.defaultTraverseM ImportsAnalysis
 
 --pass :: (MonadIO m) => Pass NoState (Unit (Label l (Module a e)) -> m ImportInfo)
 pass = Pass "Import analysis" 
