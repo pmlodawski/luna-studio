b13776d3b41b6b953e308a2709d87ea88bb6fc24
piotrMocz <piotr.moczurad@gmail.com>
2015-04-21 10:37:29 +0000

    Implicit scopes rename imports with *

--- src/Luna/Pass/Transform/Desugar/ImplicitScopes.hs
+++ src/Luna/Pass/Transform/Desugar/ImplicitScopes.hs
@@ -21,6 +21,7 @@ import qualified Luna.Syntax.Enum             as Enum
 import           Luna.Syntax.Enum             (Enumerated, IDTag(IDTag))
 import qualified Luna.Syntax.Decl             as Decl
 import           Luna.Syntax.Decl             (Decl, LDecl, Field(Field))
+import qualified Luna.Syntax.Name.Path        as QualPath
 import qualified Luna.Syntax.Module           as Module
 import           Luna.Syntax.Module           (Module(Module), LModule)
 import           Luna.Syntax.Unit             (Unit(Unit))
@@ -35,7 +36,7 @@ import qualified Luna.Syntax.Expr             as Expr
 import qualified Luna.Syntax.Lit              as Lit
 import qualified Luna.Syntax.Native           as Native
 import qualified Luna.Syntax.Name             as Name
-import           Luna.Syntax.Name             (TName(TName), TVName(TVName))
+import           Luna.Syntax.Name             (TName(TName), TVName(TVName), VName(VName))
 import           Luna.Pass                    (Pass(Pass), PassMonad, PassCtx)
 import qualified Luna.Pass                    as Pass
 
@@ -51,18 +52,23 @@ import           Luna.Syntax.Arg              (Arg(Arg))
 import           Luna.Syntax.Name.Pattern     (NamePat(NamePat), Segment(Segment))
 import           Luna.Data.StructInfo         (StructInfo)
 import qualified Luna.Data.StructInfo         as StructInfo
-import Control.Monad (join)
+import           Luna.Data.ImportInfo         (ImportInfo)
+import qualified Luna.Data.ImportInfo         as ImportInfo
+import           Control.Monad                (join)
+
+import           Data.Maybe                   (Maybe(Just), fromJust)
 
 ----------------------------------------------------------------------
 -- Base types
 ----------------------------------------------------------------------
 
 data PassState = PassState { _astInfo    :: ASTInfo 
-                           , _structInfo :: StructInfo 
+                           , _structInfo :: StructInfo
+                           , _importInfo :: ImportInfo
                            } deriving (Show)
 
-
 makeLenses ''PassState
+
 
 data ImplScopes = ImplScopes
 
@@ -87,12 +93,11 @@ defaultTraverseM = AST.defaultTraverseM ImplScopes
 ---- Pass functions
 ------------------------------------------------------------------------
 
-pass :: ISDefaultTraversal m a => Pass PassState (ASTInfo -> StructInfo -> a -> ISPass m (a, ASTInfo))
-pass = Pass "Implicit self" "Desugars AST by adding implicit self function parameters" undefined passRunner
--- TODO [wd]: ^----------------^-- add more detailed description, so the difference b/w this and ImplicitSelf would be clear
+pass :: ISDefaultTraversal m a => Pass PassState ((ASTInfo, StructInfo, ImportInfo) -> a -> ISPass m (a, ASTInfo))
+pass = Pass "Implicit scopes" "Desugars AST by adding Module qualifiers where necessary" undefined passRunner
 
-passRunner ai si ast = do
-    put $ PassState ai si
+passRunner (ai, si, ii) ast = do
+    put $ PassState ai si ii
     (,) <$> defaultTraverseM ast <*> (view astInfo <$> get)
 
 exprScopes :: ISCtx lab m a => LExpr lab a -> ISPass m (LExpr lab a)
@@ -105,14 +110,19 @@ exprScopes ast@(Label lab e) = case e of
 fmake ast@(Label lab e) f = case e of
     Expr.Var (Expr.Variable name v) -> do
         si <- view structInfo <$> get
+        ii <- view importInfo <$> get
         let parentMap = view StructInfo.parent si
             aliasMap  = view StructInfo.alias si
+            originMod = fmap (view StructInfo.mod) $ view (at id) aliasMap
+            thisMod   = ImportInfo._path ii
             pid       = view (at id) parentMap
             tgt       = fmap (view StructInfo.target) $ view (at id) aliasMap
             tgtPid    = join $ fmap (\tid -> view (at tid) parentMap) tgt
 
-        if pid == tgtPid then return ast
-                         else return $ Label (-888) $ Expr.Accessor (convert name) (f $ Label lab $ Expr.Var $ Expr.Variable "self" v)
+        if originMod == (Just thisMod)
+            then if pid == tgtPid then return ast
+                                  else return $ Label (-888) $ Expr.Accessor (convert name) (f $ Label lab $ Expr.Var $ Expr.Variable "self" v)
+            else return $ Label (-777) $ Expr.Accessor (convert name) (f $ Label lab $ Expr.Var $ Expr.Variable (VName . ImportInfo.moduleObjectName . fromJust $ originMod) v)
                          -- TODO [kgdk -> wd]: ^-- a magic constant :)
     where id = Enum.id lab
 ----------------------------------------------------------------------
