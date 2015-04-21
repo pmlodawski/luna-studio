888bcae869a4d3c69ec2eac83e15074648581e3f
piotrMocz <piotr.moczurad@gmail.com>
2015-04-21 10:37:29 +0000

    Implicit scopes rename imports with *

--- libs/luna/pass/test/Main.hs
+++ libs/luna/pass/test/Main.hs
@@ -104,9 +104,10 @@ main = do
             printHeader "SA2"
             sa             <- Pass.run2_ SA.pass sa ast
             ppPrint sa
-
+            
+            let sa2  = _info $ _namespace sa
             printHeader "ImplScopes"
-            (ast, astinfo) <- Pass.run3_ ImplScopes.pass astinfo sa1 ast
+            (ast, astinfo) <- Pass.run2_ ImplScopes.pass (astinfo, sa2, importInfo) ast
             ppPrint ast
 
             printHeader "ImplCalls"
