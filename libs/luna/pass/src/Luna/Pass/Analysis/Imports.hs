80cf529f7053f37f0a625ce9525ad8ee4f070a07
Piotr Mlodawski <remdezx@gmail.com>
2015-04-28 14:10:12 +0200

    Added copyright notice

--- libs/luna/pass/src/Luna/Pass/Analysis/Imports.hs
+++ libs/luna/pass/src/Luna/Pass/Analysis/Imports.hs
@@ -1,3 +1,10 @@
+---------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Flowbox Team <contact@flowbox.io>, 2015
+---------------------------------------------------------------------------
+
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE RankNTypes #-}
