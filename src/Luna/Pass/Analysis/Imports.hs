49390589234153c3027521dbce7e4d5c3228be8b
Piotr Mlodawski <remdezx@gmail.com>
2015-04-29 15:50:02 +0200

    Code review

--- src/Luna/Pass/Analysis/Imports.hs
+++ src/Luna/Pass/Analysis/Imports.hs
@@ -12,9 +12,11 @@
 
 module Luna.Pass.Analysis.Imports where
 
+--CR[PM->TD] : imports should be splitted in two groups: std and flowbox
 import           Data.Either                 (isLeft, isRight)
 import           Luna.Data.ModuleInfo        (ImportError(..))
 import           Luna.Data.StructInfo        (StructInfo)
+--CR[PM->TD] : use qualified import
 import           Luna.Data.ModuleInfo        (_strInfo)
 import qualified Luna.Pass.Import            as I
 import           Luna.Syntax.Traversals      ()
@@ -30,6 +32,7 @@ import           Luna.Syntax.Enum            (Enumerated)
 
 import           Luna.Syntax.Unit            (Unit(Unit))
 import           Luna.Syntax.Label           (Label(Label), _element)
+--CR[PM->TD] : use qualified import
 import           Luna.Syntax.Module          (_body, Module(Module))
 
 
@@ -40,16 +43,19 @@ data ImportsAnalysis = ImportsAnalysis
 data NoState = NoState deriving (Read, Show)
 
 --pass :: (MonadIO m) => Pass NoState (Unit (Label l (Module a e)) -> m ImportInfo)
+--CR[PM->TD] : add type signature
 pass = Pass "Import analysis" 
             "Basic import analysis that performs error checks and returns both struct info and symbol table" 
             NoState iaMain
 
 
+--CR[PM->TD] : parenthesis are not required
 iaMain :: (MonadIO m) => Unit (Label l (Module a e)) -> m ImportInfo
 iaMain ast =  do           
     let impPaths =  I.getImportPaths ast
     listEithers  <- I.moduleInfosToTuples impPaths
     let infoEithers = fromList listEithers
+--CR[PM->TD] : "let" is not required
     let mInfos   =  map (_strInfo . fromRight) $ filter isRight infoEithers
         mErrors  =  fmap fromLeft $ elems $ filter isLeft infoEithers
         info     =  ImportInfo mempty (I.getImports ast) mInfos mempty mempty mErrors
--- src/Luna/Pass/Import.hs
+++ src/Luna/Pass/Import.hs
@@ -10,9 +10,12 @@
 
 module Luna.Pass.Import where
 
+--CR[PM->TD] : imports should be splitted in two groups: std and flowbox
 import           Control.Monad.IO.Class (liftIO)
 
+--CR[PM->TD] : use qualified import
 import           Luna.Syntax.Label      (Label(Label), _element)
+--CR[PM->TD] : use qualified import
 import           Luna.Syntax.Module     (_mpath, _body, Module(Module))
 import qualified Luna.Syntax.Decl       as Dec
 import           Luna.Syntax.Name       (TName(TName), VName(VName))
@@ -23,12 +26,14 @@ import           Luna.Syntax.Unit       (Unit(Unit))
 import qualified Luna.Data.ModuleInfo as MI
 import qualified Luna.Data.ImportInfo as II
 import           Flowbox.Prelude
+--CR[PM->TD] : 3 new lines between imports and code
 
 
 type ASTUnit l a e = Unit (Label l (Module a e))
 
 
 data TargetInfo = TargetInfo {
+--CR[PM->TD] : commas should be on the left and following braces, see other files
     _hiding   :: [NamePath],
     _targets  :: [NamePath],
     _wildcard :: Bool
@@ -45,43 +50,51 @@ instance Monoid TargetInfo where
 
 getFromUnit :: Unit a -> a
 getFromUnit (Unit a) = a
+--CR[PM->TD] : too many newlines (3 instead of 2)
 
 
 
 getFromLabel :: Label l a -> a
 getFromLabel (Label l a) = a 
+--CR[PM->TD] : too many newlines (3 instead of 2)
 
 
 
 getModulePath :: ASTUnit l a e -> QualPath
 getModulePath ast = _mpath . getFromLabel . getFromUnit $ ast
+--CR[PM->TD] : too many newlines (3 instead of 2)
 
 
 
 filterImports :: Label l (Dec.Decl a e) -> Bool
 filterImports (Label _  (Dec.Imp _))   = True
 filterImports _ = False
+--CR[PM->TD] : too many newlines (3 instead of 2)
 
 
 
 unpackImport :: Dec.Decl a e -> Dec.Imp
 unpackImport (Dec.Imp x) = x
+--CR[PM->TD] : too many newlines (3 instead of 2)
 
 
 
 getImportList :: ASTUnit l a e -> [Dec.Imp]
 getImportList = fmap (unpackImport . _element) . filter filterImports . _body . getFromLabel . getFromUnit
+--CR[PM->TD] : too many newlines (3 instead of 2)
 
 
 
 getModPathsFromImportList :: [Dec.Imp] -> [QualPath]
 getModPathsFromImportList list = map getModPath list
+--CR[PM->TD] : too many newlines (3 instead of 2)
 
 
 
 getModPath :: Dec.Imp -> QualPath
 getModPath (Dec.ModImp  path _) = MI.pathToQualPath path
 getModPath (Dec.DeclImp path _) = MI.pathToQualPath path
+--CR[PM->TD] : too many newlines (3 instead of 2)
 
 
 
@@ -93,6 +106,7 @@ processTarget (Dec.ImpType tname trename) = case NP._base $ unwrap tname of
     "*" -> TargetInfo [] [] True
     _   -> TargetInfo [] [unwrap tname] False 
 processTarget (Dec.Wildcard hide)         = TargetInfo (fmap unwrap hide) [] True
+--CR[PM->TD] : too many newlines (3 instead of 2)
 
 
 
@@ -104,25 +118,32 @@ getImport :: Dec.Imp -> II.Import
 getImport (Dec.ModImp path rename) = II.Import (MI.pathToQualPath path) False [] [] (fmap unwrap rename)
 getImport (Dec.DeclImp path tgts) = II.Import (MI.pathToQualPath path) (tgtInfo ^. wildcard) (tgtInfo ^. hiding) (tgtInfo ^. targets) Nothing
     where tgtInfo = processTargets tgts
+--CR[PM->TD] : too many newlines (3 instead of 2)
                                      
 
 
 getImports :: ASTUnit l a e -> [II.Import]
+--CR[PM->TD] : use <$>
 getImports ast = fmap getImport $ getImportList ast
 
 
 getImportPaths :: ASTUnit l a e -> [QualPath]
 getImportPaths = getModPathsFromImportList . getImportList
+--CR[PM->TD] : too many newlines (3 instead of 2)
 
 
 
+--CR[PM->TD] : parenthesis are not required
 getModuleInfos :: (MonadIO m) => [QualPath] -> m [Either MI.ImportError MI.ModuleInfo]
 getModuleInfos = liftIO . MI.getModuleInfos
 
 
 
+--CR[PM->TD] : parenthesis are not required
 moduleInfosToTuples :: (MonadIO m) => [QualPath] -> m [(QualPath, Either MI.ImportError MI.ModuleInfo)]
 moduleInfosToTuples paths = do
+--CR[PM->TD] : use <$>
+--CR[PM->TD] : "do" is not required
 	eithers <- getModuleInfos paths
 	return $ zip paths eithers
 
@@ -131,8 +152,11 @@ getImportInfo :: (MonadIO m) => ASTUnit l a e -> m [(II.Import, Either MI.Import
 getImportInfo ast = do
     let imports =  getImports ast
         paths   =  getImportPaths ast
+--CR[PM->TD] : use <$>
+--CR[PM->TD] : "do" is not required
     eithers     <- getModuleInfos paths
     return $ zip imports eithers
+--CR[PM->TD] : too many newlines (should be one)
 
 
 
