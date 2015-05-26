b6c66d3c68ae0897e7ab5f75dc4636fe3a0628bf
Piotr Mlodawski <remdezx@gmail.com>
2015-05-26 15:46:15 +0200

    luna-interepreter-runtime package stub

---
+++ src/Luna/Interpreter/Runtime.hs
@@ -0,0 +1,13 @@
+--------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Flowbox Team <contact@flowbox.io>, 2015
+-- Proprietary and confidential
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+---------------------------------------------------------------------------
+
+module Luna.Interpreter.Runtime (
+    module Luna.Interpreter.Runtime,
+    module X
+) where
+
+import Luna.Interpreter.Runtime.HMap as X
---
+++ src/Luna/Interpreter/Runtime/HMap.hs
@@ -0,0 +1,8 @@
+--------------------------------------------------------------------------
+-- Copyright (C) Flowbox, Inc - All Rights Reserved
+-- Flowbox Team <contact@flowbox.io>, 2015
+-- Proprietary and confidential
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+---------------------------------------------------------------------------
+
+module Luna.Interpreter.Runtime.HMap where
