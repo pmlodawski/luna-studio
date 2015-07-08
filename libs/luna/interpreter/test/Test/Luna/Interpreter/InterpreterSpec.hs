5f6f74fde59e945ffe61b585efa0def0fb062529
Piotr Mlodawski <remdezx@gmail.com>
2015-07-08 15:00:38 +0200

    Not computing nodes which are not connected

--- libs/luna/interpreter/test/Test/Luna/Interpreter/InterpreterSpec.hs
+++ libs/luna/interpreter/test/Test/Luna/Interpreter/InterpreterSpec.hs
@@ -57,23 +57,23 @@ spec :: Spec
 spec = do
     let mm = NoManager
     describe "AST traverse" $ do
+        let lib1      = Library.ID 1
+            var_a     = [CallPoint lib1 22]
+            var_b     = [CallPoint lib1 26]
+            var_c     = [CallPoint lib1 38]
+            fooCall   = [CallPoint lib1 31]
+            var_e     = [CallPoint lib1 31, CallPoint lib1 55]
+            var_n     = [CallPoint lib1 31, CallPoint lib1 59]
+            var_d     = [CallPoint lib1 31, CallPoint lib1 71]
+            barCall   = [CallPoint lib1 31, CallPoint lib1 62]
+            conMain   = [CallPoint lib1 31, CallPoint lib1 62, CallPoint lib1 122]
+            testCall  = [CallPoint lib1 31, CallPoint lib1 62, CallPoint lib1 90]
+            tuple     = [CallPoint lib1 31, CallPoint lib1 62, CallPoint lib1 (-86)]
+            conMain2  = [CallPoint lib1 120]
+            printCall = [CallPoint lib1 41]
         it "finds function arguments" $ do
             rootLogger setIntLevel 5
             Common.runSession mm SampleCodes.traverseExample $ do
-                let lib1      = Library.ID 1
-                    var_a     = [CallPoint lib1 22]
-                    var_b     = [CallPoint lib1 26]
-                    var_c     = [CallPoint lib1 38]
-                    fooCall   = [CallPoint lib1 31]
-                    var_e     = [CallPoint lib1 31, CallPoint lib1 55]
-                    var_n     = [CallPoint lib1 31, CallPoint lib1 59]
-                    var_d     = [CallPoint lib1 31, CallPoint lib1 71]
-                    barCall   = [CallPoint lib1 31, CallPoint lib1 62]
-                    conMain   = [CallPoint lib1 31, CallPoint lib1 62, CallPoint lib1 122]
-                    testCall  = [CallPoint lib1 31, CallPoint lib1 62, CallPoint lib1 90]
-                    tuple     = [CallPoint lib1 31, CallPoint lib1 62, CallPoint lib1 (-86)]
-                    conMain2  = [CallPoint lib1 120]
-                    printCall = [CallPoint lib1 41]
                 getArgs var_a     >>= shouldBe' []
                 getArgs var_b     >>= shouldBe' []
                 getArgs var_c     >>= shouldBe' []
@@ -88,45 +88,31 @@ spec = do
                 getArgs conMain2  >>= shouldBe' []
                 getArgs printCall >>= shouldBe' [conMain2, fooCall]
 
-        it "finds node successors" $ do
+        it "finds node successors" $
             --putStrLn =<< ppShow <$> Common.readCode SampleCodes.traverseExample
             Common.runSession mm SampleCodes.traverseExample $ do
-                let lib1      = Library.ID 1
-                    var_a     = [CallPoint lib1 22]
-                    var_b     = [CallPoint lib1 26]
-                    var_c     = [CallPoint lib1 38]
-                    fooCall   = [CallPoint lib1 31]
-                    var_e     = [CallPoint lib1 31, CallPoint lib1 55]
-                    var_n     = [CallPoint lib1 31, CallPoint lib1 59]
-                    var_d     = [CallPoint lib1 31, CallPoint lib1 71]
-                    barCall   = [CallPoint lib1 31, CallPoint lib1 62]
-                    conMain   = [CallPoint lib1 31, CallPoint lib1 62, CallPoint lib1 122]
-                    testCall  = [CallPoint lib1 31, CallPoint lib1 62, CallPoint lib1 90]
-                    tuple     = [CallPoint lib1 31, CallPoint lib1 62, CallPoint lib1 (-86)]
-                    conMain2  = [CallPoint lib1 120]
-                    printCall = [CallPoint lib1 41]
-                getSuccessors var_a     >>= shouldMatchList' [var_b, barCall]
-                getSuccessors var_b     >>= shouldMatchList' [var_c, barCall]
-                getSuccessors var_c     >>= shouldMatchList' [var_e, barCall]
-                getSuccessors var_e     >>= shouldMatchList' [var_n, barCall]
-                getSuccessors var_n     >>= shouldMatchList' [var_d]
-                getSuccessors var_d     >>= shouldMatchList' [conMain, barCall]
-                getSuccessors conMain   >>= shouldMatchList' [testCall]
+                getSuccessors var_a     >>= shouldMatchList' [barCall]
+                getSuccessors var_b     >>= shouldMatchList' [barCall]
+                getSuccessors var_c     >>= shouldMatchList' [barCall]
+                getSuccessors var_e     >>= shouldMatchList' [barCall]
+                getSuccessors var_n     >>= shouldMatchList' []
+                getSuccessors var_d     >>= shouldMatchList' [barCall]
+                getSuccessors conMain   >>= shouldMatchList' []
                 getSuccessors testCall  >>= shouldMatchList' [barCall]
                 getSuccessors tuple     >>= shouldMatchList' [barCall]
                 getSuccessors barCall   >>= shouldMatchList' [fooCall]
-                getSuccessors fooCall   >>= shouldMatchList' [conMain2]
-                getSuccessors conMain2  >>= shouldMatchList' [printCall]
+                getSuccessors fooCall   >>= shouldMatchList' []
+                getSuccessors conMain2  >>= shouldMatchList' []
                 getSuccessors printCall >>= shouldMatchList' [[]]
 
     describe "interpreter" $ do
-        mapM_ (\(name, code) -> it ("executes example - " ++ name) $ do
+        mapM_ (\(name, code) -> it ("executes example - " ++ name) $
             --rootLogger setIntLevel 5
             Common.runSession mm code testProcessMain) SampleCodes.sampleCodes
 
-        mapM_ (\(name, code) -> it ("executes example 5 times - " ++ name) $ do
+        mapM_ (\(name, code) -> it ("executes example 5 times - " ++ name) $
             --rootLogger setIntLevel 5
-            Common.runSession mm code $ replicateM_ 5 testProcessMain) $ SampleCodes.sampleCodes
+            Common.runSession mm code $ replicateM_ 5 testProcessMain) SampleCodes.sampleCodes
 
 testProcessMain :: MemoryManager mm => Session mm ()
 testProcessMain = do
