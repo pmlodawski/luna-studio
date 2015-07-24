ac63d5a28e83a018b4a72433cb7257e668af1031
Wojciech Danilo <wojciech.danilo@gmail.com>
2015-07-24 15:45:13 +0200

    Refying AST.

--- Data/Convert/Base.hs
+++ Data/Convert/Base.hs
@@ -14,7 +14,12 @@ class MaybeConvertible a e b | a b -> e where
 class Convertible a b where
     convert :: a -> b
 
-type IsoConvertible a b = (Convertible a b, Convertible b a)
+class Castable a b where
+    cast :: a -> b
+
+type IsoMaybeConvertible a e b = (MaybeConvertible a e b, MaybeConvertible b e a)
+type IsoConvertible      a b   = (Convertible      a b  , Convertible      b a  )
+type IsoCastable         a b   = (Castable         a b  , Castable         b a  )
 
 unsafeConvert :: Show e => MaybeConvertible a e b => a -> b
 unsafeConvert a =
@@ -22,9 +27,11 @@ unsafeConvert a =
       Left  e -> error $ show e
       Right r -> r
 
-
 --instance {-# OVERLAPPABLE #-} Convertible a a where
 --    convert = id
 
 instance {-# OVERLAPPABLE #-} Convertible a b => Convertible (Maybe a) (Maybe b) where
     convert = fmap convert
+
+instance {-# OVERLAPPABLE #-} Convertible a b => Castable a b where
+    cast = convert
