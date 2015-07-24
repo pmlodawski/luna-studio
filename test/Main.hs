a788b600eaa9cdc75dc4dd92e9defd1495f080c9
Wojciech Danilo <wojciech.danilo@gmail.com>
2015-07-24 15:45:13 +0200

    Refying AST.

--- test/Main.hs
+++ test/Main.hs
@@ -35,32 +35,34 @@ import           FastString (FastString, mkFastString, unpackFS)
 
 import qualified Data.IntMap.Lazy as IntMap
 import           Data.IntMap.Lazy (IntMap)
-import Data.Typeable
-import Control.Monad.State
+import Data.Typeable       hiding (cast)
+import Control.Monad.State hiding (withState)
+
 
 import Data.GraphViz.Types.Canonical
-import Data.GraphViz.Attributes.Complete
+import Data.GraphViz.Attributes.Complete hiding (Label)
+import qualified Data.GraphViz.Attributes.Complete as GV
 import Data.GraphViz.Printing (toDot)
 import Data.GraphViz.Commands
 
-import Data.Vector hiding ((++), splitAt, length, convert)
+import Data.Vector (Vector)
 import qualified Data.Vector as Vector
-import Data.Vector.Mutable hiding (splitAt, length, convert)
+import Data.Vector.Mutable ()
 import Data.Maybe (fromJust)
 
 import Data.Indexable
 
 import Data.Containers
 
-add :: Int -> Int -> Int
-add = (+)
+--add :: Int -> Int -> Int
+--add = (+)
 
-add2 :: (Int, (Int, ())) -> Int
-add2 (a, (b, ())) = a + b
+--add2 :: (Int, (Int, ())) -> Int
+--add2 (a, (b, ())) = a + b
 
 
-testf :: (Int, (String, ())) -> (Int,String)
-testf (a,(b,())) = (a,b)
+--testf :: (Int, (String, ())) -> (Int,String)
+--testf (a,(b,())) = (a,b)
 
 --unpackTest :: Object -> (Int,String)
 --unpackTest = unpackRawData
@@ -69,10 +71,10 @@ testf (a,(b,())) = (a,b)
 --unpackInt :: Object -> Int
 --unpackInt = unpackRawData
 
-f = unsafeCoerce add :: Any
+--f = unsafeCoerce add :: Any
 
-appx :: a -> Any -> Any
-appx a f = (unsafeCoerce f :: Any -> Any) (unsafeCoerce a :: Any)
+--appx :: a -> Any -> Any
+--appx a f = (unsafeCoerce f :: Any -> Any) (unsafeCoerce a :: Any)
 
 --instance Default (Graph.Gr a b) where
 --    def = Graph.mkGraph def def
@@ -129,9 +131,13 @@ data Expr h = Var      { _name :: Name                                          
             -- | Tuple
             -- | List
 
-data Ptr i a = Ptr i
+data Ptr i a = Ptr { fromPtr :: i }
 
 newtype Unbounded a = Unbounded a deriving (Show, Functor)
+
+
+
+
 
 --instance At (Unbounded (Vector a)) where
     --at :: Index m -> IndexedLens' (Index m) m (Maybe (IxValue m))
@@ -160,6 +166,11 @@ unsafeAt i = lens (uncheckedGetIdx i) (flip $ uncheckedSetIdx i)
 
 --test =
 
+mapOver :: (Lens' b a) -> (a -> (a, x)) -> (b -> (b, x))
+mapOver lens f s = (s & lens .~ a, out) where
+    (a, out) = f $ s ^. lens
+
+
 
 instance (Typeable a, Show i) => Show (Ptr i a) where
     show (Ptr i) = "Ptr \"" <> show (typeOf (undefined :: a)) <> "\" " <> show i
@@ -168,14 +179,14 @@ instance (Typeable a, Show i) => Show (Ptr i a) where
 
 deriving instance Show (HExpr h) => Show (Expr h)
 
-        --instance Repr Expr where
-        --    repr = \case
-        --        Var      n   -> "Var "      <> show (unpackFS n)
-        --        Cons     n   -> "Cons "     <> show (unpackFS n)
-        --        Accessor n _ -> "Accessor " <> show (unpackFS n)
-        --        App      {}  -> "App"
+instance Repr (Expr h) where
+    repr = \case
+        Var      n   -> "Var "      <> show (unpackFS n)
+        Cons     n   -> "Cons "     <> show (unpackFS n)
+        Accessor n _ -> "Accessor " <> show (unpackFS n)
+        App      {}  -> "App"
 
-        ----data Var = Var Name deriving (Show)
+--data Var = Var Name deriving (Show)
 
 data Arg h = Arg { _label :: Maybe Name, _val :: HExpr h }
 
@@ -183,6 +194,36 @@ deriving instance Show (HExpr h) => Show (Arg h)
 
 makeLenses ''Expr
 makeLenses ''Arg
+
+
+
+class Node n inp | n -> inp where
+    inputs :: n -> [inp]
+
+
+
+instance Node (Expr h) (HExpr h) where
+    inputs = \case
+        Accessor _    base -> [base]
+        App      base args -> base : fmap (view val) args
+        _                  -> []
+
+
+
+
+--unsafeGet :: NodeGraph m a => NodePtr m a -> Graph m -> a
+--unsafeGet ptr = view $ unsafeOverPtr ptr
+
+      --inputs :: Graph -> ID -> [Key Expr]
+        --inputs g id = case IntMap.lookup id (g ^. nodes) of
+        --    Nothing -> []
+        --    Just n  -> exprOutputs (fromNodeObject n)
+        --    where exprOutputs :: Expr -> [Key Expr]
+        --          exprOutputs = \case
+        --              Accessor _ c  -> [c]
+        --              App      c as -> c : fmap (view val) as
+        --              _             -> []
+
 
 
         --class ArgCons a b | a -> b where
@@ -234,12 +275,6 @@ makeLenses ''Arg
 --- === [TOEXTRACT] Instances ===
 instance Default (Vector a) where def = mempty
 
---- === [TOEXTRACT] GraphClass ===
-
--- TODO: GraphClass -> Graph w module
-class GraphClass g where
-    type Node g
-
 
 --- === NodeObject ===
 
@@ -249,116 +284,233 @@ data NodeObject where
     NodeObject :: NodeVal a => a -> NodeObject
 
 
-fromNodeObject :: NodeObject -> a
-fromNodeObject (NodeObject a) = unsafeCoerce a
 
-hiddenLens :: NodeVal a => Iso' NodeObject a
-hiddenLens = iso fromNodeObject NodeObject
+hiddenLens :: IsoCastable a b => Iso' a b
+hiddenLens = iso cast cast
 
 -- instances
 
 instance Show NodeObject where show (NodeObject a) = show a
 instance Repr NodeObject where repr (NodeObject a) = repr a
 
+instance {-# OVERLAPPABLE #-} Castable NodeObject a where
+    cast (NodeObject a) = unsafeCoerce a
+
+instance {-# OVERLAPPABLE #-} NodeVal a => Castable a NodeObject where
+    cast = NodeObject
 
 --- === Graph ===
 
-data Graph cont = Graph { _nodes      :: cont NodeObject
-                        , _argNames   :: [Name]
-                        }
+data Graph nodes = Graph { _nodes      :: nodes
+                         , _argNames   :: [Name]
+                         }
 
 makeLenses ''Graph
 
-type NodePtr m a = Ptr (Index (m NodeObject)) a
-type NodeGraph m = (ElementOf (m NodeObject) ~ NodeObject, UncheckedSetIdx (m NodeObject), UncheckedGetIdx (m NodeObject))
+class HasGraph a n | a -> n where
+    graph :: Lens' a (Graph n)
 
-unsafeOverPtr :: (NodeVal a, NodeGraph m)
-              => NodePtr m a -> Lens' (Graph m) a
+
+type NodePtr m a = Ptr (Index m) a
+type NodeGraph m a = (UncheckedSetIdx m, UncheckedGetIdx m, IsoCastable a (ElementOf m), NodeVal a)
+
+unsafeOverPtr :: NodeGraph m a => NodePtr m a -> Lens' (Graph m) a
 unsafeOverPtr (Ptr i) = nodes . unsafeAt i . hiddenLens
 
-unsafeSet :: (NodeVal a, NodeGraph m) => NodePtr m a -> a -> Graph m -> Graph m
+unsafeSet :: NodeGraph m a => NodePtr m a -> a -> Graph m -> Graph m
 unsafeSet ptr a = unsafeOverPtr ptr .~ a
 
-unsafeGet :: (NodeVal a, NodeGraph m) => NodePtr m a -> Graph m -> a
+unsafeGet :: NodeGraph m a => NodePtr m a -> Graph m -> a
 unsafeGet ptr = view $ unsafeOverPtr ptr
 
-
-
-insert :: (Convertible a NodeObject, Appendable' m, Indexable (m (NodeObject)), ElementOf (m NodeObject) ~ NodeObject)
+insert :: (Castable a (ElementOf m), Appendable m, Indexable m)
        => a -> Graph m -> (Graph m, NodePtr m a)
 insert a g = (g', Ptr . lastIdx $ g' ^. nodes) where
-    g' = g & nodes %~ (flip append' (convert a))
+    g' = g & nodes %~ (flip append (cast a))
+
+
+-- FIXME (unsafe implementation)
+inputs' :: Graph (Vector NodeObject) -> ID -> [Ptr Int (Expr (Ptr Int))]
+inputs' g id = inputs $ unsafeGet (Ptr id :: Ptr Int (Expr (Ptr Int))) g
 
 --        ---- instances
 
-deriving instance Show (m NodeObject) => Show (Graph m)
+deriving instance Show nodes => Show (Graph nodes)
 
-instance Default (m NodeObject) => Default (Graph m) where
+instance Default nodes => Default (Graph nodes) where
     def = Graph def def
 
 
 
 --- === GraphBuilder ===
 
-data GraphBuilder c = GraphBuilder { _nodeScope :: [Int]
-                                   , _graph     :: Graph c
-                                   }
+type GraphBuilder c = State (BldrState c)
 
-makeLenses ''GraphBuilder
 
-type MonadGraphBuilder c s m = (MonadState s m, HasGraphBuilder s c)
+type family ConnectionType (m :: * -> *) :: * -> *
 
-class HasGraphBuilder a c | a -> c where
-    graphBuilder :: Simple Lens a (GraphBuilder c)
+type instance ConnectionType (GraphBuilder c) = Ptr (Index c)
+
+data BldrState c = BldrState { _nodeScope :: [Int]
+                             , __graph     :: Graph c
+                             }
+
+
+
+makeLenses ''BldrState
+
+instance Default (Graph c) => Default (BldrState c) where
+    def = BldrState def def
+
+instance HasGraph (BldrState c) c where
+    graph = _graph
+
+class Monad m => MonadBldrState c m | m -> c where
+    getBldrState :: m (BldrState c)
+    putBldrState :: BldrState c -> m ()
+
+
+class HasBldrState a c | a -> c where
+    bldrState :: Simple Lens a (BldrState c)
+
+instance HasBldrState (BldrState c) c where
+    bldrState = id
 
 -- utils
 
-withGraphBuilder :: MonadGraphBuilder c s m => (GraphBuilder c -> GraphBuilder c) -> m ()
-withGraphBuilder f = do
-    s <- get
-    put $ s & graphBuilder %~ f
+withBldrState_ :: MonadBldrState c m => (BldrState c -> BldrState c) -> m ()
+withBldrState_ f = withBldrState $ fmap (,()) f
 
-requestNodeID :: MonadGraphBuilder c s m => m ID
+
+withBldrState :: MonadBldrState c m => (BldrState c -> (BldrState c, a)) -> m a
+withBldrState f = do
+    bldr <- getBldrState
+    let (bldr', out) = f bldr
+    putBldrState $ bldr'
+    return out
+
+withGraph :: MonadBldrState c m => (Graph c -> (Graph c, a)) -> m a
+withGraph = withBldrState . mapOver graph
+
+requestNodeID :: MonadBldrState c m => m ID
 requestNodeID = do
-    s <- get
-    let nsLens     = graphBuilder . nodeScope
+    s <- getBldrState
+    let nsLens     = bldrState . nodeScope
         (id : ids) = s ^. nsLens
-    put $ s & nsLens .~ ids
+    putBldrState $ s & nsLens .~ ids
     return id
 
-releaseNodeID :: MonadGraphBuilder c s m => ID -> m ()
-releaseNodeID id = withGraphBuilder (nodeScope %~ (id:))
+releaseNodeID :: MonadBldrState c m => ID -> m ()
+releaseNodeID id = withBldrState_ (nodeScope %~ (id:))
+
+--add :: (Appendable c, Indexable c, Castable a (ElementOf c), MonadBldrState c m)
+--    => a -> m (Ptr (Index c) a)
+--add = withGraph . insert
+
+class Monad m => ASTBuilder a m where
+    add :: ASTNode m a -> Ref m a
+
+type ASTNode m a = a (ConnectionType m)
+type Ref m a = m (ConnectionType m (a (ConnectionType m)))
+
+type GraphBuilderMonad c a = (Appendable c, Indexable c, Castable (a (Ptr (Index c))) (ElementOf c))
+
+
+
+instance GraphBuilderMonad c a => ASTBuilder a (GraphBuilder c) where
+    add = withGraph . insert
+
+--add' :: el (ConnectionType bldr) -> bldr
+--add' = add
+
+--type X =
+
+--instance (Castable a (ElementOf c), idx ~ Index c, Appendable c, Indexable c)
+--      => ASTBuilder a (GraphBuilder idx c (Ptr idx a)) where
+--    add' = add
 
 -- instances
 
-instance Default (Graph c) => Default (GraphBuilder c) where
-    def = GraphBuilder def def
+--Expr (Ptr Int)
+
+--data Nu f=forall a.Nu a (a->f a)
+--newtype Mu f = Mu (f (Mu f))
+
+--a = undefined :: Mu ()
 
 
---registerNode :: (MonadGraphBuilder m, IsExpr a) => Key a -> a -> m ()
---registerNode k a = withBuilder $ graph %~ insert k a
 
---registerInput :: MonadGraphBuilder m => Name -> m ()
---registerInput name = withBuilder $ graph . argNames %~ (<> [name])
+--foo :: State (BldrState (Vector NodeObject)) (Ptr Int (Expr (Ptr Int)))
+--foo :: (Appendable c, LastIdx c, SetIdx c, GetIdx c,
+--                       CheckedSetIdx c, CheckedGetIdx c, UncheckedSetIdx c,
+--                       UncheckedGetIdx c, Castable (Expr h) (ElementOf c),
+--                       MonadBldrState c m) =>
+--                      m (Ptr (Index c) (Expr h))
 
---mkNode :: (MonadGraphBuilder m, IsExpr a) => a -> m (Key a)
---mkNode a = do
---    key <- newNodeKey
---    registerNode key a
---    return key
+var = add . Var
+accessor name el = add $ Accessor name el
 
--------------------------------
+foo :: ASTBuilder Expr m => m ()
+foo = do
+    foo <- var "foo"
+    bar <- var "bar"
+    x   <- accessor "x" foo
+    y   <- accessor "y" foo
+    return ()
 
---g :: Graph (Vector NodeObject)
---g = def
+
+instance MonadBldrState c (State (BldrState c)) where
+    getBldrState = get
+    putBldrState = put
+
+--bar :: (Graph (Vector NodeObject))
+--bar :: (Default s, Appendable c, LastIdx c, SetIdx c, GetIdx c,
+--                       CheckedSetIdx c, CheckedGetIdx c, UncheckedSetIdx c,
+--                       UncheckedGetIdx c, Castable (Expr h) (ElementOf c),
+--                       MonadBldrState c (StateT s Identity)) =>
+--                      (Ptr (Index c) (Expr h), s)
+--bar = flip runState def foo
+runNodeBuilder :: GraphBuilder (Vector NodeObject) a -> Graph (Vector NodeObject)
+runNodeBuilder = view graph . flip execState def
+
+g1 :: Graph (Vector NodeObject)
+g1 = runNodeBuilder foo
 
 main = do
-    --print g
-    let v = Vector.fromList $ [1,2,3] :: Vector Int
-        --v2 = Vector.modify (\v -> write v 3 0) v
-    --print v2
+    print g1
+    print $ toDot (toGraphViz g1)
+    runGraphviz (toGraphViz g1) Png "/tmp/out.png"
     print "end"
 
+
+
+data Label a e = Label a e
+
+newtype Mu f = Mu (f (Mu f))
+
+
+--inputs' :: Graph (Vector NodeObject) -> ID -> [Ptr Int (Expr (Ptr Int))]
+
+toGraphViz :: Graph (Vector NodeObject) -> DotGraph Int
+toGraphViz g = DotGraph { strictGraph     = False
+                        , directedGraph   = True
+                        , graphID         = Nothing
+                        , graphStatements = DotStmts { attrStmts = []
+                                                     , subGraphs = []
+                                                     , nodeStmts = nodeStmts
+                                                     , edgeStmts = edgeStmts
+                                                     }
+                        }
+    where ns              = g ^. nodes
+          nodeIds         = [0 .. Vector.length ns - 1] :: [Int]
+          elems           = fmap ((Vector.!) ns) nodeIds
+          nodeLabels      = fmap repr elems
+          labeledNode s a = DotNode a [GV.Label . StrLabel $ fromString s]
+          nodeStmts       = fmap (uncurry labeledNode) $ zip nodeLabels nodeIds
+          nodeInEdges   n = zip3 [0..] (fmap fromPtr $ inputs' g n) (repeat n)
+          inEdges         = concat $ fmap nodeInEdges nodeIds
+          mkEdge  (n,a,b) = DotEdge a b [GV.Label . StrLabel $ fromString $ show n]
+          edgeStmts       = fmap mkEdge inEdges
 
 
 
@@ -420,27 +572,27 @@ main = do
 
 
 
-        --type MonadGraphBuilder m = MonadState GraphBuilder m
+        --type MonadBldrState2 m = MonadState BldrState m
 
-        --data GraphBuilder = GraphBuilder { _lastKey :: Int
+        --data BldrState = BldrState { _lastKey :: Int
         --                                 , _lastNode       :: Int
         --                                 , _graph          :: Graph
         --                                 }
 
-        --instance Default GraphBuilder where
-        --    def = GraphBuilder def def def
+        --instance Default BldrState where
+        --    def = BldrState def def def
 
 
 
-        --makeLenses ''GraphBuilder
+        --makeLenses ''BldrState
 
-        --getBuilder :: MonadGraphBuilder m => m GraphBuilder
+        --getBuilder :: MonadBldrState2 m => m BldrState
         --getBuilder = get
 
-        --putBuilder :: MonadGraphBuilder m => GraphBuilder -> m ()
+        --putBuilder :: MonadBldrState2 m => BldrState -> m ()
         --putBuilder = put
 
-        --newKey :: (IsExpr a, MonadGraphBuilder m) => m (Key a)
+        --newKey :: (IsExpr a, MonadBldrState2 m) => m (Key a)
         --newKey = do
         --    bldr <- getBuilder
         --    let i = bldr ^. lastKey
@@ -448,20 +600,20 @@ main = do
         --    return $ unsafeKeyByID i
 
 
-        --newNodeKey :: (IsExpr a, MonadGraphBuilder m) => m (Key a)
+        --newNodeKey :: (IsExpr a, MonadBldrState2 m) => m (Key a)
         --newNodeKey = do
         --    bldr <- getBuilder
         --    let i = bldr ^. lastNode
         --    put $ bldr & lastNode .~ (i + 1)
         --    return $ unsafeKeyByID i
 
-        --registerNode :: (MonadGraphBuilder m, IsExpr a) => Key a -> a -> m ()
+        --registerNode :: (MonadBldrState2 m, IsExpr a) => Key a -> a -> m ()
         --registerNode k a = withBuilder $ graph %~ insert k a
 
-        --registerInput :: MonadGraphBuilder m => Name -> m ()
+        --registerInput :: MonadBldrState2 m => Name -> m ()
         --registerInput name = withBuilder $ graph . argNames %~ (<> [name])
 
-        --mkNode :: (MonadGraphBuilder m, IsExpr a) => a -> m (Key a)
+        --mkNode :: (MonadBldrState2 m, IsExpr a) => a -> m (Key a)
         --mkNode a = do
         --    key <- newNodeKey
         --    registerNode key a
@@ -469,25 +621,25 @@ main = do
 
 
 
-        --withBuilder :: MonadGraphBuilder m => (GraphBuilder -> GraphBuilder) -> m ()
+        --withBuilder :: MonadBldrState2 m => (BldrState -> BldrState) -> m ()
         --withBuilder f = do
         --    bldr <- getBuilder
         --    putBuilder $ f bldr
 
-        ----input :: MonadGraphBuilder m => Name -> m (Key Expr)
+        ----input :: MonadBldrState2 m => Name -> m (Key Expr)
 
-        --var :: MonadGraphBuilder m => Name -> m (Key Expr)
+        --var :: MonadBldrState2 m => Name -> m (Key Expr)
         --var n = mkNode $ Var n
 
-        --input :: MonadGraphBuilder m => Name -> m (Key Expr)
+        --input :: MonadBldrState2 m => Name -> m (Key Expr)
         --input n = do registerInput n
         --             var n
 
-        ----var :: MonadGraphBuilder m => Name -> m (Key Expr)
+        ----var :: MonadBldrState2 m => Name -> m (Key Expr)
         ----var n = mkNode $ VarE $ Var n
 
 
-        --cons :: MonadGraphBuilder m => Name -> m (Key Expr)
+        --cons :: MonadBldrState2 m => Name -> m (Key Expr)
         --cons n = mkNode $ Cons n
 
 
@@ -502,19 +654,19 @@ main = do
 
 
 
-        ----access :: (PolyFunc1 (Key Expr) m a, MonadGraphBuilder m) => a -> Name -> m (Key Expr)
+        ----access :: (PolyFunc1 (Key Expr) m a, MonadBldrState2 m) => a -> Name -> m (Key Expr)
         ----access key name = polyFunc1 (mkNode . Accessor name) key
 
         ----app ::
 
 
-        --access :: MonadGraphBuilder m => Name -> Key Expr -> m (Key Expr)
+        --access :: MonadBldrState2 m => Name -> Key Expr -> m (Key Expr)
         --access name key = mkNode $ Accessor name key
 
-        --app :: MonadGraphBuilder m => Key Expr -> [Arg] -> m (Key Expr)
+        --app :: MonadBldrState2 m => Key Expr -> [Arg] -> m (Key Expr)
         --app base args = mkNode $ App base args
 
-        --appArgs :: MonadGraphBuilder m => Key Expr -> [Key Expr] -> m (Key Expr)
+        --appArgs :: MonadBldrState2 m => Key Expr -> [Key Expr] -> m (Key Expr)
         --appArgs base = app base . fmap arg
 
         --g1 :: Graph
