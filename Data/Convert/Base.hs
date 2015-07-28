18521da3a26db010afdf63444de33682de0acbaf
Wojciech Danilo <wojciech.danilo@gmail.com>
2015-07-28 13:49:06 +0200

    Moving Utils to GHC-7.10

--- Data/Convert/Base.hs
+++ Data/Convert/Base.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Data.Convert.Base where
 
@@ -27,11 +29,17 @@ unsafeConvert a =
       Left  e -> error $ show e
       Right r -> r
 
---instance {-# OVERLAPPABLE #-} Convertible a a where
---    convert = id
+instance {-# OVERLAPPABLE #-} Convertible a a where
+    convert = id
 
 instance {-# OVERLAPPABLE #-} Convertible a b => Convertible (Maybe a) (Maybe b) where
     convert = fmap convert
 
 instance {-# OVERLAPPABLE #-} Convertible a b => Castable a b where
     cast = convert
+
+class ConvertibleM  m n where convertM  :: m (t1 :: k) -> n (t1 :: k)
+class ConvertibleM2 m n where convertM2 :: m (t1 :: k) (t2 :: k) -> n (t1 :: k) (t2 :: k)
+class ConvertibleM3 m n where convertM3 :: m (t1 :: k) (t2 :: k) (t3 :: k) -> n (t1 :: k) (t2 :: k) (t3 :: k)
+class ConvertibleM4 m n where convertM4 :: m (t1 :: k) (t2 :: k) (t3 :: k) (t4 :: k) -> n (t1 :: k) (t2 :: k) (t3 :: k) (t4 :: k)
+class ConvertibleM5 m n where convertM5 :: m (t1 :: k) (t2 :: k) (t3 :: k) (t4 :: k) (t5 :: k) -> n (t1 :: k) (t2 :: k) (t3 :: k) (t4 :: k) (t5 :: k)
