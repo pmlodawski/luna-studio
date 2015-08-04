567142e94d567caa26df73bb7fe3fc6e88c5b4d8
Adam Szlachta <adam.szlachta@gmail.com>
2015-08-04 18:24:49 +0200

    Pancakes ports ver 1

--- app/js/port.js
+++ app/js/port.js
@@ -13,80 +13,48 @@ var nodeRadius    = 30.0;
 // var inputColor  = new THREE.Vector4(0xFF, 0x99, 0x33, 0xAA).divideScalar(0xFF);
 var inputColor   = new THREE.Vector4(0x00, 0x99, 0x99, 0xAA).divideScalar(0xFF);
 var outputColor  = new THREE.Vector4(0xBB, 0x33, 0x00, 0xAA).divideScalar(0xFF);
-var colorFar     = new THREE.Vector4(0.8, 0.2, 0.2, 0.6);
+var colorFar     = new THREE.Vector4(0.2, 0.2, 0.2, 0.6);
 
-var triangleHeight = size * Math.sqrt(3.0) / 2.0;
-var halfHeight = triangleHeight / 2.0;
-var dist  = nodeRadius + halfHeight + distFromRim;
+// var triangleHeight = size * Math.sqrt(3.0) / 2.0;
+// var halfHeight = triangleHeight / 2.0;
+// var dist  = nodeRadius + halfHeight + distFromRim;
+
+var height = 12.0;
+var width  = 12.0;
+var halfWidth = width / 2.0;
+var margin = 0.0;
+var dist = nodeRadius + halfWidth + margin;
+
+var nodeSize = 30.0;
 
 function Port(id, angle, out) {
   var _this = this;
   this.id = id;
   this.out = out;
 
-  var outputPort = [ new THREE.Vector3( halfHeight,                         0.0, 0.0),
-                     new THREE.Vector3(-halfHeight, size *  triangleRatio * 0.5, 0.0),
-                     new THREE.Vector3(-halfHeight, size * -triangleRatio * 0.5, 0.0)];
-
-  var inputPort  = [ new THREE.Vector3(-halfHeight,                         0.0, 0.0),
-                     new THREE.Vector3( halfHeight, size * -triangleRatio * 0.5, 0.0),
-                     new THREE.Vector3( halfHeight, size *  triangleRatio * 0.5, 0.0)];
-
-  var portVert = out ? outputPort  : inputPort;
   var color    = out ? outputColor : inputColor;
-
-  this.attributes = {
-    posL: {
-      type: 'v3',
-      value: [
-        new THREE.Vector3(0.0, 1.0, 1.0),
-        new THREE.Vector3(0.0, 1.0, 1.0),
-        new THREE.Vector3(1.0, 1.0, 0.0)
-      ]
-    },
-    posR: {
-      type: 'v3',
-      value: [
-        new THREE.Vector3(0.0, 1.0, 1.0),
-        new THREE.Vector3(1.0, 1.0, 0.0),
-        new THREE.Vector3(0.0, 1.0, 1.0)
-      ]
-    },
-    posB: {
-      type: 'v3',
-      value: [
-        new THREE.Vector3(1.0, 1.0, 0.0),
-        new THREE.Vector3(0.0, 1.0, 1.0),
-        new THREE.Vector3(0.0, 1.0, 1.0)
-      ]
-    }
-
-  };
 
   this.uniforms = {
     color:     { type: 'v4', value: color },
     colorFar:  { type: 'v4', value: colorFar },
-    mouseDist: { type: 'f',  value: 100000.0 }
+    mouseDist: { type: 'f',  value: 100000.0 },
+    nodeSize:  { type: 'f',  value: nodeSize },
+    portSize:  { type: 'f',  value: height }
   };
 
   Object.keys($$.commonUniforms).forEach(function(k) {
     _this.uniforms[k] = $$.commonUniforms[k];
   });
 
-  this.geometry = new THREE.Geometry();
-  portVert.forEach(function(vert) {
-    _this.geometry.vertices.push(vert);
-  });
-  this.geometry.faces.push(new THREE.Face3(0, 1, 2));
   this.mesh = new THREE.Mesh(
-			this.geometry,
+      new THREE.PlaneBufferGeometry(width, height),
 			new THREE.ShaderMaterial( {
         uniforms:       this.uniforms,
-        attributes:     this.attributes,
 				vertexShader:   vs,
 				fragmentShader: fs,
 				transparent:    true,
-				blending:       THREE.NormalBlending
+				blending:       THREE.NormalBlending,
+        side:           THREE.DoubleSide
   	})
 	);
 
