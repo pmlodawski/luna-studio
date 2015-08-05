47776f2c2bf2a22e1c6087254677ffdbd998a873
Konrad Gądek <kgadek@gmail.com>
2015-08-05 17:00:02 +0200

    Hook: pre-push cleanup

--- hooks/pre-push.py
+++ hooks/pre-push.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import sys
 import traceback
 from git_utils import releasing
-from io_utils import fprint, fmt
+from io_utils import fprint
 from shtack_exceptions import ShtackHookAbort
 
 
@@ -30,49 +30,18 @@ def main():
 
         with releasing(this_repo.config_reader()) as cfg:
             for submodule in this_repo.submodules:
-                # fprint("submod: submodule.name = {submodule.name}", colour='blue')
-                # tmp = type(submodule.hexsha)
-                # fprint("        " + "submodule.hexsha = {submodule.hexsha} :: {tmp}")
-                # tmp = type(submodule.branch)
-                # fprint("        " + "submodule.branch = {submodule.branch} :: {tmp}")
-                # tmp = type(submodule.branch_path)
-                # fprint("        " + "submodule.branch_path = {submodule.branch_path} :: {tmp}")
-
                 submod_repo = git.Repo(submodule.name)
-                # fprint("        " + "submod_repo.head = {submod_repo.head}")
-                # fprint("        " + "submod_repo.head.commit = {submod_repo.head.commit}")
-                # fprint("        " + "submod_repo.head.reference = {submod_repo.head.reference}")
-                #
-                # getinfo(submod_repo.head.ref.commit)
-                # getinfo(submod_repo.head.ref.tracking_branch().commit)
 
                 if submod_repo.head.commit.hexsha != submodule.hexsha:
                     fprint("Submodule {submodule} has changes.", colour='yellow')
+
                 if submod_repo.head.ref.tracking_branch().commit.hexsha != submod_repo.head.ref.commit.hexsha:
                     raise ShtackHookAbort("Submodule {submodule} points to commits that are not on remote.")
+
     except ShtackHookAbort as e:
         fprint(e.args[0], colour='red')
         traceback.print_stack()
         sys.exit(1)
-
-
-def getinfo(ob):
-    ob_s = str(ob)
-    tob = type(ob).__name__
-    fprint("{ob_s:<32} :: {tob}", colour='blue')
-    for dob_e in dir(ob):
-        if dob_e.startswith("_"):
-            pass
-        else:
-            try:
-                dob_eo = getattr(ob, dob_e)
-                dob_et = type(dob_eo).__name__
-            except:
-                dob_et = "???"
-            if dob_et == 'method':
-                fprint("  > {dob_e:30} :: {dob_et:15}")
-            else:
-                fprint("  > {dob_e:30} :: {dob_et:15} = {dob_eo}")
 
 
 if __name__ == '__main__':
