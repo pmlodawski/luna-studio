8ff035fe07904c631101ab0c017a95726bb4df6d
Konrad Gądek <kgadek@gmail.com>
2015-08-06 19:09:33 +0200

    Hook/post-checkout: test, shall work

--- hooks/post-checkout.py
+++ hooks/post-checkout.py
@@ -3,10 +3,16 @@
 # noinspection PyUnresolvedReferences
 import git
 from io_utils import finfo
+# noinspection PyUnresolvedReferences
+from plumbum import local
 
 
 def main():
     finfo("post-checkout (master branch)")
+    git_cmd = local["git"]
+    git_cmd["submodule", "update", "--init", "--remote", "--no-fetch"]()
+    git_cmd["submodule", "foreach", "-q", "--recursive",
+            'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git checkout $branch']()
 
 
 if __name__ == '__main__':
