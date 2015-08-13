3fcbd39a995e8dc88e5d94ce3e2ce9db476a9b6b
Kamil Figiela <kamil.figiela@gmail.com>
2015-08-13 12:58:33 +0200

    Remove camPan and screeenSize from common uniforms

--- app/js/common.js
+++ app/js/common.js
@@ -1,18 +1,18 @@
 "use strict";
 
 var commonUniforms =  {
-  camPan:     {type: 'v2', value: new THREE.Vector2(0.0, 0.0)},
+  // camPan:     {type: 'v2', value: new THREE.Vector2(0.0, 0.0)},
   camFactor:  {type: 'f',  value: 1.0},
-  screenSize: {type: 'v2', value: new THREE.Vector2(1280.0, 800.0)},
+  // screenSize: {type: 'v2', value: new THREE.Vector2(1280.0, 800.0)},
   antialias:  {type: 'i',  value: 0},
   objectMap:  {type: 'i',  value: 1}
 };
 
 module.exports = {
   commonUniforms: commonUniforms,
-  screenSize:     commonUniforms.screenSize.value,
+  screenSize:     new THREE.Vector2(1280.0, 800.0),
   camFactor:      commonUniforms.camFactor,
-  camPan:         commonUniforms.camPan.value,
+  camPan:         new THREE.Vector2(0.0, 0.0),
   scene:          undefined,
   camera:         undefined,
   renderer:       undefined,
