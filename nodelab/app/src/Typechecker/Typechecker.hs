8ca426d84176401eda85b0552aa787cc1ed373e1
Adam Szlachta <adam.szlachta@gmail.com>
2015-08-25 16:54:15 +0200

    Adjusted typechecker test to newest API

--- nodelab/app/src/Typechecker/Typechecker.hs
+++ nodelab/app/src/Typechecker/Typechecker.hs
@@ -9,19 +9,19 @@ import           Utils.PreludePlus
 
 import           Control.Monad.State
 
-import           Luna.Syntax.Graph.Builder.State (BldrState, GraphBuilder, GraphBuilderT)
-import           Luna.Syntax.Graph.Builder
-import           Luna.Syntax.Graph
-import           Luna.Syntax.Decl
-import           Luna.Syntax.Term
+import           Luna.Syntax.Builder.Graph
+import           Luna.Syntax.Builder
+import           Luna.Syntax.Layer.Labeled
+import           Luna.Syntax.Layer.Typed
+import           Luna.Syntax.AST.Term
+import           Luna.Syntax.AST.Decl.Function
 
-import           Typechecker.Label as X
 import           Typechecker.Meta  as X
 
 
-
-type GraphMeta            = HomoNet  (Label Meta) Term
-type GraphRefMeta         = GraphRef (Label Meta) Term
+type LabeledMeta          = Labeled Meta (Typed Term)
+type GraphMeta            = HomoGraph ArcPtr LabeledMeta
+type GraphRefMeta         = Arc              LabeledMeta
 
 type FunctionGraphMeta    = Function  GraphMeta
 type StateGraphMeta       = BldrState GraphMeta
@@ -29,9 +29,10 @@ type StateGraphMeta       = BldrState GraphMeta
 type RefFunctionGraphMeta = (GraphRefMeta, FunctionGraphMeta)
 
 
-evalFunctionBuilderState :: Default s => GraphBuilderT g (StateT s Identity) a -> BldrState g -> a
-execFunctionBuilderState :: Default s => GraphBuilderT g (StateT s Identity) a -> BldrState g -> Function g
-runFunctionBuilderState  :: Default s => GraphBuilderT g (StateT s Identity) a -> BldrState g -> (a, Function g)
+evalFunctionBuilderState :: Default d => GraphStarBuilderT s g (StateT d Identity) a -> BldrState g -> a
+execFunctionBuilderState :: Default d => GraphStarBuilderT s g (StateT d Identity) a -> BldrState g -> Function g
+runFunctionBuilderState  :: Default d => GraphStarBuilderT s g (StateT d Identity) a -> BldrState g -> (a, Function g)
+
 
 evalFunctionBuilderState = (runIdentity . flip evalStateT def) .: evalFunctionBuilderT
 execFunctionBuilderState = (runIdentity . flip evalStateT def) .: execFunctionBuilderT
