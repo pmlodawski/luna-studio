b7d24cff3ba49f7db8b32bc8f697c412e959d517
Marcin Kostrzewa <marcinkostrzewa@yahoo.com>
2015-10-01 18:24:58 +0200

    graph unrendering

--- nodelab/app/src/Reactive/Plugins/Core/Action/Executors/EnterNode.hs
+++ nodelab/app/src/Reactive/Plugins/Core/Action/Executors/EnterNode.hs
@@ -1,10 +1,16 @@
 module Reactive.Plugins.Core.Action.Executors.EnterNode where
 
 import Utils.PreludePlus
-import Reactive.Plugins.Core.Action.State.Global (State)
-import Object.Node
+import Reactive.Plugins.Core.Action.State.Global            (State)
+import Reactive.Plugins.Core.Action.Executors.UnrenderGraph (unrender)
+
+import Object.Node (Node, isDef)
 
 enterNode :: Node -> State -> (State, IO ())
 enterNode node state = if   isDef node
-                       then (state, print $ "would enter: " ++ (show node))
+                       then performEnter node state
                        else (state, print "would not enter")
+
+performEnter :: Node -> State -> (State, IO ())
+performEnter node state = (newState, action) where
+    (newState, action) = unrender state
