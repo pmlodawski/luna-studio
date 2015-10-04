8f814481f4d4eab3bea5f165127ff016f10d28b5
Kamil Figiela <kamil.figiela@gmail.com>
2015-10-04 16:45:20 +0200

    Prototype deployment script

---
+++ nodelab/build_dist.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set +x
+set -e
+
+
+if ! git diff-index --quiet HEAD --; then
+  echo "Can't deploy - repository is in dirty state"
+  exit 1
+fi
+
+git_hash=$(git rev-parse HEAD)
+s3_path=s3://rednosedreindeer/$git_hash/
+
+rm -rf www/
+
+brunch build --env production
+
+tmp=$(mktemp -d)
+for i in $(find www -type f \( -name '*.html' -o -name '*.css' -o -name '*.woff' -o -name '*.ttf' -o -name '*.eot' -o -name '*.js' \))
+do
+	file $i
+	mkdir -p $tmp/$(dirname $i)
+	gzip -c $i > $tmp/$i
+done
+
+s3cmd sync --progress --acl-public --reduced-redundancy --verbose --add-header 'Content-Encoding: gzip' $tmp/www/* $s3_path --exclude '*.*'    --include '*.html' --include '*.js'  --include '*.css'  --include '*.woff' --include '*.ttf' --include '*.eot'
+s3cmd sync --progress --acl-public --reduced-redundancy --verbose                                        www/*     $s3_path                    --exclude '*.html' --exclude '*.js'   --exclude '*.css' --exclude '*.woff' --exclude '*.ttf' --exclude '*.eot'
+
+rm -rf $tmp
