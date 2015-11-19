5c4ff4f9793db9a25801c81b64e02e31129fe20f
Wojciech Danilo <wojciech.danilo@gmail.com>
2015-11-19 09:51:33 +0100

    Added unified devrun script.

--- libs/luna/typechecker/devrun.sh
+++ libs/luna/typechecker/devrun.sh
@@ -1,42 +1,55 @@
-#!/bin.bash
+#!/bin/bash
+function realpath()
+{
+    f=$@
+    if [ -d "$f" ]; then
+        base=""
+        dir="$f"
+    else
+        base="/$(basename "$f")"
+        dir=$(dirname "$f")
+    fi
+    dir=$(cd "$dir" && /bin/pwd)
+    echo "$dir$base"
+}
 
-runhaskell                                                                                                 \
-    -XDoAndIfThenElse                                                                                      \
-    -XNoImplicitPrelude                                                                                    \
-    -XTemplateHaskell                                                                                      \
-    -XOverloadedStrings                                                                                    \
-    -XDeriveGeneric                                                                                        \
-    -XStandaloneDeriving                                                                                   \
-    -XFlexibleContexts                                                                                     \
-    -XMultiParamTypeClasses                                                                                \
-    -XDefaultSignatures                                                                                    \
-    -XDeriveFunctor                                                                                        \
-    -XDeriveTraversable                                                                                    \
-    -XDeriveFoldable                                                                                       \
-    -XFlexibleInstances                                                                                    \
-    -XLambdaCase                                                                                           \
-    -XConstraintKinds                                                                                      \
-    -XGeneralizedNewtypeDeriving                                                                           \
-    -XDeriveDataTypeable                                                                                   \
-    -XEmptyDataDecls                                                                                       \
-    -XTupleSections                                                                                        \
-    -XTypeOperators                                                                                        \
-    -XDataKinds                                                                                            \
-    -XInstanceSigs                                                                                         \
-    -XViewPatterns                                                                                         \
-    -XTypeFamilies                                                                                         \
-	-i/Users/wdanilo/dev/hs/private/nbo/libs/utils/src                                                     \
-    -i/Users/wdanilo/dev/hs/private/nbo/libs/luna/typechecker/src                                          \
-    -i/Users/wdanilo/dev/hs/public/convert/src                                                             \
-    -i/Users/wdanilo/dev/hs/public/data-repr/src                                                           \
-    -i/Users/wdanilo/dev/hs/public/data-layer/src                                                          \
-    -i/Users/wdanilo/dev/hs/public/data-rtuple/src                                                         \
-    -i/Users/wdanilo/dev/hs/public/prologue/src                                                            \
-    -i/Users/wdanilo/dev/hs/public/typelevel/src                                                           \
-    -i/Users/wdanilo/dev/hs/public/container/src                                                           \
-    -i/Users/wdanilo/dev/hs/public/functor-utils/src                                                       \
-    -i/Users/wdanilo/dev/hs/public/lens-utils/src                                                          \
-	-i/Users/wdanilo/dev/hs/public/data-construction/src                                                   \
-	-package-db=/Users/wdanilo/dev/hs/env/tc/.cabal-sandbox/x86_64-osx-ghc-7.10.2-packages.conf.d          \
-	-ddump-splices                                                                                         \
-	/Users/wdanilo/dev/nbo/libs/luna/typechecker/test/Main.hs                                              
+runhaskell                                                                                         \
+    -XDoAndIfThenElse                                                                              \
+    -XNoImplicitPrelude                                                                            \
+    -XTemplateHaskell                                                                              \
+    -XOverloadedStrings                                                                            \
+    -XDeriveGeneric                                                                                \
+    -XStandaloneDeriving                                                                           \
+    -XFlexibleContexts                                                                             \
+    -XMultiParamTypeClasses                                                                        \
+    -XDefaultSignatures                                                                            \
+    -XDeriveFunctor                                                                                \
+    -XDeriveTraversable                                                                            \
+    -XDeriveFoldable                                                                               \
+    -XFlexibleInstances                                                                            \
+    -XLambdaCase                                                                                   \
+    -XConstraintKinds                                                                              \
+    -XGeneralizedNewtypeDeriving                                                                   \
+    -XDeriveDataTypeable                                                                           \
+    -XEmptyDataDecls                                                                               \
+    -XTupleSections                                                                                \
+    -XTypeOperators                                                                                \
+    -XDataKinds                                                                                    \
+    -XInstanceSigs                                                                                 \
+    -XViewPatterns                                                                                 \
+    -XTypeFamilies                                                                                 \
+    -i$NBO/libs/utils/src                                                                          \
+    -i$NBO/libs/luna/typechecker/src                                                               \
+    -i$HS_PUBLIC_LIBS/convert/src                                                                  \
+    -i$HS_PUBLIC_LIBS/data-repr/src                                                                \
+    -i$HS_PUBLIC_LIBS/data-layer/src                                                               \
+    -i$HS_PUBLIC_LIBS/data-rtuple/src                                                              \
+    -i$HS_PUBLIC_LIBS/prologue/src                                                                 \
+    -i$HS_PUBLIC_LIBS/typelevel/src                                                                \
+    -i$HS_PUBLIC_LIBS/container/src                                                                \
+    -i$HS_PUBLIC_LIBS/functor-utils/src                                                            \
+    -i$HS_PUBLIC_LIBS/lens-utils/src                                                               \
+    -i$HS_PUBLIC_LIBS/data-construction/src                                                        \
+    -package-db=$HS_SBOX_PGS                                                                       \
+    -ddump-splices                                                                                 \
+    $(realpath)/test/Main.hs                                              
