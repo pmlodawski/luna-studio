ea4ff55553f670a30e9f177ff84d25b9c89e7f20
Kamil Figiela <kamil.figiela@gmail.com>
2015-11-23 13:29:05 +0100

    Add TextBox value changed handler

--- nodelab/app/src/UI/Handlers/TextBox.hs
+++ nodelab/app/src/UI/Handlers/TextBox.hs
@@ -35,7 +35,6 @@ dblClickHandler _ _ id = do
     takeFocus undefined id
     inRegistry $ UICmd.update_ id $ Model.isEditing .~ True
 
-
 foreign import javascript unsafe "$1.registry[$2].input.val()" getValue' :: JSState -> WidgetId -> JSString
 
 keyDownHandler :: KeyDownHandler Global.State
@@ -43,6 +42,8 @@ keyDownHandler '\r' _ jsState id = do
     let value = lazyTextFromJSString $ getValue' jsState id
     inRegistry $ UICmd.update_ id $ (Model.isEditing .~ False)
                                   . (Model.value     .~ value)
+    triggerValueChanged value id
+
 keyDownHandler '\27' _ jsState id = do
     let value = lazyTextFromJSString $ getValue' jsState id
     inRegistry $ UICmd.update_ id $ (Model.isEditing .~ False)
