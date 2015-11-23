e628c9cb6ab88879f7f5f8b1d94a19d6494c578d
Kamil Figiela <kamil.figiela@gmail.com>
2015-11-23 17:23:56 +0100

    Fix GHC/GHCI syntax incompatibility

--- nodelab/app/src/UI/Layout.hs
+++ nodelab/app/src/UI/Layout.hs
@@ -14,15 +14,10 @@ import qualified Reactive.State.UIRegistry     as UIRegistry
 import qualified Reactive.Commands.UIRegistry  as UICmd
 
 verticalLayout :: Double -> WidgetId -> Command UIRegistry.State ()
-verticalLayout spacing id = do
-    children    <- UICmd.children id
-
-    heights     <- forM children $ \id -> do
+verticalLayout spacing id = UICmd.children id >>= mapM getHeight >>= foldM_ move 0.0 where
+    getHeight id = do
         size <- UICmd.get' id widgetSize
-        return (id, size ^. y)
-
-    let move = \offset (id, height) -> do
+        return $ (id, size ^. y)
+    move offset (id, height) = do
         UICmd.moveY id offset
         return $ offset + spacing + height
-
-    foldM_ move 0.0 heights
