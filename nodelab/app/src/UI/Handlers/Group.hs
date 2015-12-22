411d9d9836e1af08867ba801434ec2d9e9b5a250
Kamil Figiela <kamil.figiela@gmail.com>
2015-12-22 21:05:37 +0100

    Forgot to commit new file…

---
+++ nodelab/app/src/UI/Handlers/Group.hs
@@ -0,0 +1,28 @@
+module UI.Handlers.Group where
+
+import           Utils.PreludePlus
+
+import           Utils.Vector
+import           Data.HMap.Lazy               (TypeKey (..))
+
+import           Object.Widget                (WidgetId, ResizableWidget, resizeWidget)
+import qualified Object.Widget.Group          as Model
+import           Reactive.Commands.Command    (Command)
+import qualified Reactive.Commands.UIRegistry as UICmd
+import           Reactive.State.Global        (inRegistry)
+import qualified Reactive.State.Global        as Global
+import qualified Reactive.State.UIRegistry    as UIRegistry
+
+import           UI.Generic                   (defaultResize)
+
+newtype WidgetResizedHandler = WidgetResizedHandler (WidgetId -> Vector2 Double -> Command UIRegistry.State ())
+triggerWidgetResized :: WidgetId -> Vector2 Double -> Command UIRegistry.State ()
+triggerWidgetResized id vec = do
+    let key = TypeKey :: TypeKey WidgetResizedHandler
+    maybeHandler <- UICmd.handler id key
+    forM_ maybeHandler $ \(WidgetResizedHandler handler) -> handler id vec
+
+instance ResizableWidget Model.Group where
+    resizeWidget id vec model = do
+        defaultResize id vec model
+        triggerWidgetResized id vec
