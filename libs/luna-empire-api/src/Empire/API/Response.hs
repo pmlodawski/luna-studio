3b61310f1c443a7b66eafee241bf2747c7c252bc
Kamil Figiela <kamil.figiela@gmail.com>
2016-01-08 12:30:36 +0100

    Add prisms for Empire.API.Response.Response

--- libs/luna-empire-api/src/Empire/API/Response.hs
+++ libs/luna-empire-api/src/Empire/API/Response.hs
@@ -19,5 +19,12 @@ data Response req upd = Update    { _request  :: req
 type SimpleResponse req = Response req ResultOk
 
 makeLenses ''Response
+makePrisms ''Response
+
+-- _UpdatePayload :: Getter (Response req upd) upd -- FIXME: Jak to otypowac?
+-- _UpdatePayload = _Update . _2
+
+-- _UpdateRequest :: Getter (Response req upd) req
+-- _UpdateRequest = _Update . _1
 
 instance (Binary req, Binary upd) => Binary (Response req upd)
