f8e7192c58e6718aee36b622182b5288b47cdf6e
Piotr Młodawski <pmlodawski@gmail.com>
2016-01-14 14:46:34 +0100

    Add more interpreter api methods

--- libs/luna/typechecker/src/Luna/Interpreter/Dirty.hs
+++ libs/luna/typechecker/src/Luna/Interpreter/Dirty.hs
@@ -1,11 +1,18 @@
 module Luna.Interpreter.Dirty where
 
 import           Development.Placeholders
-import           Luna.Interpreter.Env     (Env)
-import qualified Luna.Interpreter.Env     as Env
-import           Luna.Interpreter.Monad   (InterpreterMonad, get)
-import           Luna.Syntax.Repr.Graph   (Node, Ref)
+import           Luna.Interpreter.Env       (Env)
+import qualified Luna.Interpreter.Env       as Env
+import           Luna.Interpreter.Label     (Label)
+import qualified Luna.Interpreter.Label     as Label
+import           Luna.Interpreter.Monad     (InterpreterMonad)
+import           Luna.Syntax.Builder        (readRef, writeRef)
+import           Luna.Syntax.Builder.Class  (BuilderMonad)
+import           Luna.Syntax.Layer.Labeled  (label)
+import           Luna.Syntax.Repr.Graph     (Node, Ref)
+import           Luna.Syntax.Symbol.Network (Network)
 import           Prologue
+
 
 
 follow :: InterpreterMonad Env m => Ref Node -> m ()
@@ -13,5 +20,15 @@ follow node = do
     Env.addReqNode node
     $notImplemented
 
-markSuccessors = do
-    $notImplemented
+markSuccessors :: BuilderMonad (Network Label) m => Ref Node -> m ()
+markSuccessors ref = do
+    node <- readRef ref
+    unless (node ^. label . Label.dirty) $ do
+        writeRef ref (node & label . Label.dirty .~ True)
+        $notImplemented
+
+    -- unless (node ^. dirty) $ do
+    --         node.dirty = True
+    --         if node.required || isIO node
+    --             reqNodes.add node
+    --         mapM markSuccs node.succ
--- libs/luna/typechecker/src/Luna/Interpreter/Interpreter.hs
+++ libs/luna/typechecker/src/Luna/Interpreter/Interpreter.hs
@@ -1,24 +1,70 @@
 module Luna.Interpreter.Interpreter where
 
 import           Development.Placeholders
-import qualified Luna.Interpreter.Dirty   as Dirty
-import           Luna.Interpreter.Env     (Env)
-import qualified Luna.Interpreter.Env     as Env
-import           Luna.Interpreter.Monad   (InterpreterMonad, get)
+import qualified Luna.Interpreter.Dirty     as Dirty
+import           Luna.Interpreter.Env       (Env)
+import qualified Luna.Interpreter.Env       as Env
+import           Luna.Interpreter.Label     (Label)
+import qualified Luna.Interpreter.Label     as Label
+import           Luna.Interpreter.Monad     (InterpreterMonad, get)
+import           Luna.Syntax.Builder        (readRef, withRef)
+import           Luna.Syntax.Builder.Class  (BuilderMonad)
+import           Luna.Syntax.Layer.Labeled  (label)
+import           Luna.Syntax.Repr.Graph     (Node, Ref)
+import           Luna.Syntax.Symbol.Network (Network)
 import           Prologue
 
 
-execute :: InterpreterMonad Env m => m ()
+
+execute :: (InterpreterMonad Env m, BuilderMonad (Network Label) m) => m ()
 execute = do
     mapM_ Dirty.follow =<< Env.getReqNodes
     mapM_ run =<< topsort =<< Env.getReqNodes
     Env.clearReqNodes
 
-connect prev next = $notImplemented
+connect :: BuilderMonad (Network Label) m => Ref Node -> Ref Node -> m ()
+connect prev next = do
+    isPrevDirty <- view (label . Label.dirty) <$> readRef prev
+    Dirty.markSuccessors $ if isPrevDirty
+        then prev
+        else next
 
+markModified :: BuilderMonad (Network Label) m => Ref Node -> m ()
 markModified = Dirty.markSuccessors
 
-run = $notImplemented
+run :: BuilderMonad (Network Label) m => Ref Node -> m ()
+run ref = do
+    $(todo "inline node")
+    $(todo "compute node")
+    withRef ref (label . Label.dirty .~ False)
 
 topsort :: [a] -> m [a]
 topsort = $notImplemented
+
+
+
+-- def inline node:
+--     specialization = SymbolMap.lookup node.type node.name
+--     node.spec = specialization
+--
+-- def compute node:
+--     if node.isIO:
+--         computeIO node
+--     elif node.isReq:
+--         for node.reqests as req:
+--             reportValue req node
+--
+-- def computeIO node:
+--     v <- runIO =<< compile node.spec
+--     node.spec = v
+--
+-- def reportValue req node:
+--     v <- req =<< compile node.spec
+--     reportToGUI node.location v
+--
+-- def compile node: -- # Node -> a
+--     ... -- call ghc
+--
+-- def decompile node:
+--     for s in node.succ:
+--         s.pre = node
