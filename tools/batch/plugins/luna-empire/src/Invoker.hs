bcbed435a3c51449632d6b0626179fe7fa1330b0
Adam Szlachta <adam.szlachta@gmail.com>
2016-01-14 16:17:37 +0100

    Luna empire invoker command line tool

---
+++ tools/batch/plugins/luna-empire/src/Invoker.hs
@@ -0,0 +1,125 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module Main where
+
+import           Prologue                         hiding (argument)
+import           System.Environment               (getArgs)
+import           System.Console.Docopt
+import qualified Data.Binary                      as Bin
+import qualified Data.ByteString                  as ByteString
+import qualified Data.ByteString.Char8            as Char8 (pack)
+import           Data.ByteString.Lazy             (fromStrict, toStrict)
+import qualified Flowbox.Config.Config            as Config
+import qualified Flowbox.Bus.EndPoint             as EP
+import qualified Flowbox.Bus.Bus                  as Bus
+import qualified Flowbox.Bus.Data.Flag            as Flag
+import qualified Flowbox.Bus.Data.Message         as Message
+import           Flowbox.Options.Applicative      (short, long, help, metavar)
+import qualified Flowbox.Options.Applicative      as Opt
+import qualified Empire.API.Data.Node             as Node
+import qualified Empire.API.Data.NodeMeta         as NodeMeta
+import qualified Empire.API.Data.NodeMeta         as NodeMeta
+import qualified Empire.API.Data.GraphLocation    as GraphLocation
+import qualified Empire.API.Data.Breadcrumb       as Breadcrumb
+import qualified Empire.API.Graph.AddNode         as AddNode
+import qualified Empire.API.Graph.RemoveNode      as RemoveNode
+import qualified Empire.API.Graph.UpdateNodeMeta  as UpdateNodeMeta
+import qualified Empire.API.Graph.GetProgram      as GetProgram
+import qualified Empire.API.Project.CreateProject as CreateProject
+import qualified Empire.API.Project.ListProjects  as ListProjects
+import qualified Empire.API.Library.CreateLibrary as CreateLibrary
+import qualified Empire.API.Library.ListLibraries as ListLibraries
+import qualified Empire.API.Topic                 as Topic
+import qualified Empire.API.Update                as Update
+
+gl :: Int -> Int -> GraphLocation.GraphLocation
+gl pid lid = GraphLocation.GraphLocation pid lid Breadcrumb.Breadcrumb
+
+patterns :: Docopt
+patterns = [docoptFile|src/InvokerUsage.txt|]
+
+getArgOrExit = getArgOrExitWith patterns
+
+main :: IO ()
+main = do
+    args <- parseArgsOrExit patterns =<< getArgs
+    endPoints <- EP.clientFromConfig <$> Config.load
+    when (args `isPresent` (command "addNode")) $ do
+        pid    <- args `getArgOrExit` (argument "pid")
+        lid    <- args `getArgOrExit` (argument "lid")
+        expr   <- args `getArgOrExit` (argument "expression")
+        x      <- args `getArgOrExit` (argument "x")
+        y      <- args `getArgOrExit` (argument "y")
+        tag    <- args `getArgOrExit` (argument "tag")
+        addNode endPoints (read pid) (read lid) expr (read x) (read y) (read tag)
+    when (args `isPresent` (command "removeNode")) $ do
+        pid    <- args `getArgOrExit` (argument "pid")
+        lid    <- args `getArgOrExit` (argument "lid")
+        nodeId <- args `getArgOrExit` (argument "nodeId")
+        removeNode endPoints (read pid) (read lid) (read nodeId)
+    when (args `isPresent` (command "updateNodeMeta")) $ do
+        pid    <- args `getArgOrExit` (argument "pid")
+        lid    <- args `getArgOrExit` (argument "lid")
+        nodeId <- args `getArgOrExit` (argument "nodeId")
+        x      <- args `getArgOrExit` (argument "x")
+        y      <- args `getArgOrExit` (argument "y")
+        updateNodeMeta endPoints (read pid) (read lid) (read nodeId) (read x) (read y)
+    when (args `isPresent` (command "getProgram")) $ do
+        pid    <- args `getArgOrExit` (argument "pid")
+        lid    <- args `getArgOrExit` (argument "lid")
+        getProgram endPoints (read pid) (read lid)
+    when (args `isPresent` (command "createLibrary")) $ do
+        pid    <- args `getArgOrExit` (argument "pid")
+        path   <- args `getArgOrExit` (argument "path")
+        let name = args `getArg` (longOption "name")
+        createLibrary endPoints (read pid) name path
+    when (args `isPresent` (command "libs")) $ do
+        pid    <- args `getArgOrExit` (argument "pid")
+        listLibraries endPoints $ read pid
+    when (args `isPresent` (command "createProject")) $ do
+        path   <- args `getArgOrExit` (argument "path")
+        let name = args `getArg` (longOption "name")
+        createProject endPoints Nothing path
+    when (args `isPresent` (command "projects")) $ do
+        listProjects endPoints
+
+addNode :: EP.BusEndPoints -> Int -> Int -> String -> Double -> Double -> Int -> IO ()
+addNode endPoints pid lid expression x y tag = do
+    let content = toStrict . Bin.encode $ AddNode.Request (gl pid lid) expression (NodeMeta.NodeMeta (x, y)) tag
+    void $ Bus.runBus endPoints $ Bus.send Flag.Enable $ Message.Message Topic.addNodeRequest content
+
+removeNode :: EP.BusEndPoints -> Int -> Int -> Int -> IO ()
+removeNode endPoints pid lid nodeId = do
+    let content = toStrict . Bin.encode $ RemoveNode.Request (gl pid lid) nodeId
+    void $ Bus.runBus endPoints $ Bus.send Flag.Enable $ Message.Message Topic.removeNodeRequest content
+
+updateNodeMeta :: EP.BusEndPoints -> Int -> Int -> Int -> Double -> Double -> IO ()
+updateNodeMeta endPoints pid lid nodeId x y = do
+    let content = toStrict . Bin.encode $ UpdateNodeMeta.Request (gl pid lid) nodeId (NodeMeta.NodeMeta (x, y))
+    void $ Bus.runBus endPoints $ Bus.send Flag.Enable $ Message.Message Topic.updateNodeMetaRequest content
+
+getProgram :: EP.BusEndPoints -> Int -> Int -> IO ()
+getProgram endPoints pid lid = do
+    let content = toStrict . Bin.encode $ GetProgram.Request (gl pid lid)
+    void $ Bus.runBus endPoints $ Bus.send Flag.Enable $ Message.Message Topic.programRequest content
+
+createProject :: EP.BusEndPoints -> Maybe String -> String -> IO ()
+createProject endPoints name path = do
+    let content = toStrict . Bin.encode $ CreateProject.Request name path
+    void $ Bus.runBus endPoints $ Bus.send Flag.Enable $ Message.Message Topic.createProjectRequest content
+
+listProjects :: EP.BusEndPoints -> IO ()
+listProjects endPoints = do
+    let content = toStrict . Bin.encode $ ListProjects.Request
+    void $ Bus.runBus endPoints $ Bus.send Flag.Enable $ Message.Message Topic.listProjectsRequest content
+
+createLibrary :: EP.BusEndPoints -> Int -> Maybe String -> String -> IO ()
+createLibrary endPoints pid name path = do
+    let content = toStrict . Bin.encode $ CreateLibrary.Request pid name path
+    void $ Bus.runBus endPoints $ Bus.send Flag.Enable $ Message.Message Topic.createLibraryRequest content
+
+listLibraries :: EP.BusEndPoints -> Int -> IO ()
+listLibraries endPoints pid = do
+    let content = toStrict . Bin.encode $ ListLibraries.Request pid
+    void $ Bus.runBus endPoints $ Bus.send Flag.Enable $ Message.Message Topic.listLibrariesRequest content
--- tools/batch/plugins/luna-empire/test/Main.hs
+++ tools/batch/plugins/luna-empire/test/Main.hs
@@ -33,10 +33,6 @@ data Cmd = TestNotRecognizedRequest
          | TestAddNode
          | TestRemoveNode
          | TestNodeUpdate
-         | TestCreateProject
-         | TestListProjects
-         | TestCreateLibrary { pid :: Int }
-         | TestListLibraries { pid :: Int }
          deriving Show
 
 parser :: Opt.Parser Cmd
@@ -45,10 +41,6 @@ parser = Opt.flag' TestNotRecognizedRequest (long "nrr" <> help "Not Recognized 
      <|> Opt.flag' TestAddNode              (long "an"  <> help "Add Node")
      <|> Opt.flag' TestRemoveNode           (long "rn"  <> help "Remove Node")
      <|> Opt.flag' TestNodeUpdate           (long "nu"  <> help "Node Update")
-     <|> Opt.flag' TestCreateProject        (short 'p' <> long "cPrj" <> help "Create Project")
-     <|> Opt.flag' TestListProjects         (short 'r' <> long "lPrj" <> help "List Projects")
-     <|>           TestCreateLibrary <$>    Opt.optIntFlag (Just "cLib") 'b' 2 0 "Create Library"
-     <|>           TestListLibraries <$>    Opt.optIntFlag (Just "lLib") 'l' 2 0 "List Libraries"
 
 run :: Cmd -> IO ()
 run cmd = case cmd of
@@ -57,10 +49,6 @@ run cmd = case cmd of
     TestAddNode              -> testAddNode
     TestRemoveNode           -> testRemoveNode
     TestNodeUpdate           -> testNodeUpdate
-    TestCreateProject        -> testCreateProject
-    TestListProjects         -> testListProjects
-    TestCreateLibrary pid    -> testCreateLibrary pid
-    TestListLibraries pid    -> testListLibraries pid
 
 
 opts :: Opt.ParserInfo Cmd
@@ -77,73 +65,36 @@ gl pid lid = GraphLocation.GraphLocation pid lid Breadcrumb.Breadcrumb
 testNotRecognizedRequest :: IO ()
 testNotRecognizedRequest = do
     endPoints <- EP.clientFromConfig <$> Config.load
-    Bus.runBus endPoints $ do
+    void $ Bus.runBus endPoints $ do
         Bus.send Flag.Enable $ Message.Message "empire.hello.request" (Char8.pack "basic string")
-    return ()
 
 testBadTopic :: IO ()
 testBadTopic = do
     endPoints <- EP.clientFromConfig <$> Config.load
-    Bus.runBus endPoints $ do
+    void $ Bus.runBus endPoints $ do
         Bus.send Flag.Enable $ Message.Message "any-hello" ByteString.empty
-    return ()
 
 testAddNode :: IO ()
 testAddNode = do
     endPoints <- EP.clientFromConfig <$> Config.load
     let content = toStrict . Bin.encode $ AddNode.Request (gl 0 0) "expres" (NodeMeta.NodeMeta (1.2, 3.4)) 7
-    Bus.runBus endPoints $ do
+    void $ Bus.runBus endPoints $ do
         Bus.send Flag.Enable $ Message.Message Topic.addNodeRequest content
-    return ()
 
 testRemoveNode :: IO ()
 testRemoveNode = do
     endPoints <- EP.clientFromConfig <$> Config.load
     let content = toStrict . Bin.encode $ RemoveNode.Request (gl 0 0) 3
-    Bus.runBus endPoints $ do
+    void $ Bus.runBus endPoints $ do
         Bus.send Flag.Enable $ Message.Message Topic.removeNodeRequest content
-    return ()
 
 testNodeUpdate :: IO ()
 testNodeUpdate = do
     endPoints <- EP.clientFromConfig <$> Config.load
-    Bus.runBus endPoints $ do
+    void $ Bus.runBus endPoints $ do
         let meta    = NodeMeta.NodeMeta (20.0, 30.0)
             request = AddNode.Request (gl 0 0) "dupa123" meta 1235
             node    = Node.Node 123 "dupa123" mempty meta
             result  = AddNode.Result node
             update  = Update.Update request result
-        Bus.send Flag.Enable $ Message.Message "empire.graph.node.add.update" $ toStrict $ Bin.encode update
-    return ()
-
-testCreateProject :: IO ()
-testCreateProject = do
-    endPoints <- EP.clientFromConfig <$> Config.load
-    let content = toStrict . Bin.encode $ CreateProject.Request (Just "dupaPr") "/no/elo"
-    Bus.runBus endPoints $ do
-        Bus.send Flag.Enable $ Message.Message Topic.createProjectRequest content
-    return ()
-
-testListProjects :: IO ()
-testListProjects = do
-    endPoints <- EP.clientFromConfig <$> Config.load
-    let content = toStrict . Bin.encode $ ListProjects.Request
-    Bus.runBus endPoints $ do
-        Bus.send Flag.Enable $ Message.Message Topic.listProjectsRequest content
-    return ()
-
-testCreateLibrary :: Int -> IO ()
-testCreateLibrary pid = do
-    endPoints <- EP.clientFromConfig <$> Config.load
-    let content = toStrict . Bin.encode $ CreateLibrary.Request pid (Just "dupaLib") "/no/elo"
-    Bus.runBus endPoints $ do
-        Bus.send Flag.Enable $ Message.Message Topic.createLibraryRequest content
-    return ()
-
-testListLibraries :: Int -> IO ()
-testListLibraries pid = do
-    endPoints <- EP.clientFromConfig <$> Config.load
-    let content = toStrict . Bin.encode $ ListLibraries.Request pid
-    Bus.runBus endPoints $ do
-        Bus.send Flag.Enable $ Message.Message Topic.listLibrariesRequest content
-    return ()
+        Bus.send Flag.Enable $ Message.Message Topic.addNodeUpdate $ toStrict $ Bin.encode update
