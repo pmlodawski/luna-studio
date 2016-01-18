d467cfb57c610ec18977d034f0ffc1944fc4e552
Kamil Figiela <kamil.figiela@gmail.com>
2016-01-18 17:26:11 +0100

    Fix upload script

--- upload_backend.sh
+++ upload_backend.sh
@@ -8,11 +8,11 @@ s3_path=s3://nodelab-gui/backend/$DRONE_COMMIT.tar.gz
 
 
 cd build_7.10/.bin
-tar -czvf artifacts.tar.gz --exclude "*.tar.gz" .
+tar -czvf /tmp/artifacts.tar.gz --exclude "*.tar.gz" .
 echo "Uploading gzipped files to S3..."
 
 aws s3 cp                          \
-  artifacts.tar.gz $s3_path        \
+  /tmp/artifacts.tar.gz $s3_path   \
   --region  eu-west-1
 
 echo "Upload complete"
