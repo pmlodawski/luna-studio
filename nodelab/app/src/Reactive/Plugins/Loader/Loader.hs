b502bb8f4ccdefe19aa6cab536c1ef3428319c36
Kamil Figiela <kamil.figiela@gmail.com>
2016-01-27 09:33:04 +0100

    Remove dead code

--- nodelab/app/src/Reactive/Plugins/Loader/Loader.hs
+++ nodelab/app/src/Reactive/Plugins/Loader/Loader.hs
@@ -5,19 +5,6 @@ import qualified JS.WebSocket      as WS
 import           JS.WebSocket      (WebSocket)
 import           JS.Config         (getBackendAddress)
 import           JS.UI             (displayConnectionClosedMessage)
-{-import qualified Reactive.Plugins.Loader.ProjectManager.Network as ProjectManager-}
-{-import qualified Reactive.Plugins.Loader.Interpreter.Network    as Interpreter-}
-{-import           Reactive.Banana-}
-{-import           Reactive.Banana.Frameworks-}
-{-import           Batch.Workspace-}
-
--- runLoader :: String -> WebSocket -> (Workspace -> IO ()) -> IO ()
--- runLoader projectName socket callback = do
---     backendAddr <- getBackendAddress
---     connect socket backendAddr
---     let interpreterCallback = Interpreter.run socket callback
---     loaderNetwork <- compile $ ProjectManager.makeNetworkDescription interpreterCallback projectName socket
---     actuate loaderNetwork
 
 withActiveConnection :: (WebSocket -> IO ()) -> IO ()
 withActiveConnection action = do
