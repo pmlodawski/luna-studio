4d1404a39ccc0c0b972b58a36ddde548dfc915cb
Marcin Kostrzewa <marcinkostrzewa@yahoo.com>
2016-01-28 15:09:06 +0100

    use polymorphic types in literal typing pass

--- libs/luna/typechecker/src/Luna/Inference/Literals.hs
+++ libs/luna/typechecker/src/Luna/Inference/Literals.hs
@@ -6,15 +6,13 @@ module Luna.Inference.Literals
 
 import           Control.Monad              (forM_)
 import qualified Data.IntSet                as IntSet
-import           Data.Layer.Coat            (uncoat, Coat, Uncoated, Coated)
+import           Data.Layer.Coat            (uncoat, Coat, Uncoated, Coated, CoatConstructor)
 import           Data.Variants              (match, case', ANY(..))
 import           Development.Placeholders
 import           Prologue                   hiding (pre, succ, cons)
 import qualified Data.Text.Lazy             as Text
 import           Data.Construction
 
-import           Luna.Interpreter.Label     (Label)
-import qualified Luna.Interpreter.Label     as Label
 import           Luna.Syntax.AST.Term
 import           Luna.Syntax.AST.Lit        as Lit
 import qualified Luna.Syntax.Builder        as B
@@ -25,55 +23,48 @@ import           Luna.Syntax.Builder.Class  (BuilderMonad)
 import qualified Luna.Syntax.Builder.Node   as NodeBuilder
 import qualified Luna.Syntax.Builder.Star   as StarBuilder
 import qualified Luna.Syntax.Builder.Symbol as SymbolBuilder
-import           Luna.Syntax.Layer.Labeled  (label)
-import           Luna.Syntax.Repr.Graph     (Edge (Edge), Node, Ref (Ref))
+import           Luna.Syntax.Repr.Graph     (Edge (Edge), Node, Ref (Ref), TracksSuccs)
 import qualified Luna.Syntax.Repr.Graph     as G
-import           Luna.Syntax.Symbol.Network (Network)
-import qualified Luna.Syntax.Layer.Labeled  as Labeled
 import qualified Luna.Syntax.AST.Typed      as Typed
+import           Luna.Syntax.AST.Typed      (HasType)
 
+type NodeType a    = ( Coated a
+                     , Uncoated a ~ (Draft (Ref Edge))
+                     , HasType a (Ref Edge)
+                     , TracksSuccs a
+                     )
 
-pre :: BuilderMonad (Network Label) m => Ref Node -> m [Ref Node]
+type BuilderType m a = ( NodeType a
+                       , StarBuilder.MonadStarBuilder (Maybe (Ref Node)) m
+                       , NodeBuilder.MonadNodeBuilder (Ref Node) m
+                       , BuilderMonad (Graph a DoubleArc) m
+                       , MonadFix m
+                       , CoatConstructor m a
+                       , Destructor m (Ref Node)
+                       )
+
+pre :: BuilderType m a => Ref Node -> m [Ref Node]
 pre ref = do
     node <- B.readRef ref
     mapM (B.follow) $ inputs $ uncoat node
 
-createConsInt :: ( StarBuilder.MonadStarBuilder (Maybe (Ref Node)) m
-                 , NodeBuilder.MonadNodeBuilder (Ref Node) m
-                 , MonadFix m
-                 , MonadIO m
-                 , BuilderMonad (Network Label) m
-                 ) => m (Ref Node)
+createConsInt :: BuilderType m a => m (Ref Node)
 createConsInt = do
     nameInt <- B._string "Int"
     B.cons nameInt
 
-createConsString :: ( StarBuilder.MonadStarBuilder (Maybe (Ref Node)) m
-                    , NodeBuilder.MonadNodeBuilder (Ref Node) m
-                    , MonadFix m
-                    , MonadIO m
-                    , BuilderMonad (Network Label) m
-                    ) => m (Ref Node)
+createConsString :: BuilderType m a => m (Ref Node)
 createConsString = do
     nameString <- B._string "String"
     B.cons nameString
 
-assignLiteralTypes :: ( StarBuilder.MonadStarBuilder (Maybe (Ref Node)) m
-                      , NodeBuilder.MonadNodeBuilder (Ref Node) m
-                      , MonadFix m
-                      , MonadIO m
-                      , BuilderMonad (Network Label) m
-                      ) => Ref Node -> m ()
+assignLiteralTypes :: BuilderType m a => Ref Node -> m ()
 assignLiteralTypes ref = do
     consIntTpe    <- createConsInt
     consStringTpe <- createConsString
     assignLiteralTypesWithTypes consIntTpe consStringTpe ref
 
-assignLiteralType :: ( StarBuilder.MonadStarBuilder (Maybe (Ref Node)) m
-                     , NodeBuilder.MonadNodeBuilder (Ref Node) m
-                     , MonadIO m
-                     , BuilderMonad (Network Label) m
-                     ) => Ref Node -> Ref Node -> m ()
+assignLiteralType :: BuilderType m a => Ref Node -> Ref Node -> m ()
 assignLiteralType ref tpe = do
     node <- B.readRef ref
     tnodeRef <- B.follow $ node ^. Typed.tp
@@ -84,11 +75,7 @@ assignLiteralType ref tpe = do
             void $ B.reconnect ref Typed.tp tpe
         match $ \ANY -> return ()
 
-assignLiteralTypesWithTypes :: ( StarBuilder.MonadStarBuilder (Maybe (Ref Node)) m
-                               , NodeBuilder.MonadNodeBuilder (Ref Node) m
-                               , MonadIO m
-                               , BuilderMonad (Network Label) m
-                               ) => Ref Node -> Ref Node -> Ref Node -> m ()
+assignLiteralTypesWithTypes :: BuilderType m a => Ref Node -> Ref Node -> Ref Node -> m ()
 assignLiteralTypesWithTypes consIntTpe consStringTpe ref = do
     node <- B.readRef ref
     -- B.writeRef ref (node & label . Label.checked .~ True)
