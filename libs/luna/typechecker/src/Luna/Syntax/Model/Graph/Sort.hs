6adf219c5fe5b410f91348c64da767e2359f4e53
Piotr Młodawski <pmlodawski@gmail.com>
2016-02-02 16:29:28 +0100

    Refactor

--- libs/luna/typechecker/src/Luna/Syntax/Model/Graph/Sort.hs
+++ libs/luna/typechecker/src/Luna/Syntax/Model/Graph/Sort.hs
@@ -1,37 +1,48 @@
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Luna.Syntax.Model.Graph.Sort where
 
-import Prologue
-import qualified Data.List as List
-import Data.Container.Class
-
-import Luna.Syntax.Model.Graph (Graph)
+import           Data.Container.Class
+import qualified Data.List            as List
+import           Prologue
 
 
+
+class (Graph g, BackEdgedGraph g, MarkableGraph g, Eq (Item g)) => CompleteGraph g
+
+class Graph g where
+    listNodes :: g -> [Item g]
+
+class BackEdgedGraph g where
+    predecessors :: Item g -> g -> [Item g]
+
+class MarkableGraph g where
+    markNode :: Item g -> g -> g
+    isMarked :: Item g -> g -> Bool
 
 class SortBy g where
-    sortBy :: (Item g -> Bool) -> [Item g] -> g -> [Item g]
+    sortBy :: (Item g -> Bool) -> g -> Either SortError [Item g]
 
-instance (Eq (Item g), Predecessors g, MarkNode g) => SortBy g where
-    sortBy predicate = sortBy' [] where
+data SortError = GraphContainsCycle
+
+instance CompleteGraph g => SortBy g where
+    sortBy predicate g' = sortBy' [] (listNodes g') g' where
         sortBy' queue1 queue2 g = case findReady queue1 g of
-            (Just node, rest) -> node : sortBy' rest queue2 (markNode node g)
+            (Just node, rest) -> (node :) <$> sortBy' rest queue2 (markNode node g)
             (Nothing  , _   ) -> case findReady queue2 g of
-                (Just node, rest) -> node : sortBy' queue1 rest (markNode node g)
+                (Just node, rest) -> (node :) <$> sortBy' queue1 rest (markNode node g)
                 (Nothing  , _   ) -> if null queue2
-                    then error "Graph contains cycle"
+                    then Left GraphContainsCycle
                     else sortBy' (queue1 ++ [head queue2]) (tail queue2) g
-        findReady queue g = (found, rest) where
-            found = List.find (indegIs0 g) queue
-            rest  = case found of
-                Just f -> List.delete f queue
-                Nothing -> queue
+        findReady queue g = findExclude (indegIs0 g) queue
         indegIs0 g node = null $ filter (predicate' g) $ predecessors node g
         predicate' g node = predicate node && isMarked node g
 
-class Predecessors g where
-    predecessors :: Item g -> g -> [Item g]
 
-class MarkNode g where
-    markNode :: Item g -> g -> g
-    isMarked :: Item g -> g -> Bool
+-- TODO[PM] : move it to list utils
+findExclude :: (a -> Bool) -> [a] -> (Maybe a, [a])
+findExclude predicate (h:t) = if predicate h
+    then (Just h, t)
+    else (found, h:rest) where
+        (found, rest) = findExclude predicate t
