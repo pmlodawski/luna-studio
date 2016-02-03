563e4c81f1961dc766cd6c94d28812eb7146025d
Adam Szlachta <adam.szlachta@gmail.com>
2016-02-03 16:15:38 +0100

    Added sample graph rendering for inference

--- libs/luna/typechecker/src/Luna/Syntax/AST/Term.hs
+++ libs/luna/typechecker/src/Luna/Syntax/AST/Term.hs
@@ -214,12 +214,13 @@ instance {-# OVERLAPPABLE #-} Repr  s t      => Repr s (Unify   t) where repr (U
 instance {-# OVERLAPPABLE #-}                   Repr s  Blank      where repr _           = "Blank"
 
 -- HeaderOnly
-instance Repr HeaderOnly (Var   n  ) where repr _ = "Var"
-instance Repr HeaderOnly (Cons  n  ) where repr _ = "Cons"
-instance Repr HeaderOnly (Arrow   t) where repr _ = "Arrow"
-instance Repr HeaderOnly (Acc   n t) where repr _ = "Acc"
-instance Repr HeaderOnly (App     t) where repr _ = "App"
-instance Repr HeaderOnly (Unify   t) where repr _ = "Unify"
+instance {-# OVERLAPPABLE #-} Repr HeaderOnly (Var   n  ) where repr _ = "Var"
+instance {-# OVERLAPPABLE #-} Repr HeaderOnly (Cons  n  ) where repr _ = "Cons"
+instance {-# OVERLAPPABLE #-} Repr HeaderOnly (Arrow   t) where repr _ = "Arrow"
+instance {-# OVERLAPPABLE #-} Repr HeaderOnly (Acc   n t) where repr _ = "Acc"
+instance {-# OVERLAPPABLE #-} Repr HeaderOnly (Acc Str t) where repr (Acc (Str m) s) = fromString $ "Acc " <>  show m
+instance {-# OVERLAPPABLE #-} Repr HeaderOnly (App     t) where repr _ = "App"
+instance {-# OVERLAPPABLE #-} Repr HeaderOnly (Unify   t) where repr _ = "Unify"
 
 -- String
 
--- libs/luna/typechecker/test/InferenceTest.hs
+++ libs/luna/typechecker/test/InferenceTest.hs
@@ -8,44 +8,6 @@
 
 module Main where
 
--- import Prologue hiding (Cons, Indexable, Ixed, Repr, Simple, children, cons, empty, index, lookup, maxBound, minBound, repr, s, simple)
-
--- import           Control.Monad.Trans.Except
--- import           Control.Monad.Trans.Identity
--- import qualified Data.GraphViz.Attributes            as GV
--- import qualified Data.GraphViz.Attributes.Colors.X11 as GVC
--- import           Data.Layer.Coat
--- import qualified Data.Map                            as Map
--- import           Data.Variants                       hiding (cons)
--- import           Data.Vector.Mutable                 ()
--- import           Debug.Trace
--- import           Development.Placeholders
-
--- import           Luna.Diagnostic.AST          as Diag (LabelAttrs (..), open, render, toGraphViz)
--- import qualified Luna.Inference.Literals      as Literals
--- import qualified Luna.Inference.Applications  as Applications
--- import qualified Luna.Interpreter.Interpreter as Interpreter
--- import           Luna.Interpreter.Label       (Label)
--- import qualified Luna.Interpreter.Label       as Label
--- import qualified Luna.Interpreter.Monad       as InterpreterMonad
--- import qualified Luna.Interpreter.NodeRunner  as NodeRunner
--- import qualified Luna.Interpreter.Session     as Session
--- import           Luna.Syntax.AST.Term
--- import           Luna.Syntax.AST.Arg
--- import           Luna.Syntax.AST.Typed        (Typed)
--- import           Luna.Syntax.Builder
--- import qualified Luna.Syntax.Builder          as Builder
--- import qualified Luna.Syntax.Builder.Node     as NodeBuilder
--- import qualified Luna.Syntax.Builder.Star     as StarBuilder
--- import qualified Luna.Syntax.Builder.Symbol   as SymbolBuilder
--- import           Luna.Syntax.Layer.Labeled    (label)
--- import           Luna.Syntax.Layer.Labeled    (Labeled2)
--- import           Luna.Syntax.Repr.Graph
--- import           Luna.Syntax.Symbol.Map       (SymbolMap)
--- import qualified Luna.Syntax.Symbol.Map       as Symbol
--- import           Luna.Syntax.Network          (Network)
-
-
 import           Data.Attribute
 import           Data.Layer.Cover
 import           Data.Record                     hiding (Layout)
@@ -58,14 +20,9 @@ import           Luna.Syntax.Model.Layer
 import           Prologue                        hiding (cons, read)
 import           Tmp2
 
-
 -- ====================================
 
 -- typed a t = StarBuilder.with (const $ Just t) a
-
--- renderAndOpen lst = do
---     flip mapM_ lst $ \(name, g) -> render name $ toGraphViz g
---     open $ fmap (\s -> "/tmp/" <> s <> ".png") (reverse $ fmap fst lst)
 
 -- instance LabelAttrs (Labeled2 Label (Typed (Ref Edge) (SuccTracking (Coat (Draft (Ref Edge)))))) where
 --     labelAttrs n = []
@@ -75,38 +32,26 @@ import           Tmp2
 -- ====================================
 
 
-
-
-
-
 renderAndOpen lst = do
     flip mapM_ lst $ \(name, g) -> render name $ toGraphViz g
     open $ fmap (\s -> "/tmp/" <> s <> ".png") (reverse $ fmap fst lst)
 
 
-title s = putStrLn $ "\n" <> "-- " <> s <> " --"
+-- title s = putStrLn $ "\n" <> "-- " <> s <> " --"
 
-data IDT a = IDT a deriving (Show)
+-- data IDT a = IDT a deriving (Show)
 
 
-data MyGraph (t :: * -> *) = MyGraph deriving (Show)
+-- data MyGraph (t :: * -> *) = MyGraph deriving (Show)
 
-type instance Layout (MyGraph t) term rt = t (Term (MyGraph t) term rt)
+-- type instance Layout (MyGraph t) term rt = t (Term (MyGraph t) term rt)
 
--- --------------------------------------
---  !!! KEEP THIS ON THE BEGINNING !!! --
--- --------------------------------------
--- - vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv ---
 prebuild :: IO (Ref $ Node (NetLayers :< Draft Static), NetGraph)
 prebuild = rebuildNetworkM def $ star
 
 
-data ImgAttr = ImgAttr deriving (Show)
-type instance Attr ImgAttr (Cover x) = String
-
-
--- emptyArgList :: [Arg (Ref Node)]
--- emptyArgList = []
+-- data ImgAttr = ImgAttr deriving (Show)
+-- type instance Attr ImgAttr (Cover x) = String
 
 -- emptyNodeList :: [Ref Node]
 -- emptyNodeList = []
@@ -158,46 +103,6 @@ type instance Attr ImgAttr (Cover x) = String
 --             return (appPlus1b, def)
 --             -- return (appPlus2, def)
 
--- sampleGraph2 :: ((Ref Node, SymbolMap (Network Label (Maybe Int))), Network Label (Maybe Int))
--- sampleGraph2 = runIdentity
---       $ flip StarBuilder.evalT Nothing
---       $ flip Builder.runT def
---       $ flip NodeBuilder.evalT (Ref $ Node (0 :: Int))
---       $ do
---             -- nameInt       <- _string "Int"
---             -- nameString    <- _string "String"
---             namePlus      <- _string "+"
---             nameConc      <- _string "++"
---             nameLen       <- _string "len"
-
---             i1 <- _int 2
---             i2 <- _int 3
---             i3 <- _int 4
---             s1 <- _stringVal "abc"
---             s2 <- _stringVal "def"
---             s3 <- _stringVal "ghi"
-
---             accPlus1a  <- accessor namePlus i1
---             appPlus1a  <- app accPlus1a [arg i2]
-
---             accPlus1b  <- accessor namePlus i3
---             appPlus1b  <- app accPlus1b [arg appPlus1a]
-
---             accConc1a  <- accessor nameConc s2
---             appConc1a  <- app accConc1a [arg s1]
-
---             accConc1b  <- accessor nameConc appConc1a
---             appConc1b  <- app accConc1b [arg s3]
-
---             accLen    <- accessor nameLen appConc1b
---             appLen    <- app accLen emptyArgList
-
---             accPlus2  <- accessor namePlus appPlus1b
---             appPlus2  <- app accPlus2 [arg appLen]
-
---             return (appPlus1b, def)
-            -- return (appPlus2, def)
-
 -- runGraph gr sm = runIdentityT
 --             . flip SymbolBuilder.evalT sm
 --             . flip StarBuilder.evalT Nothing
@@ -224,26 +129,37 @@ type instance Attr ImgAttr (Cover x) = String
 --     renderAndOpen [ ("g" , g')]
 --     putStrLn "end"
 
-app2 (f :: Ref $ Node $ NetLayers :< Draft Static) (args :: [Arg (Ref $ Node $ NetLayers :< Draft Static)]) = mdo
-    return undefined
-
 
 sampleGraph2 :: NetGraph -> IO (Ref $ Node (NetLayers :< Draft Static), NetGraph)
 sampleGraph2 g = rebuildNetworkM g $ do
-    len <- string "len"
     i1 <- int 2
     i2 <- int 3
     i3 <- int 4
     s1 <- string "abc"
     s2 <- string "def"
     s3 <- string "ghi"
-    i1 <- int 7
 
     accPlus1a  <- acc "+" i1
     appPlus1a  <- app accPlus1a [arg i2]
 
-    print accPlus1a
-    return accPlus1a
+    accPlus1b  <- acc "+" i3
+    appPlus1b  <- app accPlus1b [arg appPlus1a]
+
+    accConc1a  <- acc "++" s2
+    appConc1a  <- app accConc1a [arg s1]
+
+    accConc1b  <- acc "++" appConc1a
+    appConc1b  <- app accConc1b [arg s3]
+
+    accLen     <- acc "len" appConc1b
+    appLen     <- app accLen []
+
+    accPlus2   <- acc "+" appPlus1b
+    appPlus2   <- app accPlus2 [arg appLen]
+
+
+    print appPlus2
+    return appPlus2
 
 main :: IO ()
 main = do
@@ -254,58 +170,3 @@ main = do
     (s, g') <- sampleGraph2 g
     -- print g'
     renderAndOpen [("g", g')]
-
-
-
-
-
-
-
-
--- foo :: NetGraph -> IO (Ref $ Node (NetLayers :< Draft Static), NetGraph)
--- foo g = rebuildNetworkM g
---     $ do
---     title "basic element building"
---     s1 <- star
---     s2 <- star
---     print s1
-
---     title "reading node references"
---     s1_v <- read s1
---     s2_v <- read s2
---     print s1_v
-
---     title "manual connection builing"
---     c1 <- connection s1 s2
-
---     title "reading connection references"
---     c1_v <- read c1
---     print c1_v
-
---     title "edge following"
---     c1_tgt <- follow c1
---     when (c1_tgt /= s2) $ fail "reading is broken!"
---     print "ok!"
-
---     title "pattern matching"
---     print $ uncover s1_v
---     print $ caseTest (uncover s1_v) $ do
---         match $ \Star -> "its a star! <3"
---         match $ \ANY  -> "something else!"
-
---     title "complex element building"
---     u1 <- unify s1 s2
---     print u1
---     u1_v <- read u1
-
---     title "inputs reading"
---     let u1_ins = inputs (uncover u1_v)
---     print u1_ins
-
---     title "params reading"
---     let s1t = s1_v ^. attr Type
---         s1s = s1_v ^. attr Succs
---         ca1 = case checkAttr Type of
---             Just t  -> show $ s1_v ^. t
---             Nothing -> "no type here!"
-
