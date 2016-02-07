7ef20a55ef0c7ffad6b7865bb9df187e1bb0bb42
Wojciech Danilo <wojciech.danilo@gmail.com>
2016-02-07 17:49:20 +0100

    Added option to store resulted .dot files instead of .pngs

--- libs/luna/typechecker/bin/overview.sh
+++ libs/luna/typechecker/bin/overview.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-find src -name '*.hs' | xargs graphmod -q | dot -Tpng -Gdpi=1000 > dist/doc/images/overview.png
+find src -name '*.hs' | xargs graphmod -q > dist/doc/images/overview.dot
+dot -Tpng -Gdpi=1000 dist/doc/images/overview.dot > dist/doc/images/overview.png
