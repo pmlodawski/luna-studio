24b0b4c562beb2592723b6a402750e73c79eb6cb
Kamil Figiela <kamil.figiela@gmail.com>
2016-02-24 13:16:37 +0100

    Fine tune antialiasing

--- nodelab/app/js/common.js
+++ nodelab/app/js/common.js
@@ -5,7 +5,7 @@ var commonUniforms =  {
   antialias:  {type: 'i',  value: 0},
   objectMap:  {type: 'i',  value: 0},
   dpr:        { type: 'f',  value: 1.0 }, // TODO: js_devicePixelRatio
-  aa:         { type: 'f',  value: 1.0 },
+  aa:         { type: 'f',  value: 0.8 },
   zoomScaling:{ type: 'i',  value: 0 }
 };
 
