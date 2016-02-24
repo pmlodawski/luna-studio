be25a4250a19b3d6fc03f44628168b1acb3fad2f
Kamil Figiela <kamil.figiela@gmail.com>
2016-02-24 14:12:55 +0100

    Use HSL generated colors

--- nodelab/app/js/colors.js
+++ nodelab/app/js/colors.js
@@ -1,27 +1,19 @@
 "use strict";
 
-var colors = [
-    new THREE.Vector4(0xFF, 0xFF, 0xFF, 0xFF).divideScalar(0xFF), // white 0
-    new THREE.Vector4(0xFF, 0x00, 0xFF, 0xFF).divideScalar(0xFF), // black 1
-    new THREE.Vector4(0x30, 0x30, 0x30, 0xFF).divideScalar(0x140),// grey 2
-    new THREE.Vector4(0xFF, 0x99, 0x33, 0xAA).divideScalar(0xFF), // orange 3
-    new THREE.Vector4(0x00, 0x99, 0x99, 0xAA).divideScalar(0xFF), // dark cyan 4
-    new THREE.Vector4(0xBB, 0x33, 0x00, 0xAA).divideScalar(0xFF), // light blood 5
-    new THREE.Vector4(0x18, 0x90, 0x38, 0xFF).divideScalar(0xFF), // greeny 6
-    new THREE.Vector4(0xA0, 0x36, 0x58, 0xFF).divideScalar(0xFF), // pinky 7
-    new THREE.Vector4(0x30, 0x50, 0xA0, 0xFF).divideScalar(0xFF), // bluish 8
-    new THREE.Vector4(0x20, 0x60, 0x90, 0xFF).divideScalar(0xFF), // cyano bluish 9
-    new THREE.Vector4(0x80, 0x80, 0x08, 0xFF).divideScalar(0xFF), // yellowish 10
-    new THREE.Vector4(0x50, 0x90, 0xC0, 0xFF).divideScalar(0xFF), // bluish 11
-    new THREE.Vector4(0xB0, 0x40, 0x24, 0xFF).divideScalar(0xFF), // reddish 12
-    new THREE.Vector4(0xD0, 0xB0, 0x50, 0xFF).divideScalar(0xFF), // reddish2 13
-    new THREE.Vector4(0xFF, 0x00, 0x00, 0xFF).divideScalar(0xFF), // red 14
-    new THREE.Vector4(0xB0, 0x70, 0x40, 0xFF).divideScalar(0xFF), // reddish3 15
-    new THREE.Vector4(0x40, 0x60, 0xB0, 0xFF).divideScalar(0xFF), // bluish 16
-    new THREE.Vector4(0xFF, 0xFF, 0xFF, 0xFF).divideScalar(0xFF), // white 17
-    new THREE.Vector4(0xFF, 0xFF, 0xFF, 0xFF).divideScalar(0xFF), // white 18
-    new THREE.Vector4(0xFF, 0xFF, 0xFF, 0xFF).divideScalar(0xFF), // white 19
-    new THREE.Vector4(0xFF, 0xFF, 0xFF, 0xFF).divideScalar(0xFF)  // white 20
-];
+var colors = [];
 
+var start = 90.6 / (2 * Math.PI),
+    steps = 16,
+    delta = 1.0 / steps;
+
+function hslToVec(h,s,l) {
+  var c = new THREE.Color().setHSL(h,s,l);
+  return new THREE.Vector4(c.r, c.g, c.b, 1.0);
+}
+
+
+for(var i = 0; i < steps; ++i) {
+  var hue = start + delta * i;
+  colors.push(hslToVec(hue*2*Math.PI, 0.6, 0.5));
+}
 module.exports = colors;
