a2156755c8a4df2a8af5a445fde6b374708df6f3
Kamil Figiela <kamil.figiela@gmail.com>
2016-02-25 16:29:37 +0100

    Remove third-party

--- third-party/generator2/generator2/src/Generator/AST/Lit.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE DeriveGeneric             #-}
-
-module Generator.AST.Lit where
-	
-import GHC.Generics        (Generic)
-
-data Lit = CharLit Char | IntLit Int
-    deriving (Show, Eq, Generic, Ord, Read)
