e17584d0e95177db2d472bd7db30e2648f6d3b6f
Adam Szlachta <adam.szlachta@gmail.com>
2016-04-26 23:43:22 +0200

    Nice interactive ScopeSearcher test

--- libs/scope-searcher/app/Main.hs
+++ libs/scope-searcher/app/Main.hs
@@ -5,6 +5,9 @@ module Main where
 import           System.IO
 import           Control.Lens
 import           Control.Monad               (forM_, when)
+import           Control.Monad.State         (evalStateT, StateT, liftIO, get, put, modify)
+import           Data.Default
+import           Data.Monoid                 ((<>))
 import           Data.Text.Lazy              (Text)
 import qualified Data.Text.Lazy              as Text
 import           Text.Show.Pretty
@@ -17,35 +20,69 @@ import qualified Text.ScopeSearcher.Searcher as Searcher
 
 import           MockDataNS
 
+data Env = Env { _debug       :: Bool
+               , _includePath :: Bool
+               } deriving (Show)
+
+instance Default Env where
+    def = Env False False
+
+makeLenses ''Env
+
 main :: IO ()
 main = do
-    putStrLn "ScopeSearcher test application"
-    withColor Vivid Yellow putStrLn $ ppShow mockDataNS
-    withColor Vivid Green  putStrLn "Press enter to quit."
-    showHighlights
+    withColor Vivid Magenta putStrLn "ScopeSearcher test application"
+    withColor Vivid Yellow  putStrLn "@     - show mock\n?     - toggle debug\n/     - toggle include path\nenter - quit"
+    evalStateT showHighlights def
+    withColor Vivid Red putStrLn "Leaving ScopeSearcher."
 
-showHighlights :: IO ()
+showMock :: IO ()
+showMock = withColor Vivid Green putStrLn $ ppShow mockDataNS
+
+showHighlights :: StateT Env IO ()
 showHighlights = do
+    input <- liftIO $ getUserInput
+    when (not $ null input) $ do
+        let shouldToggleDebug       = input == "?"
+            shouldToggleIncludePath = input == "/"
+            shouldShowMock          = input == "@"
+        debugState       <- use debug
+        includePathState <- use includePath
+        case (shouldToggleDebug, shouldToggleIncludePath, shouldShowMock) of
+            (False, False, False) -> liftIO $ showQueryResults debugState includePathState $ Text.pack input
+            (_, _, _) -> do
+                when shouldToggleDebug       updateDebug
+                when shouldToggleIncludePath updateIncludePath
+                when shouldShowMock $ liftIO showMock
+        showHighlights
+
+updateDebug :: StateT Env IO ()
+updateDebug = zoom debug $ do
+    modify not
+    debug <- get
+    liftIO $ withColor Vivid Yellow putStrLn $ "Toggled debug to " <> show debug
+
+updateIncludePath :: StateT Env IO ()
+updateIncludePath = zoom includePath $ do
+    modify not
+    includePath <- get
+    liftIO $ withColor Vivid Yellow putStrLn $ "Toggled includePath to " <> show includePath
+
+getUserInput :: IO String
+getUserInput = do
     withColor Vivid Blue putStr "> "
     hFlush stdout
-    input <- getLine
-    if null input
-        then do
-            withColor Vivid Red putStrLn "Leaving ScopeSearcher."
-        else do
-            let debug = head input == '!'
-                search = if debug then tail input else input
-            showQueryResults debug $ Text.pack search
-            showHighlights
+    getLine
 
-showQueryResults :: Bool -> Text -> IO ()
-showQueryResults debug searchText = do
-    let suggestions = Scope.searchInScope False (mockDataNS ^. items) searchText
+showQueryResults :: Bool -> Bool -> Text -> IO ()
+showQueryResults debug includePath searchText = do
+    let suggestions = Scope.searchInScope includePath (mockDataNS ^. items) searchText
     when debug $ withColor Dull Yellow putStrLn $ ppShow suggestions
     forM_ suggestions showQueryResult
 
 showQueryResult :: QueryResult -> IO ()
-showQueryResult queryResult = showHighlight 0 (queryResult ^. highlights) $ Text.unpack (queryResult ^. name)
+showQueryResult queryResult = do
+    showHighlight 0 (queryResult ^. highlights) $ Text.unpack (queryResult ^. name)
 
 showHighlight :: Int -> [Highlight] -> String -> IO ()
 showHighlight _ [] str = withColor Dull White putStrLn str
