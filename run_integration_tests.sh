a8043d400869523b771226ea39bd5b60b384721c
Kamil Figiela <kamil.figiela@gmail.com>
2016-06-07 11:50:05 +0200

    test(integration): Add second integration test

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -4,3 +4,6 @@ cd test/projects
 
 luna-empire-result-saver --projectFile simple.project.json --out simple.results.json.test
 diff -s simple.results.json simple.results.json.test
+
+luna-empire-result-saver --projectFile words.project.json --out words.results.json.test
+diff -s words.results.json words.results.json.test
