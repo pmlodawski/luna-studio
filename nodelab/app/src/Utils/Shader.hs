ac5a045f6ea4c94fc0cb7bd46b38668680b5171b
Adam Szlachta <adam.szlachta@gmail.com>
2016-06-09 17:12:20 +0200

    Primitives translation and shapes intersection

--- nodelab/app/src/Utils/Shader.hs
+++ nodelab/app/src/Utils/Shader.hs
@@ -14,7 +14,7 @@ import           Data.Maybe                          (catMaybes, fromMaybe)
 
 import qualified Data.Array.Linear                   as A
 import           Data.Array.Linear.Color.Class
-import           Graphics.Rendering.GLSL.SDF         (Object, diff, merge, object, translate)
+import           Graphics.Rendering.GLSL.SDF         (Object, diff, merge, intersect, object, translate)
 import           Graphics.Rendering.GLSL.SDF.Figures
 import           Graphics.Shading.Flat
 import           Graphics.Shading.Material
@@ -32,11 +32,13 @@ import qualified Graphics.API                        as G
 type Size   = Vector2 Double
 type Shader = String
 
-data ShaderBox = ShaderBox { _shader       :: Shader
-                           , _size         :: Size
+data ShaderBox = ShaderBox { _shader :: Shader
+                           , _size   :: Size
                            } deriving (Show, Eq)
 
 makeLenses ''ShaderBox
+
+-- helpers
 
 toFloat :: Double -> Float
 toFloat = realToFrac
@@ -58,46 +60,22 @@ getBound shape = let Vector2 w h = getSize shape in A.vec2 (toFloat w) (toFloat 
 toBound :: Size -> A.BVec 2 Float
 toBound (Vector2 x y) = A.vec2 (toFloat x) (toFloat y)
 
-fromMaterial :: G.Material -> Material (Layer GLSL.Expr)
-fromMaterial (G.SolidColor r g b a) = Material $ [ Fill . Solid $ color4 (toExpr r) (toExpr g) (toExpr b) (toExpr a) ]
-
--- createComponent :: G.Component -> Bounded Float (Object 2)
--- createComponent component@(G.Component shape color) =
---     Bounded (getBound shape) (createObject component)
-
-
--- -- calculateSize :: [G.Component] -> (Double, Double)
--- -- calculateSize components =
-
--- -- TODO: calculate boundings
--- createObject :: G.Component -> Object 2
--- createObject (G.Component shape color) = (createShape shape) & material .~ (createMtl color)
-
--- createCompositeComponent :: [G.Component] -> Maybe (Object 2)
--- createCompositeComponent components = createCompositeObject $ fmap createObject components
-
--- createCompositeObject :: [Object 2] -> Maybe (Object 2)
--- createCompositeObject (object:objects@(x:xs)) = Just $ foldl merge object objects
--- createCompositeObject (object:_)              = Just object
--- createCompositeObject []                      = Nothing
-
--- -- TODO: compile all components
--- createShader :: G.Shader -> (String, (Double, Double))
--- createShader (G.Shader (component:components)) = createShader' component
--- createShader _ = ("", (0.0, 0.0))
-
--- createShader' :: G.Component -> (String, (Double, Double))
--- createShader' component@(G.Component shape _) = (fst $ GLSL.compileGLSL $ createComponent component, getSize shape)
-
-
--- helpers
+-- object helpers
 
 mergeObjects :: [Object 2] -> Maybe (Object 2)
 mergeObjects (object:objects@(_:_)) = Just $ foldl merge object objects
 mergeObjects (object:_)             = Just object
 mergeObjects []                     = Nothing
 
+transObject :: Double -> Double -> Object 2 -> Object 2
+transObject 0.0 0.0 object = object
+transObject dx  dy  object = translate tr object where
+    tr = fromListUnsafe [toExpr dx, toExpr dy, toExpr 0.0] :: A.BVec 3 GLSL.Expr
+
 -- calc object
+
+fromMaterial :: G.Material -> Material (Layer GLSL.Expr)
+fromMaterial (G.SolidColor r g b a) = Material $ [ Fill . Solid $ color4 (toExpr r) (toExpr g) (toExpr b) (toExpr a) ]
 
 fromFigure :: G.Figure -> Object 2
 fromFigure (G.Square s)      = hyperrectangle (A.vec2 (toExpr s) (toExpr s) :: A.BVec 2 GLSL.Expr)
@@ -105,13 +83,13 @@ fromFigure (G.Rectangle w h) = hyperrectangle (A.vec2 (toExpr w) (toExpr h) :: A
 fromFigure (G.Circle d)      = ball (toExpr d)
 
 fromPrimitive :: G.Primitive -> Object 2
-fromPrimitive (G.Primitive figure point attr) = fromFigure figure
+fromPrimitive (G.Primitive figure (G.Point2 dx dy) attr) = transObject dx dy $ fromFigure figure
 
 fromShape :: G.Shape -> Object 2
 fromShape (G.Single    primitive)     = fromPrimitive primitive
-fromShape (G.Merge     shape1 shape2) = merge (fromShape shape1) (fromShape shape2)
-fromShape (G.Subtract  shape1 shape2) = diff  (fromShape shape1) (fromShape shape2)
-fromShape (G.Intersect shape1 shape2) = $notImplemented
+fromShape (G.Merge     shape1 shape2) = merge     (fromShape shape1) (fromShape shape2)
+fromShape (G.Subtract  shape1 shape2) = diff      (fromShape shape1) (fromShape shape2)
+fromShape (G.Intersect shape1 shape2) = intersect (fromShape shape1) (fromShape shape2)
 
 fromSurface :: G.Surface -> Object 2
 fromSurface (G.ShapeSurface shape) = fromShape shape
@@ -128,17 +106,12 @@ fromGeoComponent (G.GeoGroup geometries) = fromGeometries geometries
 fromGeometry :: G.Geometry -> Maybe (Object 2)
 fromGeometry (G.Geometry geoComp trans matMay) = go <$> fromGeoComponent geoComp where
     (G.Transformation _ _ dx dy _ _) = trans
-    tr = fromListUnsafe [toExpr dx, toExpr dy, toExpr 0.0] :: A.BVec 3 GLSL.Expr
     go :: Object 2 -> Object 2
-    go = goMat . goTrans
-    goMat :: Object 2 -> Object 2
-    goMat object = case matMay of
+    go = appMat . transObject dx dy
+    appMat :: Object 2 -> Object 2
+    appMat object = case matMay of
         Just mat -> object & material .~ (fromMaterial mat)
         Nothing  -> object
-    goTrans :: Object 2 -> Object 2
-    goTrans object = case (dx, dy) of
-        (0.0, 0.0) -> object
-        (dx,  dy)  -> translate tr object
 
 fromGeometries :: [G.Geometry] -> Maybe (Object 2)
 fromGeometries geometries = mergeObjects . catMaybes $ fromGeometry <$> geometries
@@ -148,7 +121,7 @@ createShader size objectMay = fromMaybe "" $ compileObject <$> objectMay where
     compileObject :: Object 2 -> Shader
     compileObject object = fst $ GLSL.compileGLSL $ Bounded (toBound size) object
 
--- calc size
+-- size calculation
 
 calcGeoSize :: G.Geometry -> Size
 calcGeoSize geo = Vector2 1.0 1.0
@@ -160,7 +133,7 @@ createShaderBox geometry = ShaderBox (createShader size objMay) size
     where size   = calcGeoSize geometry
           objMay = fromGeometry geometry
 
---
+-- tests
 
 test :: IO ()
 test = do
@@ -178,7 +151,8 @@ test = do
 
 
 
--- ====== test (TODO: remove) ====== --
+
+-- ====== old test (TODO: remove) ====== --
 
 mtl1     = Material $ [ Fill            . Solid $ color4 0.7 0.2 0.2 1.0
                       , Border 10.0     . Solid $ color4 0.0 1.0 0.0 1.0
