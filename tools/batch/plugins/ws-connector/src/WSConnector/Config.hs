165e4fe934b4c9aec5e4904275ff3a20ea0d5ab8
Kamil Figiela <kamil.figiela@gmail.com>
2016-08-05 13:05:02 +0200

    Fix broken build

--- tools/batch/plugins/ws-connector/src/WSConnector/Config.hs
+++ tools/batch/plugins/ws-connector/src/WSConnector/Config.hs
@@ -1,5 +1,5 @@
 module WSConnector.Config (
-    module Paths_batch_plugin_ws_connector
+    module Paths_ws_connector
 ) where
 
-import           Paths_batch_plugin_ws_connector (version)
+import           Paths_ws_connector (version)
