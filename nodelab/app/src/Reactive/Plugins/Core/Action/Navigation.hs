034bb4a950e61390420a1fee16610eb00ad47320
Adam Szlachta <adam.szlachta@gmail.com>
2016-08-10 17:43:50 +0200

    Geometric nodes navigation

--- nodelab/app/src/Reactive/Plugins/Core/Action/Navigation.hs
+++ nodelab/app/src/Reactive/Plugins/Core/Action/Navigation.hs
@@ -1,12 +1,14 @@
 module Reactive.Plugins.Core.Action.Navigation where
 
 
+import           Data.List (maximumBy)
+import           Data.Function (on)
+
 import           Utils.PreludePlus
+import           Utils.Vector
 
--- import qualified Data.Set                          as Set
-
-import           Object.Widget                     (WidgetFile, WidgetId, objectId, widget)
-import qualified Object.Widget.Node                as NodeModel
+import           Object.Widget                     (WidgetFile, WidgetId, objectId, widget, Position)
+import qualified Object.Widget.Node                as Model
 
 import           Event.Event                       (Event (Mouse, Keyboard), JSState)
 import           Event.Keyboard                    (KeyMods (..), shift)
@@ -14,65 +16,78 @@ import qualified Event.Keyboard                    as Keyboard
 import qualified Event.Mouse                       as Mouse
 
 import           Reactive.State.Global             (State, inRegistry)
--- import qualified Reactive.State.Global             as Global
 import qualified Reactive.State.UIRegistry         as UIRegistry
 
-import qualified Empire.API.Data.Node           as N
+import qualified Empire.API.Data.Node              as N
 
 import           Reactive.Commands.Command         (Command, performIO)
 import           Reactive.Commands.Graph           (allNodes)
 import           Reactive.Commands.Graph.Selection (focusSelectedNode, selectAll, selectedNodes, unselectAll)
--- import qualified Reactive.Commands.UIRegistry      as UICmd
+import qualified Reactive.Commands.UIRegistry      as UICmd
+
 
 
 toAction :: Event -> Maybe (Command State ())
 toAction (Keyboard _ (Keyboard.Event Keyboard.Down char KeyMods { _shift = True })) = case char of
+    '\t'  -> Just goLeft
     '\37' -> Just goLeft
     '\39' -> Just goRight
-    '\38' -> Just goUp
     '\40' -> Just goDown
+    '\38' -> Just goUp
     _     -> Nothing
 toAction (Keyboard _ (Keyboard.Event Keyboard.Down char (KeyMods False False False False))) = case char of
     '\37' -> Just goLeft
     '\39' -> Just goRight
-    '\38' -> Just goUp
     '\40' -> Just goDown
+    '\38' -> Just goUp
     _     -> Nothing
-toAction (Keyboard _ (Keyboard.Event Keyboard.Down '\t' KeyMods { _shift = True })) = Just goPrev
 toAction _ = Nothing
 
-goLeft, goRight, goUp, goDown :: Command State ()
-goLeft  = test
--- goRight = test
-goUp    = test
-goDown  = test
-
 goPrev, goNext :: Command State ()
-goPrev  = test
-goNext  = test
+goPrev  = return ()
+goNext  = return ()
 
-test :: Command State ()
-test = do
-    focusedWidget <- inRegistry $ use UIRegistry.focusedWidget
-    when (isNothing focusedWidget) selectAll
+goLeft, goRight, goDown, goUp :: Command State ()
+goRight = go findRightMost findNodesOnRight
+goLeft  = go findLeftMost  findNodesOnLeft
+goDown  = go findDownMost  findNodesOnDown
+goUp    = go findUpMost    findNodesOnUp
 
+go :: ([WidgetFile Model.Node] -> WidgetFile Model.Node) -> (Position -> [WidgetFile Model.Node] -> [WidgetFile Model.Node]) -> Command State ()
+go findMost findNodesOnSide = do
+    nodes <- allNodes
+    let selectedNodes = findSelected nodes
+    when (not $ null selectedNodes) $ do
+        let nodeSrc = findMost selectedNodes
+            pos = nodeSrc ^. widget . Model.position
+            nodesSide = findNodesOnSide pos nodes
+        when (not $ null nodesSide) $ do
+            let nodeDst = findNearestNode pos nodesSide
+            changeSelection selectedNodes nodeDst
 
-goRight = do
-    -- getSelectedNode
-    -- getItsParams
-    -- findNext
-    -- selectIt
-    widgets <- allNodes
-    nodesToMove <- inRegistry $ forM widgets $ \wf -> do
-        let widgetId = wf ^. objectId
-        if (wf ^. widget . NodeModel.isSelected) then do
-                -- UICmd.update_ widgetId $ NodeModel.isSelected .~ False
-                return $ Just $ wf ^. widget . NodeModel.nodeId
-        else return Nothing
+findRightMost, findLeftMost, findDownMost, findUpMost :: [WidgetFile Model.Node] -> WidgetFile Model.Node
+findRightMost = maximumBy (compare `on` (^. widget . Model.position . x))
+findLeftMost  = minimumBy (compare `on` (^. widget . Model.position . x))
+findDownMost  = maximumBy (compare `on` (^. widget . Model.position . y))
+findUpMost    = minimumBy (compare `on` (^. widget . Model.position . y))
 
-    mapM moveRight $ listToMaybe $ catMaybes nodesToMove
-    return ()
+findNodesOnRight, findNodesOnLeft, findNodesOnDown, findNodesOnUp :: Position -> [WidgetFile Model.Node] -> [WidgetFile Model.Node]
+findNodesOnRight pos = filter $ \wf -> wf ^. widget . Model.position . x > pos ^. x
+findNodesOnLeft  pos = filter $ \wf -> wf ^. widget . Model.position . x < pos ^. x
+findNodesOnDown  pos = filter $ \wf -> wf ^. widget . Model.position . y > pos ^. y
+findNodesOnUp    pos = filter $ \wf -> wf ^. widget . Model.position . y < pos ^. y
 
-moveRight :: N.NodeId -> Command State ()
-moveRight nodeId = do
-    return ()
+findSelected :: [WidgetFile Model.Node] -> [WidgetFile Model.Node]
+findSelected = filter $ \wf -> wf ^. widget . Model.isSelected
+
+findNearestNode :: Position -> [WidgetFile Model.Node] -> WidgetFile Model.Node
+findNearestNode pos = minimumBy (compare `on` (distance pos))
+
+distance :: Position -> WidgetFile Model.Node -> Double
+distance pos wf = lengthSquared (wpos - pos) where
+    wpos = wf ^. widget . Model.position
+
+changeSelection :: [WidgetFile Model.Node] -> WidgetFile Model.Node -> Command State ()
+changeSelection selectedNodes nodeDst = inRegistry $ do
+    forM selectedNodes $ \node -> UICmd.update_ (node ^. objectId) $ Model.isSelected .~ False
+    UICmd.update_ (nodeDst ^. objectId) $ Model.isSelected .~ True
