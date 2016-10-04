8b05e2f0bd6e2c9b43ee954711aeae6753eea32c
klos <janklosowski@gmail.com>
2016-10-04 19:02:27 +0200

    on boarding

--- nodelab/app/src/JS/Tutorial.hs
+++ nodelab/app/src/JS/Tutorial.hs
@@ -11,6 +11,6 @@ import           Utils.PreludePlus
 import           GHCJS.Types (JSString)
 import           Data.JSString.Text  (lazyTextToJSString)
 
-foreign import javascript safe "localStorage.getItem('onboarding') == '1'" shouldRunTutorial :: IO Bool
+foreign import javascript safe "localStorage.getItem('onboarding') != '1'" shouldRunTutorial :: IO Bool
 foreign import javascript safe "app.showOnboarding($1)" showStep :: Int -> IO ()
 foreign import javascript safe "app.closeOnboarding()" closeOnboarding :: IO ()
