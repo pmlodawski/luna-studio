51002606e0a031b4bdbd0eee4624d09cad45b4e7
Adam Szlachta <adam.szlachta@gmail.com>
2016-10-19 12:11:37 +0200

    Update to newest Luna

--- libs/luna-empire/src/Empire/Commands/AST.hs
+++ libs/luna-empire/src/Empire/Commands/AST.hs
@@ -43,10 +43,10 @@ import           Old.Luna.Syntax.Term.Class              (Acc (..), App (..), Co
 import qualified Old.Luna.Syntax.Term.Expr.Lit           as Lit
 
 import           Luna.Compilation.Error                  as TCError
-import           Luna.Compilation.Pass.Interpreter.Layer (InterpreterData (..))
-import qualified Luna.Compilation.Pass.Interpreter.Value as Value
-import           Luna.Compilation.Pass.Interpreter.Value (toExceptIO, unsafeFromData, Data, attachListener)
-import qualified Luna.Compilation.Pass.Interpreter.Layer as Interpreter
+import           Luna.Interpreter.Layer                  (InterpreterData (..))
+import qualified Luna.Interpreter.Value                  as Value
+import           Luna.Interpreter.Value                  (toExceptIO, unsafeFromData, Data, attachListener)
+import qualified Luna.Interpreter.Layer                  as Interpreter
 import           Unsafe.Coerce
 
 import           Empire.Commands.Graphics                (fromFigure, fromGeoComponent, fromGeometry, fromGraphics, fromLayer, fromMaterial,
@@ -56,8 +56,8 @@ import           Debug.Trace                             (trace)
 
 
 -- TODO: This might deserve rewriting to some more general solution
-import           Luna.Compilation.Pass.Interpreter.Charts (autoScatterChartInt, autoScatterChartDouble, autoScatterChartIntTuple, autoScatterChartDoubleTuple)
-import qualified Graphics.API                             as G
+import           Luna.Interpreter.Charts                 (autoScatterChartInt, autoScatterChartDouble, autoScatterChartIntTuple, autoScatterChartDoubleTuple)
+import qualified Graphics.API                            as G
 
 metaKey :: TypeKey NodeMeta
 metaKey = TypeKey
