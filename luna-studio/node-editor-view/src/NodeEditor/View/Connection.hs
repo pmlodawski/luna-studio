dc33d9b6cdfa8913e29dbd8bd1c03da2ab45e24c
Piotr Młodawski <pmlodawski@gmail.com>
2018-02-23 14:35:17 +0100

    Update connection model

--- luna-studio/node-editor-view/src/NodeEditor/View/Connection.hs
+++ luna-studio/node-editor-view/src/NodeEditor/View/Connection.hs
@@ -5,6 +5,7 @@ import           Common.Prelude
 import           Data.Aeson                        (ToJSON)
 import           Data.Convert                      (Convertible (convert))
 import qualified Data.HashMap.Strict               as HashMap
+import qualified LunaStudio.Data.PortRef           as PortRef
 import           NodeEditor.React.Model.Connection (Connection, ConnectionsMap)
 import qualified NodeEditor.React.Model.Connection as Connection
 
@@ -17,12 +18,20 @@ connectionsView new old =
 
 data ConnectionView = ConnectionView
         { key :: String
+        , srcNode :: String
+        , srcPort :: String
+        , dstNode :: String
+        , dstPort :: String
         } deriving (Generic, Show)
 
 instance ToJSON ConnectionView
 instance Convertible Connection ConnectionView where
     convert c = ConnectionView
         {- key        -} (c ^. Connection.connectionId . to show)
+        {- srcNode    -} (c ^. Connection.src . PortRef.srcNodeLoc . to show)
+        {- srcPort    -} (c ^. Connection.src . PortRef.srcPortId  . to show)
+        {- dstNode    -} (c ^. Connection.dst . PortRef.dstNodeLoc . to show)
+        {- dstPort    -} (c ^. Connection.dst . PortRef.dstNodeId  . to show)
 
 foreign import javascript safe "atomCallback.getNodeEditorView().setConnections($1)"
     setConnections' :: JSVal -> IO ()
