932dc298b6b6d358bd713c7a0edfc40c1eef3842
LJK <kanialj@gmail.com>
2018-10-02 17:26:51 +0200

    Create helper function for running test with tc

--- libs/luna-empire/test/Test/Hspec/Empire.hs
+++ libs/luna-empire/test/Test/Hspec/Empire.hs
@@ -5,25 +5,41 @@ import Test.Hspec.Hspec.WithReason as X
 
 import Empire.Prelude
 
-import qualified Data.Text             as Text
-import qualified Empire.Commands.Graph as Graph
-import qualified Empire.Data.Graph     as Graph
+import qualified Data.Graph.Store              as Store
+import qualified Data.Text                     as Text
+import qualified Empire.Commands.Graph         as Graph
+import qualified Empire.Commands.Typecheck     as Typecheck (run)
+import qualified Empire.Data.Graph             as Graph
+import qualified Empire.Data.Library           as Library (body, path)
+import qualified Empire.Empire                 as Empire
+import qualified Language.Haskell.TH           as TH
+import qualified Luna.Package.Structure.Name   as Project
+import qualified LunaStudio.Data.GraphLocation as GraphLocation
+import qualified System.IO.Temp                as Temp
 
-import Control.Concurrent.MVar       (newEmptyMVar)
-import Control.Concurrent.STM        (atomically)
-import Control.Concurrent.STM.TChan  (newTChan)
-import Control.Exception             (bracket)
-import Data.Char                     (isSpace)
-import Data.List                     (dropWhileEnd)
-import Empire.Commands.Library       (createLibrary)
-import Empire.Data.Graph             (CommandState (CommandState),
-                                      defaultPMState)
-import Empire.Empire                 (CommunicationEnv (CommunicationEnv),
-                                      Empire, evalEmpire)
-import LunaStudio.Data.GraphLocation (GraphLocation (GraphLocation))
-import Test.Hspec                    (Expectation, Spec, SpecWith, around,
-                                      describe, parallel, shouldBe)
-import Text.RawString.QQ             (r)
+import Control.Concurrent.MVar         (newEmptyMVar)
+import Control.Concurrent.STM          (atomically)
+import Control.Concurrent.STM.TChan    (newTChan)
+import Control.Exception               (bracket, finally)
+import Data.Char                       (isSpace)
+import Data.List                       (dropWhileEnd)
+import Empire.ASTOp                    (runASTOp)
+import Empire.Commands.Library         (createLibrary, listLibraries,
+                                        withLibrary)
+import Empire.Data.Graph               (CommandState (CommandState),
+                                        defaultPMState)
+import Empire.Empire                   (CommunicationEnv (CommunicationEnv),
+                                        Empire, Env, evalEmpire, execEmpire,
+                                        runEmpire)
+import Empire.Empire                   (InterpreterEnv (InterpreterEnv))
+import Luna.Package.Structure.Generate (genPackageStructure)
+import LunaStudio.Data.GraphLocation   (GraphLocation (GraphLocation))
+import System.Directory                (canonicalizePath, getCurrentDirectory)
+import System.Environment              (lookupEnv, setEnv)
+import System.FilePath                 (takeDirectory, (</>))
+import Test.Hspec                      (Expectation, Spec, SpecWith, around,
+                                        describe, parallel, shouldBe)
+import Text.RawString.QQ               (r)
 
 
 withChannels :: (CommunicationEnv -> IO a) -> IO a
@@ -54,31 +70,34 @@ codeCheck :: Text -> (Text -> Expectation)
 codeCheck expectedCode = \resultCode ->
     Text.strip resultCode `shouldBe` normalizeLunaCode expectedCode
 
+noAction :: GraphLocation -> Empire ()
+noAction _ = pure ()
+
+evalEmpireWithDefaultState :: CommunicationEnv -> Empire a -> IO a
+evalEmpireWithDefaultState = fmap fst .: runEmpireWithDefaultState
+
+execEmpireWithDefaultState
+    :: CommunicationEnv -> Empire a -> IO (CommandState Env)
+execEmpireWithDefaultState = fmap snd .: runEmpireWithDefaultState
+
+runEmpireWithDefaultState
+    :: CommunicationEnv -> Empire a -> IO (a, CommandState Env)
+runEmpireWithDefaultState env action = defaultPMState >>= \pm ->
+    runEmpire env (CommandState pm def) action
+
+
 testCase
     :: Text
     -> Text
     -> (GraphLocation -> Empire a)
     -> CommunicationEnv
     -> Expectation
-testCase initialCode expectedCode action env = let
-        filePath = "/TestPath"
-        topGl    = GraphLocation filePath def
-        execute  = do
-            createLibrary Nothing filePath
-            Graph.loadCode topGl $ normalizeLunaCode initialCode
-            let mainNodeName = "main"
-                withMain mainNodeId = do
-                    let gl = topGl |>= mainNodeId
-                    mockNodesLayout gl
-                    pure gl
-            mainNodeId <- findNodeIdByName topGl mainNodeName
-            gl <- maybe (pure topGl) withMain mainNodeId
-            action gl
-            Graph.getCode gl
-    in do
-        pm         <- defaultPMState
-        resultCode <- evalEmpire env (CommandState pm def) execute
-        codeCheck expectedCode resultCode
+testCase initialCode expectedCode action env = do
+    resultCode <- evalEmpireWithDefaultState env $ do
+        gl <- prepareTestEnvironment initialCode
+        action gl
+        Graph.getCode gl
+    codeCheck expectedCode resultCode
 
 --[TODO]: This function is copy paste of testCase and is meant to be removed soon, when markers are removed from Luna
 testCaseWithMarkers
@@ -87,22 +106,101 @@ testCaseWithMarkers
     -> (GraphLocation -> Empire a)
     -> CommunicationEnv
     -> Expectation
-testCaseWithMarkers initialCode expectedCode action env = let
-        filePath = "/TestPath"
-        topGl    = GraphLocation filePath def
-        execute  = do
-            createLibrary Nothing filePath
-            Graph.loadCode topGl $ normalizeLunaCode initialCode
-            let mainNodeName = "main"
-                withMain mainNodeId = do
-                    let gl = topGl |>= mainNodeId
-                    mockNodesLayout gl
-                    pure gl
-            mainNodeId <- findNodeIdByName topGl mainNodeName
-            gl <- maybe (pure topGl) withMain mainNodeId
-            action gl
-            Graph.withGraph gl (use Graph.code)
-    in do
-        pm         <- defaultPMState
-        resultCode <- evalEmpire env (CommandState pm def) execute
+testCaseWithMarkers initialCode expectedCode action env = do
+    resultCode <- evalEmpireWithDefaultState env $ do
+        gl <- prepareTestEnvironment initialCode
+        action gl
+        Graph.withGraph gl (use Graph.code)
+    codeCheck expectedCode resultCode
+
+testCaseWithTC
+    :: Text
+    -> Text
+    -> (GraphLocation -> Empire a)
+    -> (GraphLocation -> Empire b)
+    -> CommunicationEnv
+    -> Expectation
+testCaseWithTC initialCode expectedCode action tcResultCheck env = let
+    projectParentDirName = "luna-test-hspec-empire-tc"
+    inTempDirectory = Temp.withSystemTempDirectory projectParentDirName
+    in inTempDirectory $ \projectParentDirNamePath -> do
+        ((gl, resultCode, clsGraph, rooted), state)
+            <- runEmpireWithDefaultState env $ do
+                gl <- prepareTestEnvironmentWithTC
+                    projectParentDirNamePath
+                    initialCode
+                let topGl = gl & GraphLocation.breadcrumb .~ def
+                action gl
+                code <- Graph.getCode gl
+                Graph.withUnit topGl $ do
+                    clsGraph <- use Graph.userState
+                    rooted <- runASTOp . Store.serializeWithRedirectMap
+                        $ clsGraph ^. Graph.clsClass
+                    pure (gl, code, clsGraph, rooted)
         codeCheck expectedCode resultCode
+        withMockedLunaRoot . void $ do
+            pmState <- Graph.defaultPMState
+            let commandState = CommandState pmState
+                    $ InterpreterEnv (pure ()) clsGraph mempty def def def def
+            updatedState <- execEmpire env commandState $
+                Typecheck.run gl clsGraph rooted False False
+            let updatedClsGraph
+                    = updatedState ^. Graph.userState . Empire.clsGraph
+            evalEmpire env state $ do
+                libPath <- fmap (view Library.path . head) listLibraries
+                withLibrary libPath
+                    $ Graph.userState . Library.body .= updatedClsGraph
+                tcResultCheck gl
+
+prepareTestEnvironment :: Text -> Empire GraphLocation
+prepareTestEnvironment = prepareTestEnvironmentWithCustomPath defProjectPath where
+    defProjectPath = "/TestProject"
+
+prepareTestEnvironmentWithTC :: FilePath -> Text -> Empire GraphLocation
+prepareTestEnvironmentWithTC projectParentDirName initialCode = let
+    projectName             = "TestProject"
+    genProjectPath dirPath  = dirPath </> projectName
+    srcDirName              = "src"
+    mainLunaName            = "Main.luna"
+    genMainLunaPath pkgPath = pkgPath </> srcDirName </> mainLunaName
+    in do
+        --[TODO]: Find out why lack of this line causes segmentation fault
+        createLibrary Nothing $ "/" <> projectName
+        Right pkgPath <- genPackageStructure
+            (genProjectPath projectParentDirName)
+            Nothing
+            def
+        prepareTestEnvironmentWithCustomPath
+            (genMainLunaPath pkgPath)
+            initialCode
+
+prepareTestEnvironmentWithCustomPath :: FilePath -> Text -> Empire GraphLocation
+prepareTestEnvironmentWithCustomPath filePath initialCode = let
+    topGl               = GraphLocation filePath def
+    mainNodeName        = "main"
+    withMain mainNodeId = do
+        let gl = topGl |>= mainNodeId
+        mockNodesLayout gl
+        pure gl
+    in do
+        createLibrary Nothing filePath
+        Graph.loadCode topGl $ normalizeLunaCode initialCode
+        mainNodeId <- findNodeIdByName topGl mainNodeName
+        maybe (pure topGl) withMain mainNodeId
+
+withMockedLunaRoot :: IO a -> IO a
+withMockedLunaRoot action = let
+    testSpecEmpireHsPath = $(do
+        dir      <- TH.runIO getCurrentDirectory
+        fileName <- TH.loc_filename <$> TH.location
+        TH.litE . TH.stringL $ dir </> fileName)
+    envDirName  = "env"
+    repoDirPath = takeDirectory testSpecEmpireHsPath </> "../../../../.."
+    envDirPath  = repoDirPath </> envDirName
+    in do
+        userLunaRoot   <- fromMaybe mempty <$> lookupEnv Project.lunaRootEnv
+        mockedLunaRoot <- canonicalizePath envDirPath
+        finally
+            (setEnv Project.lunaRootEnv mockedLunaRoot >> action)
+            $ setEnv Project.lunaRootEnv userLunaRoot
+            
