d717d157418b0ba6dff89e029998c032a2dc7d38
Wojciech Danilo <wojciech.danilo@gmail.com>
2018-11-07 20:47:11 +0100

    First electron config

---
+++ app/src/main.coffee
@@ -0,0 +1,13 @@
+import 'setimmediate'
+
+# import * as analytics from './analytics';
+import * as config    from './config';
+# import * as websocket from './websocket';
+
+# var init = {websocket: websocket()}
+console.log(config)
+# var n = nodeEditor({analytics, config, init});
+# n.start();
+
+
+console.log "WORKING!5"
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,32 +1,40 @@
 const path = require('path');
 
-module.exports = {
-  mode: "production", // "production" | "development" | "none"
-  // Chosen mode tells webpack to use its built-in optimizations accordingly.
+function baseConfig(entryPath) {
+  return {
+    mode: "production" // "production" | "development" | "none" // Chosen mode tells webpack to use its built-in optimizations accordingly.
 
-  devtool: "eval-source-map",
-  entry: './src/index.js',
-  resolve: {
-    modules: [
-      "node_modules",
-      path.resolve(__dirname, "dist")
-    ],
-    extensions: [".js", ".coffee"]
-  },
-  module: {
-    strictExportPresence: true,
-    rules: [
-      { use: [ 'coffee-loader' ], test: /\.coffee$/ }
-    ]
-  },
-
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist/lib')
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
+    , devtool: "eval-source-map"
+    , entry: entryPath
+    , resolve:
+      { modules: 
+        [ "node_modules"
+        , path.resolve(__dirname, "dist")
+        ]
+      , extensions: [".js", ".coffee"]
+      }
+    , module: 
+      { strictExportPresence: true
+      , rules: 
+        [ { use: [ 'coffee-loader' ], test: /\.coffee$/ } ]
+      }
+    , output: 
+      { filename: '[name].js'
+      , path: path.resolve(__dirname, 'dist')
+      }
+    , devServer: 
+      { contentBase: path.join(__dirname, 'dist')
+      , publicPath: '/'
+      , compress: true
+      , port: 9000
+      }
   }
-};
+}
+
+
+serverConfig = baseConfig('./src/index.js');
+clientConfig = baseConfig('./src/main.coffee');
+
+serverConfig.target = 'electron-main';
+
+module.exports = [serverConfig, clientConfig];
